6c0851115005a2ef5c97b23fada4cd218f7cb73c
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2004-12-08 21:32:24 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2004 TVA Code Library/BuildScripts/Network/Supporting/UpdateBuildLog.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+ECHO ๚
+ECHO ษออออออออออออออออออออออออออออออออออออออออออออป
+ECHO บ            Updating Build Log...           บ
+ECHO ศออออออออออออออออออออออออออออออออออออออออออออผ
+ECHO ๚
+
+ECHO Build %BUILDVER% compiled at %DATE% %TIME% by %USERDOMAIN%\%USERNAME% from %COMPUTERNAME% >>Build.log
