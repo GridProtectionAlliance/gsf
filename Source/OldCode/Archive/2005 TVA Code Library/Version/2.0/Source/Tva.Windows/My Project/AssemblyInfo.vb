e30e1a6ec773d070b7d82b5043339679f4421207
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2005-12-09 22:21:00 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Windows/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TVA .NET Code Library: Windows")> 
+<Assembly: AssemblyDescription("Shared .NET Windows Functions Library")> 
+<Assembly: AssemblyCompany("TVA")> 
+<Assembly: AssemblyProduct("Shared .NET Code Library for TVA")> 
+<Assembly: AssemblyCopyright("Copyright © 2006, TVA - All rights reserved")> 
+<Assembly: AssemblyTrademark("Authors: J. Ritchie Carroll, Pinal C. Patel")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0bdac7da-e699-4ecd-b14a-725cb46f9683")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("2.0.12.59368")> 
+
+<Assembly: AssemblyVersion("2.0.12.59368")> 
+<Assembly: AssemblyFileVersion("2.0.12.59368")> 
