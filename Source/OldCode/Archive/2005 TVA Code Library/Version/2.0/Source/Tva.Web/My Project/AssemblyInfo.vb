4f8840ae0fb6efd52d043f14e351aace9890edca
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2005-12-13 19:42:48 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Web/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Web/My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.0.12.59368")> 
+' <Assembly: AssemblyVersion("2.0.8.52909")> 
 
-<Assembly: AssemblyVersion("2.0.12.59368")> 
-<Assembly: AssemblyFileVersion("2.0.12.59368")> 
+<Assembly: AssemblyVersion("2.0.8.52909")> 
+<Assembly: AssemblyFileVersion("2.0.8.52909")> 
