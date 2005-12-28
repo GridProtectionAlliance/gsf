6c34f6809c757d7cd9c3ba08dc8e0a723a39f074
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2005-12-28 21:29:27 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Remoting/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Remoting/My Project/AssemblyInfo.vb
@@ -1,6 +1,7 @@
 Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
+Imports System.Security.Permissions
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
@@ -15,7 +16,9 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright © 2006, TVA - All rights reserved")> 
 <Assembly: AssemblyTrademark("Authors: J. Ritchie Carroll, Pinal C. Patel")> 
 
-<Assembly: ComVisible(False)>
+<Assembly: ComVisible(False)> 
+<Assembly: CLSCompliant(True)> 
+<Assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution:=True)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("6715751f-087e-4f54-af5e-814e1f9ce936")>
