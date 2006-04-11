339cf5ba76e4eed346e0e44556def6425c688102
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-04-11 15:36:19 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Core/Tva/Common.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Core/Tva/Common.vb
@@ -26,7 +26,8 @@ Public NotInheritable Class Common
 
     End Sub
 
-    ''' <summary>Strongly typed immediate (a.k.a. inline) if.  Returns one of two objects, depending on the evaluation of an expression.</summary>
+    ''' <summary>Strongly typed immediate (a.k.a. inline) if.</summary>
+    ''' <returns>One of two objects, depending on the evaluation of given expression.</returns>
     ''' <param name="expression">The expression you want to evaluate.</param>
     ''' <param name="truePart">Returned if expression evaluates to True.</param>
     ''' <param name="falsePart">Returned if expression evaluates to False.</param>
@@ -36,5 +37,31 @@ Public NotInheritable Class Common
         If expression Then Return truePart Else Return falsePart
 
     End Function
+
+    ''' <summary>Strongly typed Array creation function.</summary>
+    ''' <returns>New array of specified type.</returns>
+    ''' <param name="length">Desired length of new array.</param>
+    ''' <typeparam name="T">Return type for new array.</typeparam>
+    ''' <remarks>
+    ''' <para>
+    ''' The Array.CreateInstance provides better performance and more direct CLR access for array creation (not to mention less
+    ''' confusion on the matter of array lengths), however the returned System.Array is not typed properly.  This function
+    ''' properly casts the return array based on the the type specification helping when Option Strict is enabled.
+    ''' </para>
+    ''' <para>
+    ''' Examples:
+    ''' <code>
+    '''     Dim buffer As Byte() = CreateArray(Of Byte)(12)
+    '''     Dim matrix As Integer()() = CreateArray(Of Integer())(10)
+    ''' </code>
+    ''' </para>
+    ''' </remarks>
+    Public Shared Function CreateArray(Of T)(ByVal length As Integer) As T()
+
+        Return DirectCast(Array.CreateInstance(GetType(T), length), T())
+
+    End Function
+
+
 
 End Class
