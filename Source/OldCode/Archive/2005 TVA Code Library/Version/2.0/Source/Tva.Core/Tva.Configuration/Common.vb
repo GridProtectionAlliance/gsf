084e403dc3c1104788d4667c8f738739e58f315f
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-04-14 18:07:08 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Core/Tva.Configuration/Common.vb
@@ -0,0 +1,41 @@
+Namespace Configuration
+
+    Public NotInheritable Class Common
+
+        Private Sub New()
+
+        End Sub
+
+        Private Shared m_defaultConfigFile As ConfigurationFile
+
+        Public Shared Property DefaultConfigFile() As ConfigurationFile
+            Get
+                If m_defaultConfigFile Is Nothing Then m_defaultConfigFile = New ConfigurationFile()
+                Return m_defaultConfigFile
+            End Get
+            Set(ByVal value As ConfigurationFile)
+                m_defaultConfigFile = value
+            End Set
+        End Property
+
+        Public Shared ReadOnly Property CustomConfigFile(ByVal filePath As String) As ConfigurationFile
+            Get
+                Return New ConfigurationFile(filePath)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property Settings() As CategorizedSettingsCollection
+            Get
+                Return DefaultConfigFile.CategorizedSettings.General
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedSettings(ByVal category As String) As CategorizedSettingsCollection
+            Get
+                Return DefaultConfigFile.CategorizedSettings(category)
+            End Get
+        End Property
+
+    End Class
+
+End Namespace
