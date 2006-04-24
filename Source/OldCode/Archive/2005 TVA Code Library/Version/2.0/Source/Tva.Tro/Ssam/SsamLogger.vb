d88adce204d36a0681ed1de10ef70ba9d5d2a1ce
pinalcpatel <pcpatel@tva.gov>
2006-04-24 17:32:27 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Tro/Ssam/SsamLogger.vb
@@ -0,0 +1,13 @@
+' 04-04-06
+
+Imports System.Drawing
+Imports System.ComponentModel
+
+Namespace Ssam
+
+    <ToolboxBitmap(GetType(SsamLogger))> _
+    Public Class SsamLogger
+
+    End Class
+
+End Namespace
