202c556a6f732906d0ea95cd0067702b1d38f03c
gsfbuildbot <support@gridprotectionalliance.org>
2006-04-27 17:45:11 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Tro/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Source/Tva.Tro/My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.0.17.49321")> 
+' <Assembly: AssemblyVersion("2.0.18.49504")> 
 
-<Assembly: AssemblyVersion("2.0.17.49321")> 
-<Assembly: AssemblyFileVersion("2.0.17.49321")> 
+<Assembly: AssemblyVersion("2.0.18.49504")> 
+<Assembly: AssemblyFileVersion("2.0.18.49504")> 
