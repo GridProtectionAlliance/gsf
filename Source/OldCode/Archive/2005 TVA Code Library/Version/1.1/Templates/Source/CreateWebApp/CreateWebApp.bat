afb0ede0e015b51c4ce3c09406a80bf036cb5e60
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-04-27 22:32:57 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/1.1/Templates/Source/CreateWebApp/CreateWebApp.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-cscript.exe c:\inetpub\adminscripts\adsutil.vbs CREATE "W3SVC/1/ROOT/%1" "IIsWebDirectory"
-cscript.exe c:\inetpub\adminscripts\adsutil.vbs APPCREATEINPROC "W3SVC/1/ROOT/%1"
-cscript.exe c:\inetpub\adminscripts\adsutil.vbs SET "W3SVC/1/ROOT/%1/AuthAnonymous" False
-cscript.exe c:\inetpub\adminscripts\adsutil.vbs SET "W3SVC/1/ROOT/%1/DefaultDoc" "FrameLoad.aspx"
--- Source/OldCode/Archive/2005 TVA Code Library/Version/1.1/Templates/Source/DeleteWebApp/DeleteWebApp.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-cscript.exe c:\inetpub\adminscripts\adsutil.vbs APPDELETE "W3SVC/1/ROOT/%1"
-cscript.exe c:\inetpub\adminscripts\adsutil.vbs DELETE "W3SVC/1/ROOT/%1"
