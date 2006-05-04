48c2f31ae6c7a6c920b76bf8528adf51a0f236a0
gsfbuildbot <support@gridprotectionalliance.org>
2006-05-04 01:03:23 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Phasors/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Phasors/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Security.Permissions
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.0.18.8901")> 
+' <Assembly: AssemblyVersion("2.0.19.10259")> 
 
-<Assembly: AssemblyVersion("2.0.18.8901")> 
-<Assembly: AssemblyFileVersion("2.0.18.8901")> 
+<Assembly: AssemblyVersion("2.0.19.10259")> 
+<Assembly: AssemblyFileVersion("2.0.19.10259")> 
--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Tro/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Tro/My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.0.18.8901")> 
+' <Assembly: AssemblyVersion("2.0.19.10259")> 
 
-<Assembly: AssemblyVersion("2.0.18.8901")> 
-<Assembly: AssemblyFileVersion("2.0.18.8901")> 
+<Assembly: AssemblyVersion("2.0.19.10259")> 
+<Assembly: AssemblyFileVersion("2.0.19.10259")> 
