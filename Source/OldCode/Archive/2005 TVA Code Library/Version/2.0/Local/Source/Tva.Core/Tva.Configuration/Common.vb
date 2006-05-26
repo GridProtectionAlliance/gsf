4cb3eeb2dcc38032f8db154024d964cf223d0b95
pinalcpatel <pcpatel@tva.gov>
2006-05-26 12:32:38 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Core/Tva.Configuration/Common.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Core/Tva.Configuration/Common.vb
@@ -477,6 +477,270 @@ Namespace Configuration
             End Get
         End Property
 
+        Public Shared ReadOnly Property CategorizedBooleanSetting(ByVal category As String, ByVal name As String) As Boolean
+            Get
+                Return CategorizedBooleanSetting(category, name, False)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedBooleanSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Boolean) As Boolean
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedByteSetting(ByVal category As String, ByVal name As String) As Byte
+            Get
+                Return CategorizedByteSetting(category, name, Byte.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedByteSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Byte) As Byte
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedSByteSetting(ByVal category As String, ByVal name As String) As SByte
+            Get
+                Return CategorizedSByteSetting(category, name, SByte.MinValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedSByteSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As SByte) As SByte
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedCharSetting(ByVal category As String, ByVal name As String) As Char
+            Get
+                Return CategorizedCharSetting(category, name, Char.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedCharSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Char) As Char
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedIntegerSetting(ByVal category As String, ByVal name As String) As Integer
+            Get
+                Return CategorizedIntegerSetting(category, name, Integer.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedIntegerSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Integer) As Integer
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUIntegerSetting(ByVal category As String, ByVal name As String) As UInteger
+            Get
+                Return CategorizedUIntegerSetting(category, name, UInteger.MinValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUIntegerSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As UInteger) As UInteger
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedLongSetting(ByVal category As String, ByVal name As String) As Long
+            Get
+                Return CategorizedLongSetting(category, name, Long.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedLongSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Long) As Long
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedULongSetting(ByVal category As String, ByVal name As String) As ULong
+            Get
+                Return CategorizedULongSetting(category, name, ULong.MinValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedULongSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As ULong) As ULong
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedInt16Setting(ByVal category As String, ByVal name As String) As Int16
+            Get
+                Return CategorizedInt16Setting(category, name, Int16.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedInt16Setting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Int16) As Int16
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedInt32Setting(ByVal category As String, ByVal name As String) As Int32
+            Get
+                Return CategorizedInt32Setting(category, name, Int32.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedInt32Setting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Int32) As Int32
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedInt64Setting(ByVal category As String, ByVal name As String) As Int64
+            Get
+                Return CategorizedInt64Setting(category, name, Int64.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedInt64Setting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Int64) As Int64
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUInt16Setting(ByVal category As String, ByVal name As String) As UInt16
+            Get
+                Return CategorizedUInt16Setting(category, name, UInt16.MinValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUInt16Setting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As UInt16) As UInt16
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUInt32Setting(ByVal category As String, ByVal name As String) As UInt32
+            Get
+                Return CategorizedUInt32Setting(category, name, UInt32.MinValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUInt32Setting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As UInt32) As UInt32
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUInt64Setting(ByVal category As String, ByVal name As String) As UInt64
+            Get
+                Return CategorizedUInt64Setting(category, name, UInt64.MinValue)
+            End Get
+        End Property
+
+        <CLSCompliant(False)> _
+        Public Shared ReadOnly Property CategorizedUInt64Setting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As UInt64) As UInt64
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedSingleSetting(ByVal category As String, ByVal name As String) As Single
+            Get
+                Return CategorizedSingleSetting(category, name, Single.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedSingleSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Single) As Single
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedDoubleSetting(ByVal category As String, ByVal name As String) As Double
+            Get
+                Return CategorizedDoubleSetting(category, name, Double.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedDoubleSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Double) As Double
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedDecimalSetting(ByVal category As String, ByVal name As String) As Decimal
+            Get
+                Return CategorizedDecimalSetting(category, name, Decimal.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedDecimalSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Decimal) As Decimal
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedDateTimeSetting(ByVal category As String, ByVal name As String) As Date
+            Get
+                Return CategorizedDateTimeSetting(category, name, Date.MinValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedDateTimeSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As Date) As Date
+            Get
+                Return CategorizedSettings(category)(name).GetTypedValue(defaultValue)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedStringSetting(ByVal category As String, ByVal name As String) As String
+            Get
+                Return CategorizedStringSetting(category, name, "")
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property CategorizedStringSetting(ByVal category As String, ByVal name As String, _
+                ByVal defaultValue As String) As String
+            Get
+                Dim setting As String = CategorizedSettings(category)(name).Value
+
+                If String.IsNullOrEmpty(setting) Then
+                    Return defaultValue
+                Else
+                    Return setting
+                End If
+            End Get
+        End Property
 #End Region
 
     End Class
