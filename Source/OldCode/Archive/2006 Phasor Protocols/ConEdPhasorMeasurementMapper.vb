dff3b38cfbeecce9bd1aeee8aebc5d8751546a78
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-05-31 19:10:02 +0000

    Vss2Git

--- Source/OldCode/Archive/2006 Phasor Protocols/ConEdPhasorMeasurementMapper.vb
+++ Source/OldCode/Archive/2006 Phasor Protocols/ConEdPhasorMeasurementMapper.vb
@@ -1,5 +1,5 @@
 '*******************************************************************************************************
-'  ConEdPhasorMeasurementMapper.vb - ConEdison timezone data corrective measurement mapper
+'  TimeZoneAdjustedMeasurementMapper.vb - Timezone data corrective measurement mapper
 '  Copyright © 2006 - TVA, all rights reserved - Gbtc
 '
 '  Build Environment: VB.NET, Visual Studio 2005
@@ -16,23 +16,28 @@
 '*******************************************************************************************************
 
 Imports Tva.Phasors
+Imports Tva.DateTime
 Imports Tva.DateTime.Common
 
 <CLSCompliant(False)> _
-Public Class ConEdPhasorMeasurementMapper
+Public Class TimeZoneAdjustedMeasurementMapper
 
     Inherits PhasorMeasurementMapper
 
-    Public Sub New(ByVal frameParser As FrameParser, ByVal source As String, ByVal pmuIDs As List(Of String), ByVal measurementIDs As Dictionary(Of String, Integer))
+    Private m_sourceTimeZone As Win32TimeZone
+
+    Public Sub New(ByVal sourceTimeZone As Win32TimeZone, ByVal frameParser As FrameParser, ByVal source As String, ByVal pmuIDs As List(Of String), ByVal measurementIDs As Dictionary(Of String, MeasurementDefinition))
 
         MyBase.New(frameParser, source, pmuIDs, measurementIDs)
+        m_sourceTimeZone = sourceTimeZone
 
     End Sub
 
     Protected Overrides Sub MapDataFrameMeasurements(ByVal frame As Tva.Phasors.IDataFrame)
 
-        ' ConEd is on Eastern Time - this will correct the issue
-        frame.Ticks = EasternTimeZone.ToUniversalTime(frame.Timestamp).Ticks
+        ' Adjust time to be on UTC based on source PDC/PMU time zone
+        ' For example, ConEd PMU is on Eastern Time - this will correct the issue
+        frame.Ticks = m_sourceTimeZone.ToUniversalTime(frame.Timestamp).Ticks
 
         ' Allow base class to map data frame measurement instances to their associated point ID's
         MyBase.MapDataFrameMeasurements(frame)
