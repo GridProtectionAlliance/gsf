ec951e0bae132c314074f8fad2945c7933f0b5c8
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-06-13 21:31:03 +0000

    Vss2Git

--- Source/OldCode/Archive/2006 Phasor Protocols/PhasorMeasurementReceiver.vb
+++ Source/OldCode/Archive/2006 Phasor Protocols/PhasorMeasurementReceiver.vb
@@ -1,6 +1,6 @@
 '*******************************************************************************************************
 '  PhasorMeasurementReceiver.vb - Phasor measurement acquisition class
-'  Copyright © 2005 - TVA, all rights reserved - Gbtc
+'  Copyright © 2006 - TVA, all rights reserved - Gbtc
 '
 '  Build Environment: VB.NET, Visual Studio 2005
 '  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
