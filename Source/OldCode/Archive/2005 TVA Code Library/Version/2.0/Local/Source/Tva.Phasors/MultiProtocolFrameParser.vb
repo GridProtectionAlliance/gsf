556f43f3faeaa234911665592b9fb0711a3bf9e2
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-07-20 17:35:48 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Phasors/MultiProtocolFrameParser.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Phasors/MultiProtocolFrameParser.vb
@@ -1,5 +1,5 @@
 '*******************************************************************************************************
-'  FrameParser.vb - Protocol independent frame parser
+'  MultiProtocolFrameParser.vb - Protocol independent frame parser
 '  Copyright © 2006 - TVA, all rights reserved - Gbtc
 '
 '  Build Environment: VB.NET, Visual Studio 2005
