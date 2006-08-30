dbf97777c657c892a09fd9b56855ddc9c3c10fab
pinalcpatel <pcpatel@tva.gov>
2006-08-30 19:59:21 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Services/ServiceHelper.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Services/ServiceHelper.vb
@@ -1,9 +1,23 @@
 ' 08-29-06
 
-Namespace Services
+Public Class ServiceHelper
 
-    Public Class ServiceHelper
+    Private m_startEventHandlerList As New List(Of EventHandler)
 
-    End Class
+    Public Custom Event Start As EventHandler
+        AddHandler(ByVal value As EventHandler)
+            m_startEventHandlerList.Add(value)
+        End AddHandler
 
-End Namespace
+        RemoveHandler(ByVal value As EventHandler)
+            m_startEventHandlerList.Remove(value)
+        End RemoveHandler
+
+        RaiseEvent(ByVal sender As Object, ByVal e As System.EventArgs)
+            For Each handler As EventHandler In m_startEventHandlerList
+                handler.BeginInvoke(sender, e, Nothing, Nothing)
+            Next
+        End RaiseEvent
+    End Event
+
+End Class
