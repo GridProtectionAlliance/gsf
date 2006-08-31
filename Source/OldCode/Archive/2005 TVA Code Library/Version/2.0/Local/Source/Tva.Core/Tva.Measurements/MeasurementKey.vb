525802a53b2131d8083c7fc68852236594a96db5
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2006-08-31 16:21:33 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Core/Tva.Measurements/MeasurementKey.vb
@@ -0,0 +1,51 @@
+'*******************************************************************************************************
+'  Tva.Measurements.MeasurementKey.vb - Defines primary key elements for a measurement
+'  Copyright © 2006 - TVA, all rights reserved - Gbtc
+'
+'  Build Environment: VB.NET, Visual Studio 2005
+'  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
+'      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+'       Phone: 423/751-2827
+'       Email: jrcarrol@tva.gov
+'
+'  Code Modification History:
+'  -----------------------------------------------------------------------------------------------------
+'  12/8/2005 - J. Ritchie Carroll
+'       Initial version of source generated
+'
+'*******************************************************************************************************
+
+Imports Tva.Text.Common
+
+Namespace Measurements
+
+    ''' <summary>Defines a primary key for a measurement</summary>
+    Public Structure MeasurementKey
+
+        Public ID As Integer
+        Public Source As String
+
+        Public Sub New(ByVal id As Integer, ByVal source As String)
+
+            Me.ID = id
+            Me.Source = source
+
+        End Sub
+
+        Public Overrides Function ToString() As String
+
+            Return Source & ":" & ID
+
+        End Function
+
+        Public Overrides Function GetHashCode() As Integer
+
+            ' 1234567890
+            ' 2147483647
+            Return Concat(Source, ID.ToString().PadLeft(10, "0"c)).GetHashCode()
+
+        End Function
+
+    End Structure
+
+End Namespace
