001b15a49f74af9be74d0887c629f4e1f0b34142
gsfbuildbot <support@gridprotectionalliance.org>
2006-10-06 15:10:52 +0000

    Vss2Git

--- Source/OldCode/Archive/2006 Phasor Protocols/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2006 Phasor Protocols/My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.1.24.47772")> 
+' <Assembly: AssemblyVersion("2.1.25.40244")> 
 
-<Assembly: AssemblyVersion("2.1.24.47772")> 
+<Assembly: AssemblyVersion("2.1.25.40244")> 
 <Assembly: AssemblyFileVersion("2.1.24.47772")> 
