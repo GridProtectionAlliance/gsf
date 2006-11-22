1db6116841e6d3ddb1ee56267197845e5d24eb6e
pinalcpatel <pcpatel@tva.gov>
2006-11-22 15:02:25 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Core/Tva/IdentifiableSourceEventArgs.vb
@@ -0,0 +1,21 @@
+Public Class IdentifiableSourceEventArgs
+
+    Private m_source As Guid
+
+    Public Sub New(ByVal source As Guid)
+
+        MyBase.New()
+        m_source = source
+
+    End Sub
+
+    Public Property Source() As Guid
+        Get
+            Return m_source
+        End Get
+        Set(ByVal value As Guid)
+            m_source = value
+        End Set
+    End Property
+
+End Class
