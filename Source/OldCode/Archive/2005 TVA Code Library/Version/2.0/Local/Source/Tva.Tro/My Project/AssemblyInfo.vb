daac0b27d569261c1db2f31689a798928b8c4f9c
gsfbuildbot <support@gridprotectionalliance.org>
2006-12-12 16:47:48 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Tro/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Tro/My Project/AssemblyInfo.vb
@@ -32,8 +32,8 @@ Imports System.Security.Permissions
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.1.43.37917")> 
+' <Assembly: AssemblyVersion("2.1.44.42463")> 
 
-<Assembly: AssemblyVersion("2.1.43.37917")> 
+<Assembly: AssemblyVersion("2.1.44.42463")> 
 <Assembly: AssemblyFileVersion("2.1.38.41791")> 
 <Assembly: AssemblyKeyFile("..\TVA.snk")> 
