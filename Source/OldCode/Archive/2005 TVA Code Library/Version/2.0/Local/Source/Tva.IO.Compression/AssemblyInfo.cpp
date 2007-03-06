3dfdf55962fd43c3e7259d30461209191326b5c0
gsfbuildbot <support@gridprotectionalliance.org>
2007-03-06 14:02:12 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.IO.Compression/AssemblyInfo.cpp
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.IO.Compression/AssemblyInfo.cpp
@@ -30,8 +30,8 @@ using namespace System::Runtime::CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersion("2.1.89.30755")];
-[assembly:AssemblyFileVersion("2.1.88.26577")];
+[assembly:AssemblyVersion("2.1.90.32525")];
+[assembly:AssemblyFileVersion("2.1.90.32525")];
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Phasors/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Phasors/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Security.Permissions
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.1.89.30755")> 
+' <Assembly: AssemblyVersion("2.1.90.32525")> 
 
-<Assembly: AssemblyVersion("2.1.89.30755")> 
-<Assembly: AssemblyFileVersion("2.1.89.30755")> 
+<Assembly: AssemblyVersion("2.1.90.32525")> 
+<Assembly: AssemblyFileVersion("2.1.90.32525")> 
