9813fcfedbccf3cbff97b38e137583fbf985ac05
pinalcpatel <pcpatel@tva.gov>
2007-03-12 12:20:19 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/Tva.Core/Tva/IdentifiableItem.vb
@@ -0,0 +1,34 @@
+' 03/12/2007
+
+Public Class IdentifiableItem(Of T)
+
+    Private m_source As Guid
+    Private m_item As T
+
+    Public Sub New(ByVal source As Guid, ByVal item As T)
+
+        MyBase.New()
+        m_source = source
+        m_item = item
+
+    End Sub
+
+    Public Property Source() As Guid
+        Get
+            Return m_source
+        End Get
+        Set(ByVal value As Guid)
+            m_source = value
+        End Set
+    End Property
+
+    Public Property Item() As T
+        Get
+            Return m_item
+        End Get
+        Set(ByVal value As T)
+            m_item = value
+        End Set
+    End Property
+
+End Class
