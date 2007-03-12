d58a4ea419bd47923722c9609ecb10393d0f3e26
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2007-03-12 18:56:51 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2006 Phasor Protocols/DatAWareAdapter/Version3Adapter.vb
@@ -0,0 +1,122 @@
+'*******************************************************************************************************
+'  LegacyAdapter.vb - DatAWare legacy system adpater
+'  Copyright © 2005 - TVA, all rights reserved - Gbtc
+'
+'  Build Environment: VB.NET, Visual Studio 2005
+'  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
+'      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+'       Phone: 423/751-2827
+'       Email: jrcarrol@tva.gov
+'
+'  Code Modification History:
+'  -----------------------------------------------------------------------------------------------------
+'  06/01/2006 - J. Ritchie Carroll
+'       Initial version of source generated
+'
+'*******************************************************************************************************
+
+Imports InterfaceAdapters
+Imports System.IO
+Imports System.Text
+Imports Tva.Common
+Imports Tva.Collections.Common
+Imports Tva.Measurements
+Imports Tva.DatAWare
+Imports Tva.Text.Common
+Imports Tva.Communication
+
+Public Class Version3Adapter
+
+    Inherits HistorianAdapterBase
+
+    Private m_archiverIP As String
+    Private m_archiverPort As Integer
+    Private m_maximumEvents As Integer
+    Private m_useTimeout As Boolean
+    Private m_bufferSize As Integer
+    Private m_buffer As Byte()
+    Private m_connection As TcpClient
+
+    Public Sub New()
+
+        MyBase.New()
+
+        m_archiverIP = "127.0.0.1"
+        m_archiverPort = 1005
+        m_maximumEvents = 100000
+        m_useTimeout = True
+
+    End Sub
+
+    Public Overrides Sub Initialize(ByVal connectionString As String)
+
+        Dim value As String
+
+        ' Example connection string:
+        ' IP=localhost; Port=1002; MaxEvents=50; UseTimeout=True
+        With ParseKeyValuePairs(connectionString)
+            If .TryGetValue("ip", value) Then m_archiverIP = value
+            If .TryGetValue("port", value) Then m_archiverPort = Convert.ToInt32(value)
+            If .TryGetValue("maxevents", value) Then m_maximumEvents = Convert.ToInt32(value)
+        End With
+
+        If String.IsNullOrEmpty(m_archiverIP) Then Throw New InvalidOperationException("Cannot start TCP stream listener connection to DatAWare Archiver without specifing a host IP")
+
+        m_bufferSize = StandardEvent.BinaryLength * m_maximumEvents
+        m_buffer = CreateArray(Of Byte)(m_bufferSize)
+
+    End Sub
+
+    Protected Overrides Sub AttemptConnection()
+
+        ' Connect to DatAWare archiver using TCP
+        m_connection = New TcpClient("server=" & m_archiverIP & "; port=" & m_archiverPort)
+        m_connection.Handshake = True
+        m_connection.PayloadAware = True
+        m_connection.Connect()
+
+    End Sub
+
+    Protected Overrides Sub AttemptDisconnection()
+
+        m_connection.Disconnect()
+
+    End Sub
+
+    Public Overrides ReadOnly Property Name() As String
+        Get
+            Return "DatAWare Archiver """ & m_archiverIP & ":" & m_archiverPort & """"
+        End Get
+    End Property
+
+    Protected Overrides Sub ArchiveMeasurements()
+
+        Dim x, totalPoints As Integer
+
+        ' Retrieve data points to be archived
+        SyncLock Measurements
+            With Measurements
+                totalPoints = Minimum(m_maximumEvents, .Count)
+
+                If totalPoints > 0 Then
+                    ' Load binary standard event images into local buffer
+                    For x = 0 To totalPoints - 1
+                        With New StandardEvent(.Item(x))
+                            System.Buffer.BlockCopy(.BinaryImage, 0, m_buffer, x * StandardEvent.BinaryLength, StandardEvent.BinaryLength)
+                        End With
+                    Next
+
+                    ' Remove measurements being processed
+                    .RemoveRange(0, totalPoints)
+                End If
+            End With
+        End SyncLock
+
+        If totalPoints > 0 Then
+            ' Post data to TCP stream
+            m_connection.Send(m_buffer, 0, totalPoints * StandardEvent.BinaryLength)
+        End If
+
+    End Sub
+
+End Class
