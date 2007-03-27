086d9368cc30db06bd1a694e0ba673554b74a289
gsfbuildbot <support@gridprotectionalliance.org>
2007-03-27 19:54:16 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.IO.Compression/AssemblyInfo.cpp
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.IO.Compression/AssemblyInfo.cpp
@@ -30,7 +30,7 @@ using namespace System::Runtime::CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersion("2.1.94.56952")];
+[assembly:AssemblyVersion("2.1.95.57251")];
 [assembly:AssemblyFileVersion("2.1.94.56952")];
 
 //
