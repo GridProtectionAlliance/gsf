0cb874b8d721d1e72b1877283e94dc554b0570f5
gsfbuildbot <support@gridprotectionalliance.org>
2007-05-25 12:00:28 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.DatAWare/My Project/AssemblyInfo.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.DatAWare/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Security.Permissions
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("2.1.97.33673")> 
+' <Assembly: AssemblyVersion("2.1.98.28813")> 
 
-<Assembly: AssemblyVersion("2.1.97.33673")> 
-<Assembly: AssemblyFileVersion("2.1.97.33673")> 
+<Assembly: AssemblyVersion("2.1.98.28813")> 
+<Assembly: AssemblyFileVersion("2.1.98.28813")> 
--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.IO.Compression/AssemblyInfo.cpp
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.IO.Compression/AssemblyInfo.cpp
@@ -30,7 +30,7 @@ using namespace System::Runtime::CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersion("2.1.97.33673")];
+[assembly:AssemblyVersion("2.1.98.28813")];
 [assembly:AssemblyFileVersion("2.1.97.33673")];
 
 //
