c4c1b56481884ed5c6e2e1cd4d3685e9cf2c6f16
pinalcpatel <pcpatel@tva.gov>
2007-06-14 18:46:14 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/Root/IBinaryDataConsumer.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/Root/IBinaryDataConsumer.vb
@@ -1,7 +1,5 @@
 Public Interface IBinaryDataConsumer
 
-    Function Initialize(ByVal binaryImage As Byte()) As Integer
-
     Function Initialize(ByVal binaryImage As Byte(), ByVal startIndex As Integer) As Integer
 
 End Interface
