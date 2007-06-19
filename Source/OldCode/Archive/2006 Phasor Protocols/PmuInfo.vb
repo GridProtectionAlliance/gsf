46b1f53ba92f568eef0e9d1525c96f1151453fe7
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2007-06-19 16:52:42 +0000

    Vss2Git

--- Source/OldCode/Archive/2006 Phasor Protocols/PmuInfo.vb
+++ Source/OldCode/Archive/2006 Phasor Protocols/PmuInfo.vb
@@ -1,5 +1,5 @@
 '*******************************************************************************************************
-'  PmuInfo.vb - PMU Information Definition
+'  ConfigurationCell.vb - Protocol Independent PMU Configuration Definition
 '  Copyright © 2006 - TVA, all rights reserved - Gbtc
 '
 '  Build Environment: VB.NET, Visual Studio 2005
@@ -15,36 +15,56 @@
 '
 '*******************************************************************************************************
 
+Imports System.Runtime.Serialization
 Imports TVA.Common
 Imports TVA.Measurements
+Imports PhasorProtocols
 
-Public Class PmuInfo
+Public Class ConfigurationCell
 
-    Implements IComparable(Of PmuInfo)
+    Inherits ConfigurationCellBase
 
-    Private m_id As UInt16
-    Private m_acronym As String
     Private m_lastReportTime As Long
     Private m_signalSynonyms As Dictionary(Of SignalType, String())
-    'Private m_activeMeasurements As Dictionary(Of String, IMeasurement)
+    Private m_analogDataFormat As PhasorProtocols.DataFormat
+    Private m_frequencyDataFormat As PhasorProtocols.DataFormat
+    Private m_phasorDataFormat As PhasorProtocols.DataFormat
+    Private m_phasorCoordinateFormat As PhasorProtocols.CoordinateFormat
 
-    Public Sub New(ByVal id As UInt16, ByVal acronym As String)
+    Protected Sub New(ByVal info As SerializationInfo, ByVal context As StreamingContext)
 
-        m_id = id
-        m_acronym = acronym
-        m_signalSynonyms = New Dictionary(Of SignalType, String())
+        MyBase.New(info, context)
+
+        m_lastReportTime = info.GetInt64("lastReportTime")
+        m_signalSynonyms = info.GetValue("signalSynonyms", GetType(Dictionary(Of SignalType, String())))
+        m_analogDataFormat = info.GetValue("analogDataFormat", GetType(DataFormat))
+        m_frequencyDataFormat = info.GetValue("frequencyDataFormat", GetType(DataFormat))
+        m_phasorDataFormat = info.GetValue("phasorDataFormat", GetType(DataFormat))
+        m_phasorCoordinateFormat = info.GetValue("phasorCoordinateFormat", GetType(CoordinateFormat))
 
     End Sub
 
-    Public ReadOnly Property ID() As UInt16
-        Get
-            Return m_id
-        End Get
-    End Property
+    Public Sub New()
 
-    Public ReadOnly Property Acronym() As String
+        m_signalSynonyms = New Dictionary(Of SignalType, String())
+        m_analogDataFormat = DataFormat.FloatingPoint
+        m_frequencyDataFormat = DataFormat.FloatingPoint
+        m_phasorDataFormat = DataFormat.FloatingPoint
+        m_phasorCoordinateFormat = CoordinateFormat.Polar
+
+    End Sub
+
+    Public Sub New(ByVal idCode As UInt16, ByVal idLabel As String)
+
+        MyClass.New()
+        MyBase.IDCode = idCode
+        MyBase.IDLabel = IDLabel
+
+    End Sub
+
+    Public Overrides ReadOnly Property DerivedType() As System.Type
         Get
-            Return m_acronym
+            Return Me.GetType()
         End Get
     End Property
 
@@ -61,7 +81,7 @@ Public Class PmuInfo
             synonyms = CreateArray(Of String)(1)
 
             ' Create and cache new non-indexed signal reference
-            synonyms(0) = SignalReference.ToString(m_acronym, signal)
+            synonyms(0) = SignalReference.ToString(IDLabel, signal)
 
             Return synonyms(0)
         End Get
@@ -82,7 +102,7 @@ Public Class PmuInfo
                     ' Lookup signal reference "synonym" value of given signal index
                     If synonyms(signalIndex) Is Nothing Then
                         ' Didn't find signal index, create and cache new signal reference
-                        synonyms(signalIndex) = SignalReference.ToString(m_acronym, signal, signalIndex + 1)
+                        synonyms(signalIndex) = SignalReference.ToString(IDLabel, signal, signalIndex + 1)
                     End If
 
                     Return synonyms(signalIndex)
@@ -93,7 +113,7 @@ Public Class PmuInfo
             synonyms = CreateArray(Of String)(signalCount)
 
             ' Create and cache new signal reference
-            synonyms(signalIndex) = SignalReference.ToString(m_acronym, signal, signalIndex + 1)
+            synonyms(signalIndex) = SignalReference.ToString(IDLabel, signal, signalIndex + 1)
 
             Return synonyms(signalIndex)
         End Get
@@ -108,17 +128,53 @@ Public Class PmuInfo
         End Set
     End Property
 
-    'Public ReadOnly Property Measurements() As Dictionary(Of String, IMeasurement)
-    '    Get
-    '        If m_activeMeasurements Is Nothing Then m_activeMeasurements = New Dictionary(Of String, IMeasurement)
-    '        Return m_activeMeasurements
-    '    End Get
-    'End Property
+    Public Overrides Property AnalogDataFormat() As PhasorProtocols.DataFormat
+        Get
+            Return m_analogDataFormat
+        End Get
+        Set(ByVal value As PhasorProtocols.DataFormat)
+            m_analogDataFormat = value
+        End Set
+    End Property
 
-    Public Function CompareTo(ByVal other As PmuInfo) As Integer Implements System.IComparable(Of PmuInfo).CompareTo
+    Public Overrides Property FrequencyDataFormat() As PhasorProtocols.DataFormat
+        Get
+            Return m_frequencyDataFormat
+        End Get
+        Set(ByVal value As PhasorProtocols.DataFormat)
+            m_frequencyDataFormat = value
+        End Set
+    End Property
 
-        Return m_id.CompareTo(other.ID)
+    Public Overrides Property PhasorDataFormat() As PhasorProtocols.DataFormat
+        Get
+            Return m_phasorDataFormat
+        End Get
+        Set(ByVal value As PhasorProtocols.DataFormat)
+            m_phasorDataFormat = value
+        End Set
+    End Property
 
-    End Function
+    Public Overrides Property PhasorCoordinateFormat() As PhasorProtocols.CoordinateFormat
+        Get
+            Return m_phasorCoordinateFormat
+        End Get
+        Set(ByVal value As PhasorProtocols.CoordinateFormat)
+            m_phasorCoordinateFormat = value
+        End Set
+    End Property
+
+    Public Overrides Sub GetObjectData(ByVal info As SerializationInfo, ByVal context As StreamingContext)
+
+        MyBase.GetObjectData(info, context)
+
+        info.AddValue("lastReportTime", m_lastReportTime)
+        info.AddValue("signalSynonyms", m_signalSynonyms, GetType(Dictionary(Of SignalType, String())))
+        info.AddValue("analogDataFormat", m_analogDataFormat, GetType(DataFormat))
+        info.AddValue("frequencyDataFormat", m_frequencyDataFormat, GetType(DataFormat))
+        info.AddValue("phasorDataFormat", m_phasorDataFormat, GetType(DataFormat))
+        info.AddValue("phasorCoordinateFormat", m_phasorCoordinateFormat, GetType(CoordinateFormat))
+
+    End Sub
 
 End Class
