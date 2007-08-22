ee54ef9f523771bf5e0b2cecd1b60a33339c6567
Dzuerche <dzuerche@tva.gov>
2007-08-22 20:13:20 +0000

    Edited code comments.

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/IO/FilePath.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/IO/FilePath.vb
@@ -11,9 +11,9 @@
 '  Code Modification History:
 '  -----------------------------------------------------------------------------------------------------
 '  ??/??/2003 - J. Ritchie Carroll
-'       Original version of source code generated
+'       Generated original version of source code.
 '  12/29/2005 - Pinal C. Patel
-'       2.0 version of source code migrated from 1.1 source (TVA.Shared.FilePath)
+'       Migrated 2.0 version of source code from 1.1 source (TVA.Shared.FilePath).
 '
 '*******************************************************************************************************
 
@@ -53,15 +53,15 @@ Namespace IO
 
         Private Sub New()
 
-            ' This class contains only global functions and is not meant to be instantiated
+            ' This class contains only global functions and is not meant to be instantiated.
 
         End Sub
 
         ''' <summary>Connects to a network share with the specified user's credentials.</summary>
-        ''' <param name="sharename">UNC share name to connect to</param>
-        ''' <param name="username">Username to use for connection</param>
-        ''' <param name="password">Password to use for connection</param>
-        ''' <param name="domain">Domain name to use for connetion - specify computer name for local system accounts</param>
+        ''' <param name="sharename">UNC share name to connect to.</param>
+        ''' <param name="username">Username to use for connection.</param>
+        ''' <param name="password">Password to use for connection.</param>
+        ''' <param name="domain">Domain name to use for connetion. Specify the computer name for local system accounts.</param>
         Public Shared Sub ConnectToNetworkShare(ByVal sharename As String, ByVal username As String, ByVal password As String, ByVal domain As String)
 
             Dim resource As NETRESOURCE = Nothing
@@ -80,7 +80,7 @@ Namespace IO
         End Sub
 
         ''' <summary>Disconnects the specified network share.</summary>
-        ''' <param name="sharename">UNC share name to disconnect from</param>
+        ''' <param name="sharename">UNC share name to disconnect from.</param>
         Public Shared Sub DisconnectFromNetworkShare(ByVal sharename As String)
 
             DisconnectFromNetworkShare(sharename, True)
@@ -88,8 +88,8 @@ Namespace IO
         End Sub
 
         ''' <summary>Disconnects the specified network share.</summary>
-        ''' <param name="sharename">UNC share name to disconnect from</param>
-        ''' <param name="force">Set to True force disconnect</param>
+        ''' <param name="sharename">UNC share name to disconnect from.</param>
+        ''' <param name="force">Set to True to force a disconnect.</param>
         Public Shared Sub DisconnectFromNetworkShare(ByVal sharename As String, ByVal force As Boolean)
 
             Dim result As Integer = WNetCancelConnection2(sharename, 0, force)
@@ -97,7 +97,7 @@ Namespace IO
 
         End Sub
 
-        ''' <summary>Returns True if specified file name matches any of the given file specs (wildcards are defined as '*' or '?' characters).</summary>
+        ''' <summary>Returns True if the specified file name matches any of the given file specs (wildcards are defined as '*' or '?' characters).</summary>
         Public Shared Function IsFilePatternMatch(ByVal fileSpecs As String(), ByVal fileName As String, ByVal ignoreCase As Boolean) As Boolean
 
             Dim found As Boolean
@@ -113,21 +113,21 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Returns True if specified file name matches given file spec (wildcards are defined as '*' or '?' characters).</summary>
+        ''' <summary>Returns True if the specified file name matches given file spec (wildcards are defined as '*' or '?' characters).</summary>
         Public Shared Function IsFilePatternMatch(ByVal fileSpec As String, ByVal fileName As String, ByVal ignoreCase As Boolean) As Boolean
 
             Return (New Regex(GetFilePatternRegularExpression(fileSpec), IIf(ignoreCase, RegexOptions.IgnoreCase, RegexOptions.None))).IsMatch(fileName)
 
         End Function
 
-        ''' <summary>Returns a regular expression that simulates wildcard matching for filenames (wildcards are defined as '*' or '?' characters)</summary>
+        ''' <summary>Returns a regular expression that simulates wildcard matching for filenames (wildcards are defined as '*' or '?' characters).</summary>
         Public Shared Function GetFilePatternRegularExpression(ByVal fileSpec As String) As String
 
             If m_fileNameCharPattern Is Nothing Then
                 With New StringBuilder
-                    ' Define a regular expression pattern for a valid file name character, we do this by
-                    ' allowing any characters except those that would not be valid as part of a filename,
-                    ' this essentially builds the "?" wildcard pattern match
+                    ' Defines a regular expression pattern for a valid file name character. We do this by
+                    ' allowing any characters except those that would not be valid as part of a filename.
+                    ' This essentially builds the "?" wildcard pattern match.
                     .Append("[^")
                     .Append(EncodeRegexChar(Path.DirectorySeparatorChar))
                     .Append(EncodeRegexChar(Path.AltDirectorySeparatorChar))
@@ -143,9 +143,9 @@ Namespace IO
                 End With
             End If
 
-            ' Replace wildcard file patterns with their equivalent regular expression
-            fileSpec = fileSpec.Replace("\", "\u005C")  ' Backslash in Regex means special sequence, here we really want a backslash
-            fileSpec = fileSpec.Replace(".", "\u002E")  ' Dot in Regex means any character, here we really want a dot
+            ' Replaces wildcard file patterns with their equivalent regular expression.
+            fileSpec = fileSpec.Replace("\", "\u005C")  ' Backslash in Regex means special sequence. Here, we really want a backslash.
+            fileSpec = fileSpec.Replace(".", "\u002E")  ' Dot in Regex means any character. Here, we really want a dot.
             fileSpec = fileSpec.Replace("?", m_fileNameCharPattern)
             fileSpec = fileSpec.Replace("*", "(" & m_fileNameCharPattern & ")*")
 
@@ -153,7 +153,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Get the size of the specified file.</summary>
+        ''' <summary>Gets the size of the specified file.</summary>
         ''' <param name="fileName">Name of file whose size is to be returned.</param>
         ''' <returns>The size of the specified file.</returns>
         Public Shared Function GetFileLength(ByVal fileName As String) As Long
@@ -175,7 +175,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Gets a unique temporary file name with path - if UseTempPath is False, application path is used for temp file.</summary>
+        ''' <summary>Gets a unique temporary file name with path. If UseTempPath is false, application path is used for temp file.</summary>
         Public Shared Function GetTempFile(ByVal useTempPath As Boolean, ByVal createZeroLengthFile As Boolean, ByVal fileExtension As String) As String
 
             If useTempPath AndAlso createZeroLengthFile Then
@@ -202,16 +202,16 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Gets a file name (with .tmp extension) guaranteed to be unique with no path.</summary>
+        ''' <summary>Gets a file name (with .tmp extension), guaranteed to be unique, with no path.</summary>
         Public Shared Function GetTempFileName() As String
 
             Return GetTempFileName("tmp")
 
         End Function
 
-        ''' <summary>Gets a file name guaranteed to be unique with no path.</summary>
+        ''' <summary>Gets a file name, guaranteed to be unique, with no path.</summary>
         ''' <param name="fileExtension">The extension of the temporary file.</param>
-        ''' <returns>The file name guaranteed to be unique with no path.</returns>
+        ''' <returns>The file name, guaranteed to be unique, with no path.</returns>
         ''' <remarks>Use GetTempFile to return unique file name with path.</remarks>
         Public Shared Function GetTempFileName(ByVal fileExtension As String) As String
 
@@ -220,7 +220,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Gets the temporary file path - path will be suffixed with standard directory separator.</summary>
+        ''' <summary>Gets the temporary file path, suffixed with standard directory separator.</summary>
         ''' <returns>The temporary file path.</returns>
         Public Shared Function GetTempFilePath() As String
 
@@ -228,7 +228,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Gets the path of the executing assembly - path will be suffixed with standard directory separator.</summary>
+        ''' <summary>Gets the path of the executing assembly, suffixed with standard directory separator.</summary>
         ''' <returns>The path of the executing assembly.</returns>
         Public Shared Function GetApplicationPath() As String
 
@@ -236,7 +236,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Returns just the drive letter (or UNC \\server\share\) from a path") - path will be suffixed with standard directory separator.</summary>
+        ''' <summary>Returns just the drive letter (or UNC (\\server\share\) from a path), suffixed with standard directory separator.</summary>
         Public Shared Function JustDrive(ByVal filePath As String) As String
 
             If String.IsNullOrEmpty(filePath) Then
@@ -286,7 +286,7 @@ Namespace IO
             If Not Path.IsPathRooted(filePath) Then
                 ' The specified path is a relative one since it is not rooted.
                 Select Case TVA.Common.GetApplicationType()
-                    ' We'll prepend the application's root to the file path.
+                    ' Prepends the application's root to the file path.
                     Case ApplicationType.WindowsGui, ApplicationType.WindowsCui
                         filePath = JustPath(TVA.Assembly.EntryAssembly.Location) & filePath
                     Case ApplicationType.Web
@@ -298,7 +298,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Returns just the path without a filename from a path - path will be suffixed with standard directory separator.</summary>
+        ''' <summary>Returns just the path, without a filename from a path, suffixed with standard directory separator.</summary>
         Public Shared Function JustPath(ByVal filePath As String) As String
 
             If String.IsNullOrEmpty(filePath) Then
@@ -309,7 +309,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Returns just the file extension from a path - keeps extension "dot".</summary>
+        ''' <summary>Returns just the file extension from a path, keeping the extension "dot".</summary>
         Public Shared Function JustFileExtension(ByVal filePath As String) As String
 
             If String.IsNullOrEmpty(filePath) Then
@@ -320,7 +320,7 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Returns just the file name with no extension from a path.</summary>
+        ''' <summary>Returns just the file name, with no extension, from a path.</summary>
         Public Shared Function NoFileExtension(ByVal filePath As String) As String
 
             If String.IsNullOrEmpty(filePath) Then
@@ -364,10 +364,10 @@ Namespace IO
 
         End Function
 
-        ''' <summary>Returns a file name for display purposes of the specified length using "..." to indicate a longer name</summary>
+        ''' <summary>Returns a file name, for display purposes, of the specified length using "..." to indicate a longer name.</summary>
         ''' <remarks>
         ''' <para>Minimum value for the <paramref name="length" /> parameter is 12.</para>
-        ''' <para>12 will be used for any value specified less than 12.</para>
+        ''' <para>12 will be used for any value specified as less than 12.</para>
         ''' </remarks>
         Public Shared Function TrimFileName(ByVal fileName As String, ByVal length As Integer) As String
 
@@ -383,7 +383,7 @@ Namespace IO
                 Dim justName As String = JustFileName(fileName)
 
                 If justName.Length = fileName.Length Then
-                    ' This is just a file name, make sure extension shows...
+                    ' This is just a file name. Make sure extension shows.
                     Dim justExtension As String = JustFileExtension(fileName)
                     Dim trimName As String = NoFileExtension(fileName)
 
@@ -392,26 +392,26 @@ Namespace IO
                         Dim offset As Single = (length - justExtension.Length() - 3) / 2
                         Return trimName.Substring(0, Ceiling(offset)) & "..." & trimName.Substring(Len(trimName) - Floor(offset) + 1) & justExtension
                     Else
-                        ' We can't trim file names less than 8 with a "...", so we truncate long extension
+                        ' We can not trim file names less than 8 with a "...", so we truncate long extension.
                         Return trimName & justExtension.Substring(0, length - trimName.Length)
                     End If
                 ElseIf justName.Length > length Then
-                    ' Just file name part exeeds length, recurse into function without path
+                    ' Just file name part exceeds length. Recurses into function without path.
                     Return TrimFileName(justName, length)
                 Else
-                    ' File name contains path, trim path before file name...
+                    ' File name contains path. Trims path before file name.
                     Dim justFilePath As String = JustPath(fileName)
                     Dim offset As Integer = length - justName.Length - 4
 
                     If Len(justFilePath) > offset And offset > 0 Then
                         Return justFilePath.Substring(0, offset) & "...\" & justName
                     Else
-                        ' Can't fit path, just trim file name
+                        ' Can not fit path. Trims file name.
                         Return TrimFileName(justName, length)
                     End If
                 End If
             Else
-                ' Full file name fits within requested length...
+                ' Full file name fits within requested length.
                 Return fileName
             End If
 
@@ -445,7 +445,7 @@ Namespace IO
                 Exit Sub
             End If
 
-            ' We use this function to keep trying for a file lock...
+            ' Keeps trying for a file lock.
             Dim targetFile As FileStream = Nothing
             Dim startTime As Double = SystemTimer
 
@@ -455,7 +455,7 @@ Namespace IO
                     targetFile.Close()
                     Exit While
                 Catch
-                    ' We'll keep trying till we can open the file...
+                    ' Keeps trying to open the file.
                 End Try
 
                 If targetFile IsNot Nothing Then
@@ -473,7 +473,7 @@ Namespace IO
                     End If
                 End If
 
-                ' Yield to all other system threads...
+                ' Yields to all other system threads.
                 Thread.Sleep(1)
             End While
 
@@ -498,7 +498,7 @@ Namespace IO
                 Exit Sub
             End If
 
-            ' We use this function to keep trying for a file lock...
+            ' Keeps trying for a file lock.
             Dim targetFile As FileStream = Nothing
             Dim startTime As Double = SystemTimer
 
@@ -508,7 +508,7 @@ Namespace IO
                     targetFile.Close()
                     Exit While
                 Catch
-                    ' We'll keep trying till we can open the file...
+                    ' Keeps trying to open the file.
                 End Try
 
                 If targetFile IsNot Nothing Then
@@ -526,7 +526,7 @@ Namespace IO
                     End If
                 End If
 
-                ' Yield to all other system threads...
+                ' Yields to all other system threads.
                 Thread.Sleep(1)
             End While
 
@@ -546,7 +546,7 @@ Namespace IO
         ''' <remarks>Set secondsToWait to zero to wait infinitely.</remarks>
         Public Shared Sub WaitTillExists(ByVal fileName As String, ByVal secondsToWait As Double)
 
-            ' We use this function to keep waiting for a file to be created...
+            ' Keeps waiting for a file to be created.
             Dim startTime As Double = SystemTimer
 
             While Not File.Exists(fileName)
@@ -557,7 +557,7 @@ Namespace IO
                     End If
                 End If
 
-                ' Yield to all other system threads...
+                ' Yields to all other system threads.
                 Thread.Sleep(1)
             End While
 
