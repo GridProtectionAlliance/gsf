0b04685fccf5c03167e7f0f7b1ac61e504c03fc4
Dzuerche <dzuerche@tva.gov>
2007-09-13 18:22:04 +0000

    Edited code comments.

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/DateTime/UnixTimeTag.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/DateTime/UnixTimeTag.vb
@@ -11,13 +11,13 @@
 '  Code Modification History:
 '  -----------------------------------------------------------------------------------------------------
 '  10/12/2005 - J. Ritchie Carroll
-'       Original version of source generated
+'       Gnerated original version of source.
 '  01/05/2006 - J. Ritchie Carroll
-'       2.0 version of source code migrated from 1.1 source (TVA.Interop.Unix.TimeTag)
+'       Migrated 2.0 version of source code from 1.1 source (TVA.Interop.Unix.TimeTag).
 '  01/24/2006 - J. Ritchie Carroll
-'       Moved into DateTime namespace and renamed to UnixTimeTag
+'       Moved into DateTime namespace and renamed to UnixTimeTag.
 '  07/12/2006 - J. Ritchie Carroll
-'       Modified class to be derived from new "TimeTagBase" class 
+'       Modified class to be derived from new "TimeTagBase" class. 
 '
 '*******************************************************************************************************
 
@@ -31,8 +31,8 @@ Namespace DateTime
 
         Inherits TimeTagBase
 
-        ' Unix dates are measured as the number of seconds since 1/1/1970, so we calculate this
-        ' date to get offset in ticks for later conversion...
+        ' Unix dates are measured as the number of seconds since 1/1/1970, so this class calculates this
+        ' date to get the offset in ticks for later conversion.
         Private Shared m_unixDateOffsetTicks As Long = (New Date(1970, 1, 1, 0, 0, 0)).Ticks
 
         Protected Sub New(ByVal info As SerializationInfo, ByVal context As StreamingContext)
@@ -41,16 +41,16 @@ Namespace DateTime
 
         End Sub
 
-        ''' <summary>Creates new Unix timetag given number of seconds since 1/1/1900</summary>
-        ''' <param name="seconds">Number of seconds since 1/1/1970</param>
+        ''' <summary>Creates new Unix timetag, given number of seconds since 1/1/1900.</summary>
+        ''' <param name="seconds">Number of seconds since 1/1/1970.</param>
         Public Sub New(ByVal seconds As Double)
 
             MyBase.New(m_unixDateOffsetTicks, seconds)
 
         End Sub
 
-        ''' <summary>Creates new Unix timetag given standard .NET DateTime</summary>
-        ''' <param name="timestamp">.NET DateTime to create Unix timetag from (minimum valid date is 1/1/1970)</param>
+        ''' <summary>Creates new Unix timetag, given standard .NET DateTime.</summary>
+        ''' <param name="timestamp">.NET DateTime to create Unix timetag from (minimum valid date is 1/1/1970).</param>
         Public Sub New(ByVal timestamp As Date)
 
             MyBase.New(m_unixDateOffsetTicks, timestamp)
