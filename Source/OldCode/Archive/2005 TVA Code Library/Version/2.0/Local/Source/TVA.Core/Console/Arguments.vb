1e2d85d269ebbed9818805c2ef08046018d8426e
Dzuerche <dzuerche@tva.gov>
2007-09-13 18:46:52 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/Console/Arguments.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/2.0/Local/Source/TVA.Core/Console/Arguments.vb
@@ -1,4 +1,19 @@
-' 02/08/2007
+'*******************************************************************************************************
+'  TVA.Configuration.Common.vb - Common Configuration Functions
+'  Copyright © 2007 - TVA, all rights reserved - Gbtc
+'
+'  Build Environment: VB.NET, Visual Studio 2005
+'  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
+'      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+'       Phone: 423/751-2250
+'       Email: pcpatel@tva.gov
+'
+'  Code Modification History:
+'  -----------------------------------------------------------------------------------------------------
+'  02/08/2007 - Pinal C. Patel
+'       Generated original version of source code.
+'
+'*******************************************************************************************************
 
 Imports System.Collections.Specialized
 Imports System.Text.RegularExpressions
