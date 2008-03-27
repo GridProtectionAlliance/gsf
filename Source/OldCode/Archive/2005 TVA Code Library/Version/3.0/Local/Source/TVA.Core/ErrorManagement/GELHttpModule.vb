414cb09d5273c385d2e081c34ebb76cadac59b17
pinalcpatel <pcpatel@tva.gov>
2008-03-27 15:26:03 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/3.0/Local/Source/TVA.Core/ErrorManagement/GELHttpModule.vb
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/3.0/Local/Source/TVA.Core/ErrorManagement/GELHttpModule.vb
@@ -27,6 +27,33 @@ Namespace ErrorManagement
     Public Class GELHttpModule
         Implements IHttpModule
 
+        ' Usage sample in web.config:
+        '<configuration>
+        '  <configSections>
+        '    <section name="categorizedSettings" type="TVA.Configuration.CategorizedSettingsSection, TVA.Core" />
+        '  </configSections>
+        '  <categorizedSettings>
+        '    <globalExceptionLogger>
+        '      <clear />
+        '      <add name="AutoStart" value="False" description="True if the logger is to be started automatically after initialization is complete; otherwise False." encrypted="false" />
+        '      <add name="LogToUI" value="True" description="True if an encountered exception is to be logged to the User Interface; otherwise False." encrypted="false" />
+        '      <add name="LogToFile" value="False" description="True if an encountered exception is to be logged to a file; otherwise False." encrypted="false" />
+        '      <add name="LogToEmail" value="True" description="True if an email is to be sent with the details of an encountered exception; otherwise False." encrypted="false" />
+        '      <add name="LogToEventLog" value="False" description="True if an encountered exception is to be logged to the Event Log; otherwise False." encrypted="false" />
+        '      <add name="LogToScreenshot" value="False" description="True if a screenshot is to be taken when an exception is encountered; otherwise False." encrypted="false" />
+        '      <add name="EmailServer" value="mailhost.cha.tva.gov" description="Name of the email server to be used for sending the email message." encrypted="false" />
+        '      <add name="EmailRecipients" value="mpthakka@tva.gov" description="Comma-seperated list of recipients email addresses for the email message." encrypted="false" />
+        '      <add name="ContactPersonName" value="" description="Name of the person that the end-user can contact when an exception is encountered." encrypted="false" />
+        '      <add name="ContactPersonPhone" value="" description="Phone number of the person that the end-user can contact when an exception is encountered." encrypted="false" />
+        '    </globalExceptionLogger>
+        '  </categorizedSettings>
+        '  <system.web>
+        '    <httpModules>
+        '      <add name="GELHttpModule" type="TVA.ErrorManagement.GELHttpModule, TVA.Core" />
+        '    </httpModules>
+        '  </system.web>
+        '</configuration>
+
         Public Sub Dispose() Implements System.Web.IHttpModule.Dispose
 
             ' We do not have to dispose of anything.
