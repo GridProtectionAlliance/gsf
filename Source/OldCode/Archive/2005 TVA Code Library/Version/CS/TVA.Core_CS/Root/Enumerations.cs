8a1709eee4d3a82c59d7f326a6ddc190423e46c3
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-08 21:48:58 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/Enumerations.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/Enumerations.cs
@@ -1,55 +1,64 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-
-// PCP: 04-09-2007
+//*******************************************************************************************************
+//  Enumerations.vb - Global enumerations for this namespace
+//  Copyright © 2005 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: VB.NET, Visual Studio 2005
+//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//       Phone: 423/751-2250
+//       Email: pcpatel@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  04/09/2007 - Pinal C. Patel
+//      Generated original version of source code.
+//  09/08/2008 - J. Ritchie Carroll
+//      Converted to C#.
+//
+//*******************************************************************************************************
 
 /// <summary>
 /// Specifies the type of the application.
 /// </summary>
 namespace TVA
 {
-	public enum ApplicationType
-	{
-		/// <summary>
-		/// Application is of unknown type.
-		/// </summary>
-		Unknown = 0,
-		/// <summary>
-		/// Application doesn't require a subsystem.
-		/// </summary>
-		Native = 1,
-		/// <summary>
-		/// Application runs in the Windows GUI subsystem.
-		/// </summary>
-		WindowsGui = 2,
-		/// <summary>
-		/// Application runs in the Windows character subsystem.
-		/// </summary>
-		WindowsCui = 3,
-		/// <summary>
-		/// Application runs in the OS/2 character subsystem.
-		/// </summary>
-		OS2Cui = 5,
-		/// <summary>
-		/// Application runs in the Posix character subsystem.
-		/// </summary>
-		PosixCui = 7,
-		/// <summary>
-		/// Application is a native Win9x driver.
-		/// </summary>
-		NativeWindows = 8,
-		/// <summary>
-		/// Application runs in the Windows CE subsystem.
-		/// </summary>
-		WindowsCEGui = 9,
-		/// <summary>
-		/// The application is a web site or web application.
-		/// </summary>
-		Web = 15
-	}
+    public enum ApplicationType
+    {
+        /// <summary>
+        /// Application is of unknown type.
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        /// Application doesn't require a subsystem.
+        /// </summary>
+        Native = 1,
+        /// <summary>
+        /// Application runs in the Windows GUI subsystem.
+        /// </summary>
+        WindowsGui = 2,
+        /// <summary>
+        /// Application runs in the Windows character subsystem.
+        /// </summary>
+        WindowsCui = 3,
+        /// <summary>
+        /// Application runs in the OS/2 character subsystem.
+        /// </summary>
+        OS2Cui = 5,
+        /// <summary>
+        /// Application runs in the Posix character subsystem.
+        /// </summary>
+        PosixCui = 7,
+        /// <summary>
+        /// Application is a native Win9x driver.
+        /// </summary>
+        NativeWindows = 8,
+        /// <summary>
+        /// Application runs in the Windows CE subsystem.
+        /// </summary>
+        WindowsCEGui = 9,
+        /// <summary>
+        /// The application is a web site or web application.
+        /// </summary>
+        Web = 15
+    }
 }
