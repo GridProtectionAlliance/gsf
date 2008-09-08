7ea8f762f91865705d04117169d8b85b08353b23
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-08 21:50:57 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/GenericEventArgs.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/GenericEventArgs.cs
@@ -1,40 +1,45 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
+//*******************************************************************************************************
+//  GenericEventArgs.vb - Generic event arguments class
+//  Copyright © 2005 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: VB.NET, Visual Studio 2005
+//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//       Phone: 423/751-2250
+//       Email: pcpatel@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  11/24/2006 - Pinal C. Patel
+//      Generated original version of source code.
+//  09/08/2008 - J. Ritchie Carroll
+//      Converted to C#.
+//
+//*******************************************************************************************************
 
-// 11/24/20006
+using System;
 
 namespace TVA
 {
-	public class GenericEventArgs<T> : EventArgs
-	{
-		
-		
-		private T m_argument;
-		
-		public GenericEventArgs(T argument)
-		{
-			
-			m_argument = argument;
-			
-		}
-		
-		public T Argument
-		{
-			get
-			{
-				return m_argument;
-			}
-			set
-			{
-				m_argument = value;
-			}
-		}
-		
-	}
-	
+    public class GenericEventArgs<T> : EventArgs
+    {
+        private T m_argument;
+
+        public GenericEventArgs(T argument)
+        {
+            m_argument = argument;
+        }
+
+        public T Argument
+        {
+            get
+            {
+                return m_argument;
+            }
+            set
+            {
+                m_argument = value;
+            }
+        }
+    }
 }
