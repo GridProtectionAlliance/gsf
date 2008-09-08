1451e4838388af54c07bb94a44765a3c32d807d5
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-08 21:54:37 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/IdentifiableItem.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/IdentifiableItem.cs
@@ -1,54 +1,67 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
+//*******************************************************************************************************
+//  IdentifiableItem.vb - Generic identifiable item class
+//  Copyright © 2005 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: VB.NET, Visual Studio 2005
+//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//       Phone: 423/751-2250
+//       Email: pcpatel@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  03/12/2007 - Pinal C. Patel
+//      Generated original version of source code.
+//  09/08/2008 - J. Ritchie Carroll
+//      Converted to C#.
+//
+//*******************************************************************************************************
 
-// 03/12/2007
+using System;
 
 namespace TVA
 {
-	public class IdentifiableItem<TIdentifier, TItem>
-	{
-		
-		
-		private TIdentifier m_source;
-		private TItem m_item;
-		
-		public IdentifiableItem(TIdentifier source, TItem item)
-		{
-			
-			m_source = source;
-			m_item = item;
-			
-		}
-		
-		public TIdentifier Source
-		{
-			get
-			{
-				return m_source;
-			}
-			set
-			{
-				m_source = value;
-			}
-		}
-		
-		public TItem Item
-		{
-			get
-			{
-				return m_item;
-			}
-			set
-			{
-				m_item = value;
-			}
-		}
-		
-	}
-	
+    /// <summary>
+    /// Generic identifiable item class.
+    /// </summary>
+    /// <remarks>
+    /// This class is used to abstractly track items along with their source.
+    /// </remarks>
+    /// <typeparam name="TIdentifier">Type of data item source.</typeparam>
+    /// <typeparam name="TItem">Type of data item.</typeparam>
+    public class IdentifiableItem<TIdentifier, TItem>
+    {
+        private TIdentifier m_source;
+        private TItem m_item;
+
+        public IdentifiableItem(TIdentifier source, TItem item)
+        {
+            m_source = source;
+            m_item = item;
+        }
+
+        public TIdentifier Source
+        {
+            get
+            {
+                return m_source;
+            }
+            set
+            {
+                m_source = value;
+            }
+        }
+
+        public TItem Item
+        {
+            get
+            {
+                return m_item;
+            }
+            set
+            {
+                m_item = value;
+            }
+        }
+    }
 }
