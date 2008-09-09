162500cd1a09d2286d0912498126cda546b8131f
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-09 21:13:09 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Interop/BitwiseCast.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Interop/BitwiseCast.cs
@@ -1,11 +1,3 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-
 //*******************************************************************************************************
 //  TVA.Interop.BitwiseCast.vb - Bitwise Integer Data Type Conversion Functions
 //  Copyright © 2006 - TVA, all rights reserved - Gbtc
@@ -20,108 +12,141 @@ using System;
 //  -----------------------------------------------------------------------------------------------------
 //  01/05/2006 - J. Ritchie Carroll
 //       Original version of source code generated
+//  09/09/2008 - J. Ritchie Carroll
+//      Converted to C#
 //
 //*******************************************************************************************************
 
+using System;
+
 namespace TVA
 {
-	namespace Interop
-	{
-		
-		/// <summary>Defines specialized bitwise integer data type conversion functions</summary>
-		public sealed class BitwiseCast
-		{
-			
-			
-			private BitwiseCast()
-			{
-				
-				// This class contains only global functions and is not meant to be instantiated
-				
-			}
-			
-			/// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
-			/// <remarks>
-			/// <para>This function is useful because Convert.ToInt16 will throw an OverflowException for values greater than Int16.MaxValue.</para>
-			/// <para>For example, this function correctly converts unsigned 16-bit integer 65535 (i.e., UInt16.MaxValue) to signed 16-bit integer -1.</para>
-			/// </remarks>
-			public static short ToInt16(UInt16 unsignedInt)
-			{
-				return BitConverter.ToInt16(BitConverter.GetBytes(unsignedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
-			/// <remarks>
-			/// <para>This function is useful because CType(n, Int24) will throw an OverflowException for values greater than Int24.MaxValue.</para>
-			/// <para>For example, this function correctly converts unsigned 24-bit integer 16777215 (i.e., UInt24.MaxValue) to signed 24-bit integer -1.</para>
-			/// </remarks>
-			public static Int24 ToInt24(UInt24 unsignedInt)
-			{
-				return Int24.GetValue(UInt24.GetBytes(unsignedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
-			/// <remarks>
-			/// <para>This function is useful because Convert.ToInt32 will throw an OverflowException for values greater than Int32.MaxValue.</para>
-			/// <para>For example, this function correctly converts unsigned 32-bit integer 4294967295 (i.e., UInt16.MaxValue) to signed 32-bit integer -1.</para>
-			/// </remarks>
-			public static int ToInt32(UInt32 unsignedInt)
-			{
-				return BitConverter.ToInt32(BitConverter.GetBytes(unsignedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
-			/// <remarks>
-			/// <para>This function is useful because Convert.ToInt64 will throw an OverflowException for values greater than Int64.MaxValue.</para>
-			/// <para>For example, this function correctly converts unsigned 64-bit integer 18446744073709551615 (i.e., UInt64.MaxValue) to signed 64-bit integer -1.</para>
-			/// </remarks>
-			public static long ToInt64(UInt64 unsignedInt)
-			{
-				return BitConverter.ToInt64(BitConverter.GetBytes(unsignedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
-			/// <remarks>
-			/// <para>This function is useful because Convert.ToUInt16 will throw an OverflowException for values less than zero.</para>
-			/// <para>For example, this function correctly converts signed 16-bit integer -32768 (i.e., Int16.MinValue) to unsigned 16-bit integer 32768.</para>
-			/// </remarks>
-			public static UInt16 ToUInt16(short signedInt)
-			{
-				return BitConverter.ToUInt16(BitConverter.GetBytes(signedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
-			/// <remarks>
-			/// <para>This function is useful because CType(n, UInt24) will throw an OverflowException for values less than zero.</para>
-			/// <para>For example, this function correctly converts signed 24-bit integer -8388608 (i.e., Int24.MinValue) to unsigned 24-bit integer 8388608.</para>
-			/// </remarks>
-			public static UInt24 ToUInt24(Int24 signedInt)
-			{
-				return UInt24.GetValue(Int24.GetBytes(signedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
-			/// <remarks>
-			/// <para>This function is useful because Convert.ToUInt32 will throw an OverflowException for values less than zero.</para>
-			/// <para>For example, this function correctly converts signed 32-bit integer -2147483648 (i.e., Int32.MinValue) to unsigned 32-bit integer 2147483648.</para>
-			/// </remarks>
-			public static UInt32 ToUInt32(int signedInt)
-			{
-				return BitConverter.ToUInt32(BitConverter.GetBytes(signedInt), 0);
-			}
-			
-			/// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
-			/// <remarks>
-			/// <para>This function is useful because Convert.ToUInt64 will throw an OverflowException for values less than zero.</para>
-			/// <para>For example, this function correctly converts signed 64-bit integer -9223372036854775808 (i.e., Int64.MinValue) to unsigned 64-bit integer 9223372036854775808.</para>
-			/// </remarks>
-			public static UInt64 ToUInt64(long signedInt)
-			{
-				return BitConverter.ToUInt64(BitConverter.GetBytes(signedInt), 0);
-			}
-			
-		}
-		
-	}
-	
+    namespace Interop
+    {
+        /// <summary>Defines specialized bitwise integer data type conversion functions</summary>
+        /// <remarks>
+        /// This class allows for proper bitwise casting between signed and unsigned integers. It may be most
+        /// useful in languages that do not allow override of numerical overflow checks.  For example, C#
+        /// provides an "unchecked" keyword to allow for bitwise casting.
+        /// </remarks>
+        public sealed class BitwiseCast
+        {
+            private BitwiseCast()
+            {
+                // This class contains only global functions and is not meant to be instantiated
+            }
+
+            /// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
+            /// <remarks>
+            /// <para>This function is useful because Convert.ToInt16 will throw an OverflowException for values greater than Int16.MaxValue.</para>
+            /// <para>For example, this function correctly converts unsigned 16-bit integer 65535 (i.e., UInt16.MaxValue) to signed 16-bit integer -1.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static short ToInt16(ushort unsignedInt)
+            {
+                unchecked
+                {
+                    return (short)unsignedInt;
+                }
+                //return BitConverter.ToInt16(BitConverter.GetBytes(unsignedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
+            /// <remarks>
+            /// <para>This function is useful because CType(n, Int24) will throw an OverflowException for values greater than Int24.MaxValue.</para>
+            /// <para>For example, this function correctly converts unsigned 24-bit integer 16777215 (i.e., UInt24.MaxValue) to signed 24-bit integer -1.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static Int24 ToInt24(UInt24 unsignedInt)
+            {
+                return Int24.GetValue(UInt24.GetBytes(unsignedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
+            /// <remarks>
+            /// <para>This function is useful because Convert.ToInt32 will throw an OverflowException for values greater than Int32.MaxValue.</para>
+            /// <para>For example, this function correctly converts unsigned 32-bit integer 4294967295 (i.e., UInt32.MaxValue) to signed 32-bit integer -1.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static int ToInt32(uint unsignedInt)
+            {
+                unchecked
+                {
+                    return (int)unsignedInt;
+                }
+                //return BitConverter.ToInt32(BitConverter.GetBytes(unsignedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between unsigned and signed value</summary>
+            /// <remarks>
+            /// <para>This function is useful because Convert.ToInt64 will throw an OverflowException for values greater than Int64.MaxValue.</para>
+            /// <para>For example, this function correctly converts unsigned 64-bit integer 18446744073709551615 (i.e., UInt64.MaxValue) to signed 64-bit integer -1.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static long ToInt64(ulong unsignedInt)
+            {
+                unchecked
+                {
+                    return (long)unsignedInt;
+                }
+                //return BitConverter.ToInt64(BitConverter.GetBytes(unsignedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
+            /// <remarks>
+            /// <para>This function is useful because Convert.ToUInt16 will throw an OverflowException for values less than zero.</para>
+            /// <para>For example, this function correctly converts signed 16-bit integer -32768 (i.e., Int16.MinValue) to unsigned 16-bit integer 32768.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static ushort ToUInt16(short signedInt)
+            {
+                unchecked
+                {
+                    return (ushort)signedInt;
+                }
+                //return BitConverter.ToUInt16(BitConverter.GetBytes(signedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
+            /// <remarks>
+            /// <para>This function is useful because CType(n, UInt24) will throw an OverflowException for values less than zero.</para>
+            /// <para>For example, this function correctly converts signed 24-bit integer -8388608 (i.e., Int24.MinValue) to unsigned 24-bit integer 8388608.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static UInt24 ToUInt24(Int24 signedInt)
+            {
+                return UInt24.GetValue(Int24.GetBytes(signedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
+            /// <remarks>
+            /// <para>This function is useful because Convert.ToUInt32 will throw an OverflowException for values less than zero.</para>
+            /// <para>For example, this function correctly converts signed 32-bit integer -2147483648 (i.e., Int32.MinValue) to unsigned 32-bit integer 2147483648.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static uint ToUInt32(int signedInt)
+            {
+                unchecked
+                {
+                    return (uint)signedInt;
+                }
+                //return BitConverter.ToUInt32(BitConverter.GetBytes(signedInt), 0);
+            }
+
+            /// <summary>Performs proper bitwise conversion between signed and unsigned value</summary>
+            /// <remarks>
+            /// <para>This function is useful because Convert.ToUInt64 will throw an OverflowException for values less than zero.</para>
+            /// <para>For example, this function correctly converts signed 64-bit integer -9223372036854775808 (i.e., Int64.MinValue) to unsigned 64-bit integer 9223372036854775808.</para>
+            /// </remarks>
+            [CLSCompliant(false)]
+            public static ulong ToUInt64(long signedInt)
+            {
+                unchecked
+                {
+                    return (ulong)signedInt;
+                }
+                //return BitConverter.ToUInt64(BitConverter.GetBytes(signedInt), 0);
+            }
+        }
+    }
 }
