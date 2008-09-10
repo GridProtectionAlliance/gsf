1a61c4f5095968c6d34624f31317222e2c540e1d
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-10 20:17:22 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/Int24.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Root/Int24.cs
@@ -18,8 +18,6 @@
 //*******************************************************************************************************
 
 using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using System.Globalization;
 using System.ComponentModel;
