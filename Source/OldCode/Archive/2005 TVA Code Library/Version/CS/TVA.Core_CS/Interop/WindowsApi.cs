a3283d3da7e0874029b09cfff1578817f87ea9f1
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-10 21:33:10 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Interop/WindowsApi.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Interop/WindowsApi.cs
@@ -1,12 +1,3 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-using System.Runtime.InteropServices;
-
 //*******************************************************************************************************
 //  TVA.Interop.WindowsApi.vb - Common Windows API Functions
 //  Copyright © 2006 - TVA, all rights reserved - Gbtc
@@ -21,47 +12,42 @@ using System.Runtime.InteropServices;
 //  -----------------------------------------------------------------------------------------------------
 //  01/24/2006 - J. Ritchie Carroll
 //       Initial version of source created
+//  09/10/2008 - J. Ritchie Carroll
+//      Converted to C#
 //
 //*******************************************************************************************************
 
+using System;
+using System.Runtime.InteropServices;
 
 namespace TVA
 {
-	namespace Interop
-	{
-		
-		/// <summary>Defines common Windows API functions</summary>
-		public sealed class WindowsApi
-		{
-			
-			
-			[DllImport("kernel32.dll")]private static  extern int FormatMessage(int dwFlags, ref IntPtr lpSource, int dwMessageId, int dwLanguageId, ref string lpBuffer, int nSize, ref IntPtr Arguments);
-			
-			/// <summary>Formats and returns a .NET string containing the Windows API level error message corresponding to the specified error code</summary>
-			public static string GetErrorMessage(int errorCode)
-			{
-				
-				const int FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x100;
-				const int FORMAT_MESSAGE_IGNORE_INSERTS = 0x200;
-				const int FORMAT_MESSAGE_FROM_SYSTEM = 0x1000;
-				
-				int messageSize = 255;
-				string lpMsgBuf = "";
-				int dwFlags = FORMAT_MESSAGE_ALLOCATE_BUFFER || FORMAT_MESSAGE_FROM_SYSTEM || FORMAT_MESSAGE_IGNORE_INSERTS;
-				IntPtr ptrlpSource = IntPtr.Zero;
-				IntPtr prtArguments = IntPtr.Zero;
-				
-				if (FormatMessage(dwFlags, ref ptrlpSource, errorCode, 0, ref lpMsgBuf, messageSize, ref prtArguments) == 0)
-				{
-					throw (new InvalidOperationException("Failed to format message for error code " + errorCode));
-				}
-				
-				return lpMsgBuf;
-				
-			}
-			
-		}
-		
-	}
-	
+    namespace Interop
+    {
+        /// <summary>Defines common Windows API functions</summary>
+        public static class WindowsApi
+        {
+            [DllImport("kernel32.dll")]
+            private static extern int FormatMessage(int dwFlags, ref IntPtr lpSource, int dwMessageId, int dwLanguageId, ref string lpBuffer, int nSize, ref IntPtr Arguments);
+
+            /// <summary>Formats and returns a .NET string containing the Windows API level error message corresponding to the specified error code</summary>
+            public static string GetErrorMessage(int errorCode)
+            {
+                const int FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x100;
+                const int FORMAT_MESSAGE_IGNORE_INSERTS = 0x200;
+                const int FORMAT_MESSAGE_FROM_SYSTEM = 0x1000;
+
+                int dwFlags = FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+                int messageSize = 255;
+                string lpMsgBuf = "";
+                IntPtr ptrlpSource = IntPtr.Zero;
+                IntPtr prtArguments = IntPtr.Zero;
+
+                if (FormatMessage(dwFlags, ref ptrlpSource, errorCode, 0, ref lpMsgBuf, messageSize, ref prtArguments) == 0)
+                    throw new InvalidOperationException("Failed to format message for error code " + errorCode);
+
+                return lpMsgBuf;
+            }
+        }
+    }
 }
