c8897650bb829b252b816a168554fd1b973dde7f
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-11 13:14:29 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Collections/Enumerations.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Collections/Enumerations.cs
@@ -1,11 +1,3 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-
 //*******************************************************************************************************
 //  Enumerations.vb - Global enumerations for this namespace
 //  Copyright © 2005 - TVA, all rights reserved - Gbtc
@@ -24,44 +16,46 @@ using System;
 //       Added "RequeueMode" enumeration.
 //  08/17/2007 - Darrell Zuercher
 //       Edited code comments.
+//  09/11/2008 - J. Ritchie Carroll
+//      Converted to C#
 //
 //*******************************************************************************************************
 
+using System;
+
 namespace TVA
 {
-	namespace Collections
-	{
-		
-		/// <summary>Enumeration of possible queue threading modes.</summary>
-		public enum QueueThreadingMode
-		{
-			/// <summary>Processes several items in the queue at once on different threads, where processing order is not
-			/// important.</summary>
-			Asynchronous,
-			/// <summary>Processes items in the queue one at a time on a single thread, where processing order is important.</summary>
-			Synchronous
-		}
-		
-		/// <summary>Enumeration of possible queue processing styles.</summary>
-		public enum QueueProcessingStyle
-		{
-			/// <summary>Defines queue processing delegate to process only one item at a time.</summary>
-			/// <remarks>This is the typical processing style when the threading mode is asynchronous.</remarks>
-			OneAtATime,
-			/// <summary>Defines queue processing delegate to process all currently available items in the queue. Items are
-			/// passed into delegate as an array.</summary>
-			/// <remarks>This is the optimal processing style when the threading mode is synchronous.</remarks>
-			ManyAtOnce
-		}
-		
-		/// <summary>Enumeration of possible requeue modes.</summary>
-		public enum RequeueMode
-		{
-			/// <summary>Requeues item at the beginning of the list.</summary>
-			Prefix,
-			/// <summary>Requeues item at the end of the list.</summary>
-			Suffix
-		}
-		
-	}
+    namespace Collections
+    {
+        /// <summary>Enumeration of possible queue threading modes.</summary>
+        public enum QueueThreadingMode
+        {
+            /// <summary>Processes several items in the queue at once on different threads, where processing order is not
+            /// important.</summary>
+            Asynchronous,
+            /// <summary>Processes items in the queue one at a time on a single thread, where processing order is important.</summary>
+            Synchronous
+        }
+
+        /// <summary>Enumeration of possible queue processing styles.</summary>
+        public enum QueueProcessingStyle
+        {
+            /// <summary>Defines queue processing delegate to process only one item at a time.</summary>
+            /// <remarks>This is the typical processing style when the threading mode is asynchronous.</remarks>
+            OneAtATime,
+            /// <summary>Defines queue processing delegate to process all currently available items in the queue. Items are
+            /// passed into delegate as an array.</summary>
+            /// <remarks>This is the optimal processing style when the threading mode is synchronous.</remarks>
+            ManyAtOnce
+        }
+
+        /// <summary>Enumeration of possible requeue modes.</summary>
+        public enum RequeueMode
+        {
+            /// <summary>Requeues item at the beginning of the list.</summary>
+            Prefix,
+            /// <summary>Requeues item at the end of the list.</summary>
+            Suffix
+        }
+    }
 }
