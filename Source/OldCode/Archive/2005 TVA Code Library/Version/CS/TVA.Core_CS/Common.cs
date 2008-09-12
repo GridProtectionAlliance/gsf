6a1a5fa59c5f432d8e531817f393e725e1d3546d
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-12 21:49:33 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Common.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Common.cs
@@ -24,6 +24,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using TVA.Collections;
@@ -170,11 +171,13 @@ namespace TVA
             return (IsReference(item) && !(item is string));
         }
 
+
 /*-----------------------------------------------------------------------------------------------------*\
  *
  *                    These functions were migrated here from TVA.Collections.Common
  *                  
 \*-----------------------------------------------------------------------------------------------------*/
+
 
         /// <summary>Returns the smallest item from a list of parameters.</summary>
         public static object Min(params object[] itemList)
@@ -245,5 +248,309 @@ namespace TVA
                 return (Operators.ConditionalCompareObjectLess(x, y, false) ? -1 : (Operators.ConditionalCompareObjectGreater(x, y, false) ? 1 : 0));
             }
         }
+
+
+/*-----------------------------------------------------------------------------------------------------*\
+ *
+ *                    These functions were migrated here from TVA.DateTime.Common
+ *                  
+\*-----------------------------------------------------------------------------------------------------*/
+
+
+        /// <summary>Time names enumeration used by SecondsToText function.</summary>
+        public static struct TimeName
+        {
+            static public int Year = 0;
+            static public int Years = 1;
+            static public int Day = 2;
+            static public int Days = 3;
+            static public int Hour = 4;
+            static public int Hours = 5;
+            static public int Minute = 6;
+            static public int Minutes = 7;
+            static public int Second = 8;
+            static public int Seconds = 9;
+            static public int LessThan60Seconds = 10;
+            static public int NoSeconds = 11;
+        }
+
+        /// <summary>Number of 100-nanosecond ticks in one second.</summary>
+        public const long TicksPerSecond = 10000000;
+
+        /// <summary>Number of 100-nanosecond ticks in one millisecond.</summary>
+        public const long TicksPerMillisecond = TicksPerSecond / 1000;
+
+        /// <summary>Standard time names used by SecondsToText function.</summary>
+        private static string[] m_standardTimeNames = new string[] { "Year", "Years", "Day", "Days", "Hour", "Hours", "Minute", "Minutes", "Second", "Seconds", "Less Than 60 Seconds", "0 Seconds" };
+
+        /// <summary>Standard time names, without seconds, used by SecondsToText function.</summary>
+        private static string[] m_standardTimeNamesWithoutSeconds = new string[] { "Year", "Years", "Day", "Days", "Hour", "Hours", "Minute", "Minutes", "Second", "Seconds", "Less Than 1 Minute", "0 Minutes" };
+
+        /// <summary>Converts 100-nanosecond tick intervals to seconds.</summary>
+        public static double TicksToSeconds(long ticks)
+        {
+            return ticks / TicksPerSecond;
+        }
+
+        /// <summary>Converts 100-nanosecond tick intervals to milliseconds.</summary>
+        public static double TicksToMilliseconds(long ticks)
+        {
+            return ticks / TicksPerMillisecond;
+        }
+
+        /// <summary>Converts seconds to 100-nanosecond tick intervals.</summary>
+        public static long SecondsToTicks(double seconds)
+        {
+            return (int)(seconds * TicksPerSecond);
+        }
+
+        /// <summary>Converts milliseconds to 100-nanosecond tick intervals.</summary>
+        public static long MillisecondsToTicks(double milliseconds)
+        {
+            return (int)(milliseconds * TicksPerMillisecond);
+        }
+
+        /// <summary>Gets  the number of seconds in the local timezone, including fractional seconds, that have
+        /// elapsed since 12:00:00 midnight, January 1, 0001.</summary>
+        public static double SystemTimer
+        {
+            get
+            {
+                return TicksToSeconds(DateTime.Now.Ticks);
+            }
+        }
+
+        /// <summary>Gets the number of seconds in the universally coordinated timezone, including fractional
+        /// seconds, that have elapsed since 12:00:00 midnight, January 1, 0001.</summary>
+        public static double UtcSystemTimer
+        {
+            get
+            {
+                return TicksToSeconds(DateTime.UtcNow.Ticks);
+            }
+        }
+
+        /// <summary>Turns the given number of seconds into textual representation of years, days, hours, minutes
+        /// and whole integer seconds.</summary>
+        /// <param name="seconds">Seconds to be converted.</param>
+        public static string SecondsToText(double seconds)
+        {
+            return SecondsToText(seconds, 0);
+        }
+
+        /// <summary>Turns the given number of seconds into textual representation of years, days, hours, minutes
+        /// and seconds.</summary>
+        /// <param name="seconds">Seconds to be converted.</param>
+        /// <param name="secondPrecision">Number of fractional digits to display for seconds.</param>
+        /// <remarks>Set second precision to -1 to suppress seconds display.</remarks>
+        public static string SecondsToText(double seconds, int secondPrecision)
+        {
+            if (secondPrecision < 0)
+                return SecondsToText(seconds, secondPrecision, m_standardTimeNamesWithoutSeconds);
+            else
+                return SecondsToText(seconds, secondPrecision, m_standardTimeNames);
+        }
+
+        /// <summary>Turns the given number of seconds into textual representation of years, days, hours, minutes
+        /// and seconds given string array of time names. Need one for each TimeName enum item.</summary>
+        /// <param name="seconds">Seconds to be converted.</param>
+        /// <param name="secondPrecision">Number of fractional digits to display for seconds.</param>
+        /// <param name="timeNames">Time names array to use during textal conversion.</param>
+        /// <remarks>
+        /// <para>Set second precision to -1 to suppress seconds display.</para>
+        /// <para>Time names array needs one string entry per element in <see cref="TimeName">TimeName</see>
+        /// enumeration.</para>
+        /// <para>Example timeNames array: "Year", "Years", "Day", "Days", "Hour", "Hours", "Minute", "Minutes",
+        /// "Second", "Seconds", "Less Than 60 Seconds", "0 Seconds".</para>
+        /// </remarks>
+        public static string SecondsToText(double seconds, int secondPrecision, string[] timeNames)
+        {
+            StringBuilder timeImage = new StringBuilder();
+
+            int years; // 1 year   = 365.2425 days or 31556952 seconds
+            int days; // 1 day    = 86400 seconds
+            int hours; // 1 hour   = 3600 seconds
+            int minutes; // 1 minute = 60 seconds
+
+            // checks if number of seconds ranges in years.
+            years = (int)(seconds / 31556952);
+
+            if (years >= 1)
+            {
+                // Removes whole years from remaining seconds.
+                seconds = seconds - years * 31556952;
+
+                // Appends textual representation of years.
+                timeImage.Append(years);
+                timeImage.Append(' ');
+                if (years == 1)
+                {
+                    timeImage.Append(timeNames[TimeName.Year]);
+                }
+                else
+                {
+                    timeImage.Append(timeNames[TimeName.Years]);
+                }
+            }
+
+            // Checks if remaining number of seconds ranges in days.
+            days = (int)(seconds / 86400);
+            if (days >= 1)
+            {
+                // Removes whole days from remaining seconds.
+                seconds = seconds - days * 86400;
+
+                // Appends textual representation of days.
+                timeImage.Append(' ');
+                timeImage.Append(days);
+                timeImage.Append(' ');
+                if (days == 1)
+                {
+                    timeImage.Append(timeNames[TimeName.Day]);
+                }
+                else
+                {
+                    timeImage.Append(timeNames[TimeName.Days]);
+                }
+            }
+
+            // Checks if remaining number of seconds ranges in hours.
+            hours = (int)(seconds / 3600);
+            if (hours >= 1)
+            {
+                // Removes whole hours from remaining seconds.
+                seconds = seconds - hours * 3600;
+
+                // Appends textual representation of hours.
+                timeImage.Append(' ');
+                timeImage.Append(hours);
+                timeImage.Append(' ');
+                if (hours == 1)
+                {
+                    timeImage.Append(timeNames[TimeName.Hour]);
+                }
+                else
+                {
+                    timeImage.Append(timeNames[TimeName.Hours]);
+                }
+            }
+
+            // Checks if remaining number of seconds ranges in minutes.
+            minutes = (int)(seconds / 60);
+            if (minutes >= 1)
+            {
+                // Removes whole minutes from remaining seconds.
+                seconds = seconds - minutes * 60;
+
+                // Appends textual representation of minutes.
+                timeImage.Append(' ');
+                timeImage.Append(minutes);
+                timeImage.Append(' ');
+                if (minutes == 1)
+                {
+                    timeImage.Append(timeNames[TimeName.Minute]);
+                }
+                else
+                {
+                    timeImage.Append(timeNames[TimeName.Minutes]);
+                }
+            }
+
+            // Handles remaining seconds.
+            if (secondPrecision == 0)
+            {
+                // No fractional seconds requested. Rounds seconds to nearest integer.
+                int wholeSeconds = Convert.ToInt32(System.Math.Round(seconds));
+
+                if (wholeSeconds > 0)
+                {
+                    // Appends textual representation of whole seconds.
+                    timeImage.Append(' ');
+                    timeImage.Append(wholeSeconds);
+                    timeImage.Append(' ');
+                    if (wholeSeconds == 1)
+                    {
+                        timeImage.Append(timeNames[TimeName.Second]);
+                    }
+                    else
+                    {
+                        timeImage.Append(timeNames[TimeName.Seconds]);
+                    }
+                }
+            }
+            else
+            {
+                // Handles fractional seconds request.
+                if (seconds > 0)
+                {
+                    if (secondPrecision < 0)
+                    {
+                        // If second display has been disabled and less than 60 seconds remain, we still need
+                        // to show something.
+                        if (timeImage.Length == 0)
+                        {
+                            timeImage.Append(timeNames[TimeName.LessThan60Seconds]);
+                        }
+                    }
+                    else
+                    {
+                        // Appends textual representation of fractional seconds.
+                        timeImage.Append(' ');
+                        timeImage.Append(seconds.ToString("0." + (new string('0', secondPrecision))));
+                        timeImage.Append(' ');
+                        if (seconds == 1)
+                        {
+                            timeImage.Append(timeNames[TimeName.Second]);
+                        }
+                        else
+                        {
+                            timeImage.Append(timeNames[TimeName.Seconds]);
+                        }
+                    }
+                }
+            }
+
+            // Handles zero seconds display.
+            if (timeImage.Length == 0)
+            {
+                timeImage.Append(timeNames[TimeName.NoSeconds]);
+            }
+
+            return timeImage.ToString().Trim();
+
+        }
+
+    	/// <summary>Returns the specified Win32 time zone, using specified name.</summary>
+		/// <param name="name">Value of name used for time zone lookup.</param>
+		/// <param name="lookupBy">Type of name used for time zone lookup.</param>
+        public static TimeZoneInfo GetTimeZone(string name, TimeZoneName lookupBy)
+		{
+            foreach (TimeZoneInfo timeZone in TimeZoneInfo.GetSystemTimeZones())
+			{
+				if (lookupBy == TimeZoneName.DaylightName)
+				{
+					if (string.Compare(timeZone.DaylightName, name, true) == 0)
+					{
+						return timeZone;
+					}
+				}
+				else if (lookupBy == TimeZoneName.DisplayName)
+				{
+					if (string.Compare(timeZone.DisplayName, name, true) == 0)
+					{
+						return timeZone;
+					}
+				}
+				else if (lookupBy == TimeZoneName.StandardName)
+				{
+					if (string.Compare(timeZone.StandardName, name, true) == 0)
+					{
+						return timeZone;
+					}
+				}
+			}
+			
+			throw new ArgumentException("Windows time zone with " + lookupBy + " of \"" + name + "\" was not found!");
+		}
     }
 }
---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/CommonTimeZones.cs
@@ -0,0 +1,73 @@
+﻿//*******************************************************************************************************
+//  TVA.CommonTimeZones.vb - Defines a few common US timezones.
+//  Copyright © 2006 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: VB.NET, Visual Studio 2005
+//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//       Phone: 423/751-2827
+//       Email: jrcarrol@tva.gov
+//
+//  This is the location for handy miscellaneous functions that are difficult to categorize elsewhere
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  09/12/2008 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//*******************************************************************************************************
+
+using System;
+
+namespace TVA
+{
+    public static class CommonTimeZones
+    {
+        // We define a few common timezones for convenience.
+        private static TimeZoneInfo m_universalTimeZone;
+        private static TimeZoneInfo m_easternTimeZone;
+        private static TimeZoneInfo m_centralTimeZone;
+        private static TimeZoneInfo m_mountainTimeZone;
+        private static TimeZoneInfo m_pacificTimeZone;
+
+        /// <summary>Gets Eastern Time Zone.</summary>
+        public static TimeZoneInfo EasternTimeZone
+        {
+            get
+            {
+                if (m_easternTimeZone == null) m_easternTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+                return m_easternTimeZone;
+            }
+        }
+
+        /// <summary>Gets Central Time Zone.</summary>
+        public static TimeZoneInfo CentralTimeZone
+        {
+            get
+            {
+                if (m_centralTimeZone == null) m_centralTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+                return m_centralTimeZone;
+            }
+        }
+
+        /// <summary>Gets Mountain Time Zone.</summary>
+        public static TimeZoneInfo MountainTimeZone
+        {
+            get
+            {
+                if (m_mountainTimeZone == null) m_mountainTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time");
+                return m_mountainTimeZone;
+            }
+        }
+
+        /// <summary>Gets Pacific Standard Time Zone.</summary>
+        public static TimeZoneInfo PacificTimeZone
+        {
+            get
+            {
+                if (m_pacificTimeZone == null) m_pacificTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+                return m_pacificTimeZone;
+            }
+        }
+    }
+}
