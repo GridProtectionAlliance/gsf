132dc0a814747d464913d6d156f9d8a7ca1bbfd8
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-12 18:43:34 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Xml/Extensions.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Xml/Extensions.cs
@@ -27,134 +27,131 @@ using System;
 using System.Xml;
 using System.Data;
 
-namespace TVA
+namespace TVA.Xml
 {
-    namespace Xml
+    /// <summary>Defines common global functions related to XML data.</summary>
+    public static class Extensions
     {
-        /// <summary>Defines common global functions related to XML data.</summary>
-        public static class Extensions
+        /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
+        /// <remarks>This overload just allows the start of the given XML document by using its root element.</remarks>
+        public static XmlNode GetXmlNode(this XmlDocument xmlDoc, string xpath)
         {
-            /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
-            /// <remarks>This overload just allows the start of the given XML document by using its root element.</remarks>
-            public static XmlNode GetXmlNode(this XmlDocument xmlDoc, string xpath)
+            bool isDirty;
+            return xmlDoc.DocumentElement.GetXmlNode(xpath, out isDirty);
+        }
+
+        /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
+        /// <remarks>
+        /// <para>This overload just allows the start of the given XML document by using its root element.</para>
+        /// <para>Note that the <paramref name="isDirty" /> parameter will be set to True if any items were added to
+        /// the tree.</para>
+        /// </remarks>
+        public static XmlNode GetXmlNode(this XmlDocument xmlDoc, string xpath, out bool isDirty)
+        {
+            return xmlDoc.DocumentElement.GetXmlNode(xpath, out isDirty);
+        }
+
+        /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
+        public static XmlNode GetXmlNode(this XmlNode parentNode, string xpath)
+        {
+            bool isDirty;
+            return parentNode.GetXmlNode(xpath, out isDirty);
+        }
+
+        /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
+        /// <remarks>Note that the <paramref name="isDirty" /> parameter will be set to True if any items were added to
+        /// the tree.</remarks>
+        public static XmlNode GetXmlNode(this XmlNode parentNode, string xpath, out bool isDirty)
+        {
+            XmlNode node = null;
+            string[] elements;
+
+            isDirty = false;
+
+            // Removes any slash prefixes.
+            while (xpath[0] == '/')
             {
-                bool isDirty;
-                return xmlDoc.DocumentElement.GetXmlNode(xpath, out isDirty);
+                xpath = xpath.Substring(1);
             }
 
-            /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
-            /// <remarks>
-            /// <para>This overload just allows the start of the given XML document by using its root element.</para>
-            /// <para>Note that the <paramref name="isDirty" /> parameter will be set to True if any items were added to
-            /// the tree.</para>
-            /// </remarks>
-            public static XmlNode GetXmlNode(this XmlDocument xmlDoc, string xpath, out bool isDirty)
+            elements = xpath.Split('/');
+
+            if (elements.Length == 1)
             {
-                return xmlDoc.DocumentElement.GetXmlNode(xpath, out isDirty);
-            }
+                XmlNodeList nodes = parentNode.SelectNodes(xpath);
 
-            /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
-            public static XmlNode GetXmlNode(this XmlNode parentNode, string xpath)
-            {
-                bool isDirty;
-                return parentNode.GetXmlNode(xpath, out isDirty);
-            }
-
-            /// <summary>Gets an XML node from given path, creating the entire path if it does not exist.</summary>
-            /// <remarks>Note that the <paramref name="isDirty" /> parameter will be set to True if any items were added to
-            /// the tree.</remarks>
-            public static XmlNode GetXmlNode(this XmlNode parentNode, string xpath, out bool isDirty)
-            {
-                XmlNode node = null;
-                string[] elements;
-
-                isDirty = false;
-
-                // Removes any slash prefixes.
-                while (xpath[0] == '/')
+                if (nodes.Count == 0)
                 {
-                    xpath = xpath.Substring(1);
-                }
-
-                elements = xpath.Split('/');
-
-                if (elements.Length == 1)
-                {
-                    XmlNodeList nodes = parentNode.SelectNodes(xpath);
-
-                    if (nodes.Count == 0)
-                    {
-                        node = parentNode.OwnerDocument.CreateElement(xpath);
-                        parentNode.AppendChild(node);
-                        isDirty = true;
-                    }
-                    else
-                    {
-                        node = nodes[0];
-                    }
+                    node = parentNode.OwnerDocument.CreateElement(xpath);
+                    parentNode.AppendChild(node);
+                    isDirty = true;
                 }
                 else
                 {
-                    foreach (string element in elements)
-                    {
-                        node = GetXmlNode(parentNode, element);
-                        parentNode = node;
-                    }
+                    node = nodes[0];
                 }
-
-                return node;
             }
-
-            /// <summary>Safely gets or sets an XML node's attribute.</summary>
-            /// <remarks>If you get an attribute that does not exist, null will be returned.</remarks>
-            public static string GetAttributeValue(this XmlNode node, string name)
+            else
             {
-                XmlAttribute attr = node.Attributes[name];
-
-                if (attr == null)
+                foreach (string element in elements)
                 {
-                    return null;
-                }
-                else
-                {
-                    return attr.Value;
+                    node = GetXmlNode(parentNode, element);
+                    parentNode = node;
                 }
             }
 
-            /// <summary>Safely sets an XML node's attribute.</summary>
-            /// <remarks>If you assign a value to an attribute that does not exist, the attribute will be created.</remarks>
-            public static void SetAttributeValue(this XmlNode node, string name, string value)
+            return node;
+        }
+
+        /// <summary>Safely gets or sets an XML node's attribute.</summary>
+        /// <remarks>If you get an attribute that does not exist, null will be returned.</remarks>
+        public static string GetAttributeValue(this XmlNode node, string name)
+        {
+            XmlAttribute attr = node.Attributes[name];
+
+            if (attr == null)
             {
-                XmlAttribute attr = node.Attributes[name];
-
-                if (attr == null)
-                {
-                    attr = node.OwnerDocument.CreateAttribute(name);
-                    node.Attributes.Append(attr);
-                }
-                else
-                {
-                    attr.Value = value;
-                    node.Attributes.SetNamedItem(attr);
-                }
+                return null;
             }
-
-            /// <summary>
-            /// Gets a data set object from an XML data set formatted as a String.
-            /// </summary>
-            /// <param name="xmlData">XML data string in standard DataSet format.</param>
-            public static DataSet GetDataSet(this string xmlData)
+            else
             {
-                DataSet dataSet = new DataSet();
-                XmlTextReader xmlReader = new XmlTextReader(xmlData, XmlNodeType.Document, null);
-
-                xmlReader.ReadOuterXml();
-
-                // Reads the outer XML into the Dataset.
-                dataSet.ReadXml(xmlReader);
-
-                return dataSet;
+                return attr.Value;
             }
+        }
+
+        /// <summary>Safely sets an XML node's attribute.</summary>
+        /// <remarks>If you assign a value to an attribute that does not exist, the attribute will be created.</remarks>
+        public static void SetAttributeValue(this XmlNode node, string name, string value)
+        {
+            XmlAttribute attr = node.Attributes[name];
+
+            if (attr == null)
+            {
+                attr = node.OwnerDocument.CreateAttribute(name);
+                node.Attributes.Append(attr);
+            }
+            else
+            {
+                attr.Value = value;
+                node.Attributes.SetNamedItem(attr);
+            }
+        }
+
+        /// <summary>
+        /// Gets a data set object from an XML data set formatted as a String.
+        /// </summary>
+        /// <param name="xmlData">XML data string in standard DataSet format.</param>
+        public static DataSet GetDataSet(this string xmlData)
+        {
+            DataSet dataSet = new DataSet();
+            XmlTextReader xmlReader = new XmlTextReader(xmlData, XmlNodeType.Document, null);
+
+            xmlReader.ReadOuterXml();
+
+            // Reads the outer XML into the Dataset.
+            dataSet.ReadXml(xmlReader);
+
+            return dataSet;
         }
     }
 }
