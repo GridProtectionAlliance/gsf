a6ecc1d32692c171ad0f33f440e1ef605d5d6c62
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-15 15:05:24 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Console/Arguments.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Console/Arguments.cs
@@ -1,12 +1,3 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-using System.Text.RegularExpressions;
-
 //*******************************************************************************************************
 //  TVA.Console.Arguments.vb - Command Line Parameter Parsing Class
 //  Copyright © 2007 - TVA, all rights reserved - Gbtc
@@ -23,203 +14,261 @@ using System.Text.RegularExpressions;
 //       Migrated 2.0 version of source code from 1.1 source (TVA.Console)
 //  10/09/2007 - J. Ritchie Carroll / Pinal C. Patel
 //       Fixed stand-alone argument bug at end of line and changed class to use generic Dictionary class
+//  09/15/2008 - J. Ritchie Carroll
+//      Converted to C#.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-namespace TVA
+namespace TVA.Console
 {
-	namespace Console
-	{
-		
-		[Serializable()]public class Arguments : IEnumerable
-		{
-			
-			
-			private string m_commandLine;
-			private string m_orderedArgID;
-			private int m_orderedArgCount;
-			private Dictionary<string, string> m_parameters;
-			
-			public Arguments(string commandLine) : this(commandLine, "OrderedArg")
-			{
-				
-				
-			}
-			
-			public Arguments(string commandLine, string orderedArgID)
-			{
-				
-				Regex spliter = new Regex("^-{1,2}|^/|=|:", RegexOptions.IgnoreCase || RegexOptions.Compiled);
-				Regex remover = new Regex("^[\'\"]?(.*?)[\'\"]?$", RegexOptions.IgnoreCase || RegexOptions.Compiled);
-				string parameter;
-				string[] parts;
-				
-				m_parameters = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
-				m_orderedArgCount = 0;
-				m_commandLine = commandLine;
-				m_orderedArgID = orderedArgID;
-				
-				// Valid parameters forms:
-				//   {-,/,--}param{=,:}((",')value(",'))
-				// Examples:
-				//   -param1=value1 --param2 /param3:"Test-:-work"
-				//   /param4=happy -param5 '--=nice=--'
-				foreach (string arg in Common.ParseCommand(m_commandLine))
-				{
-					// Found just a parameter in last pass...
-					// The last parameter is still waiting, with no value, set it to nothing.
-					if (parameter != null)
-					{
-						if (! m_parameters.ContainsKey(parameter))
-						{
-							m_parameters.Add(parameter, null);
-						}
-					}
-					parameter = null;
-					
-					if (! string.IsNullOrEmpty(arg))
-					{
-						// If this argument begins with a quote, we treat it as a stand-alone argument
-						if (arg[0] == '\"' || arg[0] == '\'')
-						{
-							// Handle stand alone ordered arguments
-							m_orderedArgCount++;
-							parameter = orderedArgID + m_orderedArgCount;
-							
-							// Remove possible enclosing characters (",')
-							if (! m_parameters.ContainsKey(parameter))
-							{
-								arg = remover.Replace(arg, "$1");
-								m_parameters.Add(parameter, arg);
-							}
-							
-							parameter = null;
-						}
-						else
-						{
-							// Look for new parameters (-,/ or --) and a
-							// possible enclosed value (=,:)
-							parts = spliter.Split(arg, 3);
-							
-							switch (parts.Length)
-							{
-								case 1:
-									// Handle stand alone ordered arguments
-									m_orderedArgCount++;
-									parameter = orderedArgID + m_orderedArgCount;
-									
-									// Remove possible enclosing characters (",')
-									if (! m_parameters.ContainsKey(parameter))
-									{
-										arg = remover.Replace(arg, "$1");
-										m_parameters.Add(parameter, arg);
-									}
-									
-									parameter = null;
-									break;
-								case 2:
-									// Found just a parameter
-									parameter = parts[1];
-									break;
-								case 3:
-									// Parameter with enclosed value
-									parameter = parts[1];
-									
-									// Remove possible enclosing characters (",')
-									if (! m_parameters.ContainsKey(parameter))
-									{
-										parts[2] = remover.Replace(parts[2], "$1");
-										m_parameters.Add(parameter, parts[2]);
-									}
-									
-									parameter = null;
-									break;
-							}
-						}
-					}
-				}
-				
-				// In case a parameter is still waiting
-				if (parameter != null)
-				{
-					if (! m_parameters.ContainsKey(parameter))
-					{
-						m_parameters.Add(parameter, null);
-					}
-				}
-				
-			}
-			
-			// Retrieve a parameter value if it exists
-			public virtual string this[string param]
-			{
-				get
-				{
-					return m_parameters(param);
-				}
-			}
-			
-			public virtual bool Exists(string param)
-			{
-				return m_parameters.ContainsKey(param);
-			}
-			
-			public virtual int Count
-			{
-				get
-				{
-					return m_parameters.Count;
-				}
-			}
-			
-			public virtual string OrderedArgID
-			{
-				get
-				{
-					return m_orderedArgID;
-				}
-			}
-			
-			public virtual int OrderedArgCount
-			{
-				get
-				{
-					return m_orderedArgCount;
-				}
-			}
-			
-			public virtual bool ContainsHelpRequest
-			{
-				get
-				{
-					return (m_parameters.ContainsKey("") || m_parameters.ContainsKey("Help"));
-				}
-			}
-			
-			public IEnumerator GetEnumerator()
-			{
-				
-				return m_parameters.GetEnumerator();
-				
-			}
-			
-			public override string ToString()
-			{
-				
-				return m_commandLine;
-				
-			}
-			
-			protected Dictionary<string, string> InternalDictionary
-			{
-				get
-				{
-					return m_parameters;
-				}
-			}
-			
-		}
-		
-	}
+    [Serializable()]
+    public class Arguments : IEnumerable, IEnumerable<KeyValuePair<string, string>>
+    {
+        private string m_commandLine;
+        private string m_orderedArgID;
+        private int m_orderedArgCount;
+        private Dictionary<string, string> m_parameters;
+
+        public Arguments(string commandLine)
+            : this(commandLine, "OrderedArg")
+        {
+        }
+
+        public Arguments(string commandLine, string orderedArgID)
+        {
+            Regex spliter = new Regex("^-{1,2}|^/|=|:", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            Regex remover = new Regex("^[\'\"]?(.*?)[\'\"]?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            string parameter = null;
+            string argument;
+            string[] parts;
+
+            // We create a case-insensitive dictionary for parameters
+            m_parameters = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+            m_orderedArgCount = 0;
+            m_commandLine = commandLine;
+            m_orderedArgID = orderedArgID;
+
+            // Valid parameters forms:
+            //   {-,/,--}param{=,:}((",')value(",'))
+            // Examples:
+            //   -param1=value1 --param2 /param3:"Test-:-work"
+            //   /param4=happy -param5 '--=nice=--'
+            foreach (string arg in ParseCommand(m_commandLine))
+            {
+                // Found just a parameter in last pass...
+                // The last parameter is still waiting, with no value, set it to nothing.
+                if (parameter != null)
+                {
+                    if (!m_parameters.ContainsKey(parameter))
+                        m_parameters.Add(parameter, null);
+                }
+                parameter = null;
+
+                if (!string.IsNullOrEmpty(arg))
+                {
+                    // If this argument begins with a quote, we treat it as a stand-alone argument
+                    if (arg[0] == '\"' || arg[0] == '\'')
+                    {
+                        // Handle stand alone ordered arguments
+                        m_orderedArgCount++;
+                        parameter = orderedArgID + m_orderedArgCount;
+
+                        // Remove possible enclosing characters (",')
+                        if (!m_parameters.ContainsKey(parameter))
+                        {
+                            argument = remover.Replace(arg, "$1");
+                            m_parameters.Add(parameter, argument);
+                        }
+
+                        parameter = null;
+                    }
+                    else
+                    {
+                        // Look for new parameters (-,/ or --) and a
+                        // possible enclosed value (=,:)
+                        parts = spliter.Split(arg, 3);
+
+                        switch (parts.Length)
+                        {
+                            case 1:
+                                // Handle stand alone ordered arguments
+                                m_orderedArgCount++;
+                                parameter = orderedArgID + m_orderedArgCount;
+
+                                // Remove possible enclosing characters (",')
+                                if (!m_parameters.ContainsKey(parameter))
+                                {
+                                    argument = remover.Replace(arg, "$1");
+                                    m_parameters.Add(parameter, argument);
+                                }
+
+                                parameter = null;
+                                break;
+                            case 2:
+                                // Found just a parameter
+                                parameter = parts[1];
+                                break;
+                            case 3:
+                                // Parameter with enclosed value
+                                parameter = parts[1];
+
+                                // Remove possible enclosing characters (",')
+                                if (!m_parameters.ContainsKey(parameter))
+                                {
+                                    parts[2] = remover.Replace(parts[2], "$1");
+                                    m_parameters.Add(parameter, parts[2]);
+                                }
+
+                                parameter = null;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            // In case a parameter is still waiting
+            if (parameter != null)
+            {
+                if (!m_parameters.ContainsKey(parameter))
+                    m_parameters.Add(parameter, null);
+            }
+
+        }
+
+        // Retrieve a parameter value if it exists
+        public virtual string this[string param]
+        {
+            get
+            {
+                return m_parameters[param];
+            }
+        }
+
+        public virtual bool Exists(string param)
+        {
+            return m_parameters.ContainsKey(param);
+        }
+
+        public virtual int Count
+        {
+            get
+            {
+                return m_parameters.Count;
+            }
+        }
+
+        public virtual string OrderedArgID
+        {
+            get
+            {
+                return m_orderedArgID;
+            }
+        }
+
+        public virtual int OrderedArgCount
+        {
+            get
+            {
+                return m_orderedArgCount;
+            }
+        }
+
+        public virtual bool ContainsHelpRequest
+        {
+            get
+            {
+                return (m_parameters.ContainsKey("?") || m_parameters.ContainsKey("Help"));
+            }
+        }
+
+        public override string ToString()
+        {
+            return m_commandLine;
+        }
+
+        protected Dictionary<string, string> InternalDictionary
+        {
+            get
+            {
+                return m_parameters;
+            }
+        }
+
+        /// <summary>This function can be used to parse a single parameterized string and turn it into an array of parameters.</summary>
+        /// <remarks>This function will always return at least one argument, even if it is an empty string.</remarks>
+        /// <param name="command">String of parameters.</param>
+        /// <returns>Array of parameters.</returns>
+        public static string[] ParseCommand(string command)
+        {
+            List<string> parsedCommand = new List<string>();
+
+            if (command.Length > 0)
+            {
+                string encodedQuote = Guid.NewGuid().ToString();
+                string encodedSpace = Guid.NewGuid().ToString();
+                StringBuilder encodedCommand = new StringBuilder();
+                bool argumentInQuotes = false;
+                char currentCharacter;
+                string argument;
+
+                // Encodes embedded quotes. It allows embedded/nested quotes encoded as \".
+                command = command.Replace("\\\"", encodedQuote);
+
+                // Combines any quoted strings into a single arg by encoding embedded spaces.
+                for (int x = 0; x <= command.Length - 1; x++)
+                {
+                    currentCharacter = command[x];
+
+                    if (currentCharacter == '\"')
+                    {
+                        if (argumentInQuotes)
+                            argumentInQuotes = false;
+                        else
+                            argumentInQuotes = true;
+                    }
+
+                    if (argumentInQuotes)
+                    {
+                        if (currentCharacter == ' ')
+                            encodedCommand.Append(encodedSpace);
+                        else
+                            encodedCommand.Append(currentCharacter);
+                    }
+                    else
+                    {
+                        encodedCommand.Append(currentCharacter);
+                    }
+                }
+
+                command = encodedCommand.ToString();
+
+                // Parses every argument out by space and combine any quoted strings into a single arg.
+                foreach (string arg in command.Split(' '))
+                {
+                    // Adds tokenized argument, making sure to unencode any embedded quotes or spaces.
+                    argument = arg.Replace(encodedQuote, "\"").Replace(encodedSpace, " ").Trim();
+                    if (argument.Length > 0)
+                        parsedCommand.Add(argument);
+                }
+            }
+
+            return parsedCommand.ToArray();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)m_parameters).GetEnumerator();
+        }
+
+        IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator()
+        {
+            return m_parameters.GetEnumerator();
+        }
+    }
 }
--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Console/Common.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Console/Common.cs
@@ -1,13 +1,3 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-using System.Text;
-using System.Runtime.InteropServices;
-
 //*******************************************************************************************************
 //  TVA.Console.Common.vb - Common Configuration Functions
 //  Copyright © 2006 - TVA, all rights reserved - Gbtc
@@ -27,273 +17,115 @@ using System.Runtime.InteropServices;
 //
 //*******************************************************************************************************
 
+using System;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
-
-namespace TVA
+namespace TVA.Console
 {
-	namespace Console
-	{
-		
-		public sealed class Common
-		{
-			
-			
-			#region " Member Declaration "
-			
-			private enum ConsoleEventType
-			{
-				CancelKeyPress = 0,
-				BreakKeyPress = 1,
-				ConsoleClosing = 2,
-				UserLoggingOff = 5,
-				SystemShutdown = 6
-			}
-			
-			private static ConsoleWindowEventHandler m_handler;
-			
-			private delegate bool ConsoleWindowEventHandler(ConsoleEventType controlType);
-			
-			#endregion
-			
-			#region " Event Declaration "
-			
-			public  delegate void CancelKeyPressEventHandler(object Of);
-			private CancelKeyPressEventHandler CancelKeyPressEvent;
-			
-			public static event CancelKeyPressEventHandler CancelKeyPress
-			{
-				add
-				{
-					CancelKeyPressEvent = (CancelKeyPressEventHandler) System.Delegate.Combine(CancelKeyPressEvent, value);
-				}
-				remove
-				{
-					CancelKeyPressEvent = (CancelKeyPressEventHandler) System.Delegate.Remove(CancelKeyPressEvent, value);
-				}
-			}
-			
-			
-			public  delegate void BreakKeyPressEventHandler(object Of);
-			private BreakKeyPressEventHandler BreakKeyPressEvent;
-			
-			public static event BreakKeyPressEventHandler BreakKeyPress
-			{
-				add
-				{
-					BreakKeyPressEvent = (BreakKeyPressEventHandler) System.Delegate.Combine(BreakKeyPressEvent, value);
-				}
-				remove
-				{
-					BreakKeyPressEvent = (BreakKeyPressEventHandler) System.Delegate.Remove(BreakKeyPressEvent, value);
-				}
-			}
-			
-			
-			public  delegate void ConsoleClosingEventHandler(object Of);
-			private ConsoleClosingEventHandler ConsoleClosingEvent;
-			
-			public static event ConsoleClosingEventHandler ConsoleClosing
-			{
-				add
-				{
-					ConsoleClosingEvent = (ConsoleClosingEventHandler) System.Delegate.Combine(ConsoleClosingEvent, value);
-				}
-				remove
-				{
-					ConsoleClosingEvent = (ConsoleClosingEventHandler) System.Delegate.Remove(ConsoleClosingEvent, value);
-				}
-			}
-			
-			
-			private EventHandler UserLoggingOffEvent;
-			public static event EventHandler UserLoggingOff
-			{
-				add
-				{
-					UserLoggingOffEvent = (EventHandler) System.Delegate.Combine(UserLoggingOffEvent, value);
-				}
-				remove
-				{
-					UserLoggingOffEvent = (EventHandler) System.Delegate.Remove(UserLoggingOffEvent, value);
-				}
-			}
-			
-			
-			private EventHandler SystemShutdownEvent;
-			public static event EventHandler SystemShutdown
-			{
-				add
-				{
-					SystemShutdownEvent = (EventHandler) System.Delegate.Combine(SystemShutdownEvent, value);
-				}
-				remove
-				{
-					SystemShutdownEvent = (EventHandler) System.Delegate.Remove(SystemShutdownEvent, value);
-				}
-			}
-			
-			
-			#endregion
-			
-			#region " Public Code "
-			
-			// VB does not provide you with an array of tokenized command line arguments; they are all in one string.
-			// So, this function creates the desired tokenized argument array from the VB command line.
-			// This function will always return at least one argument, even if it is an empty string.
-			public static string[] ParseCommand(string command)
-			{
-				
-				List<string> parsedCommand = new List<string>();
-				if (command.Length > 0)
-				{
-					string quotedArgument = "";
-					var encodedQuote = Guid.NewGuid().ToString();
-					var encodedSpace = Guid.NewGuid().ToString();
-					StringBuilder encodedCommand = new StringBuilder();
-					bool argumentInQuotes;
-					char currentCharacter;
-					
-					// Encodes embedded quotes. It allows embedded/nested quotes encoded as \".
-					command = command.Replace("\\\"", encodedQuote);
-					
-					// Combines any quoted strings into a single arg by encoding embedded spaces.
-					for (int x = 0; x <= command.Length - 1; x++)
-					{
-						currentCharacter = command[x];
-						
-						if (currentCharacter == '\"')
-						{
-							if (argumentInQuotes)
-							{
-								argumentInQuotes = false;
-							}
-							else
-							{
-								argumentInQuotes = true;
-							}
-						}
-						
-						if (argumentInQuotes)
-						{
-							if (currentCharacter == ' ')
-							{
-								encodedCommand.Append(encodedSpace);
-							}
-							else
-							{
-								encodedCommand.Append(currentCharacter);
-							}
-						}
-						else
-						{
-							encodedCommand.Append(currentCharacter);
-						}
-					}
-					
-					command = encodedCommand.ToString();
-					
-					// Parses every argument out by space and combine any quoted strings into a single arg.
-					foreach (string argument in command.Split(' '))
-					{
-						// Adds tokenized argument, making sure to unencode any embedded quotes or spaces.
-						argument = argument.Replace(encodedQuote, "\"").Replace(encodedSpace, " ").Trim();
-						if (argument.Length > 0)
-						{
-							parsedCommand.Add(argument);
-						}
-					}
-				}
-				
-				return parsedCommand.ToArray();
-				
-			}
-			
-			public static void EnableRaisingEvents()
-			{
-				
-				// Member variable is used here so that the delegate is not garbage collected by the time it is called
-				// by WIN API when any of the control events take place.
-				// http://forums.microsoft.com/MSDN/ShowPost.aspx?PostID=996045&SiteID=1
-				m_handler = new System.EventHandler(HandleConsoleWindowEvents);
-				SetConsoleWindowEventRaising(m_handler, true);
-				
-			}
-			
-			public static void DisableRaisingEvents()
-			{
-				
-				m_handler = new System.EventHandler(HandleConsoleWindowEvents);
-				SetConsoleWindowEventRaising(m_handler, false);
-				
-			}
-			
-			#endregion
-			
-			#region " Private Code "
-			
-			private Common()
-			{
-				
-				// This class contains only global functions and is not meant to be instantiated
-				
-			}
-			
-			[DllImport("kernel32.dll", EntryPoint = "SetConsoleCtrlHandler")]private static  extern bool SetConsoleWindowEventRaising(ConsoleWindowEventHandler handler, bool enable);
-			
-			
-			private static bool HandleConsoleWindowEvents(ConsoleEventType controlType)
-			{
-				
-				// ms-help://MS.VSCC.v80/MS.MSDN.v80/MS.WIN32COM.v10.en/dllproc/base/handlerroutine.htm
-				
-				// When this function does not return True, the default handler is called and the default action takes
-				// place.
-				switch (controlType)
-				{
-					case ConsoleEventType.CancelKeyPress:
-						System.ComponentModel.CancelEventArgs ctrlCKeyPressEventData = new System.ComponentModel.CancelEventArgs();
-						if (CancelKeyPressEvent != null)
-							CancelKeyPressEvent(null, ctrlCKeyPressEventData);
-						if (ctrlCKeyPressEventData.Cancel)
-						{
-							return true;
-						}
-						break;
-					case ConsoleEventType.BreakKeyPress:
-						System.ComponentModel.CancelEventArgs ctrlBreakKeyPressEventData = new System.ComponentModel.CancelEventArgs();
-						if (BreakKeyPressEvent != null)
-							BreakKeyPressEvent(null, ctrlBreakKeyPressEventData);
-						if (ctrlBreakKeyPressEventData.Cancel)
-						{
-							return true;
-						}
-						break;
-					case ConsoleEventType.ConsoleClosing:
-						System.ComponentModel.CancelEventArgs consoleClosingEventData = new System.ComponentModel.CancelEventArgs();
-						if (ConsoleClosingEvent != null)
-							ConsoleClosingEvent(null, consoleClosingEventData);
-						if (consoleClosingEventData.Cancel)
-						{
-							return true;
-						}
-						break;
-					case ConsoleEventType.UserLoggingOff:
-						if (UserLoggingOffEvent != null)
-							UserLoggingOffEvent(null, EventArgs.Empty);
-						break;
-					case ConsoleEventType.SystemShutdown:
-						if (SystemShutdownEvent != null)
-							SystemShutdownEvent(null, EventArgs.Empty);
-						break;
-				}
-				
-				return false;
-				
-			}
-			
-			#endregion
-			
-		}
-		
-	}
+    public static class Common
+    {
+        private enum ConsoleEventType
+        {
+            CancelKeyPress = 0,
+            BreakKeyPress = 1,
+            ConsoleClosing = 2,
+            UserLoggingOff = 5,
+            SystemShutdown = 6
+        }
+
+        private delegate bool ConsoleWindowEventHandler(ConsoleEventType controlType);
+
+        private static ConsoleWindowEventHandler m_handler;
+
+        public static event EventHandler<CancelEventArgs> CancelKeyPress;
+
+        public static event EventHandler<CancelEventArgs> BreakKeyPress;
+
+        public static event EventHandler<CancelEventArgs> ConsoleClosing;
+
+        public static event EventHandler UserLoggingOff;
+
+        public static event EventHandler SystemShutdown;
+
+        #region " Public Code "
+
+        public static void EnableRaisingEvents()
+        {
+            // Member variable is used here so that the delegate is not garbage collected by the time it is called
+            // by WIN API when any of the control events take place.
+            // http://forums.microsoft.com/MSDN/ShowPost.aspx?PostID=996045&SiteID=1
+            m_handler = HandleConsoleWindowEvents;
+            SetConsoleWindowEventRaising(m_handler, true);
+        }
+
+        public static void DisableRaisingEvents()
+        {
+            m_handler = HandleConsoleWindowEvents;
+            SetConsoleWindowEventRaising(m_handler, false);
+        }
+
+        #endregion
+
+        #region " Private Code "
+
+        [DllImport("kernel32.dll", EntryPoint = "SetConsoleCtrlHandler")]
+        private static extern bool SetConsoleWindowEventRaising(ConsoleWindowEventHandler handler, bool enable);
+
+        private static bool HandleConsoleWindowEvents(ConsoleEventType controlType)
+        {
+            // ms-help://MS.VSCC.v80/MS.MSDN.v80/MS.WIN32COM.v10.en/dllproc/base/handlerroutine.htm
+
+            // When this function does not return True, the default handler is called and the default action takes
+            // place.
+            switch (controlType)
+            {
+                case ConsoleEventType.CancelKeyPress:
+                    CancelEventArgs ctrlCKeyPressEventData = new CancelEventArgs();
+
+                    if (CancelKeyPress != null)
+                        CancelKeyPress(null, ctrlCKeyPressEventData);
+
+                    if (ctrlCKeyPressEventData.Cancel)
+                        return true;
+
+                    break;
+                case ConsoleEventType.BreakKeyPress:
+                    CancelEventArgs ctrlBreakKeyPressEventData = new CancelEventArgs();
+
+                    if (BreakKeyPress != null)
+                        BreakKeyPress(null, ctrlBreakKeyPressEventData);
+
+                    if (ctrlBreakKeyPressEventData.Cancel)
+                        return true;
+
+                    break;
+                case ConsoleEventType.ConsoleClosing:
+                    CancelEventArgs consoleClosingEventData = new CancelEventArgs();
+
+                    if (ConsoleClosing != null)
+                        ConsoleClosing(null, consoleClosingEventData);
+
+                    if (consoleClosingEventData.Cancel)
+                        return true;
+
+                    break;
+                case ConsoleEventType.UserLoggingOff:
+                    if (UserLoggingOff != null)
+                        UserLoggingOff(null, EventArgs.Empty);
+
+                    break;
+                case ConsoleEventType.SystemShutdown:
+                    if (SystemShutdown != null)
+                        SystemShutdown(null, EventArgs.Empty);
+
+                    break;
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
 }
