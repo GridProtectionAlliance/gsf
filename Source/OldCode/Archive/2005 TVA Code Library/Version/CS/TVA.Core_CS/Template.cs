288645be4204fa1bf26e20cbe3ce1ed1c368cafb
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-16 18:05:01 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Template.cs
@@ -0,0 +1,173 @@
+﻿/********************************************************************************************************\
+ * 
+ *                                            Generic Header
+ * 
+\********************************************************************************************************/
+
+//*******************************************************************************************************
+//  TVA.Namespace.Filename.vb - Template class description
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: <DEVNAME>, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//       Phone: <DEVPHONE>
+//       Email: <DEVEMAIL>
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  01/01/2008 - <DEVNAME>
+//       Generated original version of source code.
+//
+//*******************************************************************************************************
+
+//namespace TVA
+//{
+//    class Template
+//    {
+//        #region [ Members ]
+
+//        // Constants
+
+//        // Delegates
+
+//        // Events
+
+//        // Fields
+
+//        #endregion
+
+//        #region [ Constructors ]
+
+//        #endregion
+
+//        #region [ Properties ]
+
+//        #endregion
+
+//        #region [ Methods ]
+
+//        #endregion
+
+//        #region [ Static ]
+
+//        #endregion
+//    }
+//}
+
+
+/********************************************************************************************************\
+ * 
+ *                                            Pinal's Header
+ * 
+\********************************************************************************************************/
+
+
+//*******************************************************************************************************
+//  TVA.Namespace.Filename.vb - Template class description
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN
+//       Phone: 423/751-2250
+//       Email: pcpatel@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  01/01/2008 - Pinal C. Patel
+//       Generated original version of source code.
+//
+//*******************************************************************************************************
+
+//namespace TVA
+//{
+//    class Template
+//    {
+//        #region [ Members ]
+
+//        // Constants
+
+//        // Delegates
+
+//        // Events
+
+//        // Fields
+
+//        #endregion
+
+//        #region [ Constructors ]
+
+//        #endregion
+
+//        #region [ Properties ]
+
+//        #endregion
+
+//        #region [ Methods ]
+
+//        #endregion
+
+//        #region [ Static ]
+
+//        #endregion
+//    }
+//}
+
+/********************************************************************************************************\
+ * 
+ *                                           Ritchie's Header
+ * 
+\********************************************************************************************************/
+
+
+//*******************************************************************************************************
+//  TVA.Namespace.Filename.vb - Template class description
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
+//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN
+//       Phone: 423/751-2827
+//       Email: jrcarrol@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  01/01/2008 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//*******************************************************************************************************
+
+//namespace TVA
+//{
+//    class Template
+//    {
+//        #region [ Members ]
+
+//        // Constants
+
+//        // Delegates
+
+//        // Events
+
+//        // Fields
+
+//        #endregion
+
+//        #region [ Constructors ]
+
+//        #endregion
+
+//        #region [ Properties ]
+
+//        #endregion
+
+//        #region [ Methods ]
+
+//        #endregion
+
+//        #region [ Static ]
+
+//        #endregion
+//    }
+//}
