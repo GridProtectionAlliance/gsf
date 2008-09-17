78c330e2e308de70ac17c2e52806bee5c46561af
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-17 19:37:37 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Measurements/ImmediateMeasurements.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Measurements/ImmediateMeasurements.cs
@@ -1,92 +1,69 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-using System.Threading;
-
 //*******************************************************************************************************
-//  TVA.Measurements.ImmediateMeasurements.vb - Lastest received measurements collection
-//  Copyright © 2006 - TVA, all rights reserved - Gbtc
+//  ImmediateMeasurements.cs
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2005
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2250
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR 2W-C
+//       Phone: 423/751-2827
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
 //  11/12/2004 - J. Ritchie Carroll
-//       Initial version of source generated for Super Phasor Data Concentrator
+//       Initial version of source generated for Super Phasor Data Concentrator.
 //  02/23/2006 - J. Ritchie Carroll
-//       Classes abstracted for general use and added to TVA code library
+//       Classes abstracted for general use and added to TVA code library.
+//  09/17/2008 - James R Carroll
+//       Converted to C#.
 //
 //*******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace TVA.Measurements
 {
     /// <summary>This class represents the absolute latest received measurement values</summary>
-    public class ImmediateMeasurements
+    public class ImmediateMeasurements : IDisposable
     {
+        #region [ Members ]
 
-
+        // Fields
         private ConcentratorBase m_parent;
         private Dictionary<MeasurementKey, TemporalMeasurement> m_measurements;
         private Dictionary<string, List<MeasurementKey>> m_taggedMeasurements;
+        private bool m_disposed;
+
+        #endregion
+
+        #region [ Constructors ]
 
         internal ImmediateMeasurements(ConcentratorBase parent)
-			{
-				
-				m_parent = parent;
-				m_parent.LagTimeUpdated += new TVA.Measurements.ConcentratorBase.LagTimeUpdatedEventHandler(m_parent_LagTimeUpdated);
-				m_parent.LeadTimeUpdated += new TVA.Measurements.ConcentratorBase.LeadTimeUpdatedEventHandler(m_parent_LeadTimeUpdated);
-				m_measurements = new Dictionary<MeasurementKey, TemporalMeasurement>();
-				m_taggedMeasurements = new Dictionary<string, List<MeasurementKey>>();
-				
-			}
-
-        /// <summary>Handy instance reference to self</summary>
-        public ImmediateMeasurements This
         {
-            get
-            {
-                return this;
-            }
+            m_parent = parent;
+            m_parent.LagTimeUpdated += m_parent_LagTimeUpdated;
+            m_parent.LeadTimeUpdated += m_parent_LeadTimeUpdated;
+            m_measurements = new Dictionary<MeasurementKey, TemporalMeasurement>();
+            m_taggedMeasurements = new Dictionary<string, List<MeasurementKey>>();
         }
 
-        /// <summary>Returns key collection of measurement keys</summary>
-        public Dictionary<MeasurementKey, TemporalMeasurement> MeasurementKeys
+        ~ImmediateMeasurements()
         {
-            get
-            {
-                return m_measurements.Keys;
-            }
+            Dispose(true);
         }
 
-        /// <summary>Returns key collection for measurement tags</summary>
-        public Dictionary<string, List<MeasurementKey>> Tags
-        {
-            get
-            {
-                return m_taggedMeasurements.Keys;
-            }
-        }
+        #endregion
 
-        /// <summary>Returns measurement key list of specified tag, if it exists</summary>
-        public List<MeasurementKey> TagMeasurementKeys(string tag)
-        {
-            return m_taggedMeasurements(tag);
-        }
+        #region [ Properties ]
 
         /// <summary>We retrieve adjusted measurement values within time tolerance of concentrator real-time</summary>
         public double this[int measurementID, string source]
         {
             get
             {
-                return Value(new MeasurementKey(measurementID, source));
+                return this[new MeasurementKey(measurementID, source)];
             }
         }
 
@@ -95,132 +72,26 @@ namespace TVA.Measurements
         {
             get
             {
-                return Measurement(key)[m_parent.RealTimeTicks];
+                return Measurement(key).GetAdjustedValue(m_parent.RealTimeTicks);
             }
         }
 
-        /// <summary>We retrieve measurement values within time tolerance of concentrator real-time</summary>
-        public double Value(int measurementID, string source)
+        /// <summary>Returns key collection of measurement keys</summary>
+        public Dictionary<MeasurementKey, TemporalMeasurement>.KeyCollection MeasurementKeys
         {
-            return Value(new MeasurementKey(measurementID, source));
-        }
-
-        /// <summary>We retrieve measurement values within time tolerance of concentrator real-time</summary>
-        public double Value(MeasurementKey key)
-        {
-            return Measurement(key).Value(m_parent.RealTimeTicks);
-        }
-
-        /// <summary>We only store a new measurement value that is newer than the cached value</summary>
-        internal void UpdateMeasurementValue(IMeasurement newMeasurement)
-        {
-
-            IMeasurement with_1 = newMeasurement;
-            Measurement(with_1.Key).Value(with_1.Ticks) = with_1.Value;
-
-        }
-
-        /// <summary>Retrieves the specified immediate temporal measurement, creating it if needed</summary>
-        public TemporalMeasurement Measurement(int measurementID, string source)
-        {
-            return Measurement(new MeasurementKey(measurementID, source));
-        }
-
-        /// <summary>Retrieves the specified immediate temporal measurement, creating it if needed</summary>
-        public TemporalMeasurement Measurement(MeasurementKey key)
-        {
-            lock (m_measurements)
+            get
             {
-                TemporalMeasurement value;
-
-                if (!m_measurements.TryGetValue(key, value))
-                {
-                    // Create new temporal measurement if it doesn't exist
-                    value = new TemporalMeasurement(key.ID, key.Source, double.NaN, m_parent.RealTimeTicks, m_parent.lagTime, m_parent.leadTime);
-                    m_measurements.Add(key, value);
-                }
-
-                return value;
+                return m_measurements.Keys;
             }
         }
 
-        /// <summary>Defines tagged measurements from a data table</summary>
-        /// <remarks>Expects tag field to be aliased as "Tag", measurement ID field to be aliased as "ID" and source field to be aliased as "Source"</remarks>
-        public void DefineTaggedMeasurements(DataTable taggedMeasurements)
+        /// <summary>Returns key collection for measurement tags</summary>
+        public Dictionary<string, List<MeasurementKey>>.KeyCollection Tags
         {
-
-            foreach (DataRow row in taggedMeasurements.Rows)
+            get
             {
-                AddTaggedMeasurement(row["Tag"].ToString(), new MeasurementKey(System.Convert.ToInt32(row["ID"]), row["Source"].ToString()));
+                return m_taggedMeasurements.Keys;
             }
-
-        }
-
-        /// <summary>Associates a new measurement ID with a tag, creating the new tag if needed</summary>
-        /// <remarks>Allows you to define "grouped" points so you can aggregate certain measurements</remarks>
-        public void AddTaggedMeasurement(string tag, MeasurementKey key)
-			{
-				
-				// Check for new tag
-				if (! m_taggedMeasurements.ContainsKey(tag))
-				{
-					m_taggedMeasurements.Add(tag, new List<MeasurementKey>());
-				}
-				
-				// Add measurement to tag's measurement list
-				object with_2 = m_taggedMeasurements.Item(tag);
-				if (with_2.BinarySearch(key) < 0)
-				{
-					with_2.Add(key);
-					with_2.Sort();
-				}
-				
-			}
-
-        /// <summary>Calculates an average of all measurements</summary>
-        /// <remarks>This is only useful if all measurements represent the same type of measurement</remarks>
-        public double CalculateAverage(ref int count)
-        {
-
-            double measurement;
-            double total;
-
-            lock (m_measurements)
-            {
-                foreach (MeasurementKey key in m_measurements.Keys)
-                {
-                    measurement = Value(key);
-                    if (!double.IsNaN(measurement))
-                    {
-                        total += measurement;
-                        count++;
-                    }
-                }
-            }
-
-            return total / count;
-
-        }
-
-        /// <summary>Calculates an average of all measurements associated with the specified tag</summary>
-        public double CalculateTagAverage(string tag, ref int count)
-        {
-
-            double measurement;
-            double total;
-
-            foreach (MeasurementKey key in m_taggedMeasurements(tag))
-            {
-                measurement = Value(key);
-                if (!double.IsNaN(measurement))
-                {
-                    total += measurement;
-                    count++;
-                }
-            }
-
-            return total / count;
-
         }
 
         /// <summary>Returns the minimum value of all measurements</summary>
@@ -236,7 +107,7 @@ namespace TVA.Measurements
                 {
                     foreach (MeasurementKey key in m_measurements.Keys)
                     {
-                        measurement = value(key);
+                        measurement = this[key];
                         if (!double.IsNaN(measurement))
                         {
                             if (measurement < minValue)
@@ -264,7 +135,7 @@ namespace TVA.Measurements
                 {
                     foreach (MeasurementKey key in m_measurements.Keys)
                     {
-                        measurement = value(key);
+                        measurement = this[key];
                         if (!double.IsNaN(measurement))
                         {
                             if (measurement > maxValue)
@@ -279,15 +150,162 @@ namespace TVA.Measurements
             }
         }
 
+        #endregion
+
+        #region [ Methods ]
+
+        public void Dispose()
+        {
+            // Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!m_disposed)
+            {
+                if (disposing)
+                {
+                    if (m_parent != null)
+                    {
+                        m_parent.LagTimeUpdated -= m_parent_LagTimeUpdated;
+                        m_parent.LeadTimeUpdated -= m_parent_LeadTimeUpdated;
+                    }
+                    m_parent = null;
+
+                    if (m_measurements != null)
+                    {
+                        m_measurements.Clear();
+                    }
+                    m_measurements = null;
+
+                    if (m_taggedMeasurements != null)
+                    {
+                        m_taggedMeasurements.Clear();
+                    }
+                    m_taggedMeasurements = null;
+                }
+            }
+
+            m_disposed = true;
+        }
+
+        /// <summary>Returns measurement key list of specified tag, if it exists</summary>
+        public List<MeasurementKey> TagMeasurementKeys(string tag)
+        {
+            return m_taggedMeasurements[tag];
+        }
+
+        /// <summary>We only store a new measurement value that is newer than the cached value</summary>
+        internal void UpdateMeasurementValue(IMeasurement newMeasurement)
+        {
+            Measurement(newMeasurement.Key).SetValue(newMeasurement.Ticks, newMeasurement.Value);
+        }
+
+        /// <summary>Retrieves the specified immediate temporal measurement, creating it if needed</summary>
+        public TemporalMeasurement Measurement(int measurementID, string source)
+        {
+            return Measurement(new MeasurementKey(measurementID, source));
+        }
+
+        /// <summary>Retrieves the specified immediate temporal measurement, creating it if needed</summary>
+        public TemporalMeasurement Measurement(MeasurementKey key)
+        {
+            lock (m_measurements)
+            {
+                TemporalMeasurement value;
+
+                if (!m_measurements.TryGetValue(key, out value))
+                {
+                    // Create new temporal measurement if it doesn't exist
+                    value = new TemporalMeasurement(key.ID, key.Source, double.NaN, m_parent.RealTimeTicks, m_parent.LagTime, m_parent.LeadTime);
+                    m_measurements.Add(key, value);
+                }
+
+                return value;
+            }
+        }
+
+        /// <summary>Defines tagged measurements from a data table</summary>
+        /// <remarks>Expects tag field to be aliased as "Tag", measurement ID field to be aliased as "ID" and source field to be aliased as "Source"</remarks>
+        public void DefineTaggedMeasurements(DataTable taggedMeasurements)
+        {
+            foreach (DataRow row in taggedMeasurements.Rows)
+            {
+                AddTaggedMeasurement(row["Tag"].ToString(), new MeasurementKey(System.Convert.ToInt32(row["ID"]), row["Source"].ToString()));
+            }
+        }
+
+        /// <summary>Associates a new measurement ID with a tag, creating the new tag if needed</summary>
+        /// <remarks>Allows you to define "grouped" points so you can aggregate certain measurements</remarks>
+        public void AddTaggedMeasurement(string tag, MeasurementKey key)
+        {
+            // Check for new tag
+            if (!m_taggedMeasurements.ContainsKey(tag))
+                m_taggedMeasurements.Add(tag, new List<MeasurementKey>());
+
+            // Add measurement to tag's measurement list
+            List<MeasurementKey> measurements = m_taggedMeasurements[tag];
+
+            if (measurements.BinarySearch(key) < 0)
+            {
+                measurements.Add(key);
+                measurements.Sort();
+            }
+        }
+
+        /// <summary>Calculates an average of all measurements</summary>
+        /// <remarks>This is only useful if all measurements represent the same type of measurement</remarks>
+        public double CalculateAverage(ref int count)
+        {
+            double measurement;
+            double total = 0.0D;
+
+            lock (m_measurements)
+            {
+                foreach (MeasurementKey key in m_measurements.Keys)
+                {
+                    measurement = this[key];
+                    if (!double.IsNaN(measurement))
+                    {
+                        total += measurement;
+                        count++;
+                    }
+                }
+            }
+
+            return total / count;
+        }
+
+        /// <summary>Calculates an average of all measurements associated with the specified tag</summary>
+        public double CalculateTagAverage(string tag, ref int count)
+        {
+            double measurement;
+            double total = 0.0D;
+
+            foreach (MeasurementKey key in m_taggedMeasurements[tag])
+            {
+                measurement = this[key];
+                if (!double.IsNaN(measurement))
+                {
+                    total += measurement;
+                    count++;
+                }
+            }
+
+            return total / count;
+        }
+
         /// <summary>Returns the minimum value of all measurements associated with the specified tag</summary>
         public double TagMinimum(string tag)
         {
             double minValue = double.MaxValue;
             double measurement;
 
-            foreach (MeasurementKey key in m_taggedMeasurements(tag))
+            foreach (MeasurementKey key in m_taggedMeasurements[tag])
             {
-                measurement = Value(key);
+                measurement = this[key];
                 if (!double.IsNaN(measurement))
                 {
                     if (measurement < minValue)
@@ -306,9 +324,9 @@ namespace TVA.Measurements
             double maxValue = double.MinValue;
             double measurement;
 
-            foreach (MeasurementKey key in m_taggedMeasurements(tag))
+            foreach (MeasurementKey key in m_taggedMeasurements[tag])
             {
-                measurement = Value(key);
+                measurement = this[key];
                 if (!double.IsNaN(measurement))
                 {
                     if (measurement > maxValue)
@@ -323,7 +341,6 @@ namespace TVA.Measurements
 
         private void m_parent_LagTimeUpdated(double lagTime)
         {
-
             lock (m_measurements)
             {
                 foreach (MeasurementKey key in m_measurements.Keys)
@@ -331,12 +348,10 @@ namespace TVA.Measurements
                     Measurement(key).LagTime = lagTime;
                 }
             }
-
         }
 
         private void m_parent_LeadTimeUpdated(double leadTime)
         {
-
             lock (m_measurements)
             {
                 foreach (MeasurementKey key in m_measurements.Keys)
@@ -345,5 +360,7 @@ namespace TVA.Measurements
                 }
             }
         }
+
+        #endregion
     }
 }
