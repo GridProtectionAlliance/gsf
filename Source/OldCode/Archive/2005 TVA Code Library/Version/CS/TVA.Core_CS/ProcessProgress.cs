c045e2288ed73b53433b58257f5b2298ce39f251
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-18 16:04:29 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/ProcessProgress.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/ProcessProgress.cs
@@ -1,10 +1,10 @@
 //*******************************************************************************************************
-//  ProcessProgress.vb - Generic process progress class
-//  Copyright © 2005 - TVA, all rights reserved - Gbtc
+//  ProcessProgress.cs
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2005
-//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: Pinal C. Patel
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR 2W-C
 //       Phone: 423/751-2250
 //       Email: pcpatel@tva.gov
 //
@@ -19,82 +19,84 @@
 
 using System;
 
-namespace TVA
+/// <summary>
+/// Generic process progress class.
+/// </summary>
+/// <remarks>
+/// Used to track total progress of an identified operation.
+/// </remarks>
+/// <typeparam name="TUnit">Unit of progress used (double, int, etc.)</typeparam>
+[Serializable()]
+public class ProcessProgress<TUnit> where TUnit : struct
 {
-    /// <summary>
-    /// Generic process progress class.
-    /// </summary>
-    /// <remarks>
-    /// Used to track total progress of an identified operation.
-    /// </remarks>
-    /// <typeparam name="TUnit">Unit of progress used (double, int, etc.)</typeparam>
-    [Serializable()]
-    public class ProcessProgress<TUnit> where TUnit : struct
+    #region [ Members ]
+
+    // Fields
+    private string m_processName;
+    private string m_progressMessage;
+    private TUnit m_total;
+    private TUnit m_complete;
+
+    #endregion
+
+    #region [ Constructors ]
+
+    public ProcessProgress(string processName)
     {
-        #region " Member Declaration "
-
-        private string m_processName;
-        private string m_progressMessage;
-        private TUnit m_total;
-        private TUnit m_complete;
-
-        #endregion
-
-        #region " Code Scope: Public "
-
-        public ProcessProgress(string processName)
-        {
-            m_processName = processName;
-        }
-
-        public string ProcessName
-        {
-            get
-            {
-                return m_processName;
-            }
-            set
-            {
-                m_processName = value;
-            }
-        }
-
-        public string ProgressMessage
-        {
-            get
-            {
-                return m_progressMessage;
-            }
-            set
-            {
-                m_progressMessage = value;
-            }
-        }
-
-        public TUnit Total
-        {
-            get
-            {
-                return m_total;
-            }
-            set
-            {
-                m_total = value;
-            }
-        }
-
-        public TUnit Complete
-        {
-            get
-            {
-                return m_complete;
-            }
-            set
-            {
-                m_complete = value;
-            }
-        }
-
-        #endregion
+        m_processName = processName;
     }
+
+    #endregion
+
+    #region [ Properties ]
+
+    public string ProcessName
+    {
+        get
+        {
+            return m_processName;
+        }
+        set
+        {
+            m_processName = value;
+        }
+    }
+
+    public string ProgressMessage
+    {
+        get
+        {
+            return m_progressMessage;
+        }
+        set
+        {
+            m_progressMessage = value;
+        }
+    }
+
+    public TUnit Total
+    {
+        get
+        {
+            return m_total;
+        }
+        set
+        {
+            m_total = value;
+        }
+    }
+
+    public TUnit Complete
+    {
+        get
+        {
+            return m_complete;
+        }
+        set
+        {
+            m_complete = value;
+        }
+    }
+
+    #endregion
 }
