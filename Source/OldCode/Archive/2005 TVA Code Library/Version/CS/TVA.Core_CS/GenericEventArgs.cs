9a86c05b0803f4bc6655ece4c4ed5ffce505e4cf
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-18 14:35:11 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/GenericEventArgs.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/GenericEventArgs.cs
@@ -1,12 +1,12 @@
 //*******************************************************************************************************
-//  GenericEventArgs.vb - Generic event arguments class
-//  Copyright © 2005 - TVA, all rights reserved - Gbtc
+//  GenericEventArgs.cs
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2005
-//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2250
-//       Email: pcpatel@tva.gov
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR 2W-C
+//       Phone: 423/751-2827
+//       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
@@ -21,9 +21,7 @@ using System;
 
 namespace TVA
 {
-    /// <summary>
-    /// Generic event arguments class.
-    /// </summary>
+    /// <summary>Generic event arguments class.</summary>
     /// <typeparam name="T">Type of argument for this event arguments instance.</typeparam>
     public class GenericEventArgs<T> : EventArgs
     {
