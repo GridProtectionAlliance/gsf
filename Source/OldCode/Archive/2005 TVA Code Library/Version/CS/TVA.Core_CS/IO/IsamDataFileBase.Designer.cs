d3f303b07eb813c01c24f4e1909a1e55124b0b13
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-19 17:31:10 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/IO/IsamDataFileBase.Designer.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/IO/IsamDataFileBase.Designer.cs
@@ -1,95 +1,97 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
 using System;
+using System.IO;
 
-namespace TVA
+namespace TVA.IO
 {
-    namespace IO
+    public partial class IsamDataFileBase<T> : System.ComponentModel.Component where T : TVA.Parsing.IBinaryDataProvider
     {
-
-        public partial class IsamDataFileBase<T> : System.ComponentModel.Component where T : TVA.Parsing.IBinaryDataProvider
+        [System.Diagnostics.DebuggerNonUserCode()]
+        public IsamDataFileBase(System.ComponentModel.IContainer container)
+            : this()
         {
+            //Required for Windows.Forms Class Composition Designer support.
+            if (container != null)
+                container.Add(this);
+        }
 
-
-            [System.Diagnostics.DebuggerNonUserCode()]
-            public IsamDataFileBase(System.ComponentModel.IContainer container)
-                : this()
-            {
-
-                //Required for Windows.Forms Class Composition Designer support.
-                if (container != null)
-                {
-                    container.Add(this);
-                }
-
-            }
-
-            [System.Diagnostics.DebuggerNonUserCode()]
-            public IsamDataFileBase()
-            {
-
-                //Required by the Component Designer.
-                InitializeComponent();
-
-                m_name = this.GetType().Name + Extension;
-                m_minimumRecordCount = 0;
-                m_loadOnOpen = true;
-                m_reloadOnModify = true;
-                m_autoSaveInterval = -1;
-                m_settingsCategoryName = this.GetType().Name;
-
-                m_autoSaveTimer = new System.Timers.Timer();
-                m_autoSaveTimer.Elapsed += new System.Timers.ElapsedEventHandler(m_autoSaveTimer_Elapsed);
-                m_loadWaitHandle = new System.Threading.ManualResetEvent(true);
-                m_saveWaitHandle = new System.Threading.ManualResetEvent(true);
-
-            }
-
-            //Overrides dispose to clean up the component list.
-            [System.Diagnostics.DebuggerNonUserCode()]
-            protected override void Dispose(bool disposing)
-            {
-                try
-                {
-                    Close(); // Closes the file.
-                    SaveSettings(); // Saves settings to the config file.
-                    if (disposing && (components != null))
-                    {
-                        components.Dispose();
-                    }
-                }
-                finally
-                {
-                    base.Dispose(disposing);
-                }
-            }
-
+        [System.Diagnostics.DebuggerNonUserCode()]
+        public IsamDataFileBase()
+        {
             //Required by the Component Designer.
-            private System.ComponentModel.Container components = null;
+            InitializeComponent();
 
-            //NOTE: Required by the Component Designer.
-            //Can be modified using the Component Designer.
-            //Do not modify it using the code editor.
-            [System.Diagnostics.DebuggerStepThrough()]
-            private void InitializeComponent()
-            {
-                this.FileSystemWatcher = new System.IO.FileSystemWatcher();
-                this.FileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(FileSystemWatcher_Changed);
-                ((System.ComponentModel.ISupportInitialize)this.FileSystemWatcher).BeginInit();
-                //
-                //FileSystemWatcher
-                //
-                this.FileSystemWatcher.EnableRaisingEvents = true;
-                ((System.ComponentModel.ISupportInitialize)this.FileSystemWatcher).EndInit();
+            m_name = this.GetType().Name + Extension;
+            m_minimumRecordCount = 0;
+            m_loadOnOpen = true;
+            m_reloadOnModify = true;
+            m_autoSaveInterval = -1;
+            m_settingsCategoryName = this.GetType().Name;
 
-            }
-            internal System.IO.FileSystemWatcher FileSystemWatcher;
+            m_autoSaveTimer = new System.Timers.Timer();
+            m_autoSaveTimer.Elapsed += m_autoSaveTimer_Elapsed;
+
+            m_loadWaitHandle = new System.Threading.ManualResetEvent(true);
+            m_saveWaitHandle = new System.Threading.ManualResetEvent(true);
 
         }
 
+        //Overrides dispose to clean up the component list.
+        [System.Diagnostics.DebuggerNonUserCode()]
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing)
+                {
+                    Close();        // Closes the file.
+                    SaveSettings(); // Saves settings to the config file.
+
+                    if (FileSystemWatcher != null)
+                    {
+                        FileSystemWatcher.Changed -= FileSystemWatcher_Changed;
+                        FileSystemWatcher.Dispose();
+                    }
+                    FileSystemWatcher = null;
+
+                    if (m_loadWaitHandle != null) m_loadWaitHandle.Close();
+                    m_loadWaitHandle = null;
+
+                    if (m_saveWaitHandle != null) m_saveWaitHandle.Close();
+                    m_saveWaitHandle = null;
+
+                    if (m_autoSaveTimer != null) m_autoSaveTimer.Dispose();
+                    m_autoSaveTimer = null;
+
+                    if (components != null)
+                        components.Dispose();
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+
+        //Required by the Component Designer.
+        private System.ComponentModel.Container components = null;
+
+        //NOTE: Required by the Component Designer.
+        //Can be modified using the Component Designer.
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            this.FileSystemWatcher = new FileSystemWatcher();
+            this.FileSystemWatcher.Changed += FileSystemWatcher_Changed;
+            ((System.ComponentModel.ISupportInitialize)this.FileSystemWatcher).BeginInit();
+            //
+            //FileSystemWatcher
+            //
+            this.FileSystemWatcher.EnableRaisingEvents = true;
+            ((System.ComponentModel.ISupportInitialize)this.FileSystemWatcher).EndInit();
+
+        }
+
+        internal FileSystemWatcher FileSystemWatcher;
     }
 }
