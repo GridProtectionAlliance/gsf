c208b96dc5a45f17f2f35a7af71b8983ae476511
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-19 18:15:12 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/IO/Compression/Enumerations.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/IO/Compression/Enumerations.cs
@@ -1,48 +1,33 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-
 //*******************************************************************************************************
-//  TVA.IO.Compression.Enumerations.vb - Compression enumerations
-//  Copyright © 2007 - TVA, all rights reserved - Gbtc
+//  Enumerations.cs
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2005
-//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2250
-//       Email: pcpatel@tva.gov
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR 2W-C
+//       Phone: 423/751-2827
+//       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  03/25/2008 - Pinal C. Patel
+//  09/19/2008 - James R Carroll
 //       Generated original version of source code.
 //
 //*******************************************************************************************************
 
-namespace TVA
+using System;
+
+namespace TVA.IO.Compression
 {
-    namespace IO
+    /// <summary>
+    /// Specifies the level of compression to be performed on data.
+    /// </summary>
+    public enum CompressLevel
     {
-        namespace Compression
-        {
-
-
-            /// <summary>
-            /// Specifies the level of compression to be performed on data.
-            /// </summary>
-            public enum CompressLevel
-            {
-                DefaultCompression = -1,
-                NoCompression = 0,
-                BestSpeed = 1,
-                BestCompression = 9,
-                MultiPass = 10
-            }
-
-        }
+        DefaultCompression = -1,
+        NoCompression = 0,
+        BestSpeed = 1,
+        BestCompression = 9,
+        MultiPass = 10
     }
 }
