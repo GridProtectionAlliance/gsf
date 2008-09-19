e422069c4a263e262a1fcb1bf7a7e16aea662435
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-19 14:59:07 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/IO/ExportDestination.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/IO/ExportDestination.cs
@@ -1,19 +1,10 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Data;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-using System.IO;
-
 //*******************************************************************************************************
-//  ExportDestination.vb - Defines an Export Destination - used by MultipleDestinationExporter
+//  ExportDestination.cs
 //  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR 2W-C
 //       Phone: 423/751-2827
 //       Email: jrcarrol@tva.gov
 //
@@ -21,88 +12,80 @@ using System.IO;
 //  -----------------------------------------------------------------------------------------------------
 //  02/13/2008 - J. Ritchie Carroll
 //       Initial version of source generated
+//  09/19/2008 - James R Carroll
+//       Converted to C#.
 //
 //*******************************************************************************************************
 
+using System;
+using System.IO;
 
-namespace TVA
+namespace TVA.IO
 {
-    namespace IO
+    /// <summary>
+    /// Export destination.
+    /// </summary>
+    public struct ExportDestination
     {
+        /// <summary>
+        /// Path and file name of export destination.
+        /// </summary>
+        public string DestinationFile;
 
         /// <summary>
-        /// Export destination.
+        /// Determines whether or not to attempt network connection to share specified in DestinationFile.
         /// </summary>
-        public struct ExportDestination
+        public bool ConnectToShare;
+
+        /// <summary>
+        /// Domain used to authenticate network connection if ConnectToShare is True.
+        /// </summary>
+        public string Domain;
+
+        /// <summary>
+        /// User name used to authenticate network connection if ConnectToShare is True.
+        /// </summary>
+        public string UserName;
+
+        /// <summary>
+        /// Password used to authenticate network connection if ConnectToShare is True.
+        /// </summary>
+        public string Password;
+
+        public ExportDestination(string destinationFile, bool connectToShare, string domain, string userName, string password)
         {
-
-            /// <summary>
-            /// Path and file name of export destination.
-            /// </summary>
-            public string DestinationFile;
-
-            /// <summary>
-            /// Determines whether or not to attempt network connection to share specified in DestinationFile.
-            /// </summary>
-            public bool ConnectToShare;
-
-            /// <summary>
-            /// Domain used to authenticate network connection if ConnectToShare is True.
-            /// </summary>
-            public string Domain;
-
-            /// <summary>
-            /// User name used to authenticate network connection if ConnectToShare is True.
-            /// </summary>
-            public string UserName;
-
-            /// <summary>
-            /// Password used to authenticate network connection if ConnectToShare is True.
-            /// </summary>
-            public string Password;
-
-            public ExportDestination(string destinationFile, bool connectToShare, string domain, string userName, string password)
-            {
-
-                this.DestinationFile = destinationFile;
-                this.ConnectToShare = connectToShare;
-                this.Domain = domain;
-                this.UserName = userName;
-                this.Password = password;
-
-            }
-
-            /// <summary>
-            /// Path root of DestinationFile (e.g., E:\ or \\server\share)
-            /// </summary>
-            public string Share
-            {
-                get
-                {
-                    return Path.GetPathRoot(DestinationFile);
-                }
-            }
-
-            /// <summary>
-            /// Path and filename of DestinationFile
-            /// </summary>
-            public string FileName
-            {
-                get
-                {
-                    return DestinationFile.Substring(Share.Length);
-                }
-            }
-
-            public override string ToString()
-            {
-
-                return DestinationFile;
-
-            }
-
+            this.DestinationFile = destinationFile;
+            this.ConnectToShare = connectToShare;
+            this.Domain = domain;
+            this.UserName = userName;
+            this.Password = password;
         }
 
-    }
+        /// <summary>
+        /// Path root of DestinationFile (e.g., E:\ or \\server\share)
+        /// </summary>
+        public string Share
+        {
+            get
+            {
+                return Path.GetPathRoot(DestinationFile);
+            }
+        }
 
+        /// <summary>
+        /// Path and filename of DestinationFile
+        /// </summary>
+        public string FileName
+        {
+            get
+            {
+                return DestinationFile.Substring(Share.Length);
+            }
+        }
+
+        public override string ToString()
+        {
+            return DestinationFile;
+        }
+    }
 }
