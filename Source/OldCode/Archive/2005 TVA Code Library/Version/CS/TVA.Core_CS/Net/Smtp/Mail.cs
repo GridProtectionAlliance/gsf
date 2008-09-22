c88da33340219012308a4cf320e956cdeda56081
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-22 14:41:59 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Net/Smtp/Mail.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Net/Smtp/Mail.cs
@@ -31,6 +31,216 @@ namespace TVA.Net.Smtp
     /// <summary>Defines common e-mail related functions.</summary>
     public static class Mail
     {
+        /// <summary>Simple mail message instance.</summary>
+        public class Message
+        {
+            #region [ Members ]
+
+            // Fields
+            private string m_from;
+            private string m_recipients;
+            private string m_ccRecipients;
+            private string m_bccRecipients;
+            private string m_subject;
+            private string m_body;
+            private string m_smtpServer;
+            private string m_attachments;
+            private bool m_isBodyHtml;
+
+            #endregion
+
+            #region [ Constructors ]
+
+            public Message()
+            {
+            }
+
+            public Message(string smtpServer)
+            {
+                m_smtpServer = smtpServer;
+            }
+
+            #endregion
+
+            #region [ Properties ]
+
+            /// <summary>
+            /// Gets or sets the sender's address for this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The sender's address for this e-mail message.</returns>
+            public string From
+            {
+                get
+                {
+                    return m_from;
+                }
+                set
+                {
+                    if (!string.IsNullOrEmpty(value))
+                        m_from = value;
+                    else
+                        throw new ArgumentNullException("From");
+                }
+            }
+
+            /// <summary>
+            /// Gets the comma (,) or semicolon (;) delimited list of recipients for this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The comma (,) or semicolon (;) delimited list of recipients for this e-mail message.</returns>
+            public string Recipients
+            {
+                get
+                {
+                    return m_recipients;
+                }
+                set
+                {
+                    if (!string.IsNullOrEmpty(value))
+                        m_recipients = value;
+                    else
+                        throw new ArgumentNullException("Recipients");
+                }
+            }
+
+            /// <summary>
+            /// Gets the comma (,) or semicolon (;) delimited list of carbon copy recipients for this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The comma (,) or semicolon (;) delimited list of carbon copy recipients for this e-mail message.</returns>
+            public string CcRecipients
+            {
+                get
+                {
+                    return m_ccRecipients;
+                }
+                set
+                {
+                    m_ccRecipients = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets the comma (,) or semicolon (;) delimited list of blind carbon copy recipients for this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The comma (,) or semicolon (;) delimited list of blind carbon copy recipients for this e-mail message.</returns>
+            public string BccRecipients
+            {
+                get
+                {
+                    return m_bccRecipients;
+                }
+                set
+                {
+                    m_bccRecipients = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets the subject line for this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The subject line for this e-mail message.</returns>
+            public string Subject
+            {
+                get
+                {
+                    return m_subject;
+                }
+                set
+                {
+                    m_subject = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets the message body.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The body text.</returns>
+            public string Body
+            {
+                get
+                {
+                    return m_body;
+                }
+                set
+                {
+                    m_body = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets the name or IP address of the mail server for this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The name or IP address of the mail server for this e-mail message.</returns>
+            public string SmtpServer
+            {
+                get
+                {
+                    return m_smtpServer;
+                }
+                set
+                {
+                    if (!string.IsNullOrEmpty(value))
+                        m_smtpServer = value;
+                    else
+                        throw new ArgumentNullException("SmtpServer");
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets the comma (,) or semicolon (;) delimited list of file names that are to be attached to this e-mail message.
+            /// </summary>
+            /// <value></value>
+            /// <returns>The comma (,) or semicolon (;) delimited list of file names that are to be attached to this e-mail message.</returns>
+            public string Attachments
+            {
+                get
+                {
+                    return m_attachments;
+                }
+                set
+                {
+                    m_attachments = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets a boolean value indicating whether the mail message body is in Html.
+            /// </summary>
+            /// <value></value>
+            /// <returns>True if the message body is in Html; otherwise False.</returns>
+            public bool IsBodyHtml
+            {
+                get
+                {
+                    return m_isBodyHtml;
+                }
+                set
+                {
+                    m_isBodyHtml = value;
+                }
+            }
+
+            #endregion
+
+            #region [ Methods ]
+
+            /// <summary>
+            /// Send this e-mail message to its recipients.
+            /// </summary>
+            public void Send()
+            {
+                Mail.Send(m_from, m_recipients, m_ccRecipients, m_bccRecipients, m_subject, m_body, m_isBodyHtml, m_attachments, m_smtpServer);
+            }
+
+            #endregion
+        }
+
         /// <summary>Creates a mail message from the specified information, and sends it to an SMTP server for delivery.</summary>
         /// <param name="from">The address of the mail message sender.</param>
         /// <param name="toRecipients">A comma-separated address list of the mail message recipients.</param>
