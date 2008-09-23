789cb7892bacd81066c678aff06bda8fb1e5afa4
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-23 13:01:23 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/Net/Ftp/IFile.cs
@@ -0,0 +1,67 @@
+﻿//*******************************************************************************************************
+//  IFile.cs
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR 2W-C
+//       Phone: 423/751-2827
+//       Email: jrcarrol@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  09/23/2008 - James R Carroll
+//       Generated original version of source code.
+//
+//*******************************************************************************************************
+
+using System;
+
+namespace TVA.Net.Ftp
+{
+    public interface IFile
+    {
+        Directory Parent
+        {
+            get;
+        }
+
+        string Name
+        {
+            get;
+        }
+
+        string FullPath
+        {
+            get;
+        }
+
+        bool IsFile
+        {
+            get;
+        }
+
+        bool IsDirectory
+        {
+            get;
+        }
+
+        long Size
+        {
+            get;
+            set;
+        }
+
+        string Permission
+        {
+            get;
+            set;
+        }
+
+        DateTime TimeStamp
+        {
+            get;
+            set;
+        }
+    }
+}
