7ff7389a2bbb07394f15dfd95e16f798a576023a
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-09-29 14:24:13 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Communication_CS/Enumerations.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Communication_CS/Enumerations.cs
@@ -1,11 +1,21 @@
-using System.Diagnostics;
-using System.Linq;
-using System.Collections;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
-using System;
-
-// 06-01-06
+//*******************************************************************************************************
+//  Enumerations.cs
+//  Copyright © 2008 - TVA, all rights reserved - Gbtc
+//
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: Pinal C. Patel, Operations Data Architecture [TVA]
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-3024
+//       Email: pcpatel@tva.gov
+//
+//  Code Modification History:
+//  -----------------------------------------------------------------------------------------------------
+//  06/01/2006 - Pinal C. Patel
+//       Original version of source code generated
+//  09/29/2008 - James R Carroll
+//       Converted to C#.
+//
+//*******************************************************************************************************
 
 namespace TVA.Communication
 {
