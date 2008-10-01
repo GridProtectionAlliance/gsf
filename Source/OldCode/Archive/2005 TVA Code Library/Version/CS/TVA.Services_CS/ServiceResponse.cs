1b837ff67df7e9dc9814c10aab81152520115bea
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-10-01 13:13:12 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Services_CS/ServiceResponse.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Services_CS/ServiceResponse.cs
@@ -3,10 +3,10 @@
 //  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
 //  Build Environment: C#, Visual Studio 2008
-//  Primary Developer: James R Carroll
+//  Primary Developer: Pinal C. Patel
 //      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
-//       Phone: 423/751-4165
-//       Email: jrcarrol@tva.gov
+//       Phone: 423/751-3024
+//       Email: pcpatel@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
