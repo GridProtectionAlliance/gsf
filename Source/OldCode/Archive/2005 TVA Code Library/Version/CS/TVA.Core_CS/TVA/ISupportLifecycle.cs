8833603a5e39dd955c6ef1ab8f8ccffd8f68815b
pinalcpatel <pcpatel@tva.gov>
2008-10-09 19:04:09 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/TVA/ISupportLifecycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVA
+{
+    /// <summary>
+    /// Specifies that this object provides supporting methods throughout its lifecycle from birth 
+    /// (<see cref="Initialize()"/>) to death (<see cref="IDisposable.Dispose()"/>).
+    /// </summary>
+    interface ISupportLifecycle : IDisposable
+    {
+        /// <summary>
+        /// Initializes the state of the object.
+        /// </summary>
+        /// <remarks>Typically this method should be implemented to allow the object state to be initialized only once.</remarks>
+        void Initialize();
+
+        /// <summary>
+        /// Gets or set a boolean value that indicates whether the object is enabled.
+        /// </summary>
+        bool Enabled { get; set; }
+    }
+}
