c3b2c927a25e195fd3c5f05b0f8901e8aa87a498
pinalcpatel <pcpatel@tva.gov>
2008-10-10 16:57:10 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/TVA/ISupportLifecycle.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/TVA/ISupportLifecycle.cs
@@ -20,21 +20,47 @@ using System;
 namespace TVA
 {
     /// <summary>
-    /// Specifies that this object provides supporting methods throughout its lifecycle from birth 
-    /// (<see cref="Initialize()"/>) to death (<see cref="IDisposable.Dispose()"/>).
+    /// Specifies that this object provides support for performing tasks during the key stages of object lifecycle. 
     /// </summary>
+    /// <remarks>
+    /// <list type="table">
+    ///     <listheader>
+    ///         <term>Lifecycle Stage</term>
+    ///         <description>Equivalent Member</description>
+    ///     </listheader>
+    ///     <item>
+    ///         <term>Birth</term>
+    ///         <description><see cref="Initialize()"/></description>
+    ///     </item>
+    ///     <item>
+    ///         <term>Life (Work/Sleep)</term>
+    ///         <description><see cref="Enabled"/></description>
+    ///     </item>
+    ///     <item>
+    ///         <term>Death</term>
+    ///         <description><see cref="IDisposable.Dispose()"/></description>
+    ///     </item>
+    /// </list>
+    /// </remarks>
     public interface ISupportLifecycle : IDisposable
     {
         /// <summary>
         /// Initializes the state of the object.
         /// </summary>
         /// <remarks>
-        /// Typical implementation of this method should allow the object state to be initialized only once.
+        /// Typical implementation of <see cref="Initialize()"/> should allow the object state to be initialized only 
+        /// once. <see cref="Initialize()"/> should be called automatically from one or more key entry points of the 
+        /// object. For example, if the object is a <see cref="System.ComponentModel.Component"/> and it implements 
+        /// the <see cref="System.ComponentModel.ISupportInitialize"/> interface then <see cref="Initialize()"/> should 
+        /// be called from the <see cref="System.ComponentModel.ISupportInitialize.EndInit()"/> method so that the object 
+        /// gets initialized automatically when consumed through the IDE designer surface. In addition to this 
+        /// <see cref="Initialize()"/> should also be called from key or mandatory methods of the object, like 'Start()'
+        /// or 'Connect()', so that the object gets initialized even when not consumed through the IDE designer surface.
         /// </remarks>
         void Initialize();
 
         /// <summary>
-        /// Gets or set a boolean value that indicates whether the object is enabled.
+        /// Gets or sets a boolean value that indicates whether the object is enabled.
         /// </summary>
         /// <remarks>
         /// Typical implementation of this property should suspend the internal processing when the object is 
