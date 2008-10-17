079e00022b77209776096353ffc81404e1f86fe3
pinalcpatel <pcpatel@tva.gov>
2008-10-17 17:38:37 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/TVA/ErrorManagement/ErrorModule.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/TVA/ErrorManagement/ErrorModule.cs
@@ -16,10 +16,13 @@
 //       Edited code comments.
 //  09/24/2008 - Pinal C Patel
 //       Converted code to C#.
+//  10/17/2008 - Pinal C Patel
+//       Edited code comments.
 //
 //*******************************************************************************************************
 
 using System.Web;
+using System.Diagnostics;
 
 namespace TVA.ErrorManagement
 {
@@ -28,8 +31,9 @@ namespace TVA.ErrorManagement
     /// </summary>
     /// <seealso cref="ErrorLogger"/>
     /// <example>
-    /// Usage in web.config:
+    /// Below is the config file entry required for enabling error handling using <see cref="ErrorModule"/>:
     /// <code>
+    /// <![CDATA[
     /// <configuration>
     ///   <system.web>
     ///     <httpModules>
@@ -37,6 +41,41 @@ namespace TVA.ErrorManagement
     ///     </httpModules>
     ///   </system.web>
     /// </configuration>
+    /// ]]>
+    /// </code>
+    /// Below is the config file entry required for changing the settings of <see cref="ErrorModule.Logger"/>:
+    /// <code>
+    /// <![CDATA[
+    /// <configSections>
+    ///   <section name="categorizedSettings" type="TVA.Configuration.CategorizedSettingsSection, TVA.Core" />
+    /// </configSections>
+    /// <categorizedSettings>
+    ///   <errorLogger>
+    ///     <add name="LogToUI" value="False" description="True if an encountered exception is to be logged to the User Interface; otherwise False."
+    ///       encrypted="false" />
+    ///     <add name="LogToFile" value="True" description="True if an encountered exception is to be logged to a file; otherwise False."
+    ///       encrypted="false" />
+    ///     <add name="LogToEmail" value="False" description="True if an email is to be sent to ContactEmail with the details of an encountered exception; otherwise False."
+    ///       encrypted="false" />
+    ///     <add name="LogToEventLog" value="True" description="True if an encountered exception is to be logged to the Event Log; otherwise False."
+    ///       encrypted="false" />
+    ///     <add name="LogToScreenshot" value="False" description="True if a screenshot is to be taken when an exception is encountered; otherwise False."
+    ///       encrypted="false" />
+    ///     <add name="SmtpServer" value="smtp.xyzcorp.com" description="Name of the SMTP server to be used for sending the email messages."
+    ///       encrypted="false" />
+    ///     <add name="ContactEmail" value="" description="Comma-seperated list of recipient email addresses for the email message."
+    ///       encrypted="false" />
+    ///     <add name="ContactName" value="" description="Name of the person that the end-user can contact when an exception is encountered."
+    ///       encrypted="false" />
+    ///     <add name="ContactPhone" value="" description="Phone number of the person that the end-user can contact when an exception is encountered."
+    ///       encrypted="false" />
+    ///     <add name="HandleUnhandledException" value="True" description="True if unhandled exceptions are to be handled automatically; otherwise False."
+    ///       encrypted="false" />
+    ///     <add name="ExitOnUnhandledException" value="False" description="True if the application must exit when an unhandled exception is encountered; otherwise False."
+    ///       encrypted="false" />
+    ///   </errorLogger>
+    /// </categorizedSettings>
+    /// ]]>
     /// </code>
     /// </example>
     public class ErrorModule : IHttpModule
@@ -56,8 +95,11 @@ namespace TVA.ErrorManagement
         /// <param name="context"></param>
         public void Init(HttpApplication context)
         {
-            m_context = context;        // Save reference to the application.   
-            m_context.Error += OnError; // Register to be notified for unhandled exceptions.
+            if (Logger.HandleUnhandledException && !Debugger.IsAttached)
+            {
+                m_context = context;        // Save reference to the application.   
+                m_context.Error += OnError; // Register to be notified for unhandled exceptions.                
+            }
         }
 
         /// <summary>
@@ -65,7 +107,10 @@ namespace TVA.ErrorManagement
         /// </summary>
         public void Dispose()
         {
-            m_context.Error -= OnError; // Unregister from being notified for unhandled exceptions.
+            if (Logger.HandleUnhandledException && !Debugger.IsAttached)
+            {
+                m_context.Error -= OnError; // Unregister from being notified for unhandled exceptions.
+            }
         }
 
         private void OnError(object sender, System.EventArgs e)
@@ -84,12 +129,41 @@ namespace TVA.ErrorManagement
         {
             m_logger = new ErrorLogger();
             m_logger.PersistSettings = true;
-            m_logger.Initialize();  // This will cause settings, if persisted previously, to be loaded.
+            m_logger.Initialize();          // Initialize error logger for use.
         }
 
         /// <summary>
         /// Gets the <see cref="ErrorLogger"/> object used by the <see cref="ErrorModule"/> object for logging exceptions.
         /// </summary>
+        /// <remarks>
+        /// <see cref="Logger"/> property can be used for logging handled exception throughout the web application.
+        /// </remarks>
+        /// <example>
+        /// This example shows the use of <see cref="ErrorModule.Logger"/> for logging handled exception:
+        /// <code>
+        /// using System;
+        /// using TVA.ErrorManagement;
+        ///
+        /// namespace WebApp
+        /// {
+        ///     public partial class _Default : System.Web.UI.Page
+        ///     {
+        ///         protected void Page_Load(object sender, EventArgs e)
+        ///         {
+        ///             try
+        ///             {
+        ///                 string s = null;
+        ///                 s.ToCharArray();            // This will result in NullReferenceException.
+        ///             }
+        ///             catch (Exception ex)
+        ///             {
+        ///                 ErrorModule.Logger.Log(ex); // Log the encountered exception.
+        ///             }
+        ///         }
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         public static ErrorLogger Logger
         {
             get 
