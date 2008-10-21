addfb85a9e2fce9d77944b7e7225cdd25a9fac74
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-10-21 17:07:30 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/System/BinaryValue.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/CS/TVA.Core_CS/System/BinaryValue.cs
@@ -1,5 +1,5 @@
 ﻿/**************************************************************************\
-   Copyright (c) 2008, James Ritchie Carroll
+   Copyright (c) 2008 - Gbtc, James Ritchie Carroll
    All rights reserved.
   
    Redistribution and use in source and binary forms, with or without
