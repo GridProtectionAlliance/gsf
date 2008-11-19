132aa04087f35eb5a254dea94c1bc7983e0968c0
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-11-19 16:51:51 +0000

    Vss2Git

--- Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Parsing/IFrameParser.cs
+++ Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Parsing/IFrameParser.cs
@@ -27,23 +27,55 @@ namespace PCS.Parsing
     {
         //event EventHandler<EventArgs<IBinaryImageConsumer>> ReceivedImage;
         //event EventHandler<EventArgs<IBinaryImageConsumer>> ReceivedUndeterminedImage;
-        //event EventHandler<EventArgs<Exception>> DataStreamException;
 
+        /// <summary>
+        /// Occurs when an <see cref="Exception"/> is encountered while attempting to parse data.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="EventArgs{T}.Argument"/> is the <see cref="Exception"/> encountered while parsing data.
+        /// </remarks>
+        public event EventHandler<EventArgs<Exception>> ParsingException;
+
+        /// <summary>
+        /// Start the data parser.
+        /// </summary>
         void Start();
 
+        /// <summary>
+        /// Stops the data parser.
+        /// </summary>
         void Stop();
 
+        /// <summary>
+        /// Gets or sets flag that detemines if the internal buffer queue is enabled.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// When interface implementations set this property to true, an internal buffer queue should be enabled causing
+        /// any data to added to <see cref="Write"/> method to be queued for parsing on a separate thread. If the property
+        /// is set to false, the parse should occur immediately on the thread that invoked the <see cref="Write"/> method.
+        /// </para>
+        /// </remarks>
         bool ExecuteParseOnSeparateThread
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets the total number of buffers that are currently queued for processing, if any.
+        /// </summary>
         int QueuedBuffers
         {
             get;
         }
 
+        /// <summary>
+        /// Writes a sequence of bytes onto the <see cref="IFrameParser"/> stream for parsing.
+        /// </summary>
+        /// <param name="buffer">An array of bytes. This method copies count bytes from buffer to the current stream.</param>
+        /// <param name="offset">The zero-based byte offset in buffer at which to begin copying bytes to the current stream.</param>
+        /// <param name="count">The number of bytes to be written to the current stream.</param>
         void Write(byte[] buffer, int offset, int count);
     }
 }
