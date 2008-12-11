c572db3d919262ba9fdd6363d9dbf9158976246e
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2008-12-11 20:34:31 +0000

    Vss2Git

--- Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Parsing/MultiSourceFrameImageParserBase.cs
+++ Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Parsing/MultiSourceFrameImageParserBase.cs
@@ -1,5 +1,5 @@
 ﻿//*******************************************************************************************************
-//  MultiSourceFrameParserBase.cs
+//  MultiSourceFrameImageParserBase.cs
 //  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
 //  Build Environment: C#, Visual Studio 2008
