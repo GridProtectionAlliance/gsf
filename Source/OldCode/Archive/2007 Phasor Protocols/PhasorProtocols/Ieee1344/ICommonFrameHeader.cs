2962730587a0e9f25c1f3be730e271079e247cf6
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-01-05 17:54:49 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/Ieee1344/ICommonFrameHeader.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/Ieee1344/ICommonFrameHeader.cs
@@ -1,72 +1,30 @@
 //*******************************************************************************************************
-//  ICommonFrameHeader.vb - IEEE 1344 Common frame header interface
+//  IFrameImage.cs
 //  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  01/14/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  01/14/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 using System;
+using PCS.Parsing;
 
-namespace PCS.PhasorProtocols
+namespace PCS.PhasorProtocols.Ieee1344
 {
-    namespace Ieee1344
+    /// <summary>
+    /// Represents the interface that uniquely identifies IEEE 1344 frame images (see <see cref="FrameImageParserBase{TTypeIdentifier,TOutputType}"/>).
+    /// </summary>
+    [CLSCompliant(false)]
+    public interface IFrameImage : ISupportFrameImage<FrameType>, IChannelFrame
     {
-        /// <summary>IEEE 1344 Common frame header interface</summary>
-        [CLSCompliant(false)]
-        public interface ICommonFrameHeader : IChannelFrame, IDisposable
-        {
-            new ulong IDCode
-            {
-                get;
-                set;
-            }
-
-            new FrameType FrameType
-            {
-                get;
-            }
-
-            FundamentalFrameType FundamentalFrameType
-            {
-                get;
-            }
-
-            ushort FrameLength
-            {
-                get;
-            }
-
-            ushort DataLength
-            {
-                get;
-            }
-
-            new NtpTimeTag TimeTag
-            {
-                get;
-            }
-
-            short InternalSampleCount
-            {
-                get;
-                set;
-            }
-
-            short InternalStatusFlags
-            {
-                get;
-                set;
-            }
-        }
     }
 }
