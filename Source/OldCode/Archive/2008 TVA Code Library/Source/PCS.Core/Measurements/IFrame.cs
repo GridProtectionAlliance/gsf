27c511e25578df7e13b87fd140f896883246855a
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-01-06 18:35:02 +0000

    Vss2Git

--- Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Measurements/IFrame.cs
+++ Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Measurements/IFrame.cs
@@ -22,28 +22,30 @@ using System.Collections.Generic;
 
 namespace PCS.Measurements
 {
-    /// <summary>Abstract frame interface representing a collection of measurements at an exact moment in time.</summary>
+    /// <summary>
+    /// Abstract frame interface representing a collection of measurements at an exact moment in time.
+    /// </summary>
     public interface IFrame : IEquatable<IFrame>, IComparable<IFrame>, IComparable
     {
-        /// <summary>Keyed measurements in this frame.</summary>
+        /// <summary>Keyed measurements in this <see cref="IFrame"/>.</summary>
         /// <remarks>Represents a dictionary of measurements, keyed by <see cref="MeasurementKey"/>.</remarks>
         IDictionary<MeasurementKey, IMeasurement> Measurements { get; }
 
-        /// <summary>Gets or sets published state of this frame.</summary>
+        /// <summary>Gets or sets published state of this <see cref="IFrame"/>.</summary>
         bool Published { get; set; }
 
-        /// <summary>Gets or sets total number of measurements that have been pubilshed for this frame.</summary>
+        /// <summary>Gets or sets total number of measurements that have been pubilshed for this <see cref="IFrame"/>.</summary>
         /// <remarks>If this property has not been assigned a value, implementors should return measurement count.</remarks>
         int PublishedMeasurements { get; set; }
 
-        /// <summary>Gets or sets exact timestamp, in ticks, of the data represented in this frame.</summary>
+        /// <summary>Gets or sets exact timestamp, in ticks, of the data represented in this <see cref="IFrame"/>.</summary>
         /// <remarks>The value of this property represents the number of 100-nanosecond intervals that have elapsed since 12:00:00 midnight, January 1, 0001.</remarks>
         long Ticks { get; set; }
 
-        /// <summary>Gets the DateTime representation of ticks of this frame.</summary>
+        /// <summary>Gets the DateTime representation of ticks of this <see cref="IFrame"/>.</summary>
         DateTime Timestamp { get; }
 
-        /// <summary>Gets ot sets reference to last measurement that was sorted into this frame.</summary>
+        /// <summary>Gets ot sets reference to last measurement that was sorted into this <see cref="IFrame"/>.</summary>
         /// <remarks>
         /// <para>This value is used to help monitor slow moving measurements that are being sorted into the frame.</para>
         /// <para>Implementors need only track the value.</para>
