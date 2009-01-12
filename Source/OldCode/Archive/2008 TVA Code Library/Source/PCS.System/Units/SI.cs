7875ebadbbd6808aab3e7cb2716d1758ca3e95ce
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-01-12 19:22:25 +0000

    Vss2Git

---
+++ Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.System/Units/SI.cs
@@ -0,0 +1,138 @@
+﻿/**************************************************************************\
+   Copyright (c) 2008 - Gbtc, James Ritchie Carroll
+   All rights reserved.
+  
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+  
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+       
+      * Redistributions in binary form must reproduce the above
+        copyright notice, this list of conditions and the following
+        disclaimer in the documentation and/or other materials provided
+        with the distribution.
+  
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
+   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+   OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  
+\**************************************************************************/
+
+namespace System
+{
+    /// <summary>
+    /// Defines constant factors for SI prefix (i.e., metric) conversions.
+    /// </summary>
+    public static class SI
+    {
+        /// <summary>
+        /// SI prefix Y, 10^24
+        /// </summary>
+        public const double yotta = 1.0e+24D;
+
+        /// <summary>
+        /// SI prefix Z, 10^21
+        /// </summary>
+        public const double zetta = 1.0e+21D;
+
+        /// <summary>
+        /// SI prefix E, 10^18
+        /// </summary>
+        public const double exa = 1.0e+18D;
+
+        /// <summary>
+        /// SI prefix P, 10^15
+        /// </summary>
+        public const double peta = 1.0e+15D;
+
+        /// <summary>
+        /// SI prefix T, 10^12
+        /// </summary>
+        public const double tera = 1.0e+12D;
+
+        /// <summary>
+        /// SI prefix G, 10^9
+        /// </summary>
+        public const double giga = 1.0e+9D;
+
+        /// <summary>
+        /// SI prefix M, 10^6
+        /// </summary>
+        public const double mega = 1.0e+6D;
+
+        /// <summary>
+        /// SI prefix k, 10^3
+        /// </summary>
+        public const double kilo = 1.0e+3D;
+
+        /// <summary>
+        /// SI prefix h, 10^2
+        /// </summary>
+        public const double hecto = 1.0e+2D;
+
+        /// <summary>
+        /// SI prefix da, 10^1
+        /// </summary>
+        public const double deca = 1.0e+1D;
+
+        /// <summary>
+        /// SI prefix d, 10^-1
+        /// </summary>
+        public const double deci = 1.0e-1D;
+
+        /// <summary>
+        /// SI prefix c, 10^-2
+        /// </summary>
+        public const double centi = 1.0e-2D;
+
+        /// <summary>
+        /// SI prefix m, 10^-3
+        /// </summary>
+        public const double milli = 1.0e-3D;
+
+        /// <summary>
+        /// SI prefix µ, 10^-6
+        /// </summary>
+        public const double micro = 1.0e-6D;
+
+        /// <summary>
+        /// SI prefix n, 10^-9
+        /// </summary>
+        public const double nano = 1.0e-9D;
+
+        /// <summary>
+        /// SI prefix p, 10^-12
+        /// </summary>
+        public const double pico = 1.0e-12D;
+
+        /// <summary>
+        /// SI prefix f, 10^-15
+        /// </summary>
+        public const double femto = 1.0e-15D;
+
+        /// <summary>
+        /// SI prefix a, 10^-18
+        /// </summary>
+        public const double atto = 1.0e-18D;
+
+        /// <summary>
+        /// SI prefix z, 10^-21
+        /// </summary>
+        public const double zepto = 1.0e-21D;
+
+        /// <summary>
+        /// SI prefix y, 10^-24
+        /// </summary>
+        public const double yocto = 1.0e-24D;
+    }
+}
