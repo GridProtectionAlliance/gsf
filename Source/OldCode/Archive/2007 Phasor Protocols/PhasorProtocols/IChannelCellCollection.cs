c077b6af777a6fdc149192a4e5d407a7a7f9c18e
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-18 20:08:36 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IChannelCellCollection.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IChannelCellCollection.cs
@@ -1,38 +1,30 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IChannelCellCollection.vb - Channel cell collection interface
+//  IChannelCellCollection.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents a protocol independent representation of a collection of any kind of data cell.</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent representation of a collection of any kind of data cell.
+    /// </summary>
     public interface IChannelCellCollection<T> : IChannelCollection<T> where T : IChannelCell
     {
-
-
+        /// <summary>
+        /// Gets flag that determines if <see cref="IChannelCell"/> collection is constant.
+        /// </summary>
+        bool ConstantCellLength { get; }
     }
 }
