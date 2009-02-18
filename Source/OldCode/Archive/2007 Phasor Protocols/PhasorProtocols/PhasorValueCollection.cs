1b060ae1ae5d3e3c616ec8f8afbcde41485caf5f
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-18 17:55:35 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/PhasorValueCollection.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/PhasorValueCollection.cs
@@ -1,68 +1,65 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  PhasorValueCollection.vb - Phasor value collection class
+//  PhasorValueCollection.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
-//
-//  Note: Phasors are stored in rectangular format internally
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  11/12/2004 - J. Ritchie Carroll
-//       Initial version of source generated
+//  11/12/2004 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Runtime.Serialization;
+
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the protocol independent collection of phasor values.</summary>
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents the protocol independent collection of phasor values.
+    /// </summary>
+    [Serializable()]
     public class PhasorValueCollection : ChannelValueCollectionBase<IPhasorDefinition, IPhasorValue>
     {
+        #region [ Constructors ]
 
-
-
+        /// <summary>
+        /// Creates a new <see cref="PhasorValueCollection"/>.
+        /// </summary>
         protected PhasorValueCollection()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="PhasorValueCollection"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected PhasorValueCollection(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
         }
 
-        public PhasorValueCollection(int maximumCount)
-            : base(maximumCount)
+        /// <summary>
+        /// Creates a new <see cref="PhasorValueCollection"/> from specified parameters.
+        /// </summary>
+        /// <param name="lastValidIndex">Last valid index for the collection (i.e., maximum count - 1).</param>
+        /// <remarks>
+        /// <paramref name="lastValidIndex"/> is used instead of maximum count so that maximum type values may
+        /// be specified as needed. For example, if the protocol specifies a collection with a signed 16-bit
+        /// maximum length you can specify <see cref="Int16.MaxValue"/> (i.e., 32,767) as the last valid index
+        /// for the collection since total number of items supported would be 32,768.
+        /// </remarks>
+        public PhasorValueCollection(int lastValidIndex)
+            : base(lastValidIndex)
         {
-
-
         }
 
-        public override Type DerivedType
-        {
-            get
-            {
-                return this.GetType();
-            }
-        }
-
+        #endregion
     }
 }
