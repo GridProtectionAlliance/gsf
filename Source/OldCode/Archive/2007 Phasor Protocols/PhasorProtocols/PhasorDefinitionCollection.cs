cda299304ddc144b5359df63e81e456f47964b41
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-18 17:58:27 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/PhasorDefinitionCollection.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/PhasorDefinitionCollection.cs
@@ -1,64 +1,65 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  PhasorDefinitionCollection.vb - Phasor value definition collection class
+//  PhasorDefinitionCollection.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Runtime.Serialization;
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the common implementation collection of protocol independent definitions of phasor values.</summary>
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents the protocol independent collection of definitions of phasor values.
+    /// </summary>
+    [Serializable()]
     public class PhasorDefinitionCollection : ChannelDefinitionCollectionBase<IPhasorDefinition>
     {
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinitionCollection"/>.
+        /// </summary>
         protected PhasorDefinitionCollection()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinitionCollection"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected PhasorDefinitionCollection(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
         }
 
-        public PhasorDefinitionCollection(int maximumCount)
-            : base(maximumCount)
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinitionCollection"/> from specified parameters.
+        /// </summary>
+        /// <param name="lastValidIndex">Last valid index for the collection (i.e., maximum count - 1).</param>
+        /// <remarks>
+        /// <paramref name="lastValidIndex"/> is used instead of maximum count so that maximum type values may
+        /// be specified as needed. For example, if the protocol specifies a collection with a signed 16-bit
+        /// maximum length you can specify <see cref="Int16.MaxValue"/> (i.e., 32,767) as the last valid index
+        /// for the collection since total number of items supported would be 32,768.
+        /// </remarks>
+        public PhasorDefinitionCollection(int lastValidIndex)
+            : base(lastValidIndex)
         {
-
-
         }
 
-        public override Type DerivedType
-        {
-            get
-            {
-                return this.GetType();
-            }
-        }
-
+        #endregion
     }
 }
