e3864233a90047e83b12eba257c150a2bdb7cddb
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-18 17:34:05 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IPhasorDefinition.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IPhasorDefinition.cs
@@ -1,55 +1,46 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IPhasorDefinition.vb - Phasor value definition interface
+//  IPhasorDefinition.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the protocol independent interface of a phasor value definition.</summary>
+    /// <summary>
+    /// Represents the protocol independent interface of a phasor value definition.
+    /// </summary>
     [CLSCompliant(false)]
     public interface IPhasorDefinition : IChannelDefinition
     {
+        /// <summary>
+        /// Gets or sets the <see cref="PhasorProtocols.CoordinateFormat"/> of this <see cref="IPhasorDefinition"/>.
+        /// </summary>
+        CoordinateFormat CoordinateFormat { get; }
 
+        /// <summary>
+        /// Gets or sets the <see cref="PhasorType"/> of this <see cref="IPhasorDefinition"/>.
+        /// </summary>
+        PhasorType Type { get; set; }
 
-        CoordinateFormat CoordinateFormat
-        {
-            get;
-        }
-
-        PhasorType Type
-        {
-            get;
-            set;
-        }
-
-        IPhasorDefinition VoltageReference
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets the associated <see cref="IPhasorDefinition"/> that represents the voltage reference (if any).
+        /// </summary>
+        /// <remarks>
+        /// This only applies to current phasors.
+        /// </remarks>
+        IPhasorDefinition VoltageReference { get; set; }
     }
 }
