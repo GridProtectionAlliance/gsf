c34394bb101f76b553d58a4b87d37e63f367a7c5
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-19 15:12:03 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ChannelCellCollectionBase.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ChannelCellCollectionBase.cs
@@ -1,66 +1,99 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  ChannelCellCollectionBase.vb - Channel data cell collection base class
+//  ChannelCellCollectionBase.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  3/7/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  03/07/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the common implementation of the protocol independent representation of a collection of any kind of data cell.</summary>
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents the common implementation of the protocol independent representation of a collection of any kind of data cell.
+    /// </summary>
+    [Serializable()]
     public abstract class ChannelCellCollectionBase<T> : ChannelCollectionBase<T>, IChannelCellCollection<T> where T : IChannelCell
     {
+        #region [ Members ]
 
-
-
+        // Fields
         private bool m_constantCellLength;
 
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new <see cref="ChannelCellCollectionBase{T}"/>.
+        /// </summary>
         protected ChannelCellCollectionBase()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="ChannelCellCollectionBase{T}"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected ChannelCellCollectionBase(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
             // Deserialize extra elements
             m_constantCellLength = info.GetBoolean("constantCellLength");
-
         }
 
-        protected ChannelCellCollectionBase(int maximumCount, bool constantCellLength)
-            : base(maximumCount)
+        /// <summary>
+        /// Creates a new <see cref="ChannelCellCollectionBase{T}"/> from specified parameters.
+        /// </summary>
+        /// <param name="lastValidIndex">Last valid index for the collection (i.e., maximum count - 1).</param>
+        /// <param name="constantCellLength">Sets flag that determines if the lengths of <see cref="IChannelCell"/> elements in this <see cref="ChannelCellCollectionBase{T}"/> are constant.</param>
+        /// <remarks>
+        /// <paramref name="lastValidIndex"/> is used instead of maximum count so that maximum type values may
+        /// be specified as needed. For example, if the protocol specifies a collection with a signed 16-bit
+        /// maximum length you can specify <see cref="Int16.MaxValue"/> (i.e., 32,767) as the last valid index
+        /// for the collection since total number of items supported would be 32,768.
+        /// </remarks>
+        protected ChannelCellCollectionBase(int lastValidIndex, bool constantCellLength)
+            : base(lastValidIndex)
         {
-
-
             m_constantCellLength = constantCellLength;
-
         }
 
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets flag that determines if the lengths of <see cref="IChannelCell"/> elements in this <see cref="ChannelCellCollectionBase{T}"/> are constant.
+        /// </summary>
+        public bool ConstantCellLength
+        {
+            get
+            {
+                return m_constantCellLength;
+            }
+        }
+
+        /// <summary>
+        /// Gets the length of the <see cref="BinaryImage"/>.
+        /// </summary>
+        /// <remarks>
+        /// The length of the <see cref="ChannelCellCollectionBase{T}"/> binary image is the combined length of all the items in the collection.
+        /// </remarks>
         public override int BinaryLength
         {
             get
@@ -73,28 +106,14 @@ namespace PCS.PhasorProtocols
                 else
                 {
                     // Cells will be different lengths, so we must manually sum lengths
-                    int length = 0;
-
-                    for (int x = 0; x <= Count - 1; x++)
-                    {
-                        length += this[x].BinaryLength;
-                    }
-
-                    return length;
+                    return this.Sum(frame => frame.BinaryLength);
                 }
             }
         }
 
-        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
-        {
-
-            base.GetObjectData(info, context);
-
-            // Serialize extra elements
-            info.AddValue("constantCellLength", m_constantCellLength);
-
-        }
-
+        /// <summary>
+        /// Gets a <see cref="Dictionary{TKey,TValue}"/> of string based property names and values for this <see cref="ChannelCellCollectionBase{T}"/> object.
+        /// </summary>
         public override Dictionary<string, string> Attributes
         {
             get
@@ -107,5 +126,23 @@ namespace PCS.PhasorProtocols
             }
         }
 
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination <see cref="StreamingContext"/> for this serialization.</param>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            // Serialize extra elements
+            info.AddValue("constantCellLength", m_constantCellLength);
+        }
+
+        #endregion
     }
 }
