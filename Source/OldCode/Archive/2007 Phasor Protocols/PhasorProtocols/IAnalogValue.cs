21b72c4264604c1a5c1e5a40ee0c3423de4cc7c8
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 18:54:17 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IAnalogValue.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IAnalogValue.cs
@@ -1,50 +1,35 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IAnalogValue.vb - Analog value interface
+//  IAnalogValue.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents a protocol independent analog value.</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent interface representation of an analog value.
+    /// </summary>
     public interface IAnalogValue : IChannelValue<IAnalogDefinition>
     {
+        /// <summary>
+        /// Gets or sets the floating point value that represents this <see cref="IAnalogValue"/>.
+        /// </summary>
+        double Value { get; set; }
 
-
-        float Value
-        {
-            get;
-            set;
-        }
-
-        short IntegerValue
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets the integer representation of this <see cref="IAnalogValue"/> value.
+        /// </summary>
+        int IntegerValue { get; set; }
     }
 }
