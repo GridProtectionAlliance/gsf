50f7bee51c2b8c2b3352d82e2528a24be0f9825a
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 19:28:56 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IChannelFrameParsingState.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IChannelFrameParsingState.cs
@@ -1,48 +1,53 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IChannelFrameParsingState.vb - Channel data frame parsing state interface
+//  IChannelFrameParsingState.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  01/14/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  01/14/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
+    /// <summary>
+    /// Defines function signature for creating new <see cref="IChannelCell"/> objects.
+    /// </summary>
+    /// <param name="parent">Reference to parent <see cref="IChannelFrame"/>.</param>
+    /// <param name="state">Current parsing state of associated <see cref="IChannelFrame"/>.</param>
+    /// <param name="index">Index of cell within its collection.</param>
+    /// <param name="binaryImage">Binary image to parse <see cref="IChannelCell"/> from.</param>
+    /// <param name="startIndex">Start index into <paramref name="binaryImage"/> to begin parsing.</param>
+    /// <returns>New <see cref="IChannelCell"/> object.</returns>
+    /// <typeparam name="T">Specific <see cref="IChannelCell"/> type that the <see cref="IChannelFrameParsingState{T}"/> references.</typeparam>
     public delegate T CreateNewCellFunctionSignature<T>(IChannelFrame parent, IChannelFrameParsingState<T> state, int index, byte[] binaryImage, int startIndex) where T : IChannelCell;
 
-    /// <summary>This interface represents the protocol independent parsing state of any frame of data.</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent interface representation of the parsing state of any kind of <see cref="IChannelFrame"/>.
+    /// </summary>
+    /// <typeparam name="T">Specific <see cref="IChannelCell"/> type that the <see cref="IChannelFrameParsingState{T}"/> references.</typeparam>
     public interface IChannelFrameParsingState<T> : IChannelParsingState where T : IChannelCell
     {
+        /// <summary>
+        /// Gets reference to delegate used to create a new <see cref="IChannelCell"/> object.
+        /// </summary>
         CreateNewCellFunctionSignature<T> CreateNewCellFunction { get; }
 
-        //IChannelCellCollection<T> Cells
-        //{
-        //    get;
-        //}
-
+        /// <summary>
+        /// Gets or sets number of cells that are expected in associated <see cref="IChannelFrame"/>.
+        /// </summary>
         int CellCount { get; set; }
 
+        /// <summary>
+        /// Gets or sets the length of associated <see cref="IChannelFrame"/> that was parsed from the binary image.
+        /// </summary>
         int ParsedBinaryLength { get; set; }
     }
 }
