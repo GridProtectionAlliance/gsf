86975b2f398b780d0207575e23905d658cddc500
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 17:31:58 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ConfigurationFrameCollection.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ConfigurationFrameCollection.cs
@@ -1,67 +1,65 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  ConfigurationFrameCollection.vb - Configuration frame collection class
+//  ConfigurationFrameCollection.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  01/14/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  01/14/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Runtime.Serialization;
 
-// <summary>This class represents the protocol independent collection of the common implementation of a configuration frame that can be sent or received from a PMU.</summary>
 namespace PCS.PhasorProtocols
 {
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents a protocol independent collection of <see cref="IConfigurationFrame"/>.
+    /// </summary>
+    [Serializable()]
     public class ConfigurationFrameCollection : ChannelFrameCollectionBase<IConfigurationFrame>
     {
+        #region [ Constructors ]
 
-
-
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationFrameCollection"/>.
+        /// </summary>
         protected ConfigurationFrameCollection()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationFrameCollection"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected ConfigurationFrameCollection(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
         }
 
-        public ConfigurationFrameCollection(int maximumCount)
-            : base(maximumCount)
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationFrameCollection"/> from specified parameters.
+        /// </summary>
+        /// <param name="lastValidIndex">Last valid index for the collection (i.e., maximum count - 1).</param>
+        /// <remarks>
+        /// <paramref name="lastValidIndex"/> is used instead of maximum count so that maximum type values may
+        /// be specified as needed. For example, if the protocol specifies a collection with a signed 16-bit
+        /// maximum length you can specify <see cref="Int16.MaxValue"/> (i.e., 32,767) as the last valid index
+        /// for the collection since total number of items supported would be 32,768.
+        /// </remarks>
+        public ConfigurationFrameCollection(int lastValidIndex)
+            : base(lastValidIndex)
         {
-
-
         }
 
-        public override Type DerivedType
-        {
-            get
-            {
-                return this.GetType();
-            }
-        }
-
+        #endregion
     }
 }
