7344d4660a7bba86517ba2387c9bbd5107db0883
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 19:05:40 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IChannelCellParsingState.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IChannelCellParsingState.cs
@@ -1,55 +1,40 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IChannelCellParsingState.vb - Channel data cell parsing state interface
+//  IChannelCellParsingState.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents a protocol independent parsing state of any kind of data cell.</summary>
+    /// <summary>
+    /// Represents a protocol independent interface representation of the parsing state of any kind of frame cell.
+    /// </summary>
     public interface IChannelCellParsingState : IChannelParsingState
     {
+        /// <summary>
+        /// Gets or sets the number of phasor elements associated with the <see cref="IChannelCell"/>.
+        /// </summary>
+        int PhasorCount { get; set; }
 
+        /// <summary>
+        /// Gets or sets the number of analog elements associated with the <see cref="IChannelCell"/>.
+        /// </summary>
+        int AnalogCount { get; set; }
 
-        int PhasorCount
-        {
-            get;
-            set;
-        }
-
-        int AnalogCount
-        {
-            get;
-            set;
-        }
-
-        int DigitalCount
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets the number of digital elements associated with the <see cref="IChannelCell"/>.
+        /// </summary>
+        int DigitalCount { get; set; }
     }
 }
