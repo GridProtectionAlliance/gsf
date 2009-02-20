23c07f4f833e5111975fc2585729b97a6a747389
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 20:08:45 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ICommandFrame.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ICommandFrame.cs
@@ -1,55 +1,41 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  ICommandFrame.vb - Command frame interface
+//  ICommandFrame.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  01/14/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  01/14/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents the protocol independent representation of a command frame.</summary>
-    [CLSCompliant(false)]
-    public interface ICommandFrame : IChannelFrame
+    /// <summary>
+    /// Represents a protocol independent interface representation of any kind of command frame that contains
+    /// a collection of <see cref="ICommandCell"/> objects.
+    /// </summary>
+    public interface ICommandFrame : IChannelFrame<ICommandFrame>
     {
+        /// <summary>
+        /// Gets reference to the <see cref="CommandCellCollection"/> for this <see cref="ICommandFrame"/>.
+        /// </summary>
+        new CommandCellCollection Cells { get; }
 
+        /// <summary>
+        /// Gets or sets <see cref="DeviceCommand"/> for this <see cref="ICommandFrame"/>.
+        /// </summary>
+        DeviceCommand Command { get; set; }
 
-        new CommandCellCollection Cells
-        {
-            get;
-        }
-
-        DeviceCommand Command
-        {
-            get;
-            set;
-        }
-
-        byte[] ExtendedData
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets extended binary image data for this <see cref="ICommandFrame"/>.
+        /// </summary>
+        byte[] ExtendedData { get; set; }
     }
 }
