52afd54d426da006418d35013f86df04bdb602ae
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 20:02:15 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ICommandCell.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ICommandCell.cs
@@ -1,49 +1,35 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  ICommandCell.vb - Command cell interface
+//  ICommandCell.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  04/16/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  04/16/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents the protocol independent representation of an element of extended command frame data.</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent interface representation of any kind of <see cref="ICommandFrame"/> cell.
+    /// </summary>
     public interface ICommandCell : IChannelCell
     {
+        /// <summary>
+        /// Gets a reference to the parent <see cref="ICommandFrame"/> for this <see cref="ICommandCell"/>.
+        /// </summary>
+        new ICommandFrame Parent { get; }
 
-
-        new ICommandFrame Parent
-        {
-            get;
-        }
-
-        byte ExtendedDataByte
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets extended data <see cref="Byte"/> of this <see cref="ICommandCell"/>.
+        /// </summary>
+        byte ExtendedDataByte { get; set; }
     }
 }
