2fe67a854de7be8e090846c72362b1b450b10674
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 20:58:50 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IConfigurationCellParsingState.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IConfigurationCellParsingState.cs
@@ -1,58 +1,55 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IConfigurationCellParsingState.vb - Configuration cell parsing state interface
+//  IConfigurationCellParsingState.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  04/16/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  04/16/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    public delegate T CreateNewDefinitionFunctionSignature<T>(IConfigurationCell parent, Byte[] binaryImage, int startIndex) where T : IChannelDefinition;
+    /// <summary>
+    /// Defines function signature for creating new <see cref="IChannelDefinition"/> objects.
+    /// </summary>
+    /// <param name="parent">Reference to parent <see cref="IConfigurationCell"/>.</param>
+    /// <param name="binaryImage">Binary image to parse <see cref="IChannelDefinition"/> from.</param>
+    /// <param name="startIndex">Start index into <paramref name="binaryImage"/> to begin parsing.</param>
+    /// <returns>New <see cref="IChannelDefinition"/> object.</returns>
+    /// <typeparam name="T">Specific <see cref="IChannelDefinition"/> type of object that gets created by referenced function.</typeparam>
+    public delegate T CreateNewDefinitionFunctionSignature<T>(IConfigurationCell parent, byte[] binaryImage, int startIndex) where T : IChannelDefinition;
 
-    /// <summary>This interface represents the protocol independent parsing state of a set of configuration related data settings (typically related to a PMU).</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent interface representation of the parsing state of any kind of <see cref="IConfigurationCell"/>.
+    /// </summary>
     public interface IConfigurationCellParsingState : IChannelCellParsingState
     {
-        CreateNewDefinitionFunctionSignature<IPhasorDefinition> CreateNewPhasorDefinitionFunction
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets reference to <see cref="CreateNewDefinitionFunctionSignature{T}"/> delegate used to create new <see cref="IPhasorDefinition"/> objects.
+        /// </summary>
+        CreateNewDefinitionFunctionSignature<IPhasorDefinition> CreateNewPhasorDefinitionFunction { get; }
 
-        CreateNewDefinitionFunctionSignature<IFrequencyDefinition> CreateNewFrequencyDefinitionFunction
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets reference to <see cref="CreateNewDefinitionFunctionSignature{T}"/> delegate used to create new <see cref="IFrequencyDefinition"/> objects.
+        /// </summary>
+        CreateNewDefinitionFunctionSignature<IFrequencyDefinition> CreateNewFrequencyDefinitionFunction { get; }
 
-        CreateNewDefinitionFunctionSignature<IAnalogDefinition> CreateNewAnalogDefinitionFunction
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets reference to <see cref="CreateNewDefinitionFunctionSignature{T}"/> delegate used to create new <see cref="IAnalogDefinition"/> objects.
+        /// </summary>
+        CreateNewDefinitionFunctionSignature<IAnalogDefinition> CreateNewAnalogDefinitionFunction { get; }
 
-        CreateNewDefinitionFunctionSignature<IDigitalDefinition> CreateNewDigitalDefinitionFunction
-        {
-            get;
-        }
-
+        /// <summary>
+        /// Gets reference to <see cref="CreateNewDefinitionFunctionSignature{T}"/> delegate used to create new <see cref="IDigitalDefinition"/> objects.
+        /// </summary>
+        CreateNewDefinitionFunctionSignature<IDigitalDefinition> CreateNewDigitalDefinitionFunction { get; }
     }
 }
