2350328ba6ef7d6fae48692c8b32418844b7bfc8
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 20:47:25 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IConfigurationCell.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IConfigurationCell.cs
@@ -1,137 +1,117 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IConfigurationCell.vb - Configuration cell interface
+//  IConfigurationCell.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  04/16/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  04/16/2009 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents the protocol independent representation of a set of configuration related data settings (typically related to a PMU).</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent interface representation of any kind of <see cref="IConfigurationFrame"/> cell.
+    /// </summary>
     public interface IConfigurationCell : IChannelCell, IComparable<IConfigurationCell>, IComparable
     {
+        /// <summary>
+        /// Gets a reference to the parent <see cref="IConfigurationFrame"/> for this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        new IConfigurationFrame Parent { get; }
 
+        /// <summary>
+        /// Gets or sets the station name of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        string StationName { get; set; }
 
-        new IConfigurationFrame Parent
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets the binary image of the <see cref="StationName"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        byte[] StationNameImage { get; }
 
-        string StationName
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets the maximum length of the <see cref="StationName"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        int MaximumStationNameLength { get; }
 
-        byte[] StationNameImage
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets or sets the ID label of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        string IDLabel { get; set; }
 
-        int MaximumStationNameLength
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets the binary image of the <see cref="IDLabel"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        byte[] IDLabelImage { get; }
 
-        string IDLabel
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets the length of the <see cref="IDLabel"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        int IDLabelLength { get; }
 
-        byte[] IDLabelImage
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets a reference to the <see cref="PhasorDefinitionCollection"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        PhasorDefinitionCollection PhasorDefinitions { get; }
 
-        int IDLabelLength
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets or sets the <see cref="DataFormat"/> for the <see cref="IPhasorDefinition"/> objects in the <see cref="PhasorDefinitions"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        DataFormat PhasorDataFormat { get; set; }
 
-        PhasorDefinitionCollection PhasorDefinitions
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets or sets the <see cref="CoordinateFormat"/> for the <see cref="IPhasorDefinition"/> objects in the <see cref="PhasorDefinitions"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        CoordinateFormat PhasorCoordinateFormat { get; set; }
 
-        DataFormat PhasorDataFormat
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets or sets the <see cref="IFrequenceyDefinition"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        IFrequencyDefinition FrequencyDefinition { get; set; }
 
-        CoordinateFormat PhasorCoordinateFormat
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets or sets the <see cref="DataFormat"/> of the <see cref="FrequencyDefinition"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        DataFormat FrequencyDataFormat { get; set; }
 
-        IFrequencyDefinition FrequencyDefinition
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets or sets the nominal <see cref="LineFrequency"/> of the <see cref="FrequencyDefinition"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        LineFrequency NominalFrequency { get; set; }
 
-        DataFormat FrequencyDataFormat
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets a reference to the <see cref="AnalogDefinitionCollection"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        AnalogDefinitionCollection AnalogDefinitions { get; }
 
-        LineFrequency NominalFrequency
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets or sets the <see cref="DataFormat"/> for the <see cref="IAnalogDefinition"/> objects in the <see cref="AnalogDefinitions"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        DataFormat AnalogDataFormat { get; set; }
 
-        AnalogDefinitionCollection AnalogDefinitions
-        {
-            get;
-        }
+        /// <summary>
+        /// Gets a reference to the <see cref="DigitalDefinitionCollection"/> of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        DigitalDefinitionCollection DigitalDefinitions { get; }
 
-        DataFormat AnalogDataFormat
-        {
-            get;
-            set;
-        }
+        /// <summary>
+        /// Gets the specified frame rate of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        short FrameRate { get; }
 
-        DigitalDefinitionCollection DigitalDefinitions
-        {
-            get;
-        }
-
-        short FrameRate
-        {
-            get;
-        }
-
-        ushort RevisionCount
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets the revision count of this <see cref="IConfigurationCell"/>.
+        /// </summary>
+        ushort RevisionCount { get; set; }
     }
 }
