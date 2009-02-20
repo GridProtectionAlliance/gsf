3906e433e68878b4417e0bd9df690c8a7e2fa99b
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-20 17:47:57 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ChannelValueCollectionBase.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/ChannelValueCollectionBase.cs
@@ -1,134 +1,121 @@
 //*******************************************************************************************************
-//  ChannelValueCollectionBase.vb - Channel data value collection base class
+//  ChannelValueCollectionBase.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  3/7/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  03/07/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Diagnostics.CodeAnalysis;
-using PCS;
+using System.Linq;
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the common implementation of the protocol independent representation of a collection of any kind of data value.</summary>
-    [CLSCompliant(false), Serializable(), SuppressMessage("Microsoft.Usage", "CA2240")]
+    /// <summary>
+    /// Represents a protocol independent collection of <see cref="IChannelValue{TDefinition}"/> objects.
+    /// </summary>
+    /// <typeparam name="TDefinition">Specific <see cref="IChannelDefinition"/> type that the <see cref="IChannelValue{TDefinition}"/> references.</typeparam>
+    /// <typeparam name="TValue">Specific <see cref="IChannelValue{TDefinition}"/> type that the <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/> contains.</typeparam>
+    [Serializable()]
     public abstract class ChannelValueCollectionBase<TDefinition, TValue> : ChannelCollectionBase<TValue>
         where TDefinition : IChannelDefinition
         where TValue : IChannelValue<TDefinition>
     {
+        #region [ Members ]
 
+        // Fields
         private int m_fixedCount;
         private int m_floatCount;
 
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/>.
+        /// </summary>
         protected ChannelValueCollectionBase()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected ChannelValueCollectionBase(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
+            // Deserialize extra elements
+            m_fixedCount = info.GetInt32("fixedCount");
+            m_floatCount = info.GetInt32("floatCount");
         }
 
-        protected ChannelValueCollectionBase(int maximumCount)
-            : base(maximumCount)
+        /// <summary>
+        /// Creates a new <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/> from specified parameters.
+        /// </summary>
+        /// <param name="lastValidIndex">Last valid index for the collection (i.e., maximum count - 1).</param>
+        /// <remarks>
+        /// <paramref name="lastValidIndex"/> is used instead of maximum count so that maximum type values may
+        /// be specified as needed. For example, if the protocol specifies a collection with a signed 16-bit
+        /// maximum length you can specify <see cref="Int16.MaxValue"/> (i.e., 32,767) as the last valid index
+        /// for the collection since total number of items supported would be 32,768.
+        /// </remarks>
+        protected ChannelValueCollectionBase(int lastValidIndex)
+            : base(lastValidIndex)
         {
-
-
         }
 
-        public virtual new void Add(TValue value)
-        {
+        #endregion
 
-            // In typical usage, all channel values will be of the same data type - but we can't anticipate all
-            // possible uses of collection, so we track totals of each data type so we can quickly ascertain if
-            // all the items in the collection are of the same data type
-            if (value.DataFormat == PhasorProtocols.DataFormat.FixedInteger)
-            {
-                m_fixedCount++;
-            }
-            else
-            {
-                m_floatCount++;
-            }
+        #region [ Properties ]
 
-            base.Add(value);
-
-        }
-
+        /// <summary>
+        /// Gets the length of the <see cref="BinaryImage"/>.
+        /// </summary>
         public override int BinaryLength
         {
             get
             {
-                if (Count > 0)
+                if (m_fixedCount == 0 || m_floatCount == 0)
                 {
-                    if (m_fixedCount == 0 || m_floatCount == 0)
-                    {
-                        // Data types in list are consistent, a simple calculation will derive total binary length
-                        return this[0].BinaryLength * Count;
-                    }
-                    else
-                    {
-                        // List has items of different data types, will have to traverse list to calculate total binary length
-                        int length = 0;
-
-                        for (int x = 0; x <= Count - 1; x++)
-                        {
-                            length += this[x].BinaryLength;
-                        }
-
-                        return length;
-                    }
+                    // Cells will be constant length, so we can quickly calculate lengths
+                    return base.BinaryLength;
                 }
                 else
                 {
-                    return 0;
+                    // Cells will be different lengths, so we must manually sum lengths
+                    return this.Sum(frame => frame.BinaryLength);
                 }
             }
         }
 
+        /// <summary>
+        /// Gets a boolean value indicating if all of the composite values have been assigned a value.
+        /// </summary>
+        /// <returns><c>true</c>, if all composite values have been assigned a value; otherwise, <c>false</c>.</returns>
         public virtual bool AllValuesAssigned
         {
             get
             {
-                bool allAssigned = true;
-
-                for (int x = 0; x <= Count - 1; x++)
-                {
-                    if (this[x].IsEmpty)
-                    {
-                        allAssigned = false;
-                        break;
-                    }
-                }
-
-                return allAssigned;
+                return this.All(item => !item.IsEmpty);
             }
         }
 
-        public virtual new void Clear()
-        {
-
-            base.Clear();
-            m_fixedCount = 0;
-            m_floatCount = 0;
-
-        }
-
+        /// <summary>
+        /// Gets a <see cref="Dictionary{TKey,TValue}"/> of string based property names and values for this <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/> object.
+        /// </summary>
         public override Dictionary<string, string> Attributes
         {
             get
@@ -142,5 +129,95 @@ namespace PCS.PhasorProtocols
                 return baseAttributes;
             }
         }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination <see cref="StreamingContext"/> for this serialization.</param>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            // Serialize extra elements
+            info.AddValue("fixedCount", m_fixedCount);
+            info.AddValue("floatCount", m_floatCount);
+        }
+
+        // In typical usage, all channel values will be of the same data type - but we can't anticipate all
+        // possible uses of collection, so we track totals of each data type so we can quickly ascertain if
+        // all the items in the collection are of the same data type
+
+        /// <summary>
+        /// Inserts an element into the <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/> at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which item should be inserted.</param>
+        /// <param name="item">The object to insert.</param>
+        protected override void InsertItem(int index, TValue item)
+        {
+            if (item.DataFormat == DataFormat.FixedInteger)
+                m_fixedCount++;
+            else
+                m_floatCount++;
+
+            base.InsertItem(index, item);
+        }
+
+        /// <summary>
+        /// Replaces the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to replace.</param>
+        /// <param name="item">The new value for the element at the specified index. The value can be null for reference types.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="index"/> is less than zero.  -or- <paramref name="index"/> is greater than <see cref="Count"/>.
+        /// </exception>
+        protected override void SetItem(int index, TValue item)
+        {
+            if (this[index].DataFormat == DataFormat.FixedInteger)
+                m_fixedCount--;
+            else
+                m_floatCount--;
+
+            if (item.DataFormat == DataFormat.FixedInteger)
+                m_fixedCount++;
+            else
+                m_floatCount++;
+
+            base.SetItem(index, item);
+        }
+
+        /// <summary>
+        /// Removes the element at the specified index of the <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/>.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to remove.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="index"/> is less than zero.  -or- <paramref name="index"/> is equal to or greater than <see cref="Count"/>.
+        /// </exception>
+        protected override void RemoveItem(int index)
+        {
+            if (this[index].DataFormat == DataFormat.FixedInteger)
+                m_fixedCount--;
+            else
+                m_floatCount--;
+
+            base.RemoveItem(index);
+        }
+
+        /// <summary>
+        /// Removes all elements from the <see cref="ChannelValueCollectionBase{TDefinition,TValue}"/>.
+        /// </summary>
+        protected override void ClearItems()
+        {
+            m_fixedCount = 0;
+            m_floatCount = 0;
+
+            base.ClearItems();
+        }
+
+        #endregion
     }
 }
