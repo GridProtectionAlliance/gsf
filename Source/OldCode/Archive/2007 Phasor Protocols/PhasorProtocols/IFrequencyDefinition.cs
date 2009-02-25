6d166d9d0a9d11fdca61c47d1afc696c74128a06
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-25 19:01:28 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IFrequencyDefinition.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IFrequencyDefinition.cs
@@ -1,55 +1,40 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-
 //*******************************************************************************************************
-//  IFrequencyDefinition.vb - Frequency and df/dt value definition interface
+//  IFrequencyDefinition.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This interface represents a protocol independent frequency and df/dt value definition.</summary>
-    [CLSCompliant(false)]
+    /// <summary>
+    /// Represents a protocol independent interface representation of a definition of a <see cref="IFrequencyValue"/>.
+    /// </summary>
     public interface IFrequencyDefinition : IChannelDefinition
     {
+        /// <summary>
+        /// Gets the nominal <see cref="LineFrequency"/> of this <see cref="IFrequencyDefinition"/>.
+        /// </summary>
+        LineFrequency NominalFrequency { get; }
 
+        /// <summary>
+        /// Gets or sets the df/dt offset of this <see cref="IFrequencyDefinition"/>.
+        /// </summary>
+        float DfDtOffset { get; set; }
 
-        LineFrequency NominalFrequency
-        {
-            get;
-        }
-
-        float DfDtOffset
-        {
-            get;
-            set;
-        }
-
-        int DfDtScalingFactor
-        {
-            get;
-            set;
-        }
-
+        /// <summary>
+        /// Gets or sets the df/dt scaling factor of this <see cref="IFrequencyDefinition"/>.
+        /// </summary>
+        int DfDtScalingFactor { get; set; }
     }
 }
