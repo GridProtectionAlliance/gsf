1a283c1fbb10176acc9d42e14bbc6bdc537494ad
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-26 15:08:43 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/FrequencyValueBase.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/FrequencyValueBase.cs
@@ -1,19 +1,19 @@
 //*******************************************************************************************************
-//  FrequencyValueBase.vb - Frequency and DfDt value base class
+//  FrequencyValueBase.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  11/12/2004 - J. Ritchie Carroll
-//       Initial version of source generated
+//  11/12/2004 - James R Carroll
+//       Generated original version of source code.
 //
-//*******************************************************************************************************using System.Diagnostics;
+//*******************************************************************************************************
 
 using System;
 using System.Collections.Generic;
@@ -23,66 +23,68 @@ using PCS.Interop;
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the protocol independent a frequency and dfdt value.</summary>
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents the common implementation of the protocol independent representation of a frequency and df/dt value.
+    /// </summary>
+    [Serializable()]
     public abstract class FrequencyValueBase : ChannelValueBase<IFrequencyDefinition>, IFrequencyValue
     {
-        private float m_frequency;
-        private float m_dfdt;
+        #region [ Members ]
+
+        // Fields
+        private double m_frequency;
+        private double m_dfdt;
         private bool m_frequencyAssigned;
         private bool m_dfdtAssigned;
 
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new <see cref="FrequencyValueBase"/>.
+        /// </summary>
         protected FrequencyValueBase()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="FrequencyValueBase"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected FrequencyValueBase(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
             // Deserialize frequency value
-            m_frequency = info.GetSingle("frequency");
-            m_dfdt = info.GetSingle("dfdt");
+            m_frequency = info.GetDouble("frequency");
+            m_dfdt = info.GetDouble("dfdt");
 
             m_frequencyAssigned = true;
             m_dfdtAssigned = true;
         }
 
-        protected FrequencyValueBase(IDataCell parent)
-            : base(parent)
-        {
-        }
-
-        // Derived classes are expected expose a Public Sub New(ByVal parent As IDataCell, ByVal frequencyDefinition As IFrequencyDefinition, ByVal frequency As Single, ByVal dfdt As Single)
-        protected FrequencyValueBase(IDataCell parent, IFrequencyDefinition frequencyDefinition, float frequency, float dfdt)
+        /// <summary>
+        /// Creates a new <see cref="FrequencyValueBase"/> from the specified parameters.
+        /// </summary>
+        protected FrequencyValueBase(IDataCell parent, IFrequencyDefinition frequencyDefinition, double frequency, double dfdt)
             : base(parent, frequencyDefinition)
         {
             m_frequency = frequency;
             m_dfdt = dfdt;
 
-            m_frequencyAssigned = !float.IsNaN(frequency);
-            m_dfdtAssigned = !float.IsNaN(dfdt);
+            m_frequencyAssigned = !double.IsNaN(frequency);
+            m_dfdtAssigned = !double.IsNaN(dfdt);
         }
 
-        // Derived classes are expected expose a Public Sub New(ByVal parent As IDataCell, ByVal frequencyDefinition As IFrequencyDefinition, ByVal unscaledFrequency As short, ByVal unscaledDfDt As short)
-        protected FrequencyValueBase(IDataCell parent, IFrequencyDefinition frequencyDefinition, short unscaledFrequency, short unscaledDfDt)
-            : this(parent, frequencyDefinition, (float)unscaledFrequency / (float)frequencyDefinition.ScalingFactor + frequencyDefinition.Offset, (float)unscaledDfDt / (float)frequencyDefinition.DfDtScalingFactor + frequencyDefinition.DfDtOffset)
-        {
-        }
+        #endregion
 
-        // Derived classes are expected expose a Public Sub New(ByVal parent As IDataCell, ByVal frequencyDefinition As IFrequencyDefinition, ByVal binaryImage As Byte(), ByVal startIndex As int)
-        protected FrequencyValueBase(IDataCell parent, IFrequencyDefinition frequencyDefinition, byte[] binaryImage, int startIndex)
-            : base(parent, frequencyDefinition)
-        {
-            ParseBinaryImage(null, binaryImage, startIndex);
-        }
+        #region [ Properties ]
 
-        // Derived classes are expected to expose a Public Sub New(ByVal frequencyValue As IFrequencyValue)
-        protected FrequencyValueBase(IDataCell parent, IFrequencyDefinition frequencyDefinition, IFrequencyValue frequencyValue)
-            : this(parent, frequencyDefinition, frequencyValue.Frequency, frequencyValue.DfDt)
-        {
-        }
-
-        public virtual float Frequency
+        /// <summary>
+        /// Gets or sets the floating point value that represents this <see cref="FrequencyValueBase"/>.
+        /// </summary>
+        public virtual double Frequency
         {
             get
             {
@@ -95,7 +97,10 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        public virtual float DfDt
+        /// <summary>
+        /// Gets or sets the floating point value that represents the change in this <see cref="FrequencyValueBase"/> over time.
+        /// </summary>
+        public virtual double DfDt
         {
             get
             {
@@ -108,72 +113,48 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        public virtual short UnscaledFrequency
+        /// <summary>
+        /// Gets or sets the unscaled integer representation of this <see cref="FrequencyValueBase"/>.
+        /// </summary>
+        public virtual int UnscaledFrequency
         {
             get
             {
-                return (short)((m_frequency - Definition.Offset) * (float)Definition.ScalingFactor);
+                unchecked
+                {
+                    return (int)((m_frequency - Definition.Offset) * Definition.ScalingFactor);
+                }
             }
             set
             {
-                m_frequency = (float)value / (float)Definition.ScalingFactor + Definition.Offset;
+                m_frequency = value / (double)Definition.ScalingFactor + Definition.Offset;
                 m_frequencyAssigned = true;
             }
         }
 
-        public virtual short UnscaledDfDt
+        /// <summary>
+        /// Gets or sets the unscaled integer representation of the change in this <see cref="FrequencyValueBase"/> over time.
+        /// </summary>
+        public virtual int UnscaledDfDt
         {
             get
             {
-                return (short)((m_dfdt - Definition.DfDtOffset) * (float)Definition.DfDtScalingFactor);
+                unchecked
+                {
+                    return (int)((m_dfdt - Definition.DfDtOffset) * Definition.DfDtScalingFactor);
+                }
             }
             set
             {
-                m_dfdt = (float)value / (float)Definition.DfDtScalingFactor + Definition.DfDtOffset;
+                m_dfdt = value / (double)Definition.DfDtScalingFactor + Definition.DfDtOffset;
                 m_dfdtAssigned = true;
             }
         }
 
-        public override float this[int index]
-        {
-            get
-            {
-                switch (index)
-                {
-                    case CompositeFrequencyValue.Frequency:
-                        return m_frequency;
-                    case CompositeFrequencyValue.DfDt:
-                        return m_dfdt;
-                    default:
-                        throw (new IndexOutOfRangeException("Specified frequency value composite index, " + index + ", is out of range - there are only two composite values for a frequency value: frequency (0) and df/dt (1)"));
-                }
-            }
-            set
-            {
-                switch (index)
-                {
-                    case CompositeFrequencyValue.Frequency:
-                        m_frequency = value;
-                        m_frequencyAssigned = true;
-                        break;
-                    case CompositeFrequencyValue.DfDt:
-                        m_dfdt = value;
-                        m_dfdtAssigned = true;
-                        break;
-                    default:
-                        throw (new IndexOutOfRangeException("Specified frequency value composite index, " + index + ", is out of range - there are only two composite values for a frequency value: frequency (0) and df/dt (1)"));
-                }
-            }
-        }
-
-        public override int CompositeValueCount
-        {
-            get
-            {
-                return 2;
-            }
-        }
-
+        /// <summary>
+        /// Gets boolean value that determines if none of the composite values of <see cref="FrequencyValueBase"/> have been assigned a value.
+        /// </summary>
+        /// <returns>True, if no composite values have been assigned a value; otherwise, false.</returns>
         public override bool IsEmpty
         {
             get
@@ -182,44 +163,118 @@ namespace PCS.PhasorProtocols
             }
         }
 
+        /// <summary>
+        /// Gets the composite values of this <see cref="FrequencyValueBase"/>.
+        /// </summary>
+        public override double[] CompositeValues
+        {
+            get
+            {
+                return new double[] { m_frequency, m_dfdt };
+            }
+        }
+
+        /// <summary>
+        /// Gets the length of the <see cref="BodyImage"/>.
+        /// </summary>
+        /// <remarks>
+        /// The base implementation assumes fixed integer values are represented as 16-bit signed
+        /// integers and floating point values are represented as 32-bit single-precision floating-point
+        /// values (i.e., short and float data types respectively).
+        /// </remarks>
         protected override int BodyLength
         {
             get
             {
                 if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
-                {
                     return 4;
-                }
                 else
-                {
                     return 8;
-                }
             }
         }
 
+        /// <summary>
+        /// Gets the binary body image of the <see cref="FrequencyValueBase"/> object.
+        /// </summary>
+        /// <remarks>
+        /// The base implementation assumes fixed integer values are represented as 16-bit signed
+        /// integers and floating point values are represented as 32-bit single-precision floating-point
+        /// values (i.e., short and float data types respectively).
+        /// </remarks>
         protected override byte[] BodyImage
         {
             get
             {
                 byte[] buffer = new byte[BodyLength];
 
-                if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
+                // Had to make a descision on usage versus typical protocol implementation when
+                // exposing values as double / int when protocols typically use float / short for
+                // transmission. Exposing values as double / int makes class more versatile by
+                // allowing future protocol implementations to support higher resolution values
+                // simply by overriding BodyLength, BodyImage and ParseBodyImage. Exposing class
+                // values as double / int runs the risk of providing values that are outside the
+                // data type limitations, hence the unchecked section below. However, risk should
+                // be low in typical usage scenarios since values being transmitted via a generated
+                // image were likely parsed previously from a binary image with the same constraints.
+                unchecked
                 {
-                    EndianOrder.BigEndian.CopyBytes(UnscaledFrequency, buffer, 0);
-                    EndianOrder.BigEndian.CopyBytes(UnscaledDfDt, buffer, 2);
-                }
-                else
-                {
-                    EndianOrder.BigEndian.CopyBytes(m_frequency, buffer, 0);
-                    EndianOrder.BigEndian.CopyBytes(m_dfdt, buffer, 4);
+                    if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
+                    {
+                        EndianOrder.BigEndian.CopyBytes((short)UnscaledFrequency, buffer, 0);
+                        EndianOrder.BigEndian.CopyBytes((short)UnscaledDfDt, buffer, 2);
+                    }
+                    else
+                    {
+                        EndianOrder.BigEndian.CopyBytes((float)m_frequency, buffer, 0);
+                        EndianOrder.BigEndian.CopyBytes((float)m_dfdt, buffer, 4);
+                    }
                 }
 
                 return buffer;
             }
         }
 
+        /// <summary>
+        /// <see cref="Dictionary{TKey,TValue}"/> of string based property names and values for the <see cref="FrequencyValueBase"/> object.
+        /// </summary>
+        public override Dictionary<string, string> Attributes
+        {
+            get
+            {
+                Dictionary<string, string> baseAttributes = base.Attributes;
+
+                baseAttributes.Add("Frequency Value", Frequency.ToString());
+                baseAttributes.Add("df/dt Value", DfDt.ToString());
+                baseAttributes.Add("Unscaled Frequency Value", UnscaledFrequency.ToString());
+                baseAttributes.Add("Unscaled df/dt Value", UnscaledDfDt.ToString());
+
+                return baseAttributes;
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Parses the binary body image.
+        /// </summary>
+        /// <param name="binaryImage">Binary image to parse.</param>
+        /// <param name="startIndex">Start index into <paramref name="binaryImage"/> to begin parsing.</param>
+        /// <param name="length">Length of valid data within <paramref name="binaryImage"/>.</param>
+        /// <returns>The length of the data that was parsed.</returns>
+        /// <remarks>
+        /// The base implementation assumes fixed integer values are represented as 16-bit signed
+        /// integers and floating point values are represented as 32-bit single-precision floating-point
+        /// values (i.e., short and float data types respectively).
+        /// </remarks>
         protected override void ParseBodyImage(IChannelParsingState state, byte[] binaryImage, int startIndex)
         {
+            // TODO: It is expected that parent IDataCell will validate that it has
+            // enough length to parse entire cell well in advance so that low level
+            // parsing routines do not have to re-validate that enough length is
+            // available to parse needed information as an optimization...
+
             if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
             {
                 UnscaledFrequency = EndianOrder.BigEndian.ToInt16(binaryImage, startIndex);
@@ -235,6 +290,11 @@ namespace PCS.PhasorProtocols
             }
         }
 
+        /// <summary>
+        /// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination <see cref="StreamingContext"/> for this serialization.</param>
         public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
         {
             base.GetObjectData(info, context);
@@ -244,19 +304,6 @@ namespace PCS.PhasorProtocols
             info.AddValue("dfdt", m_dfdt);
         }
 
-        public override Dictionary<string, string> Attributes
-        {
-            get
-            {
-                Dictionary<string, string> baseAttributes = base.Attributes;
-
-                baseAttributes.Add("Frequency Value", Frequency.ToString());
-                baseAttributes.Add("df/dt Value", DfDt.ToString());
-                baseAttributes.Add("Unscaled Frequency Value", UnscaledFrequency.ToString());
-                baseAttributes.Add("Unscaled df/dt Value", UnscaledDfDt.ToString());
-
-                return baseAttributes;
-            }
-        }
+        #endregion
     }
 }
