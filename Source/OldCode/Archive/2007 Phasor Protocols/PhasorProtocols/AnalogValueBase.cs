8801517bdfe6ecee2c82534128f79e44c1b2365a
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-26 14:11:00 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/AnalogValueBase.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/AnalogValueBase.cs
@@ -1,103 +1,80 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  AnalogValueBase.vb - Analog value base class
+//  AnalogValueBase.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the common implementation of the protocol independent representation of an analog value.</summary>
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents the common implementation of the protocol independent representation of an analog value.
+    /// </summary>
+    [Serializable()]
     public abstract class AnalogValueBase : ChannelValueBase<IAnalogDefinition>, IAnalogValue
     {
+        #region [ Members ]
 
-
-
-        private float m_value;
+        // Fields
+        private double m_value;
         private bool m_valueAssigned;
 
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new <see cref="AnalogValueBase"/>.
+        /// </summary>
         protected AnalogValueBase()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="AnalogValueBase"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected AnalogValueBase(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
             // Deserialize analog value
-            m_value = info.GetSingle("value");
+            m_value = info.GetDouble("value");
             m_valueAssigned = true;
-
         }
 
-        protected AnalogValueBase(IDataCell parent)
-            : base(parent)
-        {
-
-
-        }
-
-        // Derived classes are expected expose a Public Sub New(ByVal parent As IDataCell, ByVal analogDefinition As IAnalogDefinition, ByVal value As Single)
-        protected AnalogValueBase(IDataCell parent, IAnalogDefinition analogDefinition, float value)
+        /// <summary>
+        /// Creates a new <see cref="AnalogValueBase"/> from the specified parameters.
+        /// </summary>
+        protected AnalogValueBase(IDataCell parent, IAnalogDefinition analogDefinition, double value)
             : base(parent, analogDefinition)
         {
-
-
             m_value = value;
-            m_valueAssigned = !float.IsNaN(value);
-
+            m_valueAssigned = !double.IsNaN(value);
         }
 
-        // Derived classes are expected expose a Public Sub New(ByVal parent As IDataCell, ByVal analogDefinition As IAnalogDefinition, ByVal unscaledValue As short)
-        protected AnalogValueBase(IDataCell parent, IAnalogDefinition analogDefinition, short integerValue)
-            : this(parent, analogDefinition, (float)integerValue)
-        {
+        #endregion
 
+        #region [ Properties ]
 
-        }
-
-        // Derived classes are expected expose a Public Sub New(ByVal parent As IDataCell, ByVal analogDefinition As IAnalogDefinition, ByVal binaryImage As Byte(), ByVal startIndex As int)
-        protected AnalogValueBase(IDataCell parent, IAnalogDefinition analogDefinition, byte[] binaryImage, int startIndex)
-            : base(parent, analogDefinition)
-        {
-
-            ParseBinaryImage(null, binaryImage, startIndex);
-
-        }
-
-        // Derived classes are expected expose a Public Sub New(ByVal analogValue As IAnalogValue)
-        protected AnalogValueBase(IDataCell parent, IAnalogDefinition analogDefinition, IAnalogValue analogValue)
-            : this(parent, analogDefinition, analogValue.Value)
-        {
-
-
-        }
-
-        public virtual float Value
+        /// <summary>
+        /// Gets or sets the floating point value that represents this <see cref="AnalogValueBase"/>.
+        /// </summary>
+        public virtual double Value
         {
             get
             {
@@ -110,31 +87,17 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        public virtual short IntegerValue
+        /// <summary>
+        /// Gets or sets the integer representation of this <see cref="AnalogValueBase"/> value.
+        /// </summary>
+        public virtual int IntegerValue
         {
             get
             {
-                try
+                unchecked
                 {
-                    return (short)m_value;
+                    return (int)m_value;
                 }
-                catch (OverflowException)
-                {
-                    return short.MinValue;
-                }
-            }
-            set
-            {
-                m_value = (float)value;
-                m_valueAssigned = true;
-            }
-        }
-
-        public override float this[int index]
-        {
-            get
-            {
-                return m_value;
             }
             set
             {
@@ -143,14 +106,10 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        public override int CompositeValueCount
-        {
-            get
-            {
-                return 1;
-            }
-        }
-
+        /// <summary>
+        /// Gets boolean value that determines if none of the composite values of <see cref="AnalogValueBase"/> have been assigned a value.
+        /// </summary>
+        /// <returns>True, if no composite values have been assigned a value; otherwise, false.</returns>
         public override bool IsEmpty
         {
             get
@@ -159,77 +118,136 @@ namespace PCS.PhasorProtocols
             }
         }
 
+        /// <summary>
+        /// Gets the composite values of this <see cref="AnalogValueBase"/>.
+        /// </summary>
+        public override double[] CompositeValues
+        {
+            get
+            {
+                return new double[] { m_value };
+            }
+        }
+
+        /// <summary>
+        /// Gets the length of the <see cref="BodyImage"/>.
+        /// </summary>
+        /// <remarks>
+        /// The base implementation assumes fixed integer values are represented as 16-bit signed
+        /// integers and floating point values are represented as 32-bit single-precision floating-point
+        /// values (i.e., short and float data types respectively).
+        /// </remarks>
         protected override int BodyLength
         {
             get
             {
                 if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
-                {
                     return 2;
-                }
                 else
-                {
                     return 4;
-                }
             }
         }
 
+        /// <summary>
+        /// Gets the binary body image of the <see cref="AnalogValueBase"/> object.
+        /// </summary>
+        /// <remarks>
+        /// The base implementation assumes fixed integer values are represented as 16-bit signed
+        /// integers and floating point values are represented as 32-bit single-precision floating-point
+        /// values (i.e., short and float data types respectively).
+        /// </remarks>
         protected override byte[] BodyImage
         {
             get
             {
                 byte[] buffer = new byte[BodyLength];
 
-                if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
+                // Had to make a descision on usage versus typical protocol implementation when
+                // exposing values as double / int when protocols typically use float / short for
+                // transmission. Exposing values as double / int makes class more versatile by
+                // allowing future protocol implementations to support higher resolution values
+                // simply by overriding BodyLength, BodyImage and ParseBodyImage. Exposing class
+                // values as double / int runs the risk of providing values that are outside the
+                // data type limitations, hence the unchecked section below. However, risk should
+                // be low in typical usage scenarios since values being transmitted via a generated
+                // image were likely parsed previously from a binary image with the same constraints.
+                unchecked
                 {
-                    EndianOrder.BigEndian.CopyBytes(IntegerValue, buffer, 0);
-                }
-                else
-                {
-                    EndianOrder.BigEndian.CopyBytes(m_value, buffer, 0);
+                    if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
+                        EndianOrder.BigEndian.CopyBytes((short)m_value, buffer, 0);
+                    else
+                        EndianOrder.BigEndian.CopyBytes((float)m_value, buffer, 0);
                 }
 
                 return buffer;
             }
         }
 
-        protected override void ParseBodyImage(IChannelParsingState state, byte[] binaryImage, int startIndex)
-        {
-
-            if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
-            {
-                IntegerValue = EndianOrder.BigEndian.ToInt16(binaryImage, startIndex);
-            }
-            else
-            {
-                m_value = EndianOrder.BigEndian.ToSingle(binaryImage, startIndex);
-                m_valueAssigned = true;
-            }
-
-        }
-
-        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
-        {
-
-            base.GetObjectData(info, context);
-
-            // Serialize analog value
-            info.AddValue("value", m_value);
-
-        }
-
+        /// <summary>
+        /// <see cref="Dictionary{TKey,TValue}"/> of string based property names and values for the <see cref="ChannelValueBase{T}"/> object.
+        /// </summary>
         public override Dictionary<string, string> Attributes
         {
             get
             {
                 Dictionary<string, string> baseAttributes = base.Attributes;
 
-                baseAttributes.Add("Analog Value (Floating Point)", Value.ToString());
-                baseAttributes.Add("Analog Value (Integer)", IntegerValue.ToString());
+                baseAttributes.Add("Analog Value", Value.ToString());
 
                 return baseAttributes;
             }
         }
 
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Parses the binary body image.
+        /// </summary>
+        /// <param name="binaryImage">Binary image to parse.</param>
+        /// <param name="startIndex">Start index into <paramref name="binaryImage"/> to begin parsing.</param>
+        /// <param name="length">Length of valid data within <paramref name="binaryImage"/>.</param>
+        /// <returns>The length of the data that was parsed.</returns>
+        /// <remarks>
+        /// The base implementation assumes fixed integer values are represented as 16-bit signed
+        /// integers and floating point values are represented as 32-bit single-precision floating-point
+        /// values (i.e., short and float data types respectively).
+        /// </remarks>
+        protected override int ParseBodyImage(byte[] binaryImage, int startIndex, int length)
+        {
+            // TODO: It is expected that parent IDataCell will validate that it has
+            // enough length to parse entire cell well in advance so that low level
+            // parsing routines do not have to re-validate that enough length is
+            // available to parse needed information as an optimization...
+
+            if (DataFormat == PhasorProtocols.DataFormat.FixedInteger)
+            {
+                m_value = EndianOrder.BigEndian.ToInt16(binaryImage, startIndex);
+                m_valueAssigned = true;
+                return 2;
+            }
+            else
+            {
+                m_value = EndianOrder.BigEndian.ToSingle(binaryImage, startIndex);
+                m_valueAssigned = true;
+                return 4;
+            }
+        }
+
+        /// <summary>
+        /// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination <see cref="StreamingContext"/> for this serialization.</param>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            // Serialize analog value
+            info.AddValue("value", m_value);
+        }
+
+        #endregion
     }
 }
