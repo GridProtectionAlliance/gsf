80995d5c7bead1d423cf4225d10efc9e1f5645d7
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-02-27 15:54:14 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/PhasorDefinitionBase.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/PhasorDefinitionBase.cs
@@ -1,103 +1,83 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  PhasorDefinitionBase.vb - Phasor value definition base class
+//  PhasorDefinitionBase.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  02/18/2005 - J. Ritchie Carroll
-//       Initial version of source generated
+//  02/18/2005 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace PCS.PhasorProtocols
 {
-    /// <summary>This class represents the common implementation of the protocol independent definition of a phasor value.</summary>
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents the common implementation of the protocol independent definition of a <see cref="IPhasorValue"/>.
+    /// </summary>
+    [Serializable()]
     public abstract class PhasorDefinitionBase : ChannelDefinitionBase, IPhasorDefinition
     {
+        #region [ Members ]
 
-
-
+        // Fields
         private PhasorType m_type;
         private IPhasorDefinition m_voltageReference;
 
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinitionBase"/>.
+        /// </summary>
         protected PhasorDefinitionBase()
         {
         }
 
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinitionBase"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected PhasorDefinitionBase(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
             // Deserialize phasor definition
             m_type = (PhasorType)info.GetValue("type", typeof(PhasorType));
             m_voltageReference = (IPhasorDefinition)info.GetValue("voltageReference", typeof(IPhasorDefinition));
-
         }
 
-        protected PhasorDefinitionBase(IConfigurationCell parent)
-            : base(parent)
-        {
-
-
-            m_type = PhasorType.Voltage;
-            m_voltageReference = this;
-
-        }
-
-        protected PhasorDefinitionBase(IConfigurationCell parent, int index, string label, int scale, float offset, PhasorType type, IPhasorDefinition voltageReference)
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinitionBase"/> using the specified parameters.
+        /// </summary>
+        protected PhasorDefinitionBase(IConfigurationCell parent, int index, string label, uint scale, double offset, PhasorType type, IPhasorDefinition voltageReference)
             : base(parent, index, label, scale, offset)
         {
-
-
             m_type = type;
 
             if (type == PhasorType.Voltage)
-            {
                 m_voltageReference = this;
-            }
             else
-            {
                 m_voltageReference = voltageReference;
-            }
-
         }
 
-        protected PhasorDefinitionBase(IConfigurationCell parent, byte[] binaryImage, int startIndex)
-            : base(parent, binaryImage, startIndex)
-        {
+        #endregion
 
+        #region [ Properties ]
 
-        }
-
-        // Derived classes are expected to expose a Public Sub New(ByVal phasorDefinition As IPhasorDefinition)
-        protected PhasorDefinitionBase(IConfigurationCell parent, IPhasorDefinition phasorDefinition)
-            : this(parent, phasorDefinition.Index, phasorDefinition.Label, phasorDefinition.ScalingFactor, phasorDefinition.Offset, phasorDefinition.Type, phasorDefinition.VoltageReference)
-        {
-
-
-        }
-
+        /// <summary>
+        /// Gets the <see cref="PhasorProtocols.DataFormat"/> of this <see cref="PhasorDefinitionBase"/>.
+        /// </summary>
         public override DataFormat DataFormat
         {
             get
@@ -106,14 +86,9 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        CoordinateFormat IPhasorDefinition.CoordinateFormat
-        {
-            get
-            {
-                return this.CoordinateFormat;
-            }
-        }
-
+        /// <summary>
+        /// Gets or sets the <see cref="PhasorProtocols.CoordinateFormat"/> of this <see cref="PhasorDefinitionBase"/>.
+        /// </summary>
         public virtual CoordinateFormat CoordinateFormat
         {
             get
@@ -122,18 +97,9 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        //PhasorType IPhasorDefinition.Type
-        //{
-        //    get
-        //    {
-        //        return this.Type;
-        //    }
-        //    set
-        //    {
-        //        this.Type = value;
-        //    }
-        //}
-
+        /// <summary>
+        /// Gets or sets the <see cref="PhasorType"/> of this <see cref="PhasorDefinitionBase"/>.
+        /// </summary>
         public virtual PhasorType Type
         {
             get
@@ -146,18 +112,12 @@ namespace PCS.PhasorProtocols
             }
         }
 
-        //IPhasorDefinition IPhasorDefinition.VoltageReference
-        //{
-        //    get
-        //    {
-        //        return this.VoltageReference;
-        //    }
-        //    set
-        //    {
-        //        this.VoltageReference = value;
-        //    }
-        //}
-
+        /// <summary>
+        /// Gets or sets the associated <see cref="IPhasorDefinition"/> that represents the voltage reference (if any).
+        /// </summary>
+        /// <remarks>
+        /// This only applies to current phasors.
+        /// </remarks>
         public virtual IPhasorDefinition VoltageReference
         {
             get
@@ -167,30 +127,15 @@ namespace PCS.PhasorProtocols
             set
             {
                 if (m_type == PhasorType.Voltage)
-                {
-                    if (value != this)
-                    {
-                        throw (new NotImplementedException("Voltage phasors do not have a voltage reference"));
-                    }
-                }
+                    m_voltageReference = this;
                 else
-                {
                     m_voltageReference = value;
-                }
             }
         }
 
-        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
-        {
-
-            base.GetObjectData(info, context);
-
-            // Serialize phasor definition
-            info.AddValue("type", m_type, typeof(PhasorType));
-            info.AddValue("voltageReference", m_voltageReference, typeof(IPhasorDefinition));
-
-        }
-
+        /// <summary>
+        /// Gets a <see cref="Dictionary{TKey,TValue}"/> of string based property names and values for this <see cref="PhasorDefinitionBase"/> object.
+        /// </summary>
         public override Dictionary<string, string> Attributes
         {
             get
@@ -203,5 +148,24 @@ namespace PCS.PhasorProtocols
             }
         }
 
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination <see cref="StreamingContext"/> for this serialization.</param>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            // Serialize phasor definition
+            info.AddValue("type", m_type, typeof(PhasorType));
+            info.AddValue("voltageReference", m_voltageReference, typeof(IPhasorDefinition));
+        }
+
+        #endregion
     }
 }
