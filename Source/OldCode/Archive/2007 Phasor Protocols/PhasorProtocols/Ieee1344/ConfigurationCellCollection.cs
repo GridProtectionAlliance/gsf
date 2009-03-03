fc636efb8d1482c715c94ef615cf57c676ebec67
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-03-03 16:23:58 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/Ieee1344/ConfigurationCellCollection.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/Ieee1344/ConfigurationCellCollection.cs
@@ -1,70 +1,72 @@
-using System.Diagnostics;
-using System;
-//using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-//using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  ConfigurationCellCollection.vb - IEEE 1344 specific configuration cell collection
+//  ConfigurationCellCollection.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  11/12/2004 - J. Ritchie Carroll
-//       Initial version of source generated
+//  11/12/2004 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
 
 namespace PCS.PhasorProtocols.Ieee1344
 {
-
-    [CLSCompliant(false), Serializable()]
+    /// <summary>
+    /// Represents a IEEE 1344 implementation of a collection of <see cref="IConfigurationCell"/> objects.
+    /// </summary>
+    [Serializable()]
     public class ConfigurationCellCollection : PhasorProtocols.ConfigurationCellCollection
     {
+        #region [ Constructors ]
 
-
-
-        public ConfigurationCellCollection()
-            : base(1, true)
-        {
-
-            // IEEE 1344 only supports a single PMU - so there should only be one cell - since there's only one cell, cell lengths will be constant :)
-
-        }
-
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationCellCollection"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
         protected ConfigurationCellCollection(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
-
         }
 
-        public void Add(ConfigurationCell value)
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationCellCollection"/>.
+        /// </summary>
+        public ConfigurationCellCollection()
+            : base(0, true)
         {
-
-            base.Add(value);
-
+            // IEEE 1344 only supports a single PMU - so there should only be one cell - since there's only one cell, cell lengths will be constant :)
         }
 
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets or sets <see cref="ConfigurationCell"/> at specified <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">Index of value to get or set.</param>
+        /// <returns><see cref="DataCell"/> at specified <paramref name="index"/>.</returns>
         public new ConfigurationCell this[int index]
         {
             get
             {
-                return (ConfigurationCell)base[index];
+                return base[index] as ConfigurationCell;
+            }
+            set
+            {
+                base[index] = value;
             }
         }
+
+        #endregion
     }
 }
