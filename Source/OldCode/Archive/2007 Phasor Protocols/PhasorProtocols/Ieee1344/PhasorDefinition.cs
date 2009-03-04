0330642e29445ac42c77a5b018a95fa5fa79b9b4
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-03-04 18:11:14 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/Ieee1344/PhasorDefinition.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/Ieee1344/PhasorDefinition.cs
@@ -1,144 +1,132 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  PhasorDefinition.vb - Phasor definition
+//  PhasorDefinition.cs
 //  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  11/12/2004 - J. Ritchie Carroll
-//       Initial version of source generated
+//  11/12/2004 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
-namespace PCS.PhasorProtocols
+namespace PCS.PhasorProtocols.Ieee1344
 {
-    namespace Ieee1344
+    /// <summary>
+    /// Represents the IEEE 1344 implementation of a <see cref="IPhasorDefinition"/>.
+    /// </summary>
+    [Serializable()]
+    public class PhasorDefinition : PhasorDefinitionBase
     {
+        #region [ Constructors ]
 
-        [CLSCompliant(false), Serializable()]
-        public class PhasorDefinition : PhasorDefinitionBase
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinition"/>.
+        /// </summary>
+        protected PhasorDefinition()
         {
-
-
-
-            protected PhasorDefinition()
-            {
-            }
-
-            protected PhasorDefinition(SerializationInfo info, StreamingContext context)
-                : base(info, context)
-            {
-
-
-            }
-
-            public PhasorDefinition(ConfigurationCell parent)
-                : base(parent)
-            {
-
-
-            }
-
-            public PhasorDefinition(ConfigurationCell parent, int index, string label, int scale, float offset, PhasorType type, PhasorDefinition voltageReference)
-                : base(parent, index, label, scale, offset, type, voltageReference)
-            {
-
-
-            }
-
-            public PhasorDefinition(ConfigurationCell parent, byte[] binaryImage, int startIndex)
-                : base(parent, binaryImage, startIndex)
-            {
-
-
-            }
-
-            public PhasorDefinition(ConfigurationCell parent, IPhasorDefinition phasorDefinition)
-                : base(parent, phasorDefinition)
-            {
-
-
-            }
-
-            internal static IPhasorDefinition CreateNewPhasorDefinition(IConfigurationCell parent, byte[] binaryImage, int startIndex)
-            {
-
-                return new PhasorDefinition((ConfigurationCell)parent, binaryImage, startIndex);
-
-            }
-
-            public override System.Type DerivedType
-            {
-                get
-                {
-                    return this.GetType();
-                }
-            }
-
-            public new ConfigurationCell Parent
-            {
-                get
-                {
-                    return (ConfigurationCell)base.Parent;
-                }
-            }
-
-            internal static int ConversionFactorLength
-            {
-                get
-                {
-                    return 4;
-                }
-            }
-
-            internal byte[] ConversionFactorImage
-            {
-                get
-                {
-                    byte[] buffer = new byte[ConversionFactorLength];
-
-                    buffer[0] = (byte)(Type == PhasorType.Voltage ? 0 : 1);
-
-                    EndianOrder.BigEndian.CopyBuffer(BitConverter.GetBytes(ScalingFactor), 0, buffer, 1, 3);
-
-                    return buffer;
-                }
-            }
-
-            internal void ParseConversionFactor(byte[] binaryImage, int startIndex)
-            {
-
-                byte[] buffer = new byte[4];
-
-                // Get phasor type from first byte
-                Type = (binaryImage[startIndex] == 0) ? PhasorType.Voltage : PhasorType.Current;
-
-                // Last three bytes represent scaling factor
-                EndianOrder.BigEndian.CopyBuffer(binaryImage, startIndex + 1, buffer, 0, 3);
-                ScalingFactor = BitConverter.ToInt32(buffer, 0);
-
-            }
-
         }
 
-    }
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinition"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
+        protected PhasorDefinition(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
 
+        /// <summary>
+        /// Creates a new <see cref="PhasorDefinition"/> using the specified parameters.
+        /// </summary>
+        /// <param name="parent">The <see cref="IConfigurationCell"/> parent of this <see cref="PhasorDefinition"/>.</param>
+        /// <param name="label">The label of this <see cref="PhasorDefinition"/>.</param>
+        /// <param name="scale">The integer scaling value of this <see cref="PhasorDefinition"/>.</param>
+        /// <param name="offset">The offset of this <see cref="PhasorDefinition"/>.</param>
+        /// <param name="type">The <see cref="PhasorType"/> of this <see cref="PhasorDefinition"/>.</param>
+        /// <param name="voltageReference">The associated <see cref="IPhasorDefinition"/> that represents the voltage reference (if any).</param>
+        public PhasorDefinition(ConfigurationCell parent, string label, uint scale, float offset, PhasorType type, PhasorDefinition voltageReference)
+            : base(parent, label, scale, offset, type, voltageReference)
+        {
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets conversion factor image of this <see cref="PhasorDefinition"/>.
+        /// </summary>
+        internal byte[] ConversionFactorImage
+        {
+            get
+            {
+                byte[] buffer = new byte[ConversionFactorLength];
+                UInt24 scalingFactor = (ScalingValue > UInt24.MaxValue ? UInt24.MaxValue : (UInt24)ScalingValue);
+
+                buffer[0] = (byte)(Type == PhasorType.Voltage ? 0 : 1);
+
+                EndianOrder.BigEndian.CopyBytes(scalingFactor, buffer, 1);
+
+                return buffer;
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Parses conversion factor image from the specified <paramref name="binaryImage"/>.
+        /// </summary>
+        /// <param name="binaryImage">Binary image to parse.</param>
+        /// <param name="startIndex">Start index into <paramref name="binaryImage"/> to begin parsing.</param>
+        internal void ParseConversionFactor(byte[] binaryImage, int startIndex)
+        {
+            // Get phasor type from first byte
+            Type = (binaryImage[startIndex] == 0) ? PhasorType.Voltage : PhasorType.Current;
+
+            // Last three bytes represent scaling factor
+            ScalingValue = EndianOrder.BigEndian.ToUInt24(binaryImage, startIndex + 1);
+        }
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Properties
+
+        // Gets length of conversion factor
+        internal static int ConversionFactorLength
+        {
+            get
+            {
+                return 4;
+            }
+        }
+
+        // Static Methods
+
+        // Delegate handler to create a new IEEE 1344 phasor value
+        internal static IPhasorDefinition CreateNewDigitalDefinition(IConfigurationCell parent, byte[] binaryImage, int startIndex)
+        {
+            IPhasorDefinition phasorDefinition = new PhasorDefinition() { Parent = parent };
+
+            phasorDefinition.Initialize(binaryImage, startIndex, 0);
+
+            return phasorDefinition;
+        }
+
+        #endregion
+    }
 }
