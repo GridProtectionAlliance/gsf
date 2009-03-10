587bf6d55b4d0a15456cad12375b98f7bfa8e3fa
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-03-10 16:00:27 +0000

    Vss2Git

--- Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Parsing/ISupportFrameImage.cs
+++ Source/OldCode/Archive/2008 TVA Code Library/Source/PCS.Core/Parsing/ISupportFrameImage.cs
@@ -1,5 +1,5 @@
 //*******************************************************************************************************
-//  IBinaryDataConsumer.cs
+//  ISupportFrameImage.cs
 //  Copyright © 2008 - TVA, all rights reserved - Gbtc
 //
 //  Build Environment: C#, Visual Studio 2008
