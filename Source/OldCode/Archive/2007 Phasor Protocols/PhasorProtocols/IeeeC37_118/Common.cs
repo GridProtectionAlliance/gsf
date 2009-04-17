0d01a58bdfbf9575f9592c43b01133db5f149eea
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-04-17 16:47:35 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IeeeC37_118/Common.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IeeeC37_118/Common.cs
@@ -25,36 +25,36 @@ namespace PCS.PhasorProtocols.IeeeC37_118
     /// IEEE C37.118 frame types enumeration.
     /// </summary>
     [Serializable()]
-    public enum FrameType : short
+    public enum FrameType : ushort
     {
         /// <summary>
         /// 000 Data frame.
         /// </summary>
-        DataFrame = Bit.Nill,
+        DataFrame = (ushort)Bits.Nil,
         /// <summary>
         /// 001 Header frame.
         /// </summary>
-        HeaderFrame = Bit.Bit4,
+        HeaderFrame = (ushort)Bits.Bit04,
         /// <summary>
         /// 010 Configuration frame 1.
         /// </summary>
-        ConfigurationFrame1 = Bit.Bit5,
+        ConfigurationFrame1 = (ushort)Bits.Bit05,
         /// <summary>
         /// 011 Configuration frame 2.
         /// </summary>
-        ConfigurationFrame2 = Bit.Bit4 | Bit.Bit5,
+        ConfigurationFrame2 = (ushort)(Bits.Bit04 | Bits.Bit05),
         /// <summary>
         /// 100 Command frame.
         /// </summary>
-        CommandFrame = Bit.Bit6,
+        CommandFrame = (ushort)Bits.Bit06,
         /// <summary>.
         /// Reserved bit.
         /// </summary>
-        Reserved = Bit.Bit7,
+        Reserved = (ushort)Bits.Bit07,
         /// <summary>
         /// Version number mask.
         /// </summary>
-        VersionNumberMask = Bit.Bit0 | Bit.Bit1 | Bit.Bit2 | Bit.Bit3
+        VersionNumberMask = (ushort)(Bits.Bit00 | Bits.Bit01 | Bits.Bit02 | Bits.Bit03)
     }
 
     /// <summary>
@@ -77,112 +77,112 @@ namespace PCS.PhasorProtocols.IeeeC37_118
     /// Data format flags enumeration.
     /// </summary>
     [Flags(), Serializable()]
-    public enum FormatFlags : short
+    public enum FormatFlags : ushort
     {
         /// <summary>
         /// Frequency value format (Set = float, Clear = integer).
         /// </summary>
-        Frequency = Bit.Bit3,
+        Frequency = (ushort)Bits.Bit03,
         /// <summary>
         /// Analog value format (Set = float, Clear = integer).
         /// </summary>
-        Analog = Bit.Bit2,
+        Analog = (ushort)Bits.Bit02,
         /// <summary>
         /// Phasor value format (Set = float, Clear = integer).
         /// </summary>
-        Phasors = Bit.Bit1,
+        Phasors = (ushort)Bits.Bit01,
         /// <summary>
         /// Phasor coordinate format (Set = polar, Clear = rectangular).
         /// </summary>
-        Coordinates = Bit.Bit0,
+        Coordinates = (ushort)Bits.Bit00,
         /// <summary>
         /// Unsed format bits mask.
         /// </summary>
-        UnusedMask = short.MaxValue & ~(Bit.Bit0 | Bit.Bit1 | Bit.Bit2 | Bit.Bit3)
+        UnusedMask = unchecked(ushort.MaxValue & (ushort)~(Bits.Bit00 | Bits.Bit01 | Bits.Bit02 | Bits.Bit03))
     }
 
     /// <summary>
     /// Time quality flags enumeration.
     /// </summary>
     [Flags(), Serializable()]
-    public enum TimeQualityFlags : int
+    public enum TimeQualityFlags : uint
     {
         /// <summary>
         /// Reserved bit.
         /// </summary>
-        Reserved = Bit.Bit31,
+        Reserved = (uint)Bits.Bit31,
         /// <summary>
         /// Leap second direction – 0 for add, 1 for delete.
         /// </summary>
-        LeapSecondDirection = Bit.Bit30,
+        LeapSecondDirection = (uint)Bits.Bit30,
         /// <summary>
         /// Leap second occurred – set in the first second after the leap second occurs and remains set for 24 hours.
         /// </summary>
-        LeapSecondOccurred = Bit.Bit29,
+        LeapSecondOccurred = (uint)Bits.Bit29,
         /// <summary>
         /// Leap second pending – set before a leap second occurs and cleared in the second after the leap second occurs.
         /// </summary>
-        LeapSecondPending = Bit.Bit28,
+        LeapSecondPending = (uint)Bits.Bit28,
         /// <summary>
         /// Time quality indicator code mask.
         /// </summary>
-        TimeQualityIndicatorCodeMask = Bit.Bit27 | Bit.Bit26 | Bit.Bit25 | Bit.Bit24
+        TimeQualityIndicatorCodeMask = (uint)(Bits.Bit27 | Bits.Bit26 | Bits.Bit25 | Bits.Bit24)
     }
 
     /// <summary>
     /// Time quality indicator code enumeration.
     /// </summary>
     [Serializable()]
-    public enum TimeQualityIndicatorCode : int
+    public enum TimeQualityIndicatorCode : uint
     {
         /// <summary>
         /// 1111 - 0xF:	Fault--clock failure, time not reliable.
         /// </summary>
-        Failure = Bit.Bit27 | Bit.Bit26 | Bit.Bit25 | Bit.Bit24,
+        Failure = (uint)(Bits.Bit27 | Bits.Bit26 | Bits.Bit25 | Bits.Bit24),
         /// <summary>
         /// 1011 - 0xB:	Clock unlocked, time within 10^1 s.
         /// </summary>
-        UnlockedWithin10Seconds = Bit.Bit27 | Bit.Bit25 | Bit.Bit24,
+        UnlockedWithin10Seconds = (uint)(Bits.Bit27 | Bits.Bit25 | Bits.Bit24),
         /// <summary>
         /// 1010 - 0xA:	Clock unlocked, time within 10^0 s.
         /// </summary>
-        UnlockedWithin1Second = Bit.Bit27 | Bit.Bit25,
+        UnlockedWithin1Second = (uint)(Bits.Bit27 | Bits.Bit25),
         /// <summary>
         /// 1001 - 0x9: Clock unlocked, time within 10^-1 s.
         /// </summary>
-        UnlockedWithinPoint1Seconds = Bit.Bit27 | Bit.Bit24,
+        UnlockedWithinPoint1Seconds = (uint)(Bits.Bit27 | Bits.Bit24),
         /// <summary>
         /// 1000 - 0x8: Clock unlocked, time within 10^-2 s.
         /// </summary>
-        UnlockedWithinPoint01Seconds = Bit.Bit27,
+        UnlockedWithinPoint01Seconds = (uint)Bits.Bit27,
         /// <summary>
         /// 0111 - 0x7: Clock unlocked, time within 10^-3 s.
         /// </summary>
-        UnlockedWithinPoint001Seconds = Bit.Bit26 | Bit.Bit25 | Bit.Bit24,
+        UnlockedWithinPoint001Seconds = (uint)(Bits.Bit26 | Bits.Bit25 | Bits.Bit24),
         /// <summary>
         /// 0110 - 0x6: Clock unlocked, time within 10^-4 s.
         /// </summary>
-        UnlockedWithinPoint0001Seconds = Bit.Bit26 | Bit.Bit25,
+        UnlockedWithinPoint0001Seconds = (uint)(Bits.Bit26 | Bits.Bit25),
         /// <summary>
         /// 0101 - 0x5: Clock unlocked, time within 10^-5 s.
         /// </summary>
-        UnlockedWithinPoint00001Seconds = Bit.Bit26 | Bit.Bit24,
+        UnlockedWithinPoint00001Seconds = (uint)(Bits.Bit26 | Bits.Bit24),
         /// <summary>
         /// 0100 - 0x4: Clock unlocked, time within 10^-6 s.
         /// </summary>
-        UnlockedWithinPoint000001Seconds = Bit.Bit26,
+        UnlockedWithinPoint000001Seconds = (uint)Bits.Bit26,
         /// <summary>
         /// 0011 - 0x3: Clock unlocked, time within 10^-7 s.
         /// </summary>
-        UnlockedWithinPoint0000001Seconds = Bit.Bit25 | Bit.Bit24,
+        UnlockedWithinPoint0000001Seconds = (uint)(Bits.Bit25 | Bits.Bit24),
         /// <summary>
         /// 0010 - 0x2: Clock unlocked, time within 10^-8 s.
         /// </summary>
-        UnlockedWithinPoint00000001Seconds = Bit.Bit25,
+        UnlockedWithinPoint00000001Seconds = (uint)Bits.Bit25,
         /// <summary>
         /// 0001 - 0x1: Clock unlocked, time within 10^-9 s.
         /// </summary>
-        UnlockedWithinPoint000000001Seconds = Bit.Bit24,
+        UnlockedWithinPoint000000001Seconds = (uint)Bits.Bit24,
         /// <summary>
         /// 0000 - 0x0: Normal operation, clock locked.
         /// </summary>
@@ -193,44 +193,44 @@ namespace PCS.PhasorProtocols.IeeeC37_118
     /// Status flags enumeration.
     /// </summary>
     [Flags(), Serializable()]
-    public enum StatusFlags : short
+    public enum StatusFlags : ushort
     {
         /// <summary>
         /// Data is valid (0 when device data is valid, 1 when invalid or device is in test mode).
         /// </summary>
-        DataIsValid = Bit.Bit15,
+        DataIsValid = (ushort)Bits.Bit15,
         /// <summary>
         /// Device error including configuration error, 0 when no error.
         /// </summary>
-        DeviceError = Bit.Bit14,
+        DeviceError = (ushort)Bits.Bit14,
         /// <summary>
         /// Device synchronization error, 0 when in sync.
         /// </summary>
-        DeviceSynchronizationError = Bit.Bit13,
+        DeviceSynchronizationError = (ushort)Bits.Bit13,
         /// <summary>
         /// Data sorting type, 0 by timestamp, 1 by arrival.
         /// </summary>
-        DataSortingType = Bit.Bit12,
+        DataSortingType = (ushort)Bits.Bit12,
         /// <summary>
         /// Device trigger detected, 0 when no trigger.
         /// </summary>
-        DeviceTriggerDetected = Bit.Bit11,
+        DeviceTriggerDetected = (ushort)Bits.Bit11,
         /// <summary>
         /// Configuration changed, set to 1 for one minute when configuration changed.
         /// </summary>
-        ConfigurationChanged = Bit.Bit10,
+        ConfigurationChanged = (ushort)Bits.Bit10,
         /// <summary>
         /// Reserved bits for security, presently set to 0.
         /// </summary>
-        ReservedFlags = Bit.Bit9 | Bit.Bit8 | Bit.Bit7 | Bit.Bit6,
+        ReservedFlags = (ushort)(Bits.Bit09 | Bits.Bit08 | Bits.Bit07 | Bits.Bit06),
         /// <summary>
         /// Unlocked time mask.
         /// </summary>
-        UnlockedTimeMask = Bit.Bit5 | Bit.Bit4,
+        UnlockedTimeMask = (ushort)(Bits.Bit05 | Bits.Bit04),
         /// <summary>
         /// Trigger reason mask.
         /// </summary>
-        TriggerReasonMask = Bit.Bit3 | Bit.Bit2 | Bit.Bit1 | Bit.Bit0
+        TriggerReasonMask = (ushort)(Bits.Bit03 | Bits.Bit02 | Bits.Bit01 | Bits.Bit00)
     }
 
     /// <summary>
@@ -242,19 +242,19 @@ namespace PCS.PhasorProtocols.IeeeC37_118
         /// <summary>
         /// Sync locked, best quality.
         /// </summary>
-        SyncLocked = Bit.Nill,
+        SyncLocked = (byte)Bits.Nil,
         /// <summary>
         /// Unlocked for 10 seconds.
         /// </summary>
-        UnlockedFor10Seconds = Bit.Bit4,
+        UnlockedFor10Seconds = (byte)Bits.Bit04,
         /// <summary>
         /// Unlocked for 100 seconds.
         /// </summary>
-        UnlockedFor100Seconds = Bit.Bit5,
+        UnlockedFor100Seconds = (byte)Bits.Bit05,
         /// <summary>
         /// Unlocked for over 1000 seconds.
         /// </summary>
-        UnlockedForOver1000Seconds = Bit.Bit5 | Bit.Bit4
+        UnlockedForOver1000Seconds = (byte)(Bits.Bit05 | Bits.Bit04)
     }
 
     /// <summary>
@@ -266,67 +266,67 @@ namespace PCS.PhasorProtocols.IeeeC37_118
         /// <summary>
         /// 1111 Vendor defined trigger 8.
         /// </summary>
-        VendorDefinedTrigger8 = Bit.Bit3 | Bit.Bit2 | Bit.Bit1 | Bit.Bit0,
+        VendorDefinedTrigger8 = (byte)(Bits.Bit03 | Bits.Bit02 | Bits.Bit01 | Bits.Bit00),
         /// <summary>
         /// 1110 Vendor defined trigger 7.
         /// </summary>
-        VendorDefinedTrigger7 = Bit.Bit3 | Bit.Bit2 | Bit.Bit1,
+        VendorDefinedTrigger7 = (byte)(Bits.Bit03 | Bits.Bit02 | Bits.Bit01),
         /// <summary>
         /// 1101 Vendor defined trigger 6.
         /// </summary>
-        VendorDefinedTrigger6 = Bit.Bit3 | Bit.Bit2 | Bit.Bit0,
+        VendorDefinedTrigger6 = (byte)(Bits.Bit03 | Bits.Bit02 | Bits.Bit00),
         /// <summary>
         /// 1100 Vendor defined trigger 5.
         /// </summary>
-        VendorDefinedTrigger5 = Bit.Bit3 | Bit.Bit2,
+        VendorDefinedTrigger5 = (byte)(Bits.Bit03 | Bits.Bit02),
         /// <summary>
         /// 1011 Vendor defined trigger 4.
         /// </summary>
-        VendorDefinedTrigger4 = Bit.Bit3 | Bit.Bit1 | Bit.Bit0,
+        VendorDefinedTrigger4 = (byte)(Bits.Bit03 | Bits.Bit01 | Bits.Bit00),
         /// <summary>
         /// 1010 Vendor defined trigger 3.
         /// </summary>
-        VendorDefinedTrigger3 = Bit.Bit3 | Bit.Bit1,
+        VendorDefinedTrigger3 = (byte)(Bits.Bit03 | Bits.Bit01),
         /// <summary>
         /// 1001 Vendor defined trigger 2.
         /// </summary>
-        VendorDefinedTrigger2 = Bit.Bit3 | Bit.Bit0,
+        VendorDefinedTrigger2 = (byte)(Bits.Bit03 | Bits.Bit00),
         /// <summary>
         /// 1000 Vendor defined trigger 1.
         /// </summary>
-        VendorDefinedTrigger1 = Bit.Bit3,
+        VendorDefinedTrigger1 = (byte)Bits.Bit03,
         /// <summary>
         /// 0111 Digital.
         /// </summary>
-        Digital = Bit.Bit2 | Bit.Bit1 | Bit.Bit0,
+        Digital = (byte)(Bits.Bit02 | Bits.Bit01 | Bits.Bit00),
         /// <summary>
         /// 0101 df/dt high.
         /// </summary>
-        DfDtHigh = Bit.Bit2 | Bit.Bit0,
+        DfDtHigh = (byte)(Bits.Bit02 | Bits.Bit00),
         /// <summary>
         /// 0011 Phase angle difference.
         /// </summary>
-        PhaseAngleDifference = Bit.Bit1 | Bit.Bit0,
+        PhaseAngleDifference = (byte)(Bits.Bit01 | Bits.Bit00),
         /// <summary>
         /// 0001 Magnitude low.
         /// </summary>
-        MagnitudeLow = Bit.Bit0,
+        MagnitudeLow = (byte)Bits.Bit00,
         /// <summary>
         /// 0110 Reserved.
         /// </summary>
-        Reserved = Bit.Bit2 | Bit.Bit1,
+        Reserved = (byte)(Bits.Bit02 | Bits.Bit01),
         /// <summary>
         /// 0100 Frequency high/low.
         /// </summary>
-        FrequencyHighOrLow = Bit.Bit2,
+        FrequencyHighOrLow = (byte)Bits.Bit02,
         /// <summary>
         /// 0010 Magnitude high.
         /// </summary>
-        MagnitudeHigh = Bit.Bit1,
+        MagnitudeHigh = (byte)Bits.Bit01,
         /// <summary>
         /// 0000 Manual.
         /// </summary>
-        Manual = Bit.Nill
+        Manual = (byte)Bits.Nil
     }
 
     /// <summary>
@@ -359,31 +359,31 @@ namespace PCS.PhasorProtocols.IeeeC37_118
         /// <summary>
         /// Absolute maximum number of possible phasor values that could fit into a data frame.
         /// </summary>
-        public const int MaximumPhasorValues = ushort.MaxValue / 4 - CommonFrameHeader.BinaryLength - 8;
+        public const uint MaximumPhasorValues = ushort.MaxValue / 4U - CommonFrameHeader.FixedLength - 8U;
 
         /// <summary>
         /// Absolute maximum number of possible analog values that could fit into a data frame.
         /// </summary>
-        public const int MaximumAnalogValues = ushort.MaxValue / 2 - CommonFrameHeader.BinaryLength - 8;
+        public const uint MaximumAnalogValues = ushort.MaxValue / 2U - CommonFrameHeader.FixedLength - 8U;
 
         /// <summary>
         /// Absolute maximum number of possible digital values that could fit into a data frame.
         /// </summary>
-        public const int MaximumDigitalValues = ushort.MaxValue / 2 - CommonFrameHeader.BinaryLength - 8;
+        public const uint MaximumDigitalValues = ushort.MaxValue / 2U - CommonFrameHeader.FixedLength - 8U;
 
         /// <summary>
         /// Absolute maximum number of bytes of data that could fit into a header frame.
         /// </summary>
-        public const int MaximumHeaderDataLength = ushort.MaxValue - CommonFrameHeader.BinaryLength - 2;
+        public const uint MaximumHeaderDataLength = ushort.MaxValue - CommonFrameHeader.FixedLength - 2U;
 
         /// <summary>
         /// Absolute maximum number of bytes of extended data that could fit into a command frame.
         /// </summary>
-        public const int MaximumExtendedDataLength = ushort.MaxValue - CommonFrameHeader.BinaryLength - 4;
+        public const uint MaximumExtendedDataLength = ushort.MaxValue - CommonFrameHeader.FixedLength - 4U;
 
         /// <summary>
         /// Time quality flags mask.
         /// </summary>
-        public const int TimeQualityFlagsMask = Bit.Bit31 | Bit.Bit30 | Bit.Bit29 | Bit.Bit28 | Bit.Bit27 | Bit.Bit26 | Bit.Bit25 | Bit.Bit24;
+        public const uint TimeQualityFlagsMask = (uint)(Bits.Bit31 | Bits.Bit30 | Bits.Bit29 | Bits.Bit28 | Bits.Bit27 | Bits.Bit26 | Bits.Bit25 | Bits.Bit24);
     }
 }
