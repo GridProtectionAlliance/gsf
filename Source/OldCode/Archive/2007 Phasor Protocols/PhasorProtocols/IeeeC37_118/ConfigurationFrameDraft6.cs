371b3644e45d4ac33bc8ffa6fe8bd29ba4d61747
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-04-20 15:42:17 +0000

    Vss2Git

--- Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IeeeC37_118/ConfigurationFrameDraft6.cs
+++ Source/OldCode/Archive/2007 Phasor Protocols/PhasorProtocols/IeeeC37_118/ConfigurationFrameDraft6.cs
@@ -1,94 +1,85 @@
-using System.Diagnostics;
-using System;
-////using PCS.Common;
-using System.Collections;
-using PCS.Interop;
-using Microsoft.VisualBasic;
-using PCS;
-using System.Collections.Generic;
-////using PCS.Interop.Bit;
-using System.Linq;
-using System.Runtime.Serialization;
-
 //*******************************************************************************************************
-//  ConfigurationFrameDraft6.vb - IEEE C37.118 Draft6 Configuration Frame
-//  Copyright © 2008 - TVA, all rights reserved - Gbtc
+//  ConfigurationFrameDraft6.cs
+//  Copyright © 2009 - TVA, all rights reserved - Gbtc
 //
-//  Build Environment: VB.NET, Visual Studio 2008
-//  Primary Developer: J. Ritchie Carroll, Operations Data Architecture [TVA]
-//      Office: COO - TRNS/PWR ELEC SYS O, CHATTANOOGA, TN - MR 2W-C
-//       Phone: 423/751-2827
+//  Build Environment: C#, Visual Studio 2008
+//  Primary Developer: James R Carroll
+//      Office: PSO TRAN & REL, CHATTANOOGA - MR BK-C
+//       Phone: 423/751-4165
 //       Email: jrcarrol@tva.gov
 //
 //  Code Modification History:
 //  -----------------------------------------------------------------------------------------------------
-//  11/12/2004 - J. Ritchie Carroll
-//       Initial version of source generated
+//  11/12/2004 - James R Carroll
+//       Generated original version of source code.
 //
 //*******************************************************************************************************
 
+using System;
+using System.ComponentModel;
 
-namespace PCS.PhasorProtocols
+namespace PCS.PhasorProtocols.IeeeC37_118
 {
-    namespace IeeeC37_118
+    /// <summary>
+    /// Represents the IEEE C37.118 draft 6 implementation of a <see cref="IConfigurationFrame"/> that can be sent or received.
+    /// </summary>
+    [Serializable()]
+    public class ConfigurationFrameDraft6 : ConfigurationFrame
     {
+        #region [ Constructors ]
 
-        [CLSCompliant(false), Serializable()]
-        public class ConfigurationFrameDraft6 : ConfigurationFrame
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationFrameDraft6"/>.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is used by <see cref="FrameImageParserBase{TTypeIdentifier,TOutputType}"/> to parse an IEEE C37.118 draft 6 configuration frame.
+        /// </remarks>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected ConfigurationFrameDraft6()
         {
-
-
-
-            protected ConfigurationFrameDraft6()
-            {
-            }
-
-            protected ConfigurationFrameDraft6(SerializationInfo info, StreamingContext context)
-                : base(info, context)
-            {
-
-
-            }
-
-            public ConfigurationFrameDraft6(FrameType frameType, int timeBase, ushort idCode, long ticks, short frameRate, byte version)
-                : base(frameType, timeBase, idCode, ticks, frameRate, version)
-            {
-
-
-            }
-
-            public ConfigurationFrameDraft6(ICommonFrameHeader parsedFrameHeader, byte[] binaryImage, int startIndex)
-                : base(parsedFrameHeader, binaryImage, startIndex)
-            {
-
-
-            }
-
-            public ConfigurationFrameDraft6(IConfigurationFrame configurationFrame)
-                : base(configurationFrame)
-            {
-
-
-            }
-
-            public override System.Type DerivedType
-            {
-                get
-                {
-                    return this.GetType();
-                }
-            }
-
-            public override DraftRevision DraftRevision
-            {
-                get
-                {
-                    return IeeeC37_118.DraftRevision.Draft6;
-                }
-            }
-
         }
 
-    }
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationFrameDraft6"/> from specified parameters.
+        /// </summary>
+        /// <param name="typeID">Type of configuration frame to create (1 or 2).</param>
+        /// <param name="timebase">Timebase to use for fraction second resolution.</param>
+        /// <param name="idCode">The ID code of this <see cref="ConfigurationFrameDraft6"/>.</param>
+        /// <param name="timestamp">The exact timestamp, in <see cref="Ticks"/>, of the data represented by this <see cref="ConfigurationFrameDraft6"/>.</param>
+        /// <param name="frameRate">The defined frame rate of this <see cref="ConfigurationFrameDraft6"/>.</param>
+        /// <remarks>
+        /// This constructor is used by a consumer to generate an IEEE C37.118 draft 6 configuration frame.
+        /// </remarks>
+        public ConfigurationFrameDraft6(FrameType typeID, uint timebase, ushort idCode, Ticks timestamp, ushort frameRate)
+            : base(typeID, timebase, idCode, timestamp, frameRate)
+        {
+        }
 
+        /// <summary>
+        /// Creates a new <see cref="ConfigurationFrameDraft6"/> from serialization parameters.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> with populated with data.</param>
+        /// <param name="context">The source <see cref="StreamingContext"/> for this deserialization.</param>
+        protected ConfigurationFrameDraft6(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets the <see cref="IeeeC37_118.DraftRevision"/> of this <see cref="ConfigurationFrameDraft6"/>.
+        /// </summary>
+        public override DraftRevision DraftRevision
+        {
+            get
+            {
+                return IeeeC37_118.DraftRevision.Draft6;
+            }
+        }
+
+        #endregion
+    }
 }
