cdb2d944bf6960b6bdad8d68281917e8e05b6ed1
pinalcpatel <pcpatel@tva.gov>
2009-06-11 17:08:55 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/4.0/Source/TVA.Services/Properties/AssemblyInfo.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/4.0/Source/TVA.Services/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security.Permissions;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,10 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8822ca22-cda5-4562-9a67-2adb084730e1")]
