ad811b48ca0aae176bfdce37ae914ed0f203047d
pinalcpatel <pcpatel@tva.gov>
2009-08-18 13:48:15 +0000

    Vss2Git

--- Source/OldCode/Archive/2005 TVA Code Library/Version/4.0/Source/TVA.Historian/Services/SerializableMetadata.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/4.0/Source/TVA.Historian/Services/SerializableMetadata.cs
@@ -50,49 +50,49 @@ namespace TVA.Historian.Services
     /// <Metadata xmlns="http://schemas.datacontract.org/2004/07/TVA.Historian.Services" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
     ///   <MetadataRecords>
     ///     <MetadataRecord>
-    ///       <AccessSecurity>0</AccessSecurity> 
-    ///       <AlarmByEmail>false</AlarmByEmail> 
-    ///       <AlarmByPager>false</AlarmByPager> 
-    ///       <AlarmByPhone>false</AlarmByPhone> 
-    ///       <AlarmDelay>0</AlarmDelay> 
-    ///       <AlarmEmails /> 
-    ///       <AlarmEnabled>false</AlarmEnabled> 
-    ///       <AlarmFlags>0</AlarmFlags> 
-    ///       <AlarmPagers /> 
-    ///       <AlarmPhones /> 
-    ///       <AlarmState>0</AlarmState> 
-    ///       <AlarmToFile>false</AlarmToFile> 
-    ///       <ChangeSecurity>5</ChangeSecurity> 
-    ///       <ClearDescription /> 
-    ///       <CompressionLimit>0</CompressionLimit> 
-    ///       <CompressionMaxTime>0</CompressionMaxTime> 
-    ///       <CompressionMinTime>0</CompressionMinTime> 
-    ///       <DataType>0</DataType> 
-    ///       <Description>Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude</Description> 
-    ///       <DisplayDigits>7</DisplayDigits> 
-    ///       <Enabled>true</Enabled> 
-    ///       <EngineeringUnits>Volts</EngineeringUnits> 
-    ///       <ExceptionLimit>0</ExceptionLimit> 
-    ///       <HardwareInfo>ABB RES521</HardwareInfo> 
-    ///       <HighAlarm>550000</HighAlarm> 
-    ///       <HighRange>525000</HighRange> 
-    ///       <HighWarning>525000</HighWarning> 
     ///       <HistorianID>1</HistorianID> 
-    ///       <LowAlarm>450000</LowAlarm> 
-    ///       <LowRange>475000</LowRange> 
-    ///       <LowWarning>475000</LowWarning> 
+    ///       <DataType>0</DataType> 
     ///       <Name>TVA_CORD-BUS2:ABBV</Name> 
-    ///       <PlantCode>P1</PlantCode> 
-    ///       <Remarks /> 
-    ///       <ScanRate>0.0333333351</ScanRate> 
-    ///       <SetDescription /> 
-    ///       <SourceID>3</SourceID> 
-    ///       <StepCheck>false</StepCheck> 
     ///       <Synonym1>4-PM1</Synonym1> 
     ///       <Synonym2>VPHM</Synonym2> 
     ///       <Synonym3 /> 
-    ///       <SystemName>CORD</SystemName> 
+    ///       <Description>Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude</Description> 
+    ///       <HardwareInfo>ABB RES521</HardwareInfo> 
+    ///       <Remarks /> 
+    ///       <PlantCode>P1</PlantCode> 
     ///       <UnitNumber>1</UnitNumber> 
+    ///       <SystemName>CORD</SystemName> 
+    ///       <SourceID>3</SourceID> 
+    ///       <Enabled>true</Enabled> 
+    ///       <ScanRate>0.0333333351</ScanRate> 
+    ///       <CompressionMinTime>0</CompressionMinTime> 
+    ///       <CompressionMaxTime>0</CompressionMaxTime> 
+    ///       <EngineeringUnits>Volts</EngineeringUnits> 
+    ///       <LowWarning>475000</LowWarning> 
+    ///       <HighWarning>525000</HighWarning> 
+    ///       <LowAlarm>450000</LowAlarm> 
+    ///       <HighAlarm>550000</HighAlarm> 
+    ///       <LowRange>475000</LowRange> 
+    ///       <HighRange>525000</HighRange> 
+    ///       <CompressionLimit>0</CompressionLimit> 
+    ///       <ExceptionLimit>0</ExceptionLimit> 
+    ///       <DisplayDigits>7</DisplayDigits> 
+    ///       <SetDescription /> 
+    ///       <ClearDescription /> 
+    ///       <AlarmState>0</AlarmState> 
+    ///       <ChangeSecurity>5</ChangeSecurity> 
+    ///       <AccessSecurity>0</AccessSecurity> 
+    ///       <StepCheck>false</StepCheck> 
+    ///       <AlarmEnabled>false</AlarmEnabled> 
+    ///       <AlarmFlags>0</AlarmFlags> 
+    ///       <AlarmDelay>0</AlarmDelay> 
+    ///       <AlarmToFile>false</AlarmToFile> 
+    ///       <AlarmByEmail>false</AlarmByEmail> 
+    ///       <AlarmByPager>false</AlarmByPager> 
+    ///       <AlarmByPhone>false</AlarmByPhone> 
+    ///       <AlarmEmails /> 
+    ///       <AlarmPagers /> 
+    ///       <AlarmPhones /> 
     ///     </MetadataRecord>
     ///   </MetadataRecords>
     /// </Metadata>
@@ -102,49 +102,49 @@ namespace TVA.Historian.Services
     /// <code>
     /// {
     ///   "MetadataRecords":
-    ///     [{"AccessSecurity":0,
-    ///       "AlarmByEmail":false,
-    ///       "AlarmByPager":false,
-    ///       "AlarmByPhone":false,
-    ///       "AlarmDelay":0,
-    ///       "AlarmEmails":"",
-    ///       "AlarmEnabled":false,
-    ///       "AlarmFlags":0,
-    ///       "AlarmPagers":"",
-    ///       "AlarmPhones":"",
-    ///       "AlarmState":0,
-    ///       "AlarmToFile":false,
-    ///       "ChangeSecurity":5,
-    ///       "ClearDescription":"",
-    ///       "CompressionLimit":0,
-    ///       "CompressionMaxTime":0,
-    ///       "CompressionMinTime":0,
+    ///     [{"HistorianID":1,
     ///       "DataType":0,
-    ///       "Description":"Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude",
-    ///       "DisplayDigits":7,
-    ///       "Enabled":true,
-    ///       "EngineeringUnits":"Volts",
-    ///       "ExceptionLimit":0,
-    ///       "HardwareInfo":"ABB RES521",
-    ///       "HighAlarm":550000,
-    ///       "HighRange":525000,
-    ///       "HighWarning":525000,
-    ///       "HistorianID":1,
-    ///       "LowAlarm":450000,
-    ///       "LowRange":475000,
-    ///       "LowWarning":475000,
     ///       "Name":"TVA_CORD-BUS2:ABBV",
-    ///       "PlantCode":"P1",
-    ///       "Remarks":"",
-    ///       "ScanRate":0.0333333351,
-    ///       "SetDescription":"",
-    ///       "SourceID":3,
-    ///       "StepCheck":false,
     ///       "Synonym1":"4-PM1",
     ///       "Synonym2":"VPHM",
     ///       "Synonym3":"",
+    ///       "Description":"Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude",
+    ///       "HardwareInfo":"ABB RES521",
+    ///       "Remarks":"",
+    ///       "PlantCode":"P1",
+    ///       "UnitNumber":1,
     ///       "SystemName":"CORD",
-    ///       "UnitNumber":1}]
+    ///       "SourceID":3,
+    ///       "Enabled":true,
+    ///       "ScanRate":0.0333333351,
+    ///       "CompressionMinTime":0,
+    ///       "CompressionMaxTime":0,
+    ///       "EngineeringUnits":"Volts",
+    ///       "LowWarning":475000,
+    ///       "HighWarning":525000,
+    ///       "LowAlarm":450000,
+    ///       "HighAlarm":550000,
+    ///       "LowRange":475000,
+    ///       "HighRange":525000,
+    ///       "CompressionLimit":0,
+    ///       "ExceptionLimit":0,
+    ///       "DisplayDigits":7,
+    ///       "SetDescription":"",
+    ///       "ClearDescription":"",
+    ///       "AlarmState":0,
+    ///       "ChangeSecurity":5,
+    ///       "AccessSecurity":0,
+    ///       "StepCheck":false,
+    ///       "AlarmEnabled":false,
+    ///       "AlarmFlags":0,
+    ///       "AlarmDelay":0,
+    ///       "AlarmToFile":false,
+    ///       "AlarmByEmail":false,
+    ///       "AlarmByPager":false,
+    ///       "AlarmByPhone":false,
+    ///       "AlarmEmails":"",
+    ///       "AlarmPagers":"",
+    ///       "AlarmPhones":""}]
     /// }
     /// </code>
     /// </example>
--- Source/OldCode/Archive/2005 TVA Code Library/Version/4.0/Source/TVA.Historian/Services/SerializableMetadataRecord.cs
+++ Source/OldCode/Archive/2005 TVA Code Library/Version/4.0/Source/TVA.Historian/Services/SerializableMetadataRecord.cs
@@ -12,6 +12,8 @@
 //  -----------------------------------------------------------------------------------------------------
 //  08/07/2009 - Pinal C. Patel
 //       Generated original version of source code.
+//  08/18/2009 - Pinal C. Patel
+//       Set the order of property serialization/deserialization for DataMember attribute.
 //
 //*******************************************************************************************************
 
@@ -41,98 +43,98 @@ namespace TVA.Historian.Services
     /// <code>
     /// <![CDATA[
     /// <MetadataRecord xmlns="http://schemas.datacontract.org/2004/07/TVA.Historian.Services" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
-    ///   <AccessSecurity>0</AccessSecurity> 
-    ///   <AlarmByEmail>false</AlarmByEmail> 
-    ///   <AlarmByPager>false</AlarmByPager> 
-    ///   <AlarmByPhone>false</AlarmByPhone> 
-    ///   <AlarmDelay>0</AlarmDelay> 
-    ///   <AlarmEmails /> 
-    ///   <AlarmEnabled>false</AlarmEnabled> 
-    ///   <AlarmFlags>0</AlarmFlags> 
-    ///   <AlarmPagers /> 
-    ///   <AlarmPhones /> 
-    ///   <AlarmState>0</AlarmState> 
-    ///   <AlarmToFile>false</AlarmToFile> 
-    ///   <ChangeSecurity>5</ChangeSecurity> 
-    ///   <ClearDescription /> 
-    ///   <CompressionLimit>0</CompressionLimit> 
-    ///   <CompressionMaxTime>0</CompressionMaxTime> 
-    ///   <CompressionMinTime>0</CompressionMinTime> 
-    ///   <DataType>0</DataType> 
-    ///   <Description>Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude</Description> 
-    ///   <DisplayDigits>7</DisplayDigits> 
-    ///   <Enabled>true</Enabled> 
-    ///   <EngineeringUnits>Volts</EngineeringUnits> 
-    ///   <ExceptionLimit>0</ExceptionLimit> 
-    ///   <HardwareInfo>ABB RES521</HardwareInfo> 
-    ///   <HighAlarm>550000</HighAlarm> 
-    ///   <HighRange>525000</HighRange> 
-    ///   <HighWarning>525000</HighWarning> 
     ///   <HistorianID>1</HistorianID> 
-    ///   <LowAlarm>450000</LowAlarm> 
-    ///   <LowRange>475000</LowRange> 
-    ///   <LowWarning>475000</LowWarning> 
+    ///   <DataType>0</DataType> 
     ///   <Name>TVA_CORD-BUS2:ABBV</Name> 
-    ///   <PlantCode>P1</PlantCode> 
-    ///   <Remarks /> 
-    ///   <ScanRate>0.0333333351</ScanRate> 
-    ///   <SetDescription /> 
-    ///   <SourceID>3</SourceID> 
-    ///   <StepCheck>false</StepCheck> 
     ///   <Synonym1>4-PM1</Synonym1> 
     ///   <Synonym2>VPHM</Synonym2> 
     ///   <Synonym3 /> 
-    ///   <SystemName>CORD</SystemName> 
+    ///   <Description>Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude</Description> 
+    ///   <HardwareInfo>ABB RES521</HardwareInfo> 
+    ///   <Remarks /> 
+    ///   <PlantCode>P1</PlantCode> 
     ///   <UnitNumber>1</UnitNumber> 
+    ///   <SystemName>CORD</SystemName> 
+    ///   <SourceID>3</SourceID> 
+    ///   <Enabled>true</Enabled> 
+    ///   <ScanRate>0.0333333351</ScanRate> 
+    ///   <CompressionMinTime>0</CompressionMinTime> 
+    ///   <CompressionMaxTime>0</CompressionMaxTime> 
+    ///   <EngineeringUnits>Volts</EngineeringUnits> 
+    ///   <LowWarning>475000</LowWarning> 
+    ///   <HighWarning>525000</HighWarning> 
+    ///   <LowAlarm>450000</LowAlarm> 
+    ///   <HighAlarm>550000</HighAlarm> 
+    ///   <LowRange>475000</LowRange> 
+    ///   <HighRange>525000</HighRange> 
+    ///   <CompressionLimit>0</CompressionLimit> 
+    ///   <ExceptionLimit>0</ExceptionLimit> 
+    ///   <DisplayDigits>7</DisplayDigits> 
+    ///   <SetDescription /> 
+    ///   <ClearDescription /> 
+    ///   <AlarmState>0</AlarmState> 
+    ///   <ChangeSecurity>5</ChangeSecurity> 
+    ///   <AccessSecurity>0</AccessSecurity> 
+    ///   <StepCheck>false</StepCheck> 
+    ///   <AlarmEnabled>false</AlarmEnabled> 
+    ///   <AlarmFlags>0</AlarmFlags> 
+    ///   <AlarmDelay>0</AlarmDelay> 
+    ///   <AlarmToFile>false</AlarmToFile> 
+    ///   <AlarmByEmail>false</AlarmByEmail> 
+    ///   <AlarmByPager>false</AlarmByPager> 
+    ///   <AlarmByPhone>false</AlarmByPhone> 
+    ///   <AlarmEmails /> 
+    ///   <AlarmPagers /> 
+    ///   <AlarmPhones /> 
     /// </MetadataRecord>
     /// ]]>
     /// </code>
     /// This is the output for <see cref="SerializableMetadataRecord"/> serialized using <see cref="System.Runtime.Serialization.Json.DataContractJsonSerializer"/>:
     /// <code>
     /// {
-    ///   "AccessSecurity":0,
-    ///   "AlarmByEmail":false,
-    ///   "AlarmByPager":false,
-    ///   "AlarmByPhone":false,
-    ///   "AlarmDelay":0,
-    ///   "AlarmEmails":"",
-    ///   "AlarmEnabled":false,
-    ///   "AlarmFlags":0,
-    ///   "AlarmPagers":"",
-    ///   "AlarmPhones":"",
-    ///   "AlarmState":0,
-    ///   "AlarmToFile":false,
-    ///   "ChangeSecurity":5,
-    ///   "ClearDescription":"",
-    ///   "CompressionLimit":0,
-    ///   "CompressionMaxTime":0,
-    ///   "CompressionMinTime":0,
-    ///   "DataType":0,
-    ///   "Description":"Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude",
-    ///   "DisplayDigits":7,
-    ///   "Enabled":true,
-    ///   "EngineeringUnits":"Volts",
-    ///   "ExceptionLimit":0,
-    ///   "HardwareInfo":"ABB RES521",
-    ///   "HighAlarm":550000,
-    ///   "HighRange":525000,
-    ///   "HighWarning":525000,
     ///   "HistorianID":1,
-    ///   "LowAlarm":450000,
-    ///   "LowRange":475000,
-    ///   "LowWarning":475000,
+    ///   "DataType":0,
     ///   "Name":"TVA_CORD-BUS2:ABBV",
-    ///   "PlantCode":"P1",
-    ///   "Remarks":"",
-    ///   "ScanRate":0.0333333351,
-    ///   "SetDescription":"",
-    ///   "SourceID":3,
-    ///   "StepCheck":false,
     ///   "Synonym1":"4-PM1",
     ///   "Synonym2":"VPHM",
     ///   "Synonym3":"",
+    ///   "Description":"Cordova ABB-521 500 kV Bus 2 Positive Sequence Voltage Magnitude",
+    ///   "HardwareInfo":"ABB RES521",
+    ///   "Remarks":"",
+    ///   "PlantCode":"P1",
+    ///   "UnitNumber":1,
     ///   "SystemName":"CORD",
-    ///   "UnitNumber":1
+    ///   "SourceID":3,
+    ///   "Enabled":true,
+    ///   "ScanRate":0.0333333351,
+    ///   "CompressionMinTime":0,
+    ///   "CompressionMaxTime":0,
+    ///   "EngineeringUnits":"Volts",
+    ///   "LowWarning":475000,
+    ///   "HighWarning":525000,
+    ///   "LowAlarm":450000,
+    ///   "HighAlarm":550000,
+    ///   "LowRange":475000,
+    ///   "HighRange":525000,
+    ///   "CompressionLimit":0,
+    ///   "ExceptionLimit":0,
+    ///   "DisplayDigits":7,
+    ///   "SetDescription":"",
+    ///   "ClearDescription":"",
+    ///   "AlarmState":0,
+    ///   "ChangeSecurity":5,
+    ///   "AccessSecurity":0,
+    ///   "StepCheck":false,
+    ///   "AlarmEnabled":false,
+    ///   "AlarmFlags":0,
+    ///   "AlarmDelay":0,
+    ///   "AlarmToFile":false,
+    ///   "AlarmByEmail":false,
+    ///   "AlarmByPager":false,
+    ///   "AlarmByPhone":false,
+    ///   "AlarmEmails":"",
+    ///   "AlarmPagers":"",
+    ///   "AlarmPhones":""
     /// }
     /// </code>
     /// </example>
@@ -238,259 +240,259 @@ namespace TVA.Historian.Services
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.HistorianID"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 0)]
         public int HistorianID { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.DataType"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 1)]
         public int DataType { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.Name"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 2)]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.Synonym1"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 3)]
         public string Synonym1 { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.Synonym2"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 4)]
         public string Synonym2 { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.Synonym3"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 5)]
         public string Synonym3 { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.Description"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 6)]
         public string Description { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.HardwareInfo"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 7)]
         public string HardwareInfo { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.Remarks"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 8)]
         public string Remarks { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.PlantCode"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 9)]
         public string PlantCode { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.UnitNumber"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 10)]
         public int UnitNumber { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.SystemName"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 11)]
         public string SystemName { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.SourceID"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 12)]
         public int SourceID { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.Enabled"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 13)]
         public bool Enabled { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.ScanRate"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 14)]
         public float ScanRate { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.CompressionMinTime"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 15)]
         public int CompressionMinTime { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.CompressionMaxTime"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 16)]
         public int CompressionMaxTime { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.EngineeringUnits"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 17)]
         public string EngineeringUnits { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.LowWarning"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 18)]
         public float LowWarning { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.HighWarning"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 19)]
         public float HighWarning { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.LowAlarm"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 20)]
         public float LowAlarm { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.HighAlarm"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 21)]
         public float HighAlarm { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.LowRange"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 22)]
         public float LowRange { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.HighRange"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 23)]
         public float HighRange { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.CompressionLimit"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 24)]
         public float CompressionLimit { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.ExceptionLimit"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 25)]
         public float ExceptionLimit { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.DisplayDigits"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 26)]
         public int DisplayDigits { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordDigitalFields.SetDescription"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 27)]
         public string SetDescription { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordDigitalFields.ClearDescription"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 28)]
         public string ClearDescription { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordDigitalFields.AlarmState"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 29)]
         public int AlarmState { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordSecurityFlags.ChangeSecurity"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 30)]
         public int ChangeSecurity { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordSecurityFlags.AccessSecurity"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 31)]
         public int AccessSecurity { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.StepCheck"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 32)]
         public bool StepCheck { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.AlarmEnabled"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 33)]
         public bool AlarmEnabled { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAlarmFlags.Value"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 33)]
         public int AlarmFlags { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordAnalogFields.AlarmDelay"/> or <see cref="MetadataRecordDigitalFields.AlarmDelay"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 34)]
         public float AlarmDelay { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.AlarmToFile"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 35)]
         public bool AlarmToFile { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.AlarmByEmail"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 36)]
         public bool AlarmByEmail { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.AlarmByPager"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 37)]
         public bool AlarmByPager { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecordGeneralFlags.AlarmByPhone"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 38)]
         public bool AlarmByPhone { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.AlarmEmails"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 39)]
         public string AlarmEmails { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.AlarmPagers"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 40)]
         public string AlarmPagers { get; set; }
 
         /// <summary>
         /// Gets or sets the <see cref="MetadataRecord.AlarmPhones"/>.
         /// </summary>
-        [XmlAttribute(), DataMember()]
+        [XmlAttribute(), DataMember(Order = 41)]
         public string AlarmPhones { get; set; }
 
         #endregion
