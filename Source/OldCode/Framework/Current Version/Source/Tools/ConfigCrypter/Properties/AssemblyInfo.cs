facae557d887587e94af417ed0d0275d74048377
pinalcpatel <pcpatel@tva.gov>
2009-09-29 15:15:05 +0000

    Framework: Fixed AssemblyInfo.cs under Tools.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C30697

--- Source/OldCode/Framework/Current Version/Source/Tools/ConfigCrypter/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Tools/ConfigCrypter/Properties/AssemblyInfo.cs
@@ -1,35 +1,23 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConfigCrypter")]
-[assembly: AssemblyDescription("Utility for encrypting and decrypting configuration file values.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TVA")]
-[assembly: AssemblyProduct("ConfigCrypter")]
-[assembly: AssemblyCopyright("No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3be9bc3a-666f-40d6-abe5-588593129c95")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Assembly identity attributes.
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Informational attributes.
+[assembly: AssemblyCompany("TVA")]
+[assembly: AssemblyCopyright("No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.")]
+[assembly: AssemblyProduct("ConfigCrypter")]
+
+// Assembly manifest attributes.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+[assembly: AssemblyDescription("Utility for encrypting and decrypting configuration file values.")]
+[assembly: AssemblyTitle("ConfigCrypter")]
+
+// Other configuration attributes.
+[assembly: ComVisible(false)]
+[assembly: Guid("3be9bc3a-666f-40d6-abe5-588593129c95")]
