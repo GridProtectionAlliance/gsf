1b01a1c75aaaa7f4c0aa3fc876cec781b08be274
pinalcpatel <pcpatel@tva.gov>
2009-10-09 20:28:21 +0000

    openPDC: Made changes to the build framework for added flexibility.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C31509

--- Source/OldCode/Framework/Current Version/Build/Scripts/Build.bat
+++ Source/OldCode/Framework/Current Version/Build/Scripts/Build.bat
@@ -1,3 +1,19 @@
+::*******************************************************************************************************
+::  Build.bat - Gbtc
+::
+::  Tennessee Valley Authority, 2009
+::  No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.
+::
+::  This software is made freely available under the TVA Open Source Agreement (see below).
+::
+::  Code Modification History:
+::  -----------------------------------------------------------------------------------------------------
+::  09/05/2009 - Pinal C. Patel
+::       Generated original version of source code.
+::
+::*******************************************************************************************************
+
 @ECHO OFF
-REM Passing in "false" for argument #1 will cause the build to take place in unattended mode.
+:: Passing in "false" for argument #1 will cause the build to take place in unattended mode.
 C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild.exe Framework.buildproj /p:BuildInteractive=%1 /l:FileLogger,Microsoft.Build.Engine;logfile=Framework.output
+IF NOT "%1" == "false" PAUSE
