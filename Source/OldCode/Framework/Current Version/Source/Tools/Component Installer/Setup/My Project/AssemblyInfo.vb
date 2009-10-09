318a3249451e12180174670841a8a0c3fa83bbbb
pinalcpatel <pcpatel@tva.gov>
2009-10-09 20:06:27 +0000

    Framework: Version change for build Framework_v1.0.3.31276.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C31504

--- Source/OldCode/Framework/Current Version/Source/Tools/Component Installer/Setup/My Project/AssemblyInfo.vb
+++ Source/OldCode/Framework/Current Version/Source/Tools/Component Installer/Setup/My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.2.31221")> 
+' <Assembly: AssemblyVersion("1.0.3.31276")> 
 
-<Assembly: AssemblyVersion("1.0.2.31221")> 
+<Assembly: AssemblyVersion("1.0.3.31276")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
--- Source/OldCode/Framework/Current Version/Source/Tools/UDPRebroadcaster/UDPRebroadcasterConsole/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Tools/UDPRebroadcaster/UDPRebroadcasterConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.2.31221")]
+[assembly: AssemblyVersion("1.0.3.31276")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
