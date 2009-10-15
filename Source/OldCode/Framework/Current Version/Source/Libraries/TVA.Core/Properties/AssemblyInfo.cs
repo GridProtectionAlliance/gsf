8f5ea7fbad3af602342ebe51f97cbc3c25131a49
pinalcpatel <pcpatel@tva.gov>
2009-10-15 17:56:59 +0000

    Framework: Version change for build Framework_v1.0.10.31923.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C31927

--- Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Core/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.9.31922")]
+[assembly: AssemblyVersion("1.0.10.31923")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Web.Test/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Web.Test/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.9.31922")]
+[assembly: AssemblyVersion("1.0.10.31923")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
