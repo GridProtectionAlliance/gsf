f72183aef779a77ba749ab10709a45df53349783
pinalcpatel <pcpatel@tva.gov>
2009-10-15 19:43:19 +0000

    Historian: Version change for build Historian_v1.0.5.31859.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C31930

--- Source/OldCode/Historian/Current Version/Source/Libraries/TVA.Historian/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Libraries/TVA.Historian/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.4.31855")]
+[assembly: AssemblyVersion("1.0.5.31859")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
