b8a63810071ec0dad08413f708fcdc752ee0bea1
pinalcpatel <pcpatel@tva.gov>
2009-10-20 18:50:19 +0000

    Historian: Added initial unit test structure.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C32437

---
+++ Source/OldCode/Historian/Current Version/Source/Libraries/Test/TVA.Historian.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Assembly identity attributes.
+[assembly: AssemblyVersion("1.0.8.32347")]
+
+// Informational attributes.
+[assembly: AssemblyCompany("TVA")]
+[assembly: AssemblyCopyright("No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.")]
+[assembly: AssemblyProduct("openPDC Historian")]
+
+// Assembly manifest attributes.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+[assembly: AssemblyDefaultAlias("TVA.Historian.Test")]
+[assembly: AssemblyDescription("Unit tests for core historian specific components.")]
+[assembly: AssemblyTitle("TVA.Historian.Test")]
+
+// Other configuration attributes.
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("f17ded3b-5667-4835-bbd1-dbc10ee2e1d0")]
