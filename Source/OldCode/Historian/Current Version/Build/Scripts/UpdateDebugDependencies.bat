39346cecc8de0dc9a03def6fda8ad7837f3d98bd
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-11-06 19:23:21 +0000

    openPDC: Added batch files to script folders for Historian and Synchrophasor to allow temporary updates to dependencies for debug builds.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C36027

---
+++ Source/OldCode/Historian/Current Version/Build/Scripts/UpdateDebugDependencies.bat
@@ -0,0 +1,19 @@
+::*******************************************************************************************************
+::  UpdateDebugDependencies.bat - Gbtc
+::
+::  Tennessee Valley Authority, 2009
+::  No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.
+::
+::  This software is made freely available under the TVA Open Source Agreement (see below).
+::
+::  Code Modification History:
+::  -----------------------------------------------------------------------------------------------------
+::  11/06/2009 - J. Ritchie Carroll
+::       Generated original version of source code.
+::
+::*******************************************************************************************************
+
+@ECHO OFF
+COPY "..\..\..\..\Framework\Current Version\Build\Output\Debug\Libraries\TVA.Communication.*" "..\..\Source\Dependencies\TVA\"
+COPY "..\..\..\..\Framework\Current Version\Build\Output\Debug\Libraries\TVA.Core.*" "..\..\Source\Dependencies\TVA\"
+PAUSE
