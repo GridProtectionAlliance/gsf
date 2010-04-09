76befe2985c1b3416a886ae09804eabdb91477bb
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-04-09 04:01:15 +0000

    Historian: Version change for build Historian_v1.1.13.44083.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C44211

--- Source/OldCode/Historian/Current Version/Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.12.43552")]
+[assembly: AssemblyVersion("1.1.13.44083")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
