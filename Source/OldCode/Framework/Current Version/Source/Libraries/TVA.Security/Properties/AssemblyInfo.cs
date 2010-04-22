826d1e6cdd0d7becaa12fe4ee979761029f954a5
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-04-22 00:24:33 +0000

    Framework: Version change for build Framework_v1.1.24.46183.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C46186

--- Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Security/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Security/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.23.46027")]
+[assembly: AssemblyVersion("1.1.24.46183")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/OldCode/Framework/Current Version/Source/Tools/ConfigurationEditor/ConfigurationEditor/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Tools/ConfigurationEditor/ConfigurationEditor/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.22.45918")]
+[assembly: AssemblyVersion("1.1.24.46183")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
