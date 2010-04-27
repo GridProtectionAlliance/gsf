02878352f33692cc7cd33169c7997ad4daf60f90
pinalcpatel <pcpatel@tva.gov>
2010-04-27 19:55:46 +0000

    Framework: Version change for build Framework_v1.1.30.46705.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C46714

--- Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Communication/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Communication/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.29.46704")]
+[assembly: AssemblyVersion("1.1.30.46705")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/OldCode/Framework/Current Version/Source/Tools/ConfigCrypter/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Tools/ConfigCrypter/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.29.46704")]
+[assembly: AssemblyVersion("1.1.30.46705")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
