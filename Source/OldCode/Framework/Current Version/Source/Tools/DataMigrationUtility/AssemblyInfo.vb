6eb80e70b28c2fe5a40a435031b20135f5da4bf9
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-06-29 19:04:56 +0000

    Framework: Added general purpose database migration utility to framework tools.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Framework;C53877

---
+++ Source/OldCode/Framework/Current Version/Source/Tools/DataMigrationUtility/AssemblyInfo.vb
@@ -0,0 +1,27 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly
+
+<Assembly: AssemblyTitle("Data Migration Utility")> 
+<Assembly: AssemblyDescription("Database Migration Utility")> 
+<Assembly: AssemblyCompany("Grid Protection Alliance")> 
+<Assembly: AssemblyProduct("TVA Code Library Framework")> 
+<Assembly: AssemblyCopyright("Copyright © 2010")> 
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Version information for an assembly consists of the following four values:
+
+'	Major version
+'	Minor Version
+'	Revision
+'	Build Number
+
+' You can specify all the values or you can default the Revision and Build Numbers
+' by using the '*' as shown below
+
+<Assembly: AssemblyVersion("1.1.*")> 
