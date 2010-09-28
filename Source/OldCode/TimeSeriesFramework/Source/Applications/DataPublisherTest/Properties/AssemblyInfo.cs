e5f97ad4a9f389c28666d7bd258706e0716c613a
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-09-28 19:44:00 +0000

    Updated time-series frame work version to 1.0.1
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C57838

--- Source/OldCode/TimeSeriesFramework/Source/Applications/DataPublisherTest/Properties/AssemblyInfo.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Applications/DataPublisherTest/Properties/AssemblyInfo.cs
@@ -5,14 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataPublisherTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DataPublisherTest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTitle("Data Publisher Test")]
+[assembly: AssemblyDescription("Data Publisher Test Application")]
+[assembly: AssemblyCompany("Grid Protection Alliance")]
+[assembly: AssemblyProduct("Time-series Data Processing Framework")]
+[assembly: AssemblyCopyright("Copyright © GPA, 2010.  All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +37,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
