8ab4f5f0d56f3c4749a2ee32e8332fa614cf7b6c
pinalcpatel <pcpatel@tva.gov>
2011-02-26 19:17:38 +0000

    Added batch file for updating dependencies.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C63919

---
+++ Source/OldCode/TimeSeriesFramework/Build/Scripts/UpdateDependencies.bat
@@ -0,0 +1,52 @@
+::*******************************************************************************************************
+::  UpdateDependencies.bat - Gbtc
+::
+::  Tennessee Valley Authority, 2009
+::  No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.
+::
+::  This software is made freely available under the TVA Open Source Agreement (see below).
+::
+::  Code Modification History:
+::  -----------------------------------------------------------------------------------------------------
+::  02/26/2011 - Pinal C. Patel
+::       Generated original version of source code.
+::
+::*******************************************************************************************************
+
+@ECHO OFF
+
+SET vs="%VS100COMNTOOLS%\..\IDE\devenv.com"
+SET tfs="%VS100COMNTOOLS%\..\IDE\tf.exe"
+SET source="\\GPAWEB\NightlyBuilds\TVACodeLibrary\Beta\Libraries"
+SET target="..\..\Source\Dependencies\TVA"
+SET solution="..\..\Source\TimeSeriesFramework.sln"
+SET checkinComment="Updated dependencies."
+SET /p checkin=Check-in updates (Y or N)? 
+
+ECHO.
+ECHO Getting latest version...
+%tfs% get %target% /version:T /force /recursive /noprompt
+
+ECHO.
+ECHO Checking out dependencies...
+%tfs% checkout %target% /recursive /noprompt
+
+ECHO.
+ECHO Updating dependencies...
+XCOPY %source% %target% /Y
+
+ECHO.
+ECHO Building solution
+%vs% %solution% /Build "Release|Any CPU"
+
+IF /I "%checkin%" == "Y" GOTO Checkin
+GOTO Finalize
+
+:Checkin
+ECHO.
+ECHO Checking in dependencies...
+%tfs% checkin %target% /noprompt /recursive /comment:%checkinComment%
+
+:Finalize
+ECHO.
+ECHO Update complete
