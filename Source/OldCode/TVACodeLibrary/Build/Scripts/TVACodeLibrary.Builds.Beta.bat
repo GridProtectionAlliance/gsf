dbf5aa104a21edbab868604e59384d49d880c545
pinalcpatel <pcpatel@tva.gov>
2011-05-27 13:53:04 +0000

    Renamed build script batch files.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05]$/tvacodelibrary/Main;C67255

--- Source/OldCode/TVACodeLibrary/Build/Scripts/TVACodeLibrary.Builds.Beta.bat
+++ Source/OldCode/TVACodeLibrary/Build/Scripts/TVACodeLibrary.Builds.Beta.bat
@@ -1,5 +1,5 @@
 ::*******************************************************************************************************
-::  BuildNightly.bat - Gbtc
+::  TVACodeLibrary.Builds.Beta.bat - Gbtc
 ::
 ::  Tennessee Valley Authority, 2009
 ::  No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.
@@ -8,12 +8,15 @@
 ::
 ::  Code Modification History:
 ::  -----------------------------------------------------------------------------------------------------
-::  10/20/2009 - Pinal C. Patel
+::  10/05/2009 - Pinal C. Patel
 ::       Generated original version of source code.
+::  10/20/2009 - Pinal C. Patel
+::       Modified to force a build and suppress archives from being published to public locations.
 ::  08/02/2010 - Pinal C. Patel
 ::       Updated to use MSBuild 4.0.
 ::
 ::*******************************************************************************************************
 
 @ECHO OFF
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe TVACodeLibrary.buildproj /l:FileLogger,Microsoft.Build.Engine;logfile=TVACodeLibrary.output
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe TVACodeLibrary.buildproj /p:ForceBuild=true /l:FileLogger,Microsoft.Build.Engine;logfile=TVACodeLibrary.output
+PAUSE
