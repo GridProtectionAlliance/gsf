e9aef508749ee738df919801ef1583244142933b
mlorenzo <leeran7742@gmail.com>
2011-06-06 21:10:00 +0000

    Fixed Home User Control.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C67551

--- Source/OldCode/TimeSeriesFramework/Source/Applications/TsfManager/HomeUserControl.xaml.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Applications/TsfManager/HomeUserControl.xaml.cs
@@ -22,6 +22,7 @@ namespace TsfManager
     /// </summary>
     public partial class HomeUserControl : UserControl
     {
+
         private ObservableCollection<MenuDataItem> m_menuDataItem;
 
         public HomeUserControl()
@@ -30,12 +31,54 @@ namespace TsfManager
             m_menuDataItem = ((MainWindow)Application.Current.MainWindow).MenuDataItems;
         }
 
-        private void ButtonBrowseDevices_Click(object sender, RoutedEventArgs e)
+        private void CommonClickHandler(object sender, RoutedEventArgs e)
         {
+            MenuDataItem item = new MenuDataItem();
+            FrameworkElement source = e.Source as FrameworkElement;
 
-            (m_menuDataItem.FirstOrDefault(m => m.MenuText == "Browse")).Command.Execute(null);
-            
-            //m_menuDataItem.Select(m => m.UserControlPath.Contains("DeviceList"));
+            switch(source.Name)
+            {
+                case "ButtonBrowseDevices": GetMenuDataItem(m_menuDataItem, "Browse", ref item);
+                                            break;
+                case "ButtonSubscriberRequest": GetMenuDataItem(m_menuDataItem, "Request", ref item);
+                                                break;
+                case "ButtonSubscribers": GetMenuDataItem(m_menuDataItem, "Subscribers", ref item);
+                                          break;
+                case "ButtonMeasurementGroup": GetMenuDataItem(m_menuDataItem, "Measurement Groups", ref item);
+                                               break;
+                case "ButtonSubscriberMeasurement": GetMenuDataItem(m_menuDataItem, "Subscriber Measurement", ref item);
+                                                    break;
+                case "ButtonMeasurementSubscription": GetMenuDataItem(m_menuDataItem, "Measurement Subscription", ref item);
+                                                      break;
+                case "ButtonSecurity": GetMenuDataItem(m_menuDataItem, "Security", ref item);
+                                       break;
+                default: break;
+            }
+
+            if (item.MenuText != null)
+            {
+                item.Command.Execute(null);
+            }
+
+        }
+
+        private void GetMenuDataItem(ObservableCollection<MenuDataItem> items, string stringToMatch, ref MenuDataItem item)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].MenuText.Contains(stringToMatch))
+                {
+                    item = items[i];
+                    break;
+                }
+                else
+                {
+                    if (items[i].SubMenuItems.Count > 0)
+                    {
+                        GetMenuDataItem(items[i].SubMenuItems, stringToMatch, ref item);
+                    }
+                }
+            }
         }
     }
 }
