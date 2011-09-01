cdc27636cf0a26c16a60fbd6c469e39ad09a17c0
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2011-09-01 18:01:30 +0000

    Historian: Version change for build Historian_v1.1.88.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C70054

--- Source/OldCode/Historian/Current Version/Source/Libraries/Hadoop.Replication/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Libraries/Hadoop.Replication/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.87.0")]
+[assembly: AssemblyVersion("1.1.88.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
