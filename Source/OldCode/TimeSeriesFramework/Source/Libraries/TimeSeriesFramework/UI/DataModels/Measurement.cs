35ffcfe521a26d1d7df69abbbbea434a526a3d8a
mthakkar <hello_mehul@yahoo.com>
2011-10-03 21:22:01 +0000

    TimeSeriesFramework.UI: Added static methods to support output stream measurements assignments along with some visual changes.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C70993

--- Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/UI/DataModels/Measurement.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/UI/DataModels/Measurement.cs
@@ -1012,6 +1012,69 @@ namespace TimeSeriesFramework.UI.DataModels
             }
         }
 
+        /// <summary>
+        /// Retrieves unassigned measurements for output stream.
+        /// </summary>
+        /// <param name="database"><see cref="AdoDataConnection"/> to connection to database.</param>
+        /// <param name="outputStreamID">ID of the output stream to filter data.</param>
+        /// <returns><see cref="ObservableCollection{T}"/> type collection of Measurements.</returns>
+        public static ObservableCollection<Measurement> GetNewOutputStreamMeasurements(AdoDataConnection database, int outputStreamID)
+        {
+            bool createdConnection = false;
+
+            try
+            {
+                createdConnection = CreateConnection(ref database);
+                ObservableCollection<Measurement> measurementList = new ObservableCollection<Measurement>();
+                string query = database.ParameterizedQueryString("SELECT * FROM MeasurementDetail WHERE NodeID = {0} AND PointID NOT IN (SELECT PointID FROM OutputStreamMeasurement WHERE AdapterID = {1}) " +
+                    "ORDER BY SignalReference", "nodeID", "outputStreamID");
+                DataTable measurementTable = database.Connection.RetrieveData(database.AdapterType, query, DefaultTimeout, database.CurrentNodeID(), outputStreamID);
+
+                foreach (DataRow row in measurementTable.Rows)
+                {
+                    if (row.Field<string>("SignalAcronym") != "STAT")
+                    {
+                        measurementList.Add(new Measurement()
+                        {
+                            SignalID = database.Guid(row, "SignalID"),
+                            HistorianID = row.ConvertNullableField<int>("HistorianID"),
+                            PointID = row.ConvertField<int>("PointID"),
+                            DeviceID = row.ConvertNullableField<int>("DeviceID"),
+                            PointTag = row.Field<string>("PointTag"),
+                            AlternateTag = row.Field<string>("AlternateTag"),
+                            SignalTypeID = row.ConvertField<int>("SignalTypeID"),
+                            PhasorSourceIndex = row.ConvertNullableField<int>("PhasorSourceIndex"),
+                            SignalReference = row.Field<string>("SignalReference"),
+                            Adder = row.ConvertField<double>("Adder"),
+                            Multiplier = row.ConvertField<double>("Multiplier"),
+                            Internal = Convert.ToBoolean(row.Field<object>("Internal")),
+                            Subscribed = Convert.ToBoolean(row.Field<object>("Subscribed")),
+                            Description = row.Field<string>("Description"),
+                            Enabled = false,    // We set this false here because we are going to use this flag to see if user has selected it on the wizard.
+                            m_historianAcronym = row.Field<string>("HistorianAcronym"),
+                            m_deviceAcronym = row.Field<object>("DeviceAcronym") == null ? string.Empty : row.Field<string>("DeviceAcronym"),
+                            m_signalName = row.Field<string>("SignalName"),
+                            m_signalAcronym = row.Field<string>("SignalAcronym"),
+                            m_signalSuffix = row.Field<string>("SignalTypeSuffix"),
+                            m_phasorLabel = row.Field<string>("PhasorLabel"),
+                            m_framesPerSecond = Convert.ToInt32(row.Field<object>("FramesPerSecond") ?? 30),
+                            m_id = row.Field<string>("ID"),
+                            m_companyAcronym = row.Field<object>("CompanyAcronym") == null ? string.Empty : row.Field<string>("CompanyAcronym"),
+                            m_companyName = row.Field<object>("CompanyName") == null ? string.Empty : row.Field<string>("CompanyName"),
+                            Selected = false
+                        });
+                    }
+                }
+
+                return measurementList;
+            }
+            finally
+            {
+                if (createdConnection && database != null)
+                    database.Dispose();
+            }
+        }
+
         #endregion
     }
 }
