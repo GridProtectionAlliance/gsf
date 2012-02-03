602cbf2f6e6b8739a503aa208ee5069c115f56ca
StephenCWills <swills@gridprotectionalliance.org>
2012-02-03 20:24:29 +0000

    Created initial version of alarm adapter.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C74599

---
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/Adapters/DataQualityMonitoring/Alarm.cs
@@ -0,0 +1,132 @@
+﻿//******************************************************************************************************
+//  Alarm.cs - Gbtc
+//
+//  Copyright © 2010, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/31/2012 - Stephen C. Wills
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using Ciloci.Flee;
+using TimeSeriesFramework;
+using TVA;
+
+namespace DataQualityMonitoring
+{
+    public class Alarm
+    {
+        #region [ Members ]
+
+        // Fields
+        private string m_expressionText;
+        private ExpressionContext m_expressionContext;
+        private IGenericExpression<bool> m_expression;
+
+        private double m_lastValue;
+        private long m_lastChanged;
+        private long m_latestTimestamp;
+
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Alarm"/> class.
+        /// </summary>
+        public Alarm()
+        {
+            m_expressionContext = new ExpressionContext(this);
+            m_expressionContext.Imports.AddType(typeof(Math));
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets or sets the expression text which determines
+        /// the condition on which an alarm event occurs.
+        /// </summary>
+        public string ExpressionText
+        {
+            get
+            {
+                return m_expressionText;
+            }
+            set
+            {
+                m_expressionContext.Variables["value"] = 0.0;
+                m_expression = m_expressionContext.CompileGeneric<bool>(value);
+                m_expressionText = value;
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets the identification number of the alarm.
+        /// </summary>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identification number of the
+        /// signal whose value is monitored by the alarm.
+        /// </summary>
+        public Guid SignalID { get; set; }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Tests the given measurement to determine
+        /// whether its value triggers an alarm event.
+        /// </summary>
+        /// <param name="signal">The signal whose value is to be checked.</param>
+        /// <returns>True if the event is triggers; false otherwise.</returns>
+        public bool Condition(IMeasurement signal)
+        {
+            long signalTimestamp = signal.Timestamp;
+
+            // Keep track of the last time the value changed
+            if (signal.Value != m_lastValue)
+            {
+                m_lastValue = signal.Value;
+                m_lastChanged = signalTimestamp;
+            }
+
+            // Keep track of the latest timestamp
+            m_latestTimestamp = signalTimestamp;
+
+            // Set the value of the expression variable
+            m_expressionContext.Variables["value"] = signal.AdjustedValue;
+
+            // Evaluate the expression
+            return m_expression.Evaluate();
+        }
+
+        // Determines whether the value has
+        // flatlined over the given time interval
+        private bool Flatlined(double seconds)
+        {
+            long dist = Ticks.FromSeconds(seconds);
+            long diff = m_latestTimestamp - m_lastChanged;
+            return diff >= dist;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/Adapters/DataQualityMonitoring/AlarmAdapter.cs
@@ -0,0 +1,208 @@
+﻿//******************************************************************************************************
+//  AlarmAdapter.cs - Gbtc
+//
+//  Copyright © 2010, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/31/2012 - Stephen C. Wills
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using TimeSeriesFramework;
+using TimeSeriesFramework.Adapters;
+using TVA;
+using TVA.Data;
+
+namespace DataQualityMonitoring
+{
+    public class AlarmAdapter : FacileActionAdapterBase
+    {
+        #region [ Members ]
+
+        // Fields
+        private List<Alarm> m_alarms;
+        private ConcurrentQueue<IMeasurement> m_measurementQueue;
+        private Thread m_processThread;
+        private Semaphore m_processSemaphore;
+        private long m_eventCount;
+        private bool m_supportsTemporalProcessing;
+
+        #endregion
+
+        #region [ Properties ]
+
+        /// <summary>
+        /// Gets the flag indicating if this adapter supports temporal processing.
+        /// </summary>
+        public override bool SupportsTemporalProcessing
+        {
+            get
+            {
+                return m_supportsTemporalProcessing;
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Initializes the <see cref="AlarmAdapter"/>.
+        /// </summary>
+        public override void Initialize()
+        {
+            string filterExpression;
+
+            // Run base class initialization
+            base.Initialize();
+
+            Dictionary<string, string> settings = Settings;
+            string setting;
+
+            // Load optional parameter
+            if (settings.TryGetValue("supportsTemporalProcessing", out setting))
+                m_supportsTemporalProcessing = setting.ParseBoolean();
+            else
+                m_supportsTemporalProcessing = false;
+
+            // Create alarms using definitions from the database
+            m_alarms = DataSource.Tables["Alarms"].Rows.Cast<DataRow>()
+                .Where(row => row.ConvertField<uint>("AdapterID") == ID)
+                .Select(row => CreateAlarm(row))
+                .ToList();
+
+            // Generate filter expression for input measurements
+            filterExpression = m_alarms.Select(a => a.SignalID)
+                .Distinct()
+                .Select(id => id.ToString())
+                .Aggregate((list, id) => list + ";" + id);
+
+            // Set input measurement keys for measurement routing
+            InputMeasurementKeys = ParseInputMeasurementKeys(DataSource, filterExpression);
+        }
+
+        /// <summary>
+        /// Starts the <see cref="AlarmAdapter"/>, or restarts it if it is already running.
+        /// </summary>
+        public override void Start()
+        {
+            base.Start();
+
+            m_measurementQueue = new ConcurrentQueue<IMeasurement>();
+            m_processThread = new Thread(ProcessMeasurements);
+            m_processSemaphore = new Semaphore(0, int.MaxValue);
+            m_eventCount = 0L;
+
+            m_processThread.Start();
+        }
+
+        /// <summary>
+        /// Stops the <see cref="AlarmAdapter"/>.
+        /// </summary>
+        public override void Stop()
+        {
+            base.Stop();
+
+            if ((object)m_processSemaphore != null)
+            {
+                m_processSemaphore.Dispose();
+                m_processSemaphore = null;
+            }
+
+            if ((object)m_processThread != null && m_processThread.ThreadState == ThreadState.Running)
+            {
+                m_processThread.Join();
+                m_processThread = null;
+            }
+        }
+
+        /// <summary>
+        /// Queues a collection of measurements for processing.
+        /// </summary>
+        /// <param name="measurements">Measurements to queue for processing.</param>
+        public override void QueueMeasurementsForProcessing(IEnumerable<IMeasurement> measurements)
+        {
+            base.QueueMeasurementsForProcessing(measurements);
+
+            foreach (IMeasurement measurement in measurements)
+            {
+                m_measurementQueue.Enqueue(measurement);
+                m_processSemaphore.Release();
+            }
+        }
+
+        /// <summary>
+        /// Gets a short one-line status of this <see cref="AdapterBase"/>.
+        /// </summary>
+        /// <param name="maxLength">Maximum number of available characters for display.</param>
+        /// <returns>A short one-line summary of the current status of this <see cref="AdapterBase"/>.</returns>
+        public override string GetShortStatus(int maxLength)
+        {
+            return string.Format("{0} events processed since last start", m_eventCount).CenterText(maxLength);
+        }
+
+        // Creates an alarm using data defined in the database.
+        private Alarm CreateAlarm(DataRow row)
+        {
+            return new Alarm()
+            {
+                ExpressionText = row["ExpressionText"].ToString(),
+                ID = System.Guid.Parse(row.Field<object>("ID").ToString()),
+                SignalID = System.Guid.Parse(row.Field<object>("SignalID").ToString())
+            };
+        }
+
+        // Processes measurements in the queue.
+        private void ProcessMeasurements()
+        {
+            IMeasurement measurement, clone;
+            IEnumerable<Alarm> events;
+
+            while (Enabled)
+            {
+                if (m_processSemaphore.WaitOne() && m_measurementQueue.TryDequeue(out measurement))
+                {
+                    // Get alarms that triggered events
+                    events = m_alarms.Where(a => a.SignalID == measurement.ID)
+                        .Where(a => a.Condition(measurement));
+
+                    // Create event measurements and send them into the system
+                    foreach (Alarm a in events)
+                    {
+                        clone = Measurement.Clone(measurement);
+                        clone.ID = a.ID;
+                        OnNewMeasurement(clone);
+                        m_eventCount++;
+                    }
+                }
+            }
+        }
+
+        // Helper method to raise the NewMeasurements event
+        // when only a single measurement is to be provided.
+        private void OnNewMeasurement(IMeasurement measurement)
+        {
+            OnNewMeasurements(new IMeasurement[] { measurement });
+        }
+
+        #endregion
+    }
+}
