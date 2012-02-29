a4e6e10adb6e8da7c963ecdf4f45320ee4223302
pinalcpatel <pcpatel@tva.gov>
2012-02-29 05:40:24 +0000

    TVACodeLibrary: Version change for build TVACodeLibrary_v4.0.3.50.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05]$/tvacodelibrary/Main;C73461

--- Source/OldCode/TVACodeLibrary/Source/Libraries/TVA.Core/Properties/AssemblyInfo.cs
+++ Source/OldCode/TVACodeLibrary/Source/Libraries/TVA.Core/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("4.0.3.49")]
+[assembly: AssemblyVersion("4.0.3.50")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/OldCode/TVACodeLibrary/Source/Libraries/TVA.ServiceProcess/Properties/AssemblyInfo.cs
+++ Source/OldCode/TVACodeLibrary/Source/Libraries/TVA.ServiceProcess/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("4.0.3.49")]
+[assembly: AssemblyVersion("4.0.3.50")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
