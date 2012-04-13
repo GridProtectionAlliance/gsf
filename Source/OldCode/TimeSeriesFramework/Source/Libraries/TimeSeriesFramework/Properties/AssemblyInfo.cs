04bf4dfb15e0b791efe5678fc99e48187f2a8acd
pinalcpatel <pcpatel@tva.gov>
2012-04-13 16:52:37 +0000

    TimeSeriesFramework: Version change for build TimeSeriesFramework_v1.1.60.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C77441

--- Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/Properties/AssemblyInfo.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.59.0")]
-[assembly: AssemblyVersion("1.1.59.0")]
+// [assembly: AssemblyVersion("1.1.60.0")]
+[assembly: AssemblyVersion("1.1.60.0")]
 
