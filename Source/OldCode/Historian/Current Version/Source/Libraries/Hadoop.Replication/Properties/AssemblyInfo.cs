7b7a3f7800605a2d78e9fde48534358487e9313f
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2012-07-05 19:55:11 +0000

    Historian: Version change for build Historian_v1.1.163.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C78896

--- Source/OldCode/Historian/Current Version/Source/Libraries/Hadoop.Replication/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Libraries/Hadoop.Replication/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.162.0")]
+[assembly: AssemblyVersion("1.1.163.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/OldCode/Historian/Current Version/Source/Tools/HistorianView/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Tools/HistorianView/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.162.0")]
-[assembly: AssemblyVersion("1.1.162.0")]
+// [assembly: AssemblyVersion("1.1.163.0")]
+[assembly: AssemblyVersion("1.1.163.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
