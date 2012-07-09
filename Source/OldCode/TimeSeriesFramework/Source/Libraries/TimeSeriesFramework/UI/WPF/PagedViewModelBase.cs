333277164042f0f42600322a89ecea3f9846563a
alexfoglia <fa.foglia@ono.com>
2012-07-09 16:13:07 +0000

    This line "if (value != -1)" (Line 144) was commented out (this belongs to the PagedViewModelBase.cs file (from TimeSeries). I set this line back to work and now the selected row does not change after modifying any propoerties of any adapter.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C78942

--- Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/UI/WPF/PagedViewModelBase.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/UI/WPF/PagedViewModelBase.cs
@@ -141,8 +141,10 @@ namespace TimeSeriesFramework.UI
             }
             set
             {
-                //if (value != -1)
-                m_currentSelectedIndex = value;
+                if (value != -1)
+                {
+                    m_currentSelectedIndex = value;
+                }
                 OnPropertyChanged("CurrentSelectedIndex");
             }
         }
