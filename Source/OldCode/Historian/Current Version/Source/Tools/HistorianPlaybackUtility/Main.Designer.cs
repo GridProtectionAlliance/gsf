77c84e5c3a7c51183c3bb27ee2b1570a9519b87f
salveraniket <salver.aniket@gmail.com>
2012-07-20 13:13:33 +0000

    Historian: Solved the issue raised in Fortify,  Which complaints about a function that is failing to dispose.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C79091

--- Source/OldCode/Historian/Current Version/Source/Tools/HistorianPlaybackUtility/Main.Designer.cs
+++ Source/OldCode/Historian/Current Version/Source/Tools/HistorianPlaybackUtility/Main.Designer.cs
@@ -15,6 +15,7 @@
         {
             if (disposing && (components != null))
             {
+                this.Font.Dispose();
                 components.Dispose();
             }
             base.Dispose(disposing);
