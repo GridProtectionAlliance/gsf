f1cbc86220f50531cc4551fa1eaddc75db4cfd24
pinalcpatel <pcpatel@tva.gov>
2012-11-01 18:08:58 +0000

    Historian: Version change for build Historian_v1.1.191.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C80848

--- Source/OldCode/Historian/Current Version/Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.190.0")]
+[assembly: AssemblyVersion("1.1.191.0")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
