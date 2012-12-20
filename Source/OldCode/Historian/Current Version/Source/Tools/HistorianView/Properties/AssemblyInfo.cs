345d765aada26546a46a4f07528c5d9e04129b67
pinalcpatel <pcpatel@tva.gov>
2012-12-20 21:39:59 +0000

    Historian: Version change for build Historian_v1.1.205.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C81878

--- Source/OldCode/Historian/Current Version/Source/Tools/HistorianView/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Tools/HistorianView/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.204.0")]
-[assembly: AssemblyVersion("1.1.204.0")]
+// [assembly: AssemblyVersion("1.1.205.0")]
+[assembly: AssemblyVersion("1.1.205.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
