14cf7e60c0bf34e0239207d78b6de4a51b0089cb
StephenCWills <swills@gridprotectionalliance.org>
2013-03-28 19:18:51 +0000

    Changes migrated from GSF: TimeSeriesFramework.UI: Fixed event IDs for Windows event log messages when users or groups are deleted.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C83781

--- Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/UI/DataModels/UserAccount.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/TimeSeriesFramework/UI/DataModels/UserAccount.cs
@@ -493,7 +493,7 @@ namespace TimeSeriesFramework.UI.DataModels
                 database.Connection.ExecuteNonQuery(database.ParameterizedQueryString("DELETE FROM UserAccount WHERE ID = {0}", "userAccountID"), DefaultTimeout, database.Guid(userAccountID));
 
                 // Write to the event log
-                CommonFunctions.LogEvent(string.Format("User \"{0}\" deleted successfully by user \"{1}\".", userName, CommonFunctions.CurrentUser), 2);
+                CommonFunctions.LogEvent(string.Format("User \"{0}\" deleted successfully by user \"{1}\".", userName, CommonFunctions.CurrentUser), 12);
 
                 return "User account deleted successfully";
             }
