3acb6c06ac00b246c5e0ef09a287ede25f974254
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2013-04-23 20:29:24 +0000

    Added OSI-PI adapters project to solution.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C84191

---
+++ Source/Libraries/Adapters/PIAdapters/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Assembly identity attributes.
+[assembly: AssemblyVersion("2.0.43.0")]
+
+// Informational attributes.
+[assembly: AssemblyCompany("Grid Protection Alliance")]
+[assembly: AssemblyCopyright("Copyright © 2013.  All Rights Reserved.")]
+[assembly: AssemblyProduct("openPDC")]
+
+// Assembly manifest attributes.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+
+[assembly: AssemblyDefaultAlias("PIAdapters")]
+[assembly: AssemblyDescription("OSI-PI Adapters for openPDC.")]
+[assembly: AssemblyTitle("PIAdapters")]
+
+// Other configuration attributes.
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+[assembly: Guid("D8064F6E-0B5D-4CBB-884E-75A2E11CA779")]
