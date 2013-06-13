5640802445dec7f3524d77b852e753d123289c3a
StephenCWills <swills@gridprotectionalliance.org>
2013-06-13 18:33:49 +0000

    TimeSeriesFramework: Version change for build TimeSeriesFramework_v1.1.137.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/TimeSeriesFramework/Main;C85356

--- Source/OldCode/TimeSeriesFramework/Source/Libraries/Adapters/PIAdapters/Properties/AssemblyInfo.cs
+++ Source/OldCode/TimeSeriesFramework/Source/Libraries/Adapters/PIAdapters/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.136.0")]
+[assembly: AssemblyVersion("1.1.137.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
