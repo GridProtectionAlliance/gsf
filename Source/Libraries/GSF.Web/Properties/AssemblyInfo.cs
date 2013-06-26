2f5b19fd9335e352a62fa78c10a87c3a9f0e92e1
StephenCWills <swills@gridprotectionalliance.org>
2013-06-26 12:57:43 +0000

    GridSolutionsFramework: Version change for build GridSolutionsFramework_v2.0.55.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C85574

--- Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Web.UI;
 using GSF.Web.Hosting;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.0.54.0")]
+[assembly: AssemblyVersion("2.0.55.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
