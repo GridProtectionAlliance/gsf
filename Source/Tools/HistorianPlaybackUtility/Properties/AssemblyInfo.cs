0781796705fe5cb97801b72cfd06e16b41fbcc15
StephenCWills <swills@gridprotectionalliance.org>
2013-08-09 18:27:23 +0000

    GridSolutionsFramework: Version change for build GridSolutionsFramework_v2.0.77.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C86551

--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.0.76.0")]
+[assembly: AssemblyVersion("2.0.77.0")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
