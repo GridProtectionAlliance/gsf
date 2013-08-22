e2ffc63c1cbdd310ab8e526545c3c629def14f18
StephenCWills <swills@gridprotectionalliance.org>
2013-08-22 15:06:20 +0000

    Historian: Version change for build Historian_v1.1.240.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Historian;C86837

--- Source/OldCode/Historian/Current Version/Source/Tools/HistorianView/Properties/AssemblyInfo.cs
+++ Source/OldCode/Historian/Current Version/Source/Tools/HistorianView/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.239.0")]
-[assembly: AssemblyVersion("1.1.239.0")]
+// [assembly: AssemblyVersion("1.1.240.0")]
+[assembly: AssemblyVersion("1.1.240.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
