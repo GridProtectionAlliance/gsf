7e0ca8b0c4b789ebe6a293037db48f8ca5387513
StephenCWills <swills@gridprotectionalliance.org>
2013-10-21 14:07:51 +0000

    GSF.Core: Fixed GetOrAdd extensions for Dictionary in GSF.Collections.CollectionsExtensions.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C87594

--- Source/Libraries/GSF.Core/Collections/CollectionExtensions.cs
+++ Source/Libraries/GSF.Core/Collections/CollectionExtensions.cs
@@ -133,7 +133,14 @@ namespace GSF.Collections
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> valueFactory)
         {
             TValue value;
-            return dictionary.TryGetValue(key, out value) ? value : valueFactory(key);
+
+            if (!dictionary.TryGetValue(key, out value))
+            {
+                value = valueFactory(key);
+                dictionary.Add(key, value);
+            }
+
+            return value;
         }
 
         /// <summary>
@@ -148,7 +155,14 @@ namespace GSF.Collections
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
         {
             TValue tryGetValue;
-            return dictionary.TryGetValue(key, out tryGetValue) ? tryGetValue : value;
+
+            if (!dictionary.TryGetValue(key, out tryGetValue))
+            {
+                tryGetValue = value;
+                dictionary.Add(key, tryGetValue);
+            }
+
+            return tryGetValue;
         }
 
         /// <summary>
