ee5f59d7b7d813448abebbb7bb30bf6c6c386c88
jadamcrain <jadamcrain@gmail.com>
2014-03-27 14:35:39 +0000

    Added a config generation project to create valid XML for the adapter. Updated the example comms configuration with the new XML.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C89635

---
+++ Source/Libraries/Adapters/DNP3ConfigGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Xml.Serialization;
+
+using DNP3Adapters;
+
+namespace DNP3ConfigGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ser = new XmlSerializer(typeof(MasterConfiguration));
+            var stream = new StreamWriter("device.xml");
+            try
+            {
+                var config = new MasterConfiguration();
+                ser.Serialize(stream, config);
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+    }
+}
