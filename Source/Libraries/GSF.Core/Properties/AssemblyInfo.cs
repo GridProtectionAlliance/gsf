4c1f7d0b6993279bf0dfdb4400d1f5976b8a5533
gsfbuildbot <support@gridprotectionalliance.org>
2014-04-22 18:18:02 +0000

    GridSolutionsFramework: Version change for build GridSolutionsFramework_v2.0.177.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C90063

--- Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.0.176.0")]
+[assembly: AssemblyVersion("2.0.177.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
