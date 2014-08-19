a936a1e0039d64edd7109b7e6a511a2396f7a583
Ozias <gavineholden@gmail.com>
2014-08-19 17:55:00 +0000

    Adding NuGet Executable for Build Script Execution
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C91260

--- Build/Scripts/BuildNightly.bat
+++ Build/Scripts/BuildNightly.bat
@@ -25,5 +25,93 @@
 ::
 ::*******************************************************************************************************
 
-@ECHO OFF
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe GridSolutionsFramework.buildproj /l:FileLogger,Microsoft.Build.Engine;logfile=GridSolutionsFramework.output
+echo off
+
+setlocal EnableDelayedExpansion
+
+set show_help=0
+if [%1]==[] set show_help=1
+if "%1"=="/?" set show_help=1
+
+if %show_help%==1  (
+	echo.
+	echo.
+	echo Help File:
+	echo Explain Parameters
+	echo.
+	echo 	/u = TFS User Name
+	echo 	/p = TFS Password
+	echo 	/d = Build Deploy Location (required)
+	echo 	/pkg = Nuget Package Location (required)
+	echo 	/l = Logger
+	echo.
+	echo.
+)
+
+set tfsUser_name=""
+set tfsPassword=""
+set deploy=""
+set package=""
+set logger=""
+
+set %%y=""
+set item=""
+
+set set_user=0
+set set_pass=0
+set set_deploy=0
+set set_package=0
+set set_log=0
+
+for %%y in (%*) do (
+	
+	rem Set TFS User Name 
+	if !set_user!==1 (
+		set tfsUser_name=%%y
+		set set_user=0
+	)
+	if %%y==/u (
+		set set_user=1
+	)
+	
+	rem Set TFS Password 
+	if !set_pass!==1 (
+		set tfsPassword=%%y
+		set set_pass=0
+	)
+	if %%y==/p (
+		set set_pass=1
+	)
+	
+	rem Set Build Deploy Folder
+		if !set_deploy!==1 (
+		set deploy=%%y
+		set set_deploy=0
+	)
+	if %%y==/d (
+		set set_deploy=1
+	)
+	
+	rem Set Nuget Package Location
+		if !set_package!==1 (
+		set package=%%y
+		set package=%%y
+		set set_package=0
+	)
+	if %%y==/pkg (
+		set set_package=1
+		)
+		
+	rem Set Logger
+		if !set_log!==1 (
+		set logger=%%y
+		set set_log=0
+	)
+	if %%y==/l (
+		set set_log=1
+		)
+)
+
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe GridSolutionsFramework.buildproj /p:BuildDeployFolder=%deploy% /p:NugetApiKey=915f11e2-62dc-4680-8500-ceb11116142a /p:NugetPackagesFolder=%package% /p:TfsUserName=%tfsUser_name% /p:TfsPassword=%tfsPassword% /p:ForceBuild=true /l:FileLogger,Microsoft.Build.Engine;logfile=%logger%  
+
+endlocal
