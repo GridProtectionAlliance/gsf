6ddd5448115925c3c6ad3a0b4747de5f7371783c
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2014-09-25 19:27:08 +0000

    GSF.Core: Fixed an issue with keys on static cache for fast object factory.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C91562

--- Source/Libraries/GSF.Core/FastObjectFactory.cs
+++ Source/Libraries/GSF.Core/FastObjectFactory.cs
@@ -55,10 +55,7 @@ namespace GSF
 
             if ((object)constructor == null)
                 throw new InvalidOperationException("No parameterless constructor exists for type " + type.FullName);
-#if MONO
-            // TODO: Dynamic IL constructor code (below) needs further testing on Mono to be sure it works properly
-            s_createObjectFunction = () => (T)Activator.CreateInstance(type);
-#else
+
             // This is markedly faster than using Activator.CreateInstance
             DynamicMethod method = new DynamicMethod("ctor$" + type.Name, type, null, type);
             ILGenerator generator = method.GetILGenerator();
@@ -67,7 +64,6 @@ namespace GSF
             generator.Emit(OpCodes.Ret);
 
             s_createObjectFunction = (Func<T>)method.CreateDelegate(typeof(Func<T>));
-#endif
         }
 
         /// <summary>
@@ -127,8 +123,13 @@ namespace GSF
             if (type.IsAbstract || ((!typeT.IsClass || !type.IsSubclassOf(typeT)) && (!typeT.IsInterface || (object)type.GetInterface(typeT.Name) == null)))
                 throw new InvalidOperationException("Specified type parameter is not a subclass or interface implementation of function type definition");
 
-            int key = type.GUID.GetHashCode() ^ typeT.GUID.GetHashCode();
-
+            int key;
+#if MONO
+            // Type.GUID always returns zero guid on Mono
+            key = type.FullName.GetHashCode() ^ typeT.FullName.GetHashCode();
+#else
+            key = type.GUID.GetHashCode() ^ typeT.GUID.GetHashCode();
+#endif
             return (Func<T>)s_createObjectFunctions.GetOrAdd(key, k =>
             {
                 // Get parameterless constructor for this type
@@ -136,10 +137,7 @@ namespace GSF
 
                 if ((object)constructor == null)
                     throw new InvalidOperationException("No parameterless constructor exists for type " + type.FullName);
-#if MONO
-                // TODO: Dynamic IL constructor code (below) needs further testing on Mono to be sure it works properly
-                return (Func<T>)(() => (T)Activator.CreateInstance(type));
-#else
+
                 // This is markedly faster than using Activator.CreateInstance
                 DynamicMethod method = new DynamicMethod("ctor_type$" + type.Name, type, null, type);
                 ILGenerator generator = method.GetILGenerator();
@@ -148,7 +146,6 @@ namespace GSF
                 generator.Emit(OpCodes.Ret);
 
                 return (Func<T>)method.CreateDelegate(typeof(Func<T>));
-#endif
             });
         }
     }
