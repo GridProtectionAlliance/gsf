ee1053ea690890fe62067d71a683e59de4973f5f
StephenCWills <swills@gridprotectionalliance.org>
2014-10-22 17:32:17 +0000

    StatHistorianReportGenerator: Modified Program.cs so that correctness reports can be generated.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C91904

--- Source/Tools/StatHistorianReportGenerator/Program.cs
+++ Source/Tools/StatHistorianReportGenerator/Program.cs
@@ -37,59 +37,25 @@ namespace StatHistorianReportGenerator
         [STAThread]
         static void Main()
         {
-            CompletenessReportGenerator completenessReportGenerator;
             Arguments args;
             string arg;
 
-            string reportLocation = "";
-            string reportFileName = "";
-            DateTime reportDate;
-            double threshold;
-
             if (Environment.GetCommandLineArgs().Length > 1)
             {
-                completenessReportGenerator = new CompletenessReportGenerator();
                 args = new Arguments(Environment.CommandLine, true);
 
-                if (TryGetValue(args, "archiveLocation", out arg))
-                    completenessReportGenerator.ArchiveLocation = arg;
-
-                if (TryGetValue(args, "reportLocation", out arg))
-                    reportLocation = arg;
-
-                if (TryGetValue(args, "reportFileName", out arg))
-                    reportFileName = arg;
-
-                if (TryGetValue(args, "title", out arg))
-                    completenessReportGenerator.TitleText = arg;
-
-                if (TryGetValue(args, "company", out arg))
-                    completenessReportGenerator.CompanyText = arg;
-
-                if (TryGetValue(args, "reportDate", out arg) && DateTime.TryParse(arg, out reportDate))
-                    completenessReportGenerator.ReportDate = reportDate;
-
-                if (TryGetValue(args, "level4Threshold", out arg) && double.TryParse(arg, out threshold))
-                    completenessReportGenerator.Level4Threshold = threshold;
-
-                if (TryGetValue(args, "level3Threshold", out arg) && double.TryParse(arg, out threshold))
-                    completenessReportGenerator.Level3Threshold = threshold;
-
-                if (TryGetValue(args, "level4Alias", out arg))
-                    completenessReportGenerator.Level4Alias = arg;
-
-                if (TryGetValue(args, "level3Alias", out arg))
-                    completenessReportGenerator.Level3Alias = arg;
-
-                if (string.IsNullOrEmpty(reportFileName))
-                    reportFileName = string.Format("{0} {1:yyyy-MM-dd}.pdf", completenessReportGenerator.TitleText, completenessReportGenerator.ReportDate);
-
-                reportLocation = FilePath.GetAbsolutePath(reportLocation);
-
-                if (!Directory.Exists(reportLocation))
-                    Directory.CreateDirectory(reportLocation);
-
-                completenessReportGenerator.GenerateReport().Save(Path.Combine(reportLocation, reportFileName));
+                if (TryGetValue(args, "reportType", out arg))
+                {
+                    if (arg.Equals("completeness", StringComparison.OrdinalIgnoreCase))
+                        GenerateCompletenessReport();
+                    else if (arg.Equals("correctness", StringComparison.OrdinalIgnoreCase))
+                        GenerateCorrectnessReport();
+                }
+                else
+                {
+                    // Generate completeness report by default
+                    GenerateCompletenessReport();
+                }
             }
             else
             {
@@ -97,6 +63,97 @@ namespace StatHistorianReportGenerator
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new Main());
             }
+        }
+
+        private static void GenerateCompletenessReport()
+        {
+            CompletenessReportGenerator completenessReportGenerator = new CompletenessReportGenerator();
+            Arguments args = new Arguments(Environment.CommandLine, true);
+            string arg;
+
+            string reportLocation = "";
+            string reportFileName = "";
+            DateTime reportDate;
+            double threshold;
+
+            if (TryGetValue(args, "archiveLocation", out arg))
+                completenessReportGenerator.ArchiveLocation = arg;
+
+            if (TryGetValue(args, "reportLocation", out arg))
+                reportLocation = arg;
+
+            if (TryGetValue(args, "reportFileName", out arg))
+                reportFileName = arg;
+
+            if (TryGetValue(args, "title", out arg))
+                completenessReportGenerator.TitleText = arg;
+
+            if (TryGetValue(args, "company", out arg))
+                completenessReportGenerator.CompanyText = arg;
+
+            if (TryGetValue(args, "reportDate", out arg) && DateTime.TryParse(arg, out reportDate))
+                completenessReportGenerator.ReportDate = reportDate;
+
+            if (TryGetValue(args, "level4Threshold", out arg) && double.TryParse(arg, out threshold))
+                completenessReportGenerator.Level4Threshold = threshold;
+
+            if (TryGetValue(args, "level3Threshold", out arg) && double.TryParse(arg, out threshold))
+                completenessReportGenerator.Level3Threshold = threshold;
+
+            if (TryGetValue(args, "level4Alias", out arg))
+                completenessReportGenerator.Level4Alias = arg;
+
+            if (TryGetValue(args, "level3Alias", out arg))
+                completenessReportGenerator.Level3Alias = arg;
+
+            if (string.IsNullOrEmpty(reportFileName))
+                reportFileName = string.Format("{0} {1:yyyy-MM-dd}.pdf", completenessReportGenerator.TitleText, completenessReportGenerator.ReportDate);
+
+            reportLocation = FilePath.GetAbsolutePath(reportLocation);
+
+            if (!Directory.Exists(reportLocation))
+                Directory.CreateDirectory(reportLocation);
+
+            completenessReportGenerator.GenerateReport().Save(Path.Combine(reportLocation, reportFileName));
+        }
+
+        private static void GenerateCorrectnessReport()
+        {
+            CorrectnessReportGenerator correctnessReportGenerator = new CorrectnessReportGenerator();
+            Arguments args = new Arguments(Environment.CommandLine, true);
+            string arg;
+
+            string reportLocation = "";
+            string reportFileName = "";
+            DateTime reportDate;
+
+            if (TryGetValue(args, "archiveLocation", out arg))
+                correctnessReportGenerator.ArchiveLocation = arg;
+
+            if (TryGetValue(args, "reportLocation", out arg))
+                reportLocation = arg;
+
+            if (TryGetValue(args, "reportFileName", out arg))
+                reportFileName = arg;
+
+            if (TryGetValue(args, "title", out arg))
+                correctnessReportGenerator.TitleText = arg;
+
+            if (TryGetValue(args, "company", out arg))
+                correctnessReportGenerator.CompanyText = arg;
+
+            if (TryGetValue(args, "reportDate", out arg) && DateTime.TryParse(arg, out reportDate))
+                correctnessReportGenerator.ReportDate = reportDate;
+
+            if (string.IsNullOrEmpty(reportFileName))
+                reportFileName = string.Format("{0} {1:yyyy-MM-dd}.pdf", correctnessReportGenerator.TitleText, correctnessReportGenerator.ReportDate);
+
+            reportLocation = FilePath.GetAbsolutePath(reportLocation);
+
+            if (!Directory.Exists(reportLocation))
+                Directory.CreateDirectory(reportLocation);
+
+            correctnessReportGenerator.GenerateReport().Save(Path.Combine(reportLocation, reportFileName));
         }
 
         private static bool TryGetValue(Arguments args, string arg, out string value)
