142b6317b3d50a9203bd1473802abfd47d28ba50
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2015-02-04 15:55:06 +0000

    GSF: Cleaned up recent check-ins to properly build under Mono.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C93715

--- Source/Libraries/GSF.ServiceModel/Activation/FormatSpecificationBehavior.cs
+++ Source/Libraries/GSF.ServiceModel/Activation/FormatSpecificationBehavior.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//******************************************************************************************************
+//  FormatSpecificationBehavior.cs - Gbtc
+//
+//  Copyright © 2012, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/30/2015 - A.S. Bikle
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+#if !MONO
+
 using System.ServiceModel.Description;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GSF.ServiceModel.Activation
 {
@@ -31,3 +51,5 @@ namespace GSF.ServiceModel.Activation
         }
     }
 }
+
+#endif
--- Source/Libraries/GSF.ServiceModel/Activation/FormatSpecificationInspector.cs
+++ Source/Libraries/GSF.ServiceModel/Activation/FormatSpecificationInspector.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//******************************************************************************************************
+//  FormatSpecificationInspector.cs - Gbtc
+//
+//  Copyright © 2012, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/30/2015 - A.S. Bikle
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+#if !MONO
+
 using System.ServiceModel;
 using System.ServiceModel.Dispatcher;
 using System.ServiceModel.Web;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GSF.ServiceModel.Activation
 {
@@ -18,15 +38,15 @@ namespace GSF.ServiceModel.Activation
         public object AfterReceiveRequest(ref System.ServiceModel.Channels.Message request, IClientChannel channel, InstanceContext instanceContext)
         {
             WebOperationContext context = WebOperationContext.Current;
-            if (context == null)
-            {
+
+            if ((object)context == null)
                 return null;
-            }
-            var acceptHeader = context.IncomingRequest.Headers["Accept"];
-            if (acceptHeader != null && acceptHeader.Contains("application/json"))
-            {
+
+            string acceptHeader = context.IncomingRequest.Headers["Accept"];
+
+            if ((object)acceptHeader != null && acceptHeader.Contains("application/json"))
                 context.OutgoingResponse.Format = WebMessageFormat.Json;
-            }
+
             return null;
         }
 
@@ -35,3 +55,5 @@ namespace GSF.ServiceModel.Activation
         }
     }
 }
+
+#endif
