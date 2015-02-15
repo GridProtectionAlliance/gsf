28cf5d874ee38c558b4f6ed4ac3d1dca9e0bafa8
gsfbuildbot <support@gridprotectionalliance.org>
2015-02-15 07:25:49 +0000

    GridSolutionsFramework: Version change for build GridSolutionsFramework_v2.1.38.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C94172

--- Source/Tools/UDPRebroadcaster/UDPRebroadcaster/Properties/AssemblyInfo.cs
+++ Source/Tools/UDPRebroadcaster/UDPRebroadcaster/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.37.0")]
+[assembly: AssemblyVersion("2.1.38.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
