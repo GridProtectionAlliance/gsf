ca06a92663c1edfa964d2bae6d635df76ea5f54d
gsfbuildbot <support@gridprotectionalliance.org>
2015-04-21 06:47:22 +0000

    GridSolutionsFramework: Version change for build GridSolutionsFramework_v2.1.56.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/gsf/Main;C95372

--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.55.0")]
+[assembly: AssemblyVersion("2.1.56.0")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
