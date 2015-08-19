c1b70b80a1d2d455a8028fc8ef7bc18587b673fa
gsfbuildbot <support@gridprotectionalliance.org>
2015-08-19 11:18:32 -0400

    GridSolutionsFramework: Version change for build v2.1.98.0-master.

--- Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Core/Properties/AssemblyInfo.cs
+++ Source/OldCode/Framework/Current Version/Source/Libraries/TVA.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.97.0")]
+[assembly: AssemblyVersion("2.1.98.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
