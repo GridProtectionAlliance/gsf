075532020cb18c9ee02507f315f9ac8a7e31539c
Ryan McCoy <ryanmccoy@gmail.com>
2015-08-30 22:18:25 -0400

    Added adapter code to PowerCalculations project

---
+++ Source/Libraries/Adapters/PowerCalculations/PowerMultiCalculator/PowerCalculation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GSF.TimeSeries;
+
+namespace PowerCalculations.PowerMultiCalculator
+{
+	public class PowerCalculation
+	{
+		public int PowerCalculationId { get; set; }
+		public string CircuitDescription { get; set; }
+		public MeasurementKey VoltageAngleSignalId { get; set; }
+		public MeasurementKey VoltageMagnitudeSignalId { get; set; }
+		public MeasurementKey CurrentAngleSignalId { get; set; }
+		public MeasurementKey CurrentMagnitudeSignalId { get; set; }
+		public MeasurementKey RealPowerOutputSignalId { get; set; }
+		public MeasurementKey ReactivePowerOutputSignalId { get; set; }
+		public MeasurementKey ActivePowerOutputSignalId { get; set; }
+	}
+}
---
+++ Source/Libraries/Adapters/PowerCalculations/PowerMultiCalculator/RunningAverage.cs
@@ -0,0 +1,32 @@
+﻿namespace PowerCalculations.PowerMultiCalculator
+{
+	/// <summary>
+	/// Calculates running average of a value
+	/// </summary>
+	public class RunningAverage
+	{
+		private double _numberOfValues = 0;
+		private double _average = 0;
+
+		/// <summary>
+		/// Average calculated on values provided so far
+		/// </summary>
+		public double Average
+		{
+			get { return _average; }
+		}
+
+		/// <summary>
+		/// Calculates running average based on previous values and the new value
+		/// </summary>
+		/// <param name="value">Value to be added to the running average</param>
+		/// <returns>New running average</returns>
+		public double AddValue(double value)
+		{
+			var total = _numberOfValues * _average + value;
+			_numberOfValues++;
+			_average = total / _numberOfValues;
+			return _average;
+		}
+	}
+}
