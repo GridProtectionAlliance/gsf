b3e167a20930dce6f6c69451490c4155c30f0efe
Ryan McCoy <ryanmccoy@gmail.com>
2015-11-02 10:53:56 -0500

    Added header comments and missing XML documentation

--- Source/Libraries/Adapters/PowerCalculations/PowerMultiCalculator/RunningAverage.cs
+++ Source/Libraries/Adapters/PowerCalculations/PowerMultiCalculator/RunningAverage.cs
@@ -1,4 +1,27 @@
-﻿namespace PowerCalculations.PowerMultiCalculator
+﻿//******************************************************************************************************
+//  RunningAverage.cs - Gbtc
+//
+//  Copyright © 2012, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  11/2/2015 - Ryan McCoy
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+namespace PowerCalculations.PowerMultiCalculator
 {
 	/// <summary>
 	/// Calculates running average of a value
