c5969a825440ee125e5809cfe3d848e9b75657a5
StephenCWills <swills@gridprotectionalliance.org>
2015-12-19 16:52:17 -0500

    PQDIFExplorer: Updated tag names to better match the GSF.PQDIF library naming conventions.

--- Source/Libraries/GSF.PQDIF/Logical/ContainerRecord.cs
+++ Source/Libraries/GSF.PQDIF/Logical/ContainerRecord.cs
@@ -24,7 +24,6 @@
 //******************************************************************************************************
 
 using System;
-using System.IO;
 using System.Text;
 using GSF.PQDIF.Physical;
 
