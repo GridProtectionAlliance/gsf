7b9946e48438a2aa8ba3dab6de24c5b5c7137296
gsfbuildbot <support@gridprotectionalliance.org>
2016-02-12 00:02:48 -0500

    GridSolutionsFramework: Version change for build v2.1.180.0-master.

--- Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.179.0")]
-[assembly: AssemblyInformationalVersion("2.1.179-beta")]
+[assembly: AssemblyVersion("2.1.180.0")]
+[assembly: AssemblyInformationalVersion("2.1.180-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.179.0")]
-[assembly: AssemblyInformationalVersion("2.1.179-beta")]
+[assembly: AssemblyVersion("2.1.180.0")]
+[assembly: AssemblyInformationalVersion("2.1.180-beta")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
