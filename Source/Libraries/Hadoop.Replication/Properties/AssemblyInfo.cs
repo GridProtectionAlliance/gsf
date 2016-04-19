74d4537c00ff4e1de580dc0433bb54272bf17bd6
gsfbuildbot <support@gridprotectionalliance.org>
2016-04-19 15:25:24 -0400

    GridSolutionsFramework: Version change for build v2.1.218.0-master.

--- Source/Libraries/Hadoop.Replication/Properties/AssemblyInfo.cs
+++ Source/Libraries/Hadoop.Replication/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.217.0")]
-[assembly: AssemblyInformationalVersion("2.1.217-beta")]
+[assembly: AssemblyVersion("2.1.218.0")]
+[assembly: AssemblyInformationalVersion("2.1.218-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
