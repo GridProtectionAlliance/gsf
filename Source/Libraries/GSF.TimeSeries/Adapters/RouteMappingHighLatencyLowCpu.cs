8f32acfeaf87952551a70c33ca62bd6d899f09bc
chishose <chishose@CORPLT300225>
2016-06-30 17:35:02 -0500

    GSF.TimeSeries - Simplified RouteMappingHighLagencyLowCpu.cs

--- Source/Libraries/GSF.TimeSeries/Adapters/RouteMappingHighLatencyLowCpu.cs
+++ Source/Libraries/GSF.TimeSeries/Adapters/RouteMappingHighLatencyLowCpu.cs
@@ -22,7 +22,6 @@
 //******************************************************************************************************
 
 using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,135 +36,38 @@ namespace GSF.TimeSeries.Adapters
     /// </summary>
     public class RouteMappingHighLatencyLowCpu : IRouteMappingTables
     {
-        private class ArrayEnumerator<T> : IEnumerator<T>
+        private class Consumer
         {
-            private readonly T[] m_items;
-            private readonly int m_startingIndex;
-            private readonly int m_endingIndex;
-            private int m_index;
-            public ArrayEnumerator(T[] items, int startingIndex, int length)
-            {
-                items.ValidateParameters(startingIndex, length);
-                m_items = items;
-                m_startingIndex = startingIndex;
-                m_endingIndex = startingIndex + length - 1;
-                m_index = startingIndex - 1;
-            }
+            private readonly ScheduledTask m_task;
+            private readonly Action<IEnumerable<IMeasurement>> m_callback;
+            private readonly ConcurrentQueue<List<IMeasurement>> m_pendingMeasurements;
+            public List<IMeasurement> MeasurementsToRoute;
 
-            public bool MoveNext()
+            public Consumer(IAdapter adapter)
             {
-                m_index++;
-                return m_index <= m_endingIndex;
-            }
-
-            public void Reset()
-            {
-                m_index = m_startingIndex - 1;
-            }
-
-            public T Current
-            {
-                get
+                if (adapter is IActionAdapter)
                 {
-                    if (m_index >= m_startingIndex && m_index <= m_endingIndex)
-                        return m_items[m_index];
-                    return default(T);
+                    m_callback = ((IActionAdapter)adapter).QueueMeasurementsForProcessing;
                 }
-            }
-
-            object IEnumerator.Current
-            {
-                get
+                else
                 {
-                    return Current;
+                    m_callback = ((IOutputAdapter)adapter).QueueMeasurementsForProcessing;
                 }
-            }
 
-            void IDisposable.Dispose()
-            {
-
-            }
-        }
-
-        private class ArrayEnumerable<T> : ICollection<T>
-        {
-            public T[] Items;
-            public int StartIndex;
-            public int Length;
-            public ArrayEnumerable(int count)
-            {
-                Items = new T[count];
-                StartIndex = 0;
-                Length = 0;
-            }
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                return new ArrayEnumerator<T>(Items, StartIndex, Length);
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-
-            void ICollection<T>.Add(T item)
-            {
-                throw new NotImplementedException();
-            }
-
-            void ICollection<T>.Clear()
-            {
-                throw new NotImplementedException();
-            }
-
-            bool ICollection<T>.Contains(T item)
-            {
-                throw new NotImplementedException();
-            }
-
-            void ICollection<T>.CopyTo(T[] array, int arrayIndex)
-            {
-                Array.Copy(Items, StartIndex, array, arrayIndex, Length);
-            }
-
-            bool ICollection<T>.Remove(T item)
-            {
-                throw new NotImplementedException();
-            }
-
-            int ICollection<T>.Count
-            {
-                get
-                {
-                    return Length;
-                }
-            }
-
-            bool ICollection<T>.IsReadOnly
-            {
-                get
-                {
-                    return true;
-                }
-            }
-        }
-
-        private class DoubleBufferQueue2
-        {
-            private const int MaxItems = 1000;
-            private ScheduledTask m_task;
-            public IsolatedQueue<IMeasurement> Queue;
-            Action<IEnumerable<IMeasurement>> m_callback;
-            ArrayEnumerable<IMeasurement> m_callbackList;
-
-            public DoubleBufferQueue2(Action<IEnumerable<IMeasurement>> callback)
-            {
-                m_callbackList = new ArrayEnumerable<IMeasurement>(MaxItems);
-                Queue = new IsolatedQueue<IMeasurement>();
-                m_callback = callback;
                 m_task = new ScheduledTask();
                 m_task.Running += m_task_Running;
+                m_pendingMeasurements = new ConcurrentQueue<List<IMeasurement>>();
+                MeasurementsToRoute = new List<IMeasurement>();
+            }
+
+            public void RoutingComplete()
+            {
+                if (MeasurementsToRoute.Count > 0)
+                {
+                    m_pendingMeasurements.Enqueue(MeasurementsToRoute);
+                    MeasurementsToRoute = new List<IMeasurement>();
+                    m_task.Start();
+                }
             }
 
             void m_task_Running(object sender, EventArgs<ScheduledTaskRunningReason> e)
@@ -173,40 +75,20 @@ namespace GSF.TimeSeries.Adapters
                 if (e.Argument == ScheduledTaskRunningReason.Disposing)
                     return;
 
-                int maxCopied = 0;
-
-                while (true)
+                List<IMeasurement> lst;
+                while (m_pendingMeasurements.TryDequeue(out lst))
                 {
-                    m_callbackList.StartIndex = 0;
-                    m_callbackList.Length = Queue.Dequeue(m_callbackList.Items, 0, MaxItems);
-                    maxCopied = Math.Max(maxCopied, m_callbackList.Length);
-
-                    if (m_callbackList.Length > 0)
-                    {
-                        m_callback(m_callbackList);
-                    }
-
-                    //If the list was not filled, that means m_callbackList was completely drained.
-                    //I can safely quit. Note: ScheduledTask will rerun immediately in the event of a race condition.
-                    if (m_callbackList.Length != MaxItems)
-                    {
-                        Array.Clear(m_callbackList.Items, 0, maxCopied);
-                        return;
-                    }
+                    m_callback(lst);
                 }
             }
 
-            public void RoutingComplete()
-            {
-                if (Queue.Count > 0)
-                    m_task.Start();
-            }
         }
 
         private class GlobalCache
         {
             public readonly Dictionary<Guid, List<Consumer>> GlobalSignalLookup;
             public readonly Dictionary<IAdapter, Consumer> GlobalDestinationLookup;
+            public readonly Consumer[] GlobalDestinationList;
             public readonly List<Consumer> BroadcastConsumers;
             public readonly int Version;
 
@@ -243,6 +125,8 @@ namespace GSF.TimeSeries.Adapters
                 {
                     consumerList.AddRange(BroadcastConsumers);
                 }
+
+                GlobalDestinationList = GlobalDestinationLookup.Values.ToArray();
             }
         }
 
@@ -299,30 +183,6 @@ namespace GSF.TimeSeries.Adapters
             }
         }
 
-        private class Consumer
-        {
-            public readonly DoubleBufferQueue2 Manager;
-
-            public Consumer(IAdapter adapter)
-            {
-                IActionAdapter actionAdapter = adapter as IActionAdapter;
-
-                if ((object)actionAdapter != null)
-                {
-                    Manager = new DoubleBufferQueue2(actionAdapter.QueueMeasurementsForProcessing);
-                }
-                else
-                {
-                    var outputAdapter = adapter as IOutputAdapter;
-
-                    if ((object)outputAdapter != null)
-                        Manager = new DoubleBufferQueue2(outputAdapter.QueueMeasurementsForProcessing);
-                    else
-                        Manager = null;
-                }
-            }
-        }
-        
         // Fields
 
         private Dictionary<IAdapter, LocalCache> m_producerLookup;
@@ -332,7 +192,6 @@ namespace GSF.TimeSeries.Adapters
         private long m_measurementsRoutedInputFrames;
         private long m_measurementsRoutedInputMeasurements;
         private long m_measurementsRoutedOutput;
-        private long m_measurementsRoutedDiscarded;
         private long m_routeOperations;
         private int m_routeLatency;
 
@@ -340,6 +199,9 @@ namespace GSF.TimeSeries.Adapters
         private Action<string> m_onStatusMessage;
         private Action<Exception> m_onProcessException;
         private LocalCache m_injectMeasurementsLocalCache;
+
+        private int m_pendingMeasurements;
+        private int m_maxPendingMeasurements;
 
         /// <summary>
         /// Creates a <see cref="RouteMappingHighLatencyLowCpu"/>
@@ -350,6 +212,7 @@ namespace GSF.TimeSeries.Adapters
             if (routeLatency < 1 || routeLatency > 500)
                 throw new ArgumentOutOfRangeException("routeLatency", "Must be between 1 and 500ms");
 
+            m_maxPendingMeasurements = 1000;
             m_routeLatency = routeLatency;
             m_list = new ConcurrentQueue<IMeasurement[]>();
             m_task = new ScheduledTask(ThreadingMode.DedicatedBackground, ThreadPriority.AboveNormal);
@@ -447,10 +310,10 @@ namespace GSF.TimeSeries.Adapters
             if (m_routeOperations % 1000 == 0)
             {
                 m_onStatusMessage(string.Format(
-                            "Route Operations: {0}, Input Frames: {1}, Input Measurements: {2}, Output Measurements: {3}, DiscardedOutputs: {4}",
+                            "Route Operations: {0}, Input Frames: {1}, Input Measurements: {2}, Output Measurements: {3}",
                             m_routeOperations, m_measurementsRoutedInputFrames,
                             m_measurementsRoutedInputMeasurements,
-                            m_measurementsRoutedOutput, m_measurementsRoutedDiscarded));
+                            m_measurementsRoutedOutput));
             }
 
             var map = m_globalCache;
@@ -460,6 +323,8 @@ namespace GSF.TimeSeries.Adapters
                 IMeasurement[] measurements;
                 while (m_list.TryDequeue(out measurements))
                 {
+                    Interlocked.Add(ref m_pendingMeasurements, -measurements.Length);
+
                     m_measurementsRoutedInputFrames++;
                     m_measurementsRoutedInputMeasurements += measurements.Length;
                     foreach (var measurement in measurements)
@@ -474,24 +339,30 @@ namespace GSF.TimeSeries.Adapters
                         for (int index = 0; index < consumers.Count; index++)
                         {
                             var consumer = consumers[index];
-                            if (consumer.Manager != null)
+                            m_measurementsRoutedOutput++;
+                            consumer.MeasurementsToRoute.Add(measurement);
+                        }
+                    }
+
+                    //Limit routing to no more than 1000 measurements per sub-route.
+                    foreach (var consumer in map.GlobalDestinationList)
+                    {
+                        if (consumer.MeasurementsToRoute.Count > 1000)
+                        {
+                            foreach (var c2 in map.GlobalDestinationLookup.Values)
                             {
-                                m_measurementsRoutedOutput++;
-                                consumer.Manager.Queue.Enqueue(measurement);
+                                c2.RoutingComplete();
                             }
-                            else
-                            {
-                                m_measurementsRoutedDiscarded++;
-                            }
+                            break;
                         }
                     }
                 }
             }
             finally
             {
-                foreach (var consumer in map.GlobalDestinationLookup.Values)
+                foreach (var consumer in map.GlobalDestinationList)
                 {
-                    consumer.Manager?.RoutingComplete();
+                    consumer.RoutingComplete();
                 }
             }
         }
@@ -501,6 +372,10 @@ namespace GSF.TimeSeries.Adapters
             if (measurements.Length > 0)
             {
                 m_list.Enqueue(measurements);
+                if (Interlocked.Add(ref m_pendingMeasurements, measurements.Length) > m_maxPendingMeasurements)
+                {
+                    m_task.Start();
+                }
             }
         }
 
