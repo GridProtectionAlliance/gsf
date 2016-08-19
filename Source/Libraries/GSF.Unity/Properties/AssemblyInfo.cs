c1bfdaa00a7cf54adb182cec53d9cab237594ff7
gsfbuildbot <support@gridprotectionalliance.org>
2016-08-19 08:39:49 -0400

    GridSolutionsFramework: Version change for build v2.1.302.0-master.

--- Source/Libraries/GSF.Unity/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Unity/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.301.0")]
-[assembly: AssemblyInformationalVersion("2.1.301-beta")]
+[assembly: AssemblyVersion("2.1.302.0")]
+[assembly: AssemblyInformationalVersion("2.1.302-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Tools/UDPRebroadcaster/UDPRebroadcaster/Properties/AssemblyInfo.cs
+++ Source/Tools/UDPRebroadcaster/UDPRebroadcaster/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.301.0")]
-[assembly: AssemblyInformationalVersion("2.1.301-beta")]
+[assembly: AssemblyVersion("2.1.302.0")]
+[assembly: AssemblyInformationalVersion("2.1.302-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
