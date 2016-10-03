0fbe09e2bab3be8e39630d09faa3ff881dcc4893
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2016-10-03 10:25:34 -0400

    GSF.Web: Added endWidth prototype definition to gsf.web.client.js if function does not exist - this should fix issues with missing function on IE11.

--- Source/Libraries/GSF.Web/Model/Scripts/gsf.web.client.js
+++ Source/Libraries/GSF.Web/Model/Scripts/gsf.web.client.js
@@ -380,6 +380,21 @@ String.prototype.replaceAll = function(findText, replaceWith, ignoreCase) {
         (typeof replaceWith == "string") ? replaceWith.replace(/\$/g, "$$$$") : replaceWith);
 }
 
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+        const subjectString = this.toString() ;
+
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length)
+            position = subjectString.length;
+
+        position -= searchString.length;
+      
+        const lastIndex = subjectString.lastIndexOf(searchString, position);
+
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
+
 if (!String.format) {
     String.format = function(format) {
         var text = format;
