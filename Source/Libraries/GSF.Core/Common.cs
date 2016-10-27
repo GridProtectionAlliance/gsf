6d1e72eba87e9c71bc9835254c352223a34259cb
StephenCWills <swills@gridprotectionalliance.org>
2016-10-27 16:10:39 -0400

    GSF.Core: Moved classes and methods to GSF.Core.CLR.SharedProject so that they can be included in the GSF.Core.SqlClr library.

--- Source/Libraries/GSF.Core/Common.cs
+++ Source/Libraries/GSF.Core/Common.cs
@@ -133,7 +133,7 @@ namespace GSF
     /// <summary>
     /// Defines common global functions.
     /// </summary>
-    public static class Common
+    public static partial class Common
     {
         private static ApplicationType? s_applicationType;
         private static string s_osPlatformName;
@@ -446,96 +446,6 @@ namespace GSF
             }
 
             return processMemory;
-        }
-
-        // The following "ToNonNullString" methods extend all class based objects. Note that these extension methods can be
-        // called even if the base object is null, hence the value of these methods. Our philosophy for this project has been
-        // "not" to apply extensions to all objects (this to avoid general namespace pollution) and make sure extensions are
-        // grouped together in their own source file (e.g., StringExtensions.cs); however these items do apply to all items
-        // and are essentially type-less hence their location in the "Common" class. These extension methods are at least
-        // limited to classes and won't show up on native types and custom structures.
-
-        /// <summary>
-        /// Converts value to string; null objects (or DBNull objects) will return an empty string (""). 
-        /// </summary>
-        /// <typeparam name="T"><see cref="Type"/> of <see cref="Object"/> to convert to string.</typeparam>
-        /// <param name="value">Value to convert to string.</param>
-        /// <returns><paramref name="value"/> as a string; if <paramref name="value"/> is null, empty string ("") will be returned. </returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string ToNonNullString<T>(this T value) where T : class
-        {
-            return ((object)value == null || value is DBNull ? "" : value.ToString());
-        }
-
-        /// <summary>
-        /// Converts value to string; null objects (or DBNull objects) will return specified <paramref name="nonNullValue"/>.
-        /// </summary>
-        /// <typeparam name="T"><see cref="Type"/> of <see cref="Object"/> to convert to string.</typeparam>
-        /// <param name="value">Value to convert to string.</param>
-        /// <param name="nonNullValue"><see cref="String"/> to return if <paramref name="value"/> is null.</param>
-        /// <returns><paramref name="value"/> as a string; if <paramref name="value"/> is null, <paramref name="nonNullValue"/> will be returned.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="nonNullValue"/> cannot be null.</exception>
-        public static string ToNonNullString<T>(this T value, string nonNullValue) where T : class
-        {
-            if ((object)nonNullValue == null)
-                throw new ArgumentNullException("nonNullValue");
-
-            return ((object)value == null || value is DBNull ? nonNullValue : value.ToString());
-        }
-
-        // We handle strings as a special version of the ToNullNullString extension to handle documentation a little differently
-
-        /// <summary>
-        /// Makes sure returned string value is not null; if this string is null, empty string ("") will be returned. 
-        /// </summary>
-        /// <param name="value"><see cref="String"/> to verify is not null.</param>
-        /// <returns><see cref="String"/> value; if <paramref name="value"/> is null, empty string ("") will be returned.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string ToNonNullString(this string value)
-        {
-            return ((object)value == null ? "" : value);
-        }
-
-        /// <summary>
-        /// Converts value to string; null objects, DBNull objects or empty strings will return specified <paramref name="nonNullNorEmptyValue"/>.
-        /// </summary>
-        /// <typeparam name="T"><see cref="Type"/> of <see cref="Object"/> to convert to string.</typeparam>
-        /// <param name="value">Value to convert to string.</param>
-        /// <param name="nonNullNorEmptyValue"><see cref="String"/> to return if <paramref name="value"/> is null.</param>
-        /// <returns><paramref name="value"/> as a string; if <paramref name="value"/> is null, DBNull or an empty string <paramref name="nonNullNorEmptyValue"/> will be returned.</returns>
-        /// <exception cref="ArgumentException"><paramref name="nonNullNorEmptyValue"/> must not be null or an empty string.</exception>
-        public static string ToNonNullNorEmptyString<T>(this T value, string nonNullNorEmptyValue = " ") where T : class
-        {
-            if (string.IsNullOrEmpty(nonNullNorEmptyValue))
-                throw new ArgumentException("Must not be null or an empty string", "nonNullNorEmptyValue");
-
-            if ((object)value == null || value is DBNull)
-                return nonNullNorEmptyValue;
-
-            string valueAsString = value.ToString();
-
-            return (string.IsNullOrEmpty(valueAsString) ? nonNullNorEmptyValue : valueAsString);
-        }
-
-        /// <summary>
-        /// Converts value to string; null objects, DBNull objects, empty strings or all white space strings will return specified <paramref name="nonNullNorWhiteSpaceValue"/>.
-        /// </summary>
-        /// <typeparam name="T"><see cref="Type"/> of <see cref="Object"/> to convert to string.</typeparam>
-        /// <param name="value">Value to convert to string.</param>
-        /// <param name="nonNullNorWhiteSpaceValue"><see cref="String"/> to return if <paramref name="value"/> is null.</param>
-        /// <returns><paramref name="value"/> as a string; if <paramref name="value"/> is null, DBNull, empty or all white space, <paramref name="nonNullNorWhiteSpaceValue"/> will be returned.</returns>
-        /// <exception cref="ArgumentException"><paramref name="nonNullNorWhiteSpaceValue"/> must not be null, an empty string or white space.</exception>
-        public static string ToNonNullNorWhiteSpace<T>(this T value, string nonNullNorWhiteSpaceValue = "_") where T : class
-        {
-            if (string.IsNullOrWhiteSpace(nonNullNorWhiteSpaceValue))
-                throw new ArgumentException("Must not be null, an empty string or white space", "nonNullNorWhiteSpaceValue");
-
-            if ((object)value == null || value is DBNull)
-                return nonNullNorWhiteSpaceValue;
-
-            string valueAsString = value.ToString();
-
-            return (string.IsNullOrWhiteSpace(valueAsString) ? nonNullNorWhiteSpaceValue : valueAsString);
         }
 
         /// <summary>
