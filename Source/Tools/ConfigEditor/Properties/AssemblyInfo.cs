dd6c36d8cb80062043ac76d11ab38fe8c96f73ba
gsfbuildbot <support@gridprotectionalliance.org>
2016-11-09 00:00:36 -0500

    GridSolutionsFramework: Version change for build v2.1.369.0-master.

--- Source/Tools/ConfigEditor/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigEditor/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.368.0")]
-[assembly: AssemblyInformationalVersion("2.1.368-beta")]
+[assembly: AssemblyVersion("2.1.369.0")]
+[assembly: AssemblyInformationalVersion("2.1.369-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
