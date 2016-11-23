af70f5b61c1283e32ee1eb1d5767c37721c96284
gsfbuildbot <support@gridprotectionalliance.org>
2016-11-23 00:01:05 -0500

    GridSolutionsFramework: Version change for build v2.1.388.0-master.

--- Source/Libraries/GSF.PhasorProtocols/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.PhasorProtocols/Properties/AssemblyInfo.cs
@@ -48,8 +48,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6d59b0ed-1991-4f12-a739-2cf8543dd9b2")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.387.0")]
-[assembly: AssemblyFileVersion("2.1.387.0")]
+[assembly: AssemblyVersion("2.1.388.0")]
+[assembly: AssemblyFileVersion("2.1.388.0")]
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
-[assembly: AssemblyInformationalVersion("2.1.387-beta")]
+[assembly: AssemblyInformationalVersion("2.1.388-beta")]
