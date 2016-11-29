691943ea5db9f5f3072304c67baf8b9453b4f334
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2016-11-29 09:34:10 -0500

    GSF.Core: Added constructor to LogStackMessages for providing multiple key/value pairs - also general code clean-up.

--- Source/Libraries/GSF.Core.Shared/Diagnostics/LogStackMessages.cs
+++ Source/Libraries/GSF.Core.Shared/Diagnostics/LogStackMessages.cs
@@ -18,13 +18,13 @@
 //  ----------------------------------------------------------------------------------------------------
 //  10/24/2016 - Steven E. Chisholm
 //       Generated original version of source code. 
-//       
 //
 //******************************************************************************************************
 
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
 using GSF.IO;
@@ -32,18 +32,20 @@ using GSF.IO;
 namespace GSF.Diagnostics
 {
     /// <summary>
-    /// Immutable List of stack messages
+    /// Represents an immutable list of stack messages.
     /// </summary>
-    public class LogStackMessages
-        : IEquatable<LogStackMessages>
+    public class LogStackMessages : IEquatable<LogStackMessages>
     {
-        private readonly string[] m_attributes;
+        #region [ Members ]
 
+        // Fields
+        private readonly string[] m_attributes;
         private readonly string[] m_values;
-        /// <summary>
-        /// Gets the hashcode for this class
-        /// </summary>
         private readonly long m_hashCode;
+
+        #endregion
+
+        #region [ Constructors ]
 
         private LogStackMessages()
         {
@@ -53,20 +55,58 @@ namespace GSF.Diagnostics
         }
 
         /// <summary>
-        /// Creates a new stack message
+        /// Creates a new stack message from the provided list of key/value pairs.
         /// </summary>
-        /// <param name="key">the key</param>
-        /// <param name="value">the value</param>
+        /// <param name="keyValuePairs">Key/value pairs, e.g., key1, value1, key2, value2, ..., key(n), value(n).</param>
+        public LogStackMessages(params string[] keyValuePairs)
+        {
+            if (keyValuePairs.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(keyValuePairs));
+
+            if (keyValuePairs.Length % 2 != 0)
+                throw new ArgumentOutOfRangeException(nameof(keyValuePairs), "Key/value pair mismatch");
+
+            int count = keyValuePairs.Length / 2;
+            int index = 0;
+
+            m_attributes = new string[count];
+            m_values = new string[count];
+
+            for (int i = 0; i < keyValuePairs.Length; i += 2)
+            {
+                string key = keyValuePairs[i];
+                string value = keyValuePairs[i + 1];
+
+                if (string.IsNullOrWhiteSpace(key))
+                    throw new ArgumentNullException(nameof(key), "Specified key was null or empty.");
+
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(nameof(value), "Specified value was null or empty.");
+
+                m_attributes[index] = key.Trim();
+                m_values[index] = value.Trim();
+                index++;
+            }
+
+            m_hashCode = ComputeHashCode();
+        }
+
+        /// <summary>
+        /// Creates a new stack message from the provided <paramref name="key"/> and <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
         /// <exception cref="ArgumentNullException">if key or value are null or whitespace.</exception>
         public LogStackMessages(string key, string value)
         {
             if (string.IsNullOrWhiteSpace(key))
-                throw new ArgumentNullException(nameof(key));
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentNullException(nameof(value));
+                throw new ArgumentNullException(nameof(key), "Specified key was null or empty.");
 
-            m_attributes = new string[] { key };
-            m_values = new string[] { value };
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value), "Specified value was null or empty.");
+
+            m_attributes = new [] { key };
+            m_values = new [] { value };
             m_hashCode = ComputeHashCode();
         }
 
@@ -81,15 +121,18 @@ namespace GSF.Diagnostics
 
             for (int x = 0; x < messages.Count; x++)
             {
-                var pair = messages[x];
+                KeyValuePair<string, string> pair = messages[x];
+
                 if (string.IsNullOrWhiteSpace(pair.Key))
                     throw new ArgumentNullException(nameof(messages), "A key is null or whitespace");
+
                 if (string.IsNullOrWhiteSpace(pair.Value))
                     throw new ArgumentNullException(nameof(messages), "A value is null or whitespace");
 
                 m_attributes[x] = pair.Key;
                 m_values[x] = pair.Value;
             }
+
             m_hashCode = ComputeHashCode();
         }
 
@@ -99,26 +142,27 @@ namespace GSF.Diagnostics
         /// <param name="messages">the messages</param>
         public LogStackMessages(List<LogStackMessages> messages)
         {
-            int cnt = 0;
+            int count = 0;
+
+            for (int x = 0; x < messages.Count; x++)
+                count += messages[x].m_attributes.Length;
+
+            m_attributes = new string[count];
+            m_values = new string[count];
+
+            count = 0;
+
             for (int x = 0; x < messages.Count; x++)
             {
-                cnt += messages[x].m_attributes.Length;
-            }
-
-            m_attributes = new string[cnt];
-            m_values = new string[cnt];
-
-            cnt = 0;
-            for (int x = 0; x < messages.Count; x++)
-            {
-                var item = messages[x];
+                LogStackMessages item = messages[x];
                 for (int y = 0; y < item.m_attributes.Length; y++)
                 {
-                    m_attributes[cnt] = item.m_attributes[y];
-                    m_values[cnt] = item.m_values[y];
-                    cnt++;
+                    m_attributes[count] = item.m_attributes[y];
+                    m_values[count] = item.m_values[y];
+                    count++;
                 }
             }
+
             m_hashCode = ComputeHashCode();
         }
 
@@ -127,25 +171,30 @@ namespace GSF.Diagnostics
         /// </summary>
         private LogStackMessages(LogStackMessages a, LogStackMessages b)
         {
-            int cnt = a.m_attributes.Length + b.m_attributes.Length;
-            m_attributes = new string[cnt];
-            m_values = new string[cnt];
+            int count = a.m_attributes.Length + b.m_attributes.Length;
 
-            cnt = 0;
-            var item = a;
+            m_attributes = new string[count];
+            m_values = new string[count];
+
+            count = 0;
+            LogStackMessages item = a;
+
             for (int y = 0; y < item.m_attributes.Length; y++)
             {
-                m_attributes[cnt] = item.m_attributes[y];
-                m_values[cnt] = item.m_values[y];
-                cnt++;
+                m_attributes[count] = item.m_attributes[y];
+                m_values[count] = item.m_values[y];
+                count++;
             }
+
             item = b;
+
             for (int y = 0; y < item.m_attributes.Length; y++)
             {
-                m_attributes[cnt] = item.m_attributes[y];
-                m_values[cnt] = item.m_values[y];
-                cnt++;
+                m_attributes[count] = item.m_attributes[y];
+                m_values[count] = item.m_values[y];
+                count++;
             }
+
             m_hashCode = ComputeHashCode();
         }
 
@@ -157,12 +206,15 @@ namespace GSF.Diagnostics
         public LogStackMessages(Stream stream)
         {
             int version = stream.ReadNextByte();
+
             if (version == 0)
             {
-                int cnt = stream.ReadInt32();
-                m_attributes = new string[cnt];
-                m_values = new string[cnt];
-                for (int x = 0; x < cnt; x++)
+                int count = stream.ReadInt32();
+
+                m_attributes = new string[count];
+                m_values = new string[count];
+
+                for (int x = 0; x < count; x++)
                 {
                     m_attributes[x] = stream.ReadString();
                     m_values[x] = stream.ReadString();
@@ -172,22 +224,28 @@ namespace GSF.Diagnostics
             {
                 throw new VersionNotFoundException();
             }
+
             m_hashCode = ComputeHashCode();
         }
 
         private LogStackMessages(LogStackMessages oldMessage, string key, string value)
         {
-            int cnt = oldMessage.m_attributes.Length + 1;
-            m_attributes = new string[cnt];
-            m_values = new string[cnt];
+            int count = oldMessage.m_attributes.Length + 1;
+
+            m_attributes = new string[count];
+            m_values = new string[count];
 
             oldMessage.m_attributes.CopyTo(m_attributes, 0);
             oldMessage.m_values.CopyTo(m_values, 0);
 
-            m_attributes[cnt - 1] = key;
-            m_values[cnt - 1] = value;
+            m_attributes[count - 1] = key;
+            m_values[count - 1] = value;
             m_hashCode = ComputeHashCode();
         }
+
+        #endregion
+
+        #region [ Properties ]
 
         /// <summary>
         /// Gets the number of Key/Value pairs.
@@ -198,13 +256,7 @@ namespace GSF.Diagnostics
         /// Gets the KeyValue for the provided index.
         /// </summary>
         /// <param name="index">The Index</param>
-        public KeyValuePair<string, string> this[int index]
-        {
-            get
-            {
-                return new KeyValuePair<string, string>(m_attributes[index], m_values[index]);
-            }
-        }
+        public KeyValuePair<string, string> this[int index] => new KeyValuePair<string, string>(m_attributes[index], m_values[index]);
 
         /// <summary>
         /// Gets the first match of the provided <see pref="key"/> in this dictionary. Returns
@@ -218,25 +270,16 @@ namespace GSF.Diagnostics
                 for (int x = 0; x < m_attributes.Length; x++)
                 {
                     if (m_attributes[x] == key)
-                    {
                         return m_values[x];
-                    }
                 }
+
                 return null;
             }
         }
 
+        #endregion
 
-        private int ComputeHashCode()
-        {
-            int hashSum = m_attributes.Length;
-            for (int x = 0; x < m_attributes.Length; x++)
-            {
-                hashSum ^= m_attributes[x].GetHashCode();
-                hashSum ^= m_values[x].GetHashCode();
-            }
-            return hashSum;
-        }
+        #region [ Methods ]
 
         /// <summary>
         /// returns the union of this instance and the specified key/value
@@ -249,6 +292,7 @@ namespace GSF.Diagnostics
         {
             if (string.IsNullOrWhiteSpace(key))
                 throw new ArgumentNullException(nameof(key));
+
             if (string.IsNullOrWhiteSpace(value))
                 throw new ArgumentNullException(nameof(value));
 
@@ -261,12 +305,15 @@ namespace GSF.Diagnostics
         /// <returns></returns>
         public LogStackMessages ConcatenateWith(LogStackMessages stackMessage)
         {
-            if (stackMessage == null)
+            if ((object)stackMessage == null)
                 return this;
+
             if (ReferenceEquals(stackMessage, Empty))
                 return this;
+
             if (ReferenceEquals(this, Empty))
                 return stackMessage;
+
             return new LogStackMessages(this, stackMessage);
         }
 
@@ -278,6 +325,7 @@ namespace GSF.Diagnostics
         {
             stream.WriteByte(0);
             stream.Write(m_attributes.Length);
+
             for (int x = 0; x < m_attributes.Length; x++)
             {
                 stream.Write(m_attributes[x]);
@@ -292,10 +340,7 @@ namespace GSF.Diagnostics
         /// A hash code for the current <see cref="T:System.Object"/>.
         /// </returns>
         /// <filterpriority>2</filterpriority>
-        public override int GetHashCode()
-        {
-            return (int)m_hashCode;
-        }
+        public override int GetHashCode() => (int)m_hashCode;
 
         /// <summary>
         /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
@@ -304,10 +349,7 @@ namespace GSF.Diagnostics
         /// true if the specified object  is equal to the current object; otherwise, false.
         /// </returns>
         /// <param name="obj">The object to compare with the current object. </param><filterpriority>2</filterpriority>
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as LogStackMessages);
-        }
+        public override bool Equals(object obj) => Equals(obj as LogStackMessages);
 
         /// <summary>
         /// Indicates whether the current object is equal to another object of the same type.
@@ -320,17 +362,21 @@ namespace GSF.Diagnostics
         {
             if (ReferenceEquals(this, other))
                 return true;
+
             if ((object)other == null
                 || m_hashCode != other.m_hashCode
                 || m_attributes.Length != other.m_attributes.Length)
                 return false;
+
             for (int x = 0; x < m_attributes.Length; x++)
             {
                 if (m_attributes[x] != other.m_attributes[x])
                     return false;
+
                 if (m_values[x] != other.m_values[x])
                     return false;
             }
+
             return true;
         }
 
@@ -342,32 +388,56 @@ namespace GSF.Diagnostics
             if (m_attributes.Length == 0)
                 return string.Empty;
 
-            var sb = new StringBuilder();
+            StringBuilder image = new StringBuilder();
+
             for (int x = 0; x < m_attributes.Length; x++)
             {
-                sb.Append(m_attributes[x]);
-                sb.Append('=');
-                sb.Append(m_values[x]);
-                sb.AppendLine();
+                image.Append(m_attributes[x]);
+                image.Append('=');
+                image.Append(m_values[x]);
+                image.AppendLine();
             }
-            return sb.ToString();
+
+            return image.ToString();
         }
+
+        private int ComputeHashCode()
+        {
+            int hashSum = m_attributes.Length;
+
+            for (int x = 0; x < m_attributes.Length; x++)
+            {
+                hashSum ^= m_attributes[x].GetHashCode();
+                hashSum ^= m_values[x].GetHashCode();
+            }
+
+            return hashSum;
+        }
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Fields
 
         /// <summary>
         /// An empty stack message.
         /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes")]
+        [SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes")]
         public static readonly LogStackMessages Empty;
 
+        // Static Constructor
         static LogStackMessages()
         {
             Empty = new LogStackMessages();
         }
 
+        // Static Methods
+
         internal static void Initialize()
         {
-
         }
 
+        #endregion
     }
 }
