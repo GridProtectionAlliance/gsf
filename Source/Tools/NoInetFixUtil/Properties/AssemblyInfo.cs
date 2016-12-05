460d514a40315d64e51fb690d1019e1bccbcf7df
gsfbuildbot <support@gridprotectionalliance.org>
2016-12-05 00:01:09 -0500

    GridSolutionsFramework: Version change for build v2.1.396.0-master.

--- Source/Tools/NoInetFixUtil/Properties/AssemblyInfo.cs
+++ Source/Tools/NoInetFixUtil/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.1.395.0")]
-[assembly: AssemblyVersion("2.1.395.0")]
-[assembly: AssemblyFileVersion("2.1.395.0")]
-[assembly: AssemblyInformationalVersion("2.1.395-beta")]
+// [assembly: AssemblyVersion("2.1.396.0")]
+[assembly: AssemblyVersion("2.1.396.0")]
+[assembly: AssemblyFileVersion("2.1.396.0")]
+[assembly: AssemblyInformationalVersion("2.1.396-beta")]
