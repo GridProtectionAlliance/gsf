c8be38149a16efabea513eccc01e0d1d30bf994c
gsfbuildbot <support@gridprotectionalliance.org>
2017-02-10 00:00:19 -0500

    GridSolutionsFramework: Version change for build v2.1.445.0-master.

--- Source/Applications/TsfManager/Properties/AssemblyInfo.cs
+++ Source/Applications/TsfManager/Properties/AssemblyInfo.cs
@@ -76,7 +76,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.1.444.0")]
-[assembly: AssemblyVersion("2.1.444.0")]
-[assembly: AssemblyFileVersion("2.1.444.0")]
-[assembly: AssemblyInformationalVersion("2.1.444-beta")]
+// [assembly: AssemblyVersion("2.1.445.0")]
+[assembly: AssemblyVersion("2.1.445.0")]
+[assembly: AssemblyFileVersion("2.1.445.0")]
+[assembly: AssemblyInformationalVersion("2.1.445-beta")]
--- Source/Libraries/Adapters/GrafanaAdapters/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/GrafanaAdapters/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.444.0")]
-[assembly: AssemblyInformationalVersion("2.1.444-beta")]
+[assembly: AssemblyVersion("2.1.445.0")]
+[assembly: AssemblyInformationalVersion("2.1.445-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
