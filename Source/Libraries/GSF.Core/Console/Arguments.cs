72b83f7c043b790f2710dcd2f758ccc41ef8c532
StephenCWills <swills@gridprotectionalliance.org>
2017-05-02 14:19:48 -0400

    GSF.Core: Removed using for System.Runtime.CompilerServices from Arguments.cs.

--- Source/Libraries/GSF.Core/Console/Arguments.cs
+++ Source/Libraries/GSF.Core/Console/Arguments.cs
@@ -41,7 +41,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Security;
 using System.Text;
