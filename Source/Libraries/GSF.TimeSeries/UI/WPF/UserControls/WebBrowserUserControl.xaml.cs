f261ee6fb662ed6a504ab5ab42989b9360321a86
StephenCWills <swills@gridprotectionalliance.org>
2017-07-25 14:17:54 -0400

    GSF.TimeSeries.UI.WPF: Added code to silence script errors in release mode on the WebBrowserUserControl.

--- Source/Libraries/GSF.TimeSeries/UI/WPF/UserControls/WebBrowserUserControl.xaml.cs
+++ Source/Libraries/GSF.TimeSeries/UI/WPF/UserControls/WebBrowserUserControl.xaml.cs
@@ -23,6 +23,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Controls;
 using GSF.Data;
 
@@ -63,6 +65,34 @@ namespace GSF.TimeSeries.UI.UserControls
             }
 
             m_browser.Source = new Uri(webHostURL.TrimEnd('/') + "/" + m_urlPath.TrimStart('/'));
+        }
+
+        private void WebBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+#if !DEBUG
+            // get an IWebBrowser2 from the document
+            IOleServiceProvider sp = m_browser.Document as IOleServiceProvider;
+
+            if (sp != null)
+            {
+                Guid IID_IWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+                Guid IID_IWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
+
+                object webBrowser;
+
+                sp.QueryService(ref IID_IWebBrowserApp, ref IID_IWebBrowser2, out webBrowser);
+
+                if (webBrowser != null)
+                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { true });
+            }
+#endif
+        }
+
+        [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IOleServiceProvider
+        {
+            [PreserveSig]
+            int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
         }
     }
 }
