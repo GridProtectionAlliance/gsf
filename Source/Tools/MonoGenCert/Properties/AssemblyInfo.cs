55cb8abde8e547f70119572db61fec6c1139cb6e
gsfbuildbot <support@gridprotectionalliance.org>
2017-08-14 00:00:23 -0400

    GridSolutionsFramework: Version change for build v2.2.85.0-master.

--- Source/Tools/MonoGenCert/Properties/AssemblyInfo.cs
+++ Source/Tools/MonoGenCert/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.84.0")]
-[assembly: AssemblyVersion("2.2.84.0")]
-[assembly: AssemblyFileVersion("2.2.84.0")]
-[assembly: AssemblyInformationalVersion("2.2.84-beta")]
+// [assembly: AssemblyVersion("2.2.85.0")]
+[assembly: AssemblyVersion("2.2.85.0")]
+[assembly: AssemblyFileVersion("2.2.85.0")]
+[assembly: AssemblyInformationalVersion("2.2.85-beta")]
