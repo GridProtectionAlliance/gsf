af8377e8b25fafdebaae79ac70d2f0ef6aeeb8fa
gsfbuildbot <support@gridprotectionalliance.org>
2017-08-15 00:00:31 -0400

    GridSolutionsFramework: Version change for build v2.2.86.0-master.

--- Source/Tools/Certificate Generator/src/AssemblyInfo.cs
+++ Source/Tools/Certificate Generator/src/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 //// You can specify all the values or you can default the Revision and Build Numbers
 //// by using the '*' as shown below:
 
-//[assembly: AssemblyVersion("2.2.85.0")]
-//[assembly: AssemblyFileVersion("2.2.85.0")]
-//[assembly: AssemblyInformationalVersion("2.2.85-beta")]
+//[assembly: AssemblyVersion("2.2.86.0")]
+//[assembly: AssemblyFileVersion("2.2.86.0")]
+//[assembly: AssemblyInformationalVersion("2.2.86-beta")]
 
 ////
 //// In order to sign your assembly you must specify a key to use. Refer to the
