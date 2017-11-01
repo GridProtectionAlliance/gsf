ce2536ca40cc6b694ac8933b474126911535e48d
Billy Ernest <bernest@gridprotectionalliance.org>
2017-11-01 11:19:05 -0400

    GrafanaAdapters: Added normal data flag check to query.

--- Source/Libraries/Adapters/GrafanaAdapters/GrafanaDataSourceBase.cs
+++ Source/Libraries/Adapters/GrafanaAdapters/GrafanaDataSourceBase.cs
@@ -692,6 +692,7 @@ namespace GrafanaAdapters
 
                 uint excludedDataFlags = Convert.ToUInt32(request.options?["excludedDataFlags"].ToString() ?? "0x00000000", 16); // 0x00000000
                 uint includedDataFlags = Convert.ToUInt32(request.options?["includedDataFlags"].ToString() ?? "0xFFFFFFFF", 16); // 0xFFFFFFFF
+                bool includeNormalData = Convert.ToBoolean(request.options?["includeNormalData"].ToString() ?? "true"); 
 
                 DateTime startTime = request.range.from.ParseJsonTimestamp();
                 DateTime stopTime = request.range.to.ParseJsonTimestamp();
@@ -708,10 +709,10 @@ namespace GrafanaAdapters
                     IEnumerable<DataSourceValue> values = valueGroups.First(group => group.Target.Equals(series.target)).Source;
 
                     if (excludedDataFlags > uint.MinValue)
-                        values = values.Where(value => ((uint)value.Flags & excludedDataFlags) == 0);
+                        values = values.Where(value => (!includeNormalData && value.Flags == MeasurementStateFlags.Normal) || ((uint)value.Flags & excludedDataFlags) == 0);
 
                     if (includedDataFlags < uint.MaxValue)
-                        values = values.Where(value => ((uint)value.Flags & includedDataFlags) > 0);
+                        values = values.Where(value => (includeNormalData && value.Flags == MeasurementStateFlags.Normal) || ((uint)value.Flags & includedDataFlags) > 0);
 
                     series.datapoints = values.Select(dataValue => new[] { dataValue.Value, dataValue.Time }).ToList();
                 });
