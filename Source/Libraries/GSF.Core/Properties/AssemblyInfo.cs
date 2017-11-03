d23a967cb79379a566965803b723bf5a42562eb1
gsfbuildbot <support@gridprotectionalliance.org>
2017-11-03 00:00:28 -0400

    GridSolutionsFramework: Version change for build v2.2.131.0-master.

--- Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
@@ -28,10 +28,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.2.130.0")]
+[assembly: AssemblyVersion("2.2.131.0")]
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
-[assembly: AssemblyInformationalVersion("2.2.130-beta")]
+[assembly: AssemblyInformationalVersion("2.2.131-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Tools/Certificate Generator/src/AssemblyInfo.cs
+++ Source/Tools/Certificate Generator/src/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 //// You can specify all the values or you can default the Revision and Build Numbers
 //// by using the '*' as shown below:
 
-//[assembly: AssemblyVersion("2.2.130.0")]
-//[assembly: AssemblyFileVersion("2.2.130.0")]
-//[assembly: AssemblyInformationalVersion("2.2.130-beta")]
+//[assembly: AssemblyVersion("2.2.131.0")]
+//[assembly: AssemblyFileVersion("2.2.131.0")]
+//[assembly: AssemblyInformationalVersion("2.2.131-beta")]
 
 ////
 //// In order to sign your assembly you must specify a key to use. Refer to the
