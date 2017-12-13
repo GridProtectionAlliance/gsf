9b8ba8b8f145d8c572e132a43f9888f63063df1c
gsfbuildbot <support@gridprotectionalliance.org>
2017-12-13 00:00:21 -0500

    GridSolutionsFramework: Version change for build v2.2.161.0-master.

--- Source/Applications/TsfManager/Properties/AssemblyInfo.cs
+++ Source/Applications/TsfManager/Properties/AssemblyInfo.cs
@@ -76,7 +76,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.160.0")]
-[assembly: AssemblyVersion("2.2.160.0")]
-[assembly: AssemblyFileVersion("2.2.160.0")]
-[assembly: AssemblyInformationalVersion("2.2.160-beta")]
+// [assembly: AssemblyVersion("2.2.161.0")]
+[assembly: AssemblyVersion("2.2.161.0")]
+[assembly: AssemblyFileVersion("2.2.161.0")]
+[assembly: AssemblyInformationalVersion("2.2.161-beta")]
