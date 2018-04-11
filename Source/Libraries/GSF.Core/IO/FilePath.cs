a5f14110b2081b159fcd65248873961aa9aae9de
StephenCWills <swills@gridprotectionalliance.org>
2018-04-11 14:13:35 -0400

    GSF.Core: Cleaned up usings in FilePath.

--- Source/Libraries/GSF.Core/IO/FilePath.cs
+++ Source/Libraries/GSF.Core/IO/FilePath.cs
@@ -52,7 +52,6 @@
 //******************************************************************************************************
 
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Security.AccessControl;
