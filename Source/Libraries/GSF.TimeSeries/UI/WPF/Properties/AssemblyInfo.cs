592bf89db57b32dee7fa3a02b1431e786dc91e9b
gsfbuildbot <support@gridprotectionalliance.org>
2018-04-20 00:00:29 -0400

    GridSolutionsFramework: Version change for build v2.2.233.0-master.

--- Source/Libraries/GSF.TimeSeries/UI/WPF/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.TimeSeries/UI/WPF/Properties/AssemblyInfo.cs
@@ -76,7 +76,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.232.0")]
-[assembly: AssemblyVersion("2.2.232.0")]
-[assembly: AssemblyFileVersion("2.2.232.0")]
-[assembly: AssemblyInformationalVersion("2.2.232-beta")]
+// [assembly: AssemblyVersion("2.2.233.0")]
+[assembly: AssemblyVersion("2.2.233.0")]
+[assembly: AssemblyFileVersion("2.2.233.0")]
+[assembly: AssemblyInformationalVersion("2.2.233-beta")]
