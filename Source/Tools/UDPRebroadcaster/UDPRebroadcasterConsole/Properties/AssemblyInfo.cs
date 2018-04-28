68a384364abd99eacc27019d65fe5ce8fbd03a00
gsfbuildbot <support@gridprotectionalliance.org>
2018-04-28 00:00:22 -0400

    GridSolutionsFramework: Version change for build v2.2.239.0-master.

--- Source/Tools/UDPRebroadcaster/UDPRebroadcasterConsole/Properties/AssemblyInfo.cs
+++ Source/Tools/UDPRebroadcaster/UDPRebroadcasterConsole/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.2.238.0")]
-[assembly: AssemblyInformationalVersion("2.2.238-beta")]
+[assembly: AssemblyVersion("2.2.239.0")]
+[assembly: AssemblyInformationalVersion("2.2.239-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
