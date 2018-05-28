efe0d90571d1d75baac6ee6e5f2265ad2c489b0e
gsfbuildbot <support@gridprotectionalliance.org>
2018-05-28 00:00:18 -0400

    GridSolutionsFramework: Version change for build v2.2.258.0-master.

--- Source/Extensions/GPAVSExtension/My Project/AssemblyInfo.vb
+++ Source/Extensions/GPAVSExtension/My Project/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' [assembly: AssemblyVersion("2.2.257.0")]
-<Assembly: AssemblyVersion("2.2.257.0")>
-<Assembly: AssemblyFileVersion("2.2.257.0")>
+' [assembly: AssemblyVersion("2.2.258.0")]
+<Assembly: AssemblyVersion("2.2.258.0")>
+<Assembly: AssemblyFileVersion("2.2.258.0")>
--- Source/Tools/HistorianView/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianView/Properties/AssemblyInfo.cs
@@ -75,7 +75,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.257.0")]
-[assembly: AssemblyVersion("2.2.257.0")]
-[assembly: AssemblyFileVersion("2.2.257.0")]
-[assembly: AssemblyInformationalVersion("2.2.257-beta")]
+// [assembly: AssemblyVersion("2.2.258.0")]
+[assembly: AssemblyVersion("2.2.258.0")]
+[assembly: AssemblyFileVersion("2.2.258.0")]
+[assembly: AssemblyInformationalVersion("2.2.258-beta")]
