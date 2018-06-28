2ae0c637ce231c108ae4fe049e942e04d0546125
gsfbuildbot <support@gridprotectionalliance.org>
2018-06-28 14:42:42 -0400

    GridSolutionsFramework: Version change for build v2.3.6.0-master.

--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.3.5.0")]
-[assembly: AssemblyInformationalVersion("2.3.5-beta")]
+[assembly: AssemblyVersion("2.3.6.0")]
+[assembly: AssemblyInformationalVersion("2.3.6-beta")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
