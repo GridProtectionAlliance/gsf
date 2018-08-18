f4d685925b7645682443a20c125a32e2284e7cd8
gsfbuildbot <support@gridprotectionalliance.org>
2018-08-18 00:00:49 -0400

    GridSolutionsFramework: Version change for build v2.3.33.0-master.

--- Source/Applications/Wave Demo Apps/WavSubscriptionDemo/Properties/AssemblyInfo.cs
+++ Source/Applications/Wave Demo Apps/WavSubscriptionDemo/Properties/AssemblyInfo.cs
@@ -76,7 +76,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.32.0")]
-[assembly: AssemblyVersion("2.3.32.0")]
-[assembly: AssemblyFileVersion("2.3.32.0")]
-[assembly: AssemblyInformationalVersion("2.3.32-beta")]
+// [assembly: AssemblyVersion("2.3.33.0")]
+[assembly: AssemblyVersion("2.3.33.0")]
+[assembly: AssemblyFileVersion("2.3.33.0")]
+[assembly: AssemblyInformationalVersion("2.3.33-beta")]
