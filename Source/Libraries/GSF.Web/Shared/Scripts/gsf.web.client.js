9e8d30dd359cf1cef4afebfea045cdf8b316187d
Billy Ernest <bernest@gridprotectionalliance.org>
2018-10-10 09:58:35 -0400

    GSF.Web: Added Number prototypes for parseFloat and isInteger to fix IE11 not having these functions defined.

--- Source/Libraries/GSF.Web/Shared/Scripts/gsf.web.client.js
+++ Source/Libraries/GSF.Web/Shared/Scripts/gsf.web.client.js
@@ -1114,3 +1114,16 @@ if (!Array.prototype.fill) {
         }
     });
 }
+
+// Fix for IE11 not having this function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/parseFloat
+if (Number.parseFloat === void 0)
+    Number.parseFloat = parseFloat;
+
+// Fix for IE11 not having this function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+Number.isInteger = Number.isInteger || function (value) {
+    return typeof value === 'number' &&
+        isFinite(value) &&
+        Math.floor(value) === value;
+};
