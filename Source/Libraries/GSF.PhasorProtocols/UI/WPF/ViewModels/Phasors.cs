97b026869f2ee4e2d0ebcaefd11847fbdd8a7817
StephenCWills <swills@gridprotectionalliance.org>
2018-11-02 10:59:48 -0400

    GSF.PhasorProtocols.UI.WPF: Added line-to-line option under Phase to the Manage Phasor page of the Manager.

--- Source/Libraries/GSF.PhasorProtocols/UI/WPF/ViewModels/Phasors.cs
+++ Source/Libraries/GSF.PhasorProtocols/UI/WPF/ViewModels/Phasors.cs
@@ -69,6 +69,7 @@ namespace GSF.PhasorProtocols.UI.ViewModels
             m_phaseLookupList.Add("A", "Phase A");
             m_phaseLookupList.Add("B", "Phase B");
             m_phaseLookupList.Add("C", "Phase C");
+            m_phaseLookupList.Add("L", "Line-to-line");
 
             m_typeLookupList = new Dictionary<string, string>();
             m_typeLookupList.Add("V", "Voltage");
