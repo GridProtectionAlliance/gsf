e24e032bb785221214789058a57de8cb603a4f3a
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-01-03 03:18:31 -0500

    TimeSeriesPlatformLibrary: Added more filter expression tests.

--- Source/Libraries/TimeSeriesPlatformLibrary/Samples/FilterExpressionTests.cpp
+++ Source/Libraries/TimeSeriesPlatformLibrary/Samples/FilterExpressionTests.cpp
@@ -33,39 +33,8 @@ using namespace GSF::Data;
 using namespace GSF::TimeSeries;
 using namespace GSF::TimeSeries::Transport;
 
-// Sample application to test the filter expression parser.
-int main(int argc, char* argv[])
+void Evaluate(const FilterExpressionParserPtr& parser)
 {
-    DataSetPtr dataSet = NewSharedPtr<DataSet>();
-    DataTablePtr dataTable = NewSharedPtr<DataTable>(dataSet, "ActiveMeasurements");
-    DataColumnPtr dataColumn;
-    DataRowPtr dataRow;
-
-    dataColumn = NewSharedPtr<DataColumn>(dataTable, "SignalID", DataType::Guid);
-    dataTable->AddColumn(dataColumn);
-    const int32_t signalIDField = dataTable->Column("SignalID")->Index();
-
-    dataColumn = NewSharedPtr<DataColumn>(dataTable, "SignalType", DataType::String);
-    dataTable->AddColumn(dataColumn);
-    const int32_t signalTypeField = dataTable->Column("SignalType")->Index();
-
-    dataRow = NewSharedPtr<DataRow>(dataTable);
-    dataRow->SetGuidValue(signalIDField, GSF::TimeSeries::Guid());
-    dataRow->SetStringValue(signalTypeField, string("STAT"));
-    dataTable->AddRow(dataRow);
-
-    dataRow = NewSharedPtr<DataRow>(dataTable);
-    dataRow->SetGuidValue(signalIDField, GSF::TimeSeries::Guid());
-    dataRow->SetStringValue(signalTypeField, string("FREQ"));
-    dataTable->AddRow(dataRow);
-
-    dataSet->AddOrUpdateTable(dataTable);
-
-    FilterExpressionParserPtr parser;
-
-    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'FREQ'");
-    parser->AssignDataSet(dataSet);
-
     try
     {
         parser->Evaluate();
@@ -82,14 +51,390 @@ int main(int argc, char* argv[])
     {
         cerr << boost::current_exception_diagnostic_information(true) << endl;
     }
+}
 
-    cout << "Filtered signal ID count = " << parser->FilteredSignalIDs().size() << endl;
+// Sample application to test the filter expression parser.
+int main(int argc, char* argv[])
+{
+    DataSetPtr dataSet = NewSharedPtr<DataSet>();
+    DataTablePtr dataTable = NewSharedPtr<DataTable>(dataSet, "ActiveMeasurements");
+    DataColumnPtr dataColumn;
+    DataRowPtr dataRow;
+    int32_t test = 0;
+
+    dataColumn = NewSharedPtr<DataColumn>(dataTable, "SignalID", DataType::Guid);
+    dataTable->AddColumn(dataColumn);
+    const int32_t signalIDField = dataTable->Column("SignalID")->Index();
+
+    dataColumn = NewSharedPtr<DataColumn>(dataTable, "SignalType", DataType::String);
+    dataTable->AddColumn(dataColumn);
+    const int32_t signalTypeField = dataTable->Column("SignalType")->Index();
+
+    const GSF::TimeSeries::Guid statID = NewGuid();
+    dataRow = NewSharedPtr<DataRow>(dataTable);
+    dataRow->SetGuidValue(signalIDField, statID);
+    dataRow->SetStringValue(signalTypeField, string("STAT"));
+    dataTable->AddRow(dataRow);
+    cout << "Row 1 Statistic SignalID = " << ToString(statID) << endl;
+
+    const GSF::TimeSeries::Guid freqID = NewGuid();
+    dataRow = NewSharedPtr<DataRow>(dataTable);
+    dataRow->SetGuidValue(signalIDField, freqID);
+    dataRow->SetStringValue(signalTypeField, string("FREQ"));
+    dataTable->AddRow(dataRow);
+    cout << "Row 2 Frequency SignalID = " << ToString(freqID) << endl;
+
+    dataSet->AddOrUpdateTable(dataTable);
+    cout << endl;
+
+    FilterExpressionParserPtr parser;
+
+    // Test 1
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'FREQ'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 2
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'STAT'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 3
+    parser = NewSharedPtr<FilterExpressionParser>(";;{" + ToString(statID) + "};;;");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 4
+    parser = NewSharedPtr<FilterExpressionParser>(ToString(statID));
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 5
+    parser = NewSharedPtr<FilterExpressionParser>("'" + ToString(freqID) + "' ; ");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 6
+    parser = NewSharedPtr<FilterExpressionParser>("{" + ToString(statID) + "}; {" + ToString(freqID) + "}; {" + ToString(statID) + "};");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 7
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'FREQ'; '" + ToString(statID) + "'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // Expressions will parse literals before filter statements
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 8
+    parser = NewSharedPtr<FilterExpressionParser>(ToString(freqID) + "; FILTER ActiveMeasurements WHERE SignalType = 'STAT'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    assert(parser->FilteredSignalIDs()[1] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 9
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'FREQ'; FILTER ActiveMeasurements WHERE SignalType = 'STAT' ORDER BY SignalID");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    assert(parser->FilteredSignalIDs()[1] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 10
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'FREQ' OR SignalType = 'STAT'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // Row with stat comes before row with freq
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 11
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'FREQ' OR SignalType = 'STAT' ORDER BY SignalType");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // FREQ should sort before STAT with order by
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    assert(parser->FilteredSignalIDs()[1] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 12
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType = 'STAT' OR SignalType = 'FREQ' ORDER BY SignalType DESC");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // Now descending
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 13
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalID = '" + ToString(freqID) + "'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 14
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalID = " + ToString(freqID) + " OR SignalID = {" + ToString(statID) + "}");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // Row with stat comes before row with freq
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 15
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalID = {" + ToString(statID) + "} OR SignalID = '" + ToString(freqID) + "' ORDER BY SignalType");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // FREQ should sort before STAT with order by
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    assert(parser->FilteredSignalIDs()[1] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 16
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalID = {" + ToString(statID) + "} OR SignalID = '" + ToString(freqID) + "' ORDER BY SignalType; " + ToString(statID));
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+
+    // Because expression includes STAT as a literal (at the end), it will parse first
+    // regardless of order by in filter statement
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 17
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE True");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 18
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE IsNull(NULL, False) OR Coalesce(Null, true)");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 19
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE IIf(IsNull(NULL, False) OR Coalesce(Null, true), Len(SignalType) == 4, false)");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 20
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType IS NOT NULL");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 21
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE Len(SubStr(Coalesce(Trim(SignalType), 'OTHER'), 0, 2)) = 2");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 22
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE LEN(SignalTYPE) > 3.5");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 23
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE Len(SignalType) & 4 == 4");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 24
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE RegExVal('ST.+', SignalType) == 'STAT'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 25
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE IsRegExMatch('FR.+', SignalType)");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 26
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType IN ('FREQ', 'STAT') ORDER BY SignalType");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    assert(parser->FilteredSignalIDs()[1] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 27
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalID IN ({" + ToString(statID) + "}, {" + ToString(freqID) + "})");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 28
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType LIKE 'ST%'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 29
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType LIKE '*EQ'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 30
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType LIKE '*TA%'");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 1);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 31
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE -Len(SignalType) <= 0");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 32 - number converted to string and compared
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType == 0");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 0);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 33 - number converted to string and compared
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE SignalType > 99");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
+
+    // Test 34
+    parser = NewSharedPtr<FilterExpressionParser>("FILTER ActiveMeasurements WHERE Len(SignalType) / 2 = 2");
+    parser->AssignDataSet(dataSet);
+    Evaluate(parser);
+
+    assert(parser->FilteredSignalIDs().size() == 2);
+    assert(parser->FilteredSignalIDs()[0] == statID);
+    assert(parser->FilteredSignalIDs()[1] == freqID);
+    cout << "Test " << ++test << " succeeded..." << endl;
 
     // Wait until the user presses enter before quitting.
+    cout << endl << "Tests complete. Press enter to exit." << endl;
     string line;
     getline(cin, line);
-
-    cout << "Tests complete." << endl;
 
     return 0;
 }
