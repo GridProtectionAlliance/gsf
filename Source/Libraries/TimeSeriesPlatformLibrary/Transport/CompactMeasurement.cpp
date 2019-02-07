83590011e8c44d991855873d1baf015c9fd09b26
J. Ritchie Carroll <ritchiecarroll@users.noreply.github.com>
2019-02-06 20:10:23 -0600

    Update CompactMeasurement.cpp
    Fixed file name listed in header.

--- Source/Libraries/TimeSeriesPlatformLibrary/Transport/CompactMeasurement.cpp
+++ Source/Libraries/TimeSeriesPlatformLibrary/Transport/CompactMeasurement.cpp
@@ -1,7 +1,7 @@
 //******************************************************************************************************
-//  CompactMeasurementParser.cpp - Gbtc
+//  CompactMeasurement.cpp - Gbtc
 //
-//  Copyright © 2018, Grid Protection Alliance.  All Rights Reserved.
+//  Copyright Â© 2018, Grid Protection Alliance.  All Rights Reserved.
 //
 //  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
 //  the NOTICE file distributed with this work for additional information regarding copyright ownership.
