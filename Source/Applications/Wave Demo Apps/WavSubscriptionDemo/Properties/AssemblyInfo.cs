bc6c0cccdbfeed968a40869eba18b31f340661ec
gsfbuildbot <support@gridprotectionalliance.org>
2019-02-28 00:01:03 -0500

    GridSolutionsFramework: Version change for build v2.3.127.0-master.

--- Source/Applications/Wave Demo Apps/WavSubscriptionDemo/Properties/AssemblyInfo.cs
+++ Source/Applications/Wave Demo Apps/WavSubscriptionDemo/Properties/AssemblyInfo.cs
@@ -76,7 +76,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.126.0")]
-[assembly: AssemblyVersion("2.3.126.0")]
-[assembly: AssemblyFileVersion("2.3.126.0")]
-[assembly: AssemblyInformationalVersion("2.3.126-beta")]
+// [assembly: AssemblyVersion("2.3.127.0")]
+[assembly: AssemblyVersion("2.3.127.0")]
+[assembly: AssemblyFileVersion("2.3.127.0")]
+[assembly: AssemblyInformationalVersion("2.3.127-beta")]
--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.3.126.0")]
-[assembly: AssemblyInformationalVersion("2.3.126-beta")]
+[assembly: AssemblyVersion("2.3.127.0")]
+[assembly: AssemblyInformationalVersion("2.3.127-beta")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
