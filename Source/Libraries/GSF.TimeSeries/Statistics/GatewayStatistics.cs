3c697ecde3c356d04f7bfbc56e67ac2326cddd9c
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-03-26 13:14:30 -0400

    GSF.TimeSeries: Updated gateway style statistics to fall-back on reflection in source object in case properties are available.

--- Source/Libraries/GSF.TimeSeries/Statistics/GatewayStatistics.cs
+++ Source/Libraries/GSF.TimeSeries/Statistics/GatewayStatistics.cs
@@ -23,183 +23,108 @@
 //
 //******************************************************************************************************
 
+using System;
+using System.Reflection;
 using GSF.TimeSeries.Transport;
+using GSF.Units;
 
 // ReSharper disable UnusedParameter.Local
 namespace GSF.TimeSeries.Statistics
 {
     internal static class GatewayStatistics
     {
+        private static T GetPropertyValue<T>(object source, string name)
+        {
+            PropertyInfo property = source.GetType().GetProperty(name);
+            return property != null ? (T)property.GetValue(source) : default(T);
+        }
+
+        private static T GetFunctionValue<T>(object source, string name, params object[] parameters)
+        {
+            MethodInfo method = source.GetType().GetMethod(name);
+            return method != null ? (T)method.Invoke(source, parameters) : default(T);
+        }
+
         #region [ Subscriber Statistics ]
 
         private static double GetSubscriberStatistic_Connected(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.IsConnected ? 1.0D : 0.0D;
-
-            return statistic;
+            return (source is DataSubscriber subscriber ? subscriber.IsConnected : GetPropertyValue<bool>(source, "IsConnected")) ? 1.0D : 0.0D;
         }
 
         private static double GetSubscriberStatistic_Authenticated(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.Authenticated ? 1.0D : 0.0D;
-
-            return statistic;
+            return (source is DataSubscriber subscriber ? subscriber.Authenticated : GetPropertyValue<bool>(source, "Authenticated")) ? 1.0D : 0.0D;
         }
 
         private static double GetSubscriberStatistic_ProcessedMeasurements(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = s_statisticValueCache.GetDifference(subscriber, subscriber.ProcessedMeasurements, "ProcessedMeasurements");
-
-            return statistic;
+            double statistic = source is DataSubscriber subscriber ? subscriber.ProcessedMeasurements : GetPropertyValue<long>(source, "ProcessedMeasurements");
+            return s_statisticValueCache.GetDifference(source, statistic, "ProcessedMeasurements");
         }
 
         private static double GetSubscriberStatistic_TotalBytesReceived(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-            {
-                statistic = s_statisticValueCache.GetDifference(subscriber, subscriber.TotalBytesReceived, "TotalBytesReceived");
-
-                if (statistic < 0.0D)
-                    statistic = subscriber.TotalBytesReceived;
-            }
-
-            return statistic;
+            double statistic = source is DataSubscriber subscriber ? subscriber.TotalBytesReceived : GetPropertyValue<long>(source, "TotalBytesReceived");
+            double difference = s_statisticValueCache.GetDifference(source, statistic, "TotalBytesReceived");
+            return difference < 0.0D ? statistic : difference;
         }
 
         private static double GetSubscriberStatistic_AuthorizedCount(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.GetAuthorizedSignalIDs().Length;
-
-            return statistic;
+            Guid[] authorizedSignalIDs = source is DataSubscriber subscriber ? subscriber.GetAuthorizedSignalIDs() : GetFunctionValue<Guid[]>(source, "GetAuthorizedSignalIDs");
+            return authorizedSignalIDs?.Length ?? 0;
         }
 
         private static double GetSubscriberStatistic_UnauthorizedCount(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.GetUnauthorizedSignalIDs().Length;
-
-            return statistic;
+            Guid[] unauthorizedSignalIDs = source is DataSubscriber subscriber ? subscriber.GetUnauthorizedSignalIDs() : GetFunctionValue<Guid[]>(source, "GetUnauthorizedSignalIDs");
+            return unauthorizedSignalIDs?.Length ?? 0;
         }
 
         private static double GetSubscriberStatistic_LifetimeMeasurements(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.LifetimeMeasurements;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.LifetimeMeasurements : GetPropertyValue<long>(source, "LifetimeMeasurements");
         }
 
         private static double GetSubscriberStatistic_MinimumMeasurementsPerSecond(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.MinimumMeasurementsPerSecond;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.MinimumMeasurementsPerSecond : GetPropertyValue<long>(source, "MinimumMeasurementsPerSecond");
         }
 
         private static double GetSubscriberStatistic_MaximumMeasurementsPerSecond(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.MaximumMeasurementsPerSecond;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.MaximumMeasurementsPerSecond : GetPropertyValue<long>(source, "MaximumMeasurementsPerSecond");
         }
 
         private static double GetSubscriberStatistic_AverageMeasurementsPerSecond(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.AverageMeasurementsPerSecond;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.AverageMeasurementsPerSecond : GetPropertyValue<long>(source, "AverageMeasurementsPerSecond");
         }
 
         private static double GetSubscriberStatistic_LifetimeBytesReceived(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.TotalBytesReceived;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.TotalBytesReceived : GetPropertyValue<long>(source, "TotalBytesReceived");
         }
 
         private static double GetSubscriberStatistic_LifetimeMinimumLatency(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.LifetimeMinimumLatency;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.LifetimeMinimumLatency : GetPropertyValue<int>(source, "LifetimeMinimumLatency");
         }
 
         private static double GetSubscriberStatistic_LifetimeMaximumLatency(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.LifetimeMaximumLatency;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.LifetimeMaximumLatency : GetPropertyValue<int>(source, "LifetimeMaximumLatency");
         }
 
         private static double GetSubscriberStatistic_LifetimeAverageLatency(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.LifetimeAverageLatency;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.LifetimeAverageLatency : GetPropertyValue<int>(source, "LifetimeAverageLatency");
         }
 
         private static double GetSubscriberStatistic_UpTime(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataSubscriber subscriber = source as DataSubscriber;
-
-            if ((object)subscriber != null)
-                statistic = subscriber.RunTime;
-
-            return statistic;
+            return source is DataSubscriber subscriber ? subscriber.RunTime : GetPropertyValue<Time>(source, "RunTime");
         }
 
         #endregion
@@ -208,161 +133,76 @@ namespace GSF.TimeSeries.Statistics
 
         private static double GetPublisherStatistic_Connected(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.IsConnected ? 1.0D : 0.0D;
-
-            return statistic;
+            return (source is DataPublisher publisher ? publisher.IsConnected : GetPropertyValue<bool>(source, "IsConnected")) ? 1.0D : 0.0D;
         }
 
         private static double GetPublisherStatistic_ConnectedClientCount(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.Count;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.Count : GetPropertyValue<int>(source, "Count");
         }
 
         private static double GetPublisherStatistic_ProcessedMeasurements(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = s_statisticValueCache.GetDifference(publisher, publisher.ProcessedMeasurements, "ProcessedMeasurements");
-
-            return statistic;
+            double statistic = source is DataPublisher publisher ? publisher.ProcessedMeasurements : GetPropertyValue<long>(source, "ProcessedMeasurements");
+            return s_statisticValueCache.GetDifference(source, statistic, "ProcessedMeasurements");
         }
 
         private static double GetPublisherStatistic_TotalBytesSent(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-            {
-                statistic = s_statisticValueCache.GetDifference(publisher, publisher.TotalBytesSent, "TotalBytesSent");
-
-                if (statistic < 0.0D)
-                    statistic = publisher.TotalBytesSent;
-            }
-
-            return statistic;
+            double statistic = source is DataPublisher publisher ? publisher.TotalBytesSent : GetPropertyValue<long>(source, "TotalBytesSent");        
+            double difference = s_statisticValueCache.GetDifference(source, statistic, "TotalBytesSent");
+            return difference < 0.0D ? statistic : difference;
         }
 
         private static double GetPublisherStatistic_LifetimeMeasurements(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.LifetimeMeasurements;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.LifetimeMeasurements : GetPropertyValue<long>(source, "LifetimeMeasurements");
         }
 
         private static double GetPublisherStatistic_MinimumMeasurementsPerSecond(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.MinimumMeasurementsPerSecond;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.MinimumMeasurementsPerSecond : GetPropertyValue<long>(source, "MinimumMeasurementsPerSecond");
         }
 
         private static double GetPublisherStatistic_MaximumMeasurementsPerSecond(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.MaximumMeasurementsPerSecond;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.MaximumMeasurementsPerSecond : GetPropertyValue<long>(source, "MaximumMeasurementsPerSecond");
         }
 
         private static double GetPublisherStatistic_AverageMeasurementsPerSecond(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.AverageMeasurementsPerSecond;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.AverageMeasurementsPerSecond : GetPropertyValue<long>(source, "AverageMeasurementsPerSecond");
         }
 
         private static double GetPublisherStatistic_LifetimeBytesSent(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.TotalBytesSent;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.TotalBytesSent : GetPropertyValue<long>(source, "TotalBytesSent");
         }
 
         private static double GetPublisherStatistic_LifetimeMinimumLatency(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.LifetimeMinimumLatency;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.LifetimeMinimumLatency : GetPropertyValue<int>(source, "LifetimeMinimumLatency");
         }
 
         private static double GetPublisherStatistic_LifetimeMaximumLatency(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.LifetimeMaximumLatency;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.LifetimeMaximumLatency : GetPropertyValue<int>(source, "LifetimeMaximumLatency");
         }
 
         private static double GetPublisherStatistic_LifetimeAverageLatency(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.LifetimeAverageLatency;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.LifetimeAverageLatency : GetPropertyValue<int>(source, "LifetimeAverageLatency");
         }
 
         private static double GetPublisherStatistic_BufferBlockRetransmissions(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = s_statisticValueCache.GetDifference(publisher, publisher.BufferBlockRetransmissions, "BufferBlockRetransmissions");
-
-            return statistic;
+            double statistic = source is DataPublisher publisher ? publisher.BufferBlockRetransmissions : GetPropertyValue<long>(source, "BufferBlockRetransmissions");
+            return s_statisticValueCache.GetDifference(source, statistic, "BufferBlockRetransmissions");
         }
 
         private static double GetPublisherStatistic_UpTime(object source, string arguments)
         {
-            double statistic = 0.0D;
-            DataPublisher publisher = source as DataPublisher;
-
-            if ((object)publisher != null)
-                statistic = publisher.RunTime;
-
-            return statistic;
+            return source is DataPublisher publisher ? publisher.RunTime : GetPropertyValue<Time>(source, "RunTime");
         }
 
         #endregion
