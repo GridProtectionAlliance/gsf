fe155592f25df51a707525f8d4dc85861c333289
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-06-21 15:17:25 -0400

    AzureEventHubAdapters: Removed unused code.

--- Source/Libraries/Adapters/AzureEventHubAdapters/AzureEventHubOutputAdapter.cs
+++ Source/Libraries/Adapters/AzureEventHubAdapters/AzureEventHubOutputAdapter.cs
@@ -21,13 +21,10 @@
 //
 //******************************************************************************************************
 
-#pragma warning disable 4014
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
