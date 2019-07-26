cde95918247060e5f9ef732308d4e37ba234c2f9
gsfbuildbot <support@gridprotectionalliance.org>
2019-07-26 00:00:37 -0400

    GridSolutionsFramework: Version change for build v2.3.186.0-master.

--- Source/Libraries/GSF.SELEventParser/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.SELEventParser/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("ee39b8ec-2b03-46f8-9ea8-25bba8e33032")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.3.185.0")]
-[assembly: AssemblyFileVersion("2.3.185.0")]
-[assembly: AssemblyInformationalVersion("2.3.185-beta")]
+[assembly: AssemblyVersion("2.3.186.0")]
+[assembly: AssemblyFileVersion("2.3.186.0")]
+[assembly: AssemblyInformationalVersion("2.3.186-beta")]
