d339d2796b6036c156a71ab47f7c8b651a02a605
gsfbuildbot <support@gridprotectionalliance.org>
2019-08-19 00:00:27 -0400

    GridSolutionsFramework: Version change for build v2.3.197.0-master.

--- Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Core/Properties/AssemblyInfo.cs
@@ -28,10 +28,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.3.196.0")]
+[assembly: AssemblyVersion("2.3.197.0")]
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
-[assembly: AssemblyInformationalVersion("2.3.196-beta")]
+[assembly: AssemblyInformationalVersion("2.3.197-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Web.UI;
 using GSF.Web.Hosting;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.3.196.0")]
-[assembly: AssemblyInformationalVersion("2.3.196-beta")]
+[assembly: AssemblyVersion("2.3.197.0")]
+[assembly: AssemblyInformationalVersion("2.3.197-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
