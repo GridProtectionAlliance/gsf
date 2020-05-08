2afc45cd8351608fffdf2471a3481d3ada364554
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-05-08 11:36:45 -0400

    Added Network Throuput Testing Tool

---
+++ Source/Tools/NetworkThrouputTool/Main.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace NetworkThrouputTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxServerPort = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPacketSize = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxClientHost = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxClientPort = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxServerPort);
+            this.groupBox1.Location = new System.Drawing.Point(588, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(103, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // textBoxServerPort
+            // 
+            this.textBoxServerPort.Location = new System.Drawing.Point(72, 19);
+            this.textBoxServerPort.Name = "textBoxServerPort";
+            this.textBoxServerPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxServerPort.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxPacketSize);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxClientHost);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBoxClientPort);
+            this.groupBox2.Location = new System.Drawing.Point(588, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 162);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Size";
+            // 
+            // textBoxPacketSize
+            // 
+            this.textBoxPacketSize.Location = new System.Drawing.Point(51, 71);
+            this.textBoxPacketSize.Name = "textBoxPacketSize";
+            this.textBoxPacketSize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPacketSize.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Host";
+            // 
+            // textBoxClientHost
+            // 
+            this.textBoxClientHost.Location = new System.Drawing.Point(51, 45);
+            this.textBoxClientHost.Name = "textBoxClientHost";
+            this.textBoxClientHost.Size = new System.Drawing.Size(100, 20);
+            this.textBoxClientHost.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // textBoxClientPort
+            // 
+            this.textBoxClientPort.Location = new System.Drawing.Point(51, 19);
+            this.textBoxClientPort.Name = "textBoxClientPort";
+            this.textBoxClientPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxClientPort.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(551, 351);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Type";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Size (B)";
+            this.columnHeader2.Width = 132;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Time (ms)";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Speed (KB/s)";
+            this.columnHeader4.Width = 104;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 381);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Main";
+            this.Text = "Thropughput Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxServerPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPacketSize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxClientHost;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxClientPort;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Source/Tools/NetworkThrouputTool/Main.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkThrouputTool
+{
+    public partial class Main : Form
+    {
+        private TcpListener m_server;
+        private delegate void SafeCallDelegate(Result result);
+
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random rand = new Random();
+                int size = Convert.ToInt32(textBoxPacketSize.Text);
+
+                if (size < 8)
+                {
+                    MessageBox.Show("Please specify a packetsize > 8B", "Invalid PacketSize", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                Byte[] data = new byte[size];
+                rand.NextBytes(data);
+                Buffer.BlockCopy(BitConverter.GetBytes(size), 0, data, 0, 4);
+
+                int port = Convert.ToInt32(textBoxClientPort.Text);
+
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Reset();
+                stopWatch.Start();
+
+                TcpClient client = new TcpClient(textBoxClientHost.Text, port);
+                
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+
+                stream.Close();
+                client.Close();
+                stopWatch.Stop();
+
+                Result result = new Result()
+                {
+                    Size = size,
+                    Type = "Sent",
+                    Time = stopWatch.ElapsedTicks/ 10000.0D,
+                    Speed = Convert.ToDouble(size) / (stopWatch.ElapsedTicks / 10000.0D)
+                };
+                AppendRow(result);
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show($"Can not connect to Server. Message: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                int port = Convert.ToInt32(textBoxServerPort.Text);
+                m_server = new TcpListener(localAddr, port);
+                m_server.Start();
+                button1.Enabled = false;
+                button2.Enabled = true;
+
+                m_server.BeginAcceptTcpClient(new AsyncCallback(AccepConnection), m_server);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Can not start Server. Message: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m_server.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        // Process the client connection.
+        public void AccepConnection(IAsyncResult ar)
+        {
+            try
+            {
+                // Get the listener that handles the client request.
+                TcpListener listener = (TcpListener)ar.AsyncState;
+
+                // End the operation and display the received data on 
+                // the console.
+                TcpClient client = listener.EndAcceptTcpClient(ar);
+                
+                NetworkStream stream = client.GetStream();
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Reset();
+                stopWatch.Start();
+
+                Byte[] hbuffer = new Byte[4]; // 8 Byte buffer for size
+                int actualReadBytes = stream.Read(hbuffer, 0, hbuffer.Length);
+              
+                int size = BitConverter.ToInt32(hbuffer, 0);
+
+                Byte[] buffer = new Byte[1024]; // 8 Byte buffer for size
+
+                while (actualReadBytes < size)
+                {
+                    actualReadBytes = actualReadBytes + stream.Read(buffer, actualReadBytes, buffer.Length);
+                }
+
+                stopWatch.Stop();
+
+                var d = new SafeCallDelegate(AppendRow);
+
+                m_server.BeginAcceptTcpClient(new AsyncCallback(AccepConnection), m_server);
+
+                Result result = new Result() { 
+                    Size = size, 
+                    Type = "Recieved",
+                    Time = stopWatch.ElapsedTicks / 10000.0D,
+                    Speed = size / (stopWatch.ElapsedTicks / 10000.0D)
+                };
+                this.listView1.Invoke(d, result);
+            }
+            catch { }
+        }
+
+        private void AppendRow(Result result)
+        {
+            ListViewItem it = new ListViewItem(result.Type);
+            
+            it.SubItems.Add(result.Size.ToString());
+            it.SubItems.Add(result.Time.ToString());
+            it.SubItems.Add(result.Speed.ToString());
+            this.listView1.Items.Add(it);
+        }
+
+        private class Result
+        {
+            public int Size;
+            public string Type;
+            public double Time;
+            public double Speed;
+
+        }
+    }
+
+}
