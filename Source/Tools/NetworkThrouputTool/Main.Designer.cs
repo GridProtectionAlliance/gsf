57de9c6bf6ffb46371ae7973be893fca77e4df33
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-05-08 14:34:15 -0400

    Added button to clear screen

--- Source/Tools/NetworkThrouputTool/Main.Designer.cs
+++ Source/Tools/NetworkThrouputTool/Main.Designer.cs
@@ -47,6 +47,7 @@
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -218,11 +219,22 @@
             this.columnHeader4.Text = "Speed (KB/s)";
             this.columnHeader4.Width = 104;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 381);
+            this.ClientSize = new System.Drawing.Size(800, 406);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -257,6 +269,7 @@
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Source/Tools/NetworkThrouputTool/Main.cs
+++ Source/Tools/NetworkThrouputTool/Main.cs
@@ -177,6 +177,11 @@ namespace NetworkThrouputTool
             public double Speed;
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.listView1.Clear();
+        }
     }
 
 }
