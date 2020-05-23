a8afc870a159a646708931b58669eff40c5fb59b
gsfbuildbot <support@gridprotectionalliance.org>
2020-05-23 00:11:41 -0400

    GridSolutionsFramework: Version change for build v2.3.314.0-master.

--- Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.313.0")]
-[assembly: AssemblyFileVersion("2.3.313.0")]
-[assembly: AssemblyInformationalVersion("2.3.313-beta")]
+[assembly: AssemblyVersion("2.3.314.0")]
+[assembly: AssemblyFileVersion("2.3.314.0")]
+[assembly: AssemblyInformationalVersion("2.3.314-beta")]
--- Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Web.UI;
 using GSF.Web.Hosting;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.3.313.0")]
-[assembly: AssemblyInformationalVersion("2.3.313-beta")]
+[assembly: AssemblyVersion("2.3.314.0")]
+[assembly: AssemblyInformationalVersion("2.3.314-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
