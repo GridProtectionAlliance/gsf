34f5629a5cb00ea8b48059d8f47045587c9f3f55
gsfbuildbot <support@gridprotectionalliance.org>
2020-06-11 00:00:43 -0400

    GridSolutionsFramework: Version change for build v2.3.321.0-master.

--- Source/Tools/HistorianView/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianView/Properties/AssemblyInfo.cs
@@ -75,7 +75,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.3.320.0")]
-[assembly: AssemblyVersion("2.3.320.0")]
-[assembly: AssemblyFileVersion("2.3.320.0")]
-[assembly: AssemblyInformationalVersion("2.3.320-beta")]
+// [assembly: AssemblyVersion("2.3.321.0")]
+[assembly: AssemblyVersion("2.3.321.0")]
+[assembly: AssemblyFileVersion("2.3.321.0")]
+[assembly: AssemblyInformationalVersion("2.3.321-beta")]
