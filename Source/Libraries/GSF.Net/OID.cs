03012f72acca908f8b16fec36a2b3ae1ef95c409
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2020-09-11 01:34:32 -0500

    GSF.Net: Added common OID values and SNMP send trap functions.

---
+++ Source/Libraries/GSF.Net/OID.cs
@@ -0,0 +1,82 @@
+﻿//******************************************************************************************************
+//  OID.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/10/2020 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace GSF.Net
+{
+    /// <summary>
+    /// Defines global object identifiers used by the Grid Solutions Framework.
+    /// </summary>
+    public static class OID
+    {
+        /// <summary>
+        /// Root GSF OID (1.3.6.1.4.1.56056). This is the private enterprise number defined for the Grid Protection Alliance.
+        /// </summary>
+        public static readonly uint[] EnterpriseRoot = { 1, 3, 6, 1, 4, 1, 56056 };
+
+        /// <summary>
+        /// Root GSF OID for SNMP objects (1.3.6.1.4.1.56056.1).
+        /// </summary>
+        public static readonly uint[] SnmpRoot = EnterpriseRoot.Append(1U);
+
+        /// <summary>
+        /// Root GSF OID for SNMP statistic values (1.3.6.1.4.1.56056.1.1).
+        /// </summary>
+        public static readonly uint[] SnmpStatsRoot = SnmpRoot.Append(1U);
+
+        /// <summary>
+        /// GSF SNMP OIDs for known statistic sources (1.3.6.1.4.1.56056.1.1.x).
+        /// </summary>
+        /// <remarks>
+        /// Each source category will subdivide specific statistics by signal index.
+        /// </remarks>
+        public static readonly ReadOnlyDictionary<string, uint[]> SnmpStats = new ReadOnlyDictionary<string, uint[]>(new Dictionary<string, uint[]>(StringComparer.OrdinalIgnoreCase)
+        {
+            ["System"] = SnmpStatsRoot.Append(1U),
+            ["Device"] = SnmpStatsRoot.Append(2U),
+            ["InputStream"] = SnmpStatsRoot.Append(3U),
+            ["OutputStream"] = SnmpStatsRoot.Append(4U),
+            ["Subscriber"] = SnmpStatsRoot.Append(5U),
+            ["Publisher"] = SnmpStatsRoot.Append(6U),
+            ["Process"] = SnmpStatsRoot.Append(7U),
+            ["Downloader"] = SnmpStatsRoot.Append(8U)
+        });
+
+        /// <summary>
+        /// Root GSF OID for LDAP objects (1.3.6.1.4.1.56056.2).
+        /// </summary>
+        public static readonly uint[] LdapRoot = EnterpriseRoot.Append(2U);
+
+        /// <summary>
+        /// Appends an new OID value to an existing OID value array.
+        /// </summary>
+        /// <param name="oid">Root OID value array.</param>
+        /// <param name="value">New OID value to append.</param>
+        /// <returns>Combined OID value array.</returns>
+        public static uint[] Append(this uint[] oid, uint value) => oid.Concat(new[] { value }).ToArray();
+    }
+}
---
+++ Source/Libraries/GSF.Net/Snmp/Snmp.cs
@@ -0,0 +1,197 @@
+﻿//******************************************************************************************************
+//  Snmp.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/10/2020 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Threading.Tasks;
+using GSF.Configuration;
+using GSF.Diagnostics;
+using GSF.Net.Snmp.Messaging;
+using GSF.Net.Snmp.Security;
+using GSF.Security.Cryptography;
+
+namespace GSF.Net.Snmp
+{
+    /// <summary>
+    /// Exposes simple SNMP functionality for GSF.
+    /// </summary>
+    public static class Snmp
+    {
+        /// <summary>
+        /// Defines default IP end point for SNMP client agent.
+        /// </summary>
+        public const string DefaultIPEndPoint = "255.255.255.255:162";
+
+        /// <summary>
+        /// Root GSF OID (1.3.6.1.4.1.56056). This is the private enterprise number defined for the Grid Protection Alliance.
+        /// </summary>
+        public static readonly ObjectIdentifier EnterpriseRoot = new ObjectIdentifier(OID.EnterpriseRoot);
+
+        /// <summary>
+        /// GSF SNMP engine ID (D3F30A26E61843918ED5ABD87E).
+        /// </summary>
+        public static readonly OctetString EngineID = new OctetString(ByteTool.Convert("D3F30A26E61843918ED5ABD87E"));
+
+        /// <summary>
+        /// Sends an integer based SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="value">Notification value.</param>
+        public static void SendTrap(uint[] oid, int value) => SendTrapAsync(oid, value).Wait();
+
+        /// <summary>
+        /// Sends a floating-point based SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="value">Notification value.</param>
+        /// <remarks>
+        /// SNMP does not support floating point values via binary encoding, so value is encoded as a string.
+        /// </remarks>
+        public static void SendTrap(uint[] oid, double value) => SendTrapAsync(oid, value).Wait();
+
+        /// <summary>
+        /// Sends a string based SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="value">Notification value.</param>
+        public static void SendTrap(uint[] oid, string value) => SendTrapAsync(oid, value).Wait();
+
+        /// <summary>
+        /// Sends an SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="data">Notification data.</param>
+        public static void SendTrap(uint[] oid, ISnmpData data) => SendTrapAsync(oid, data).Wait();
+
+        /// <summary>
+        /// Sends an integer based SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="value">Notification value.</param>
+        public static Task SendTrapAsync(uint[] oid, int value) => SendTrapAsync(oid, new Integer32(value));
+
+        /// <summary>
+        /// Sends a floating-point based SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="value">Notification value.</param>
+        /// <remarks>
+        /// SNMP does not support floating point values via binary encoding, so value is encoded as a string.
+        /// </remarks>
+        public static Task SendTrapAsync(uint[] oid, double value) => SendTrapAsync(oid, new OctetString(value.ToString(CultureInfo.InvariantCulture)));
+
+        /// <summary>
+        /// Sends a string based SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="value">Notification value.</param>
+        public static Task SendTrapAsync(uint[] oid, string value) => SendTrapAsync(oid, new OctetString(value));
+
+        /// <summary>
+        /// Sends an SNMP version 3 trap.
+        /// </summary>
+        /// <param name="oid">Target OID.</param>
+        /// <param name="data">Notification data.</param>
+        public static async Task SendTrapAsync(uint[] oid, ISnmpData data)
+        {
+            await new TrapV2Message
+            (
+                VersionCode.V3,
+                Messenger.NextMessageId,
+                Messenger.NextRequestId,
+                s_community,
+                EnterpriseRoot,
+                (uint)Environment.TickCount / 10,
+                new List<Variable> { new Variable(new ObjectIdentifier(oid), data) },
+                s_privacyProvider,
+                Messenger.MaxMessageSize,
+                EngineID, 0, 0
+            )
+            .SendAsync(s_endPoint);
+        }
+
+        private static readonly OctetString s_community;
+        private static readonly IPEndPoint s_endPoint;
+        private static readonly DESPrivacyProvider s_privacyProvider;
+
+        static Snmp()
+        {
+            try
+            {
+                // Get configured SNMP agent settings
+                CategorizedSettingsElementCollection settings = ConfigurationFile.Current.Settings["snmpAgent"];
+                PasswordGenerator passwordGenerator = new PasswordGenerator(PasswordCharacterGroups);
+
+                settings.Add("Community", "Eval(securityProvider.ApplicationName)", "Defines the SNMP agent community identifier for trap messages.");
+                settings.Add("IPEndPoint", DefaultIPEndPoint, "Defines the SNMP agent target IP end point for trap messages.");
+                settings.Add("AuthPhrase", passwordGenerator.GeneratePassword(16), "Defines the SNMP agent authentication phrase for trap messages.");
+                settings.Add("EncryptKey", passwordGenerator.GeneratePassword(16), "Defines the SNMP agent encryption key for trap messages.");
+
+                s_community = new OctetString(settings["Community"].ValueAs(nameof(GSF)));
+
+                string endPoint = settings["IPEndPoint"].ValueAs(DefaultIPEndPoint);
+                string[] parts = endPoint.Split(':');
+
+                if (parts.Length != 2)
+                {
+                    parts = DefaultIPEndPoint.Split(':');
+                    Logger.SwallowException(new Exception($"Configured SNMP agent IP end point \"{endPoint}\" format is invalid. Using default IP end point \"{DefaultIPEndPoint}\"."));
+                }
+
+                if (!IPAddress.TryParse(parts[0], out IPAddress address))
+                {
+                    address = IPAddress.Broadcast;
+                    Logger.SwallowException(new Exception($"Configured SNMP agent IP end point \"{endPoint}\" address is invalid. Using broadcast address \"255.255.255.255\"."));
+                }
+
+                if (!ushort.TryParse(parts[1], out ushort port))
+                {
+                    port = 162;
+                    Logger.SwallowException(new Exception($"Configured SNMP agent IP end point \"{endPoint}\" port is invalid. Using default port \"162\"."));
+                }
+
+                s_endPoint = new IPEndPoint(address, port);
+
+                string authPhrase = settings["AuthPhrase"].Value.PadRight(16, '#');
+                string encryptKey = settings["EncryptKey"].Value.PadRight(16, '#');
+
+                s_privacyProvider = new DESPrivacyProvider(
+                    new OctetString(encryptKey),
+                    new MD5AuthenticationProvider(new OctetString(authPhrase)));
+            }
+            catch (Exception ex)
+            {
+                Logger.SwallowException(new Exception($"Failed to load SNMP agent configuration: {ex.Message}", ex));
+            }
+        }
+
+        private static readonly IReadOnlyList<CharacterGroup> PasswordCharacterGroups = new List<CharacterGroup>()
+        {
+            new CharacterGroup { Characters = "abcdefghijklmnopqrstuvwxyz", MinOccurrence = 1 },
+            new CharacterGroup { Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", MinOccurrence = 1 },
+            new CharacterGroup { Characters = "0123456789", MinOccurrence = 1 },
+        };
+    }
+}
