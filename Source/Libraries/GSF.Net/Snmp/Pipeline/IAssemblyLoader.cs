fc3706e9406b470648c7c3fe1238bdcffba7ec48
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2020-09-10 23:02:04 -0500

    GSF.Net: Added SNMP engine components for hosting

---
+++ Source/Libraries/GSF.Net/Snmp/Pipeline/IAssemblyLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace GSF.Net.Snmp.Pipeline
+{
+    /// <summary>
+    /// .NET standard 1.3 only helper.
+    /// </summary>
+    public interface ITypeResolver
+    {
+        /// <summary>
+        /// Loads desired type from an assembly.
+        /// </summary>
+        /// <param name="assembly">Assembly name.</param>
+        /// <param name="name">Type name.</param>
+        /// <returns>Type metadata.</returns>
+        Type Load(string assembly, string name);
+
+        /// <summary>
+        /// Returns assemblies loaded.
+        /// </summary>
+        /// <returns></returns>
+        Assembly[] GetAssemblies();
+    }
+}
