113501a98c673d64bd48aa60dbcbe1844b038f7f
Billy Ernest <bernest@gridprotectionalliance.org>
2021-06-11 11:31:39 -0400

    GSF.Core: Added class and property attributes for use in GSF.Web.Model.ModelController class.

---
+++ Source/Libraries/GSF.Core/Data/Model/ParentKeyAttribute.cs
@@ -0,0 +1,53 @@
+﻿//******************************************************************************************************
+//  ParentKeyAttribute.cs - Gbtc
+//
+//  Copyright © 2021, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  06/11/2021 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+
+using System;
+
+namespace GSF.Data.Model
+{
+    /// <summary>
+    /// Defines an attribute that will allow a foreign key in the model to point back to parent table 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class ParentKeyAttribute : Attribute 
+    {
+        /// <summary>
+        /// Gets field name to use for property.
+        /// </summary>
+        public Type Model
+        {
+            get;
+        }
+
+
+        /// <summary>
+        /// Creates a new <see cref="ParentKeyAttribute"/>.
+        /// </summary>
+        /// <param name="model">Type of modeled table that key points back to.</param>
+        public ParentKeyAttribute(Type model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ Source/Libraries/GSF.Core/Data/Model/PatchRolesAttribute.cs
@@ -0,0 +1,51 @@
+﻿//******************************************************************************************************
+//  PatchRolesAttribute.cs - Gbtc
+//
+//  Copyright © 2021, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  06/11/2021 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+
+namespace GSF.Data.Model
+{
+    /// <summary>
+    /// Defines an attribute that will allow setting PATCH function roles for a modeled table.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class PatchRolesAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets field name to use for property.
+        /// </summary>
+        public string Roles
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="PatchRolesAttribute"/>.
+        /// </summary>
+        /// <param name="roles">Comma separated string of roles allowed for PATCH functions.</param>
+        public PatchRolesAttribute(string roles)
+        {
+            Roles = roles;
+        }
+    }
+}
