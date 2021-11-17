13db1acbd14a205946b91d84bc504cfe9a4be99e
gsfbuildbot <support@gridprotectionalliance.org>
2021-11-17 00:02:31 -0500

    GridSolutionsFramework: Version change for build v2.4.1.0-master.

--- Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.519.0")]
-[assembly: AssemblyFileVersion("2.3.519.0")]
-[assembly: AssemblyInformationalVersion("2.3.519-beta")]
+[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
+[assembly: AssemblyInformationalVersion("2.4.1-beta")]
