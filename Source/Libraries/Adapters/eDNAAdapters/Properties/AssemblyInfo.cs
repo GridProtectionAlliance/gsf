3cbf53b990db42bcb7cc4451b72a8a9a8e91ae41
gsfbuildbot <support@gridprotectionalliance.org>
2022-03-29 00:01:27 -0400

    GridSolutionsFramework: Version change for build v2.4.27.0-master.

--- Source/Libraries/Adapters/eDNAAdapters/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/eDNAAdapters/Properties/AssemblyInfo.cs
@@ -46,6 +46,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("1c165fad-7d51-4d8e-9311-4a3be266d877")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.4.26.0")]
-[assembly: AssemblyFileVersion("2.4.26.0")]
-[assembly: AssemblyInformationalVersion("2.4.26-beta")]
+[assembly: AssemblyVersion("2.4.27.0")]
+[assembly: AssemblyFileVersion("2.4.27.0")]
+[assembly: AssemblyInformationalVersion("2.4.27-beta")]
