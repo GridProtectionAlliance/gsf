dfbfd99b1bab86de412ec87e6ac5e24fdfa8e17a
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2022-03-29 13:26:29 -0500

    GSF.PhasorProtocols: Fixed AutoRepeatPlayback deserialization for ConnectionSettings

--- Source/Libraries/GSF.PhasorProtocols/ConnectionSettings.cs
+++ Source/Libraries/GSF.PhasorProtocols/ConnectionSettings.cs
@@ -112,8 +112,7 @@ namespace GSF.PhasorProtocols
             if (!int.TryParse(info.GetOrDefault("FrameRate", "30"), out FrameRate))
                 FrameRate = 30;
 
-            if (!info.GetOrDefault("AutoRepeatPlayback", "false").ParseBoolean())
-                AutoRepeatPlayback = false;
+            AutoRepeatPlayback = info.GetOrDefault("AutoRepeatPlayback", "false").ParseBoolean();
 
             if (!int.TryParse(info.GetOrDefault("ByteEncodingDisplayFormat", "0"), out ByteEncodingDisplayFormat))
                 ByteEncodingDisplayFormat = 0;
