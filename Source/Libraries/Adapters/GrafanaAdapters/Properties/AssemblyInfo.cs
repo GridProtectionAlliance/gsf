d2e615d8cb80068381d19b61bb8979555bfd785d
gsfbuildbot <support@gridprotectionalliance.org>
2022-06-13 21:33:00 -0400

    GridSolutionsFramework: Version change for build v2.4.39.0-master.

--- Source/Libraries/Adapters/GrafanaAdapters/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/GrafanaAdapters/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.4.38.0")]
-[assembly: AssemblyInformationalVersion("2.4.38-beta")]
+[assembly: AssemblyVersion("2.4.39.0")]
+[assembly: AssemblyInformationalVersion("2.4.39-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Web.UI;
 using GSF.Web.Hosting;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.4.38.0")]
-[assembly: AssemblyInformationalVersion("2.4.38-beta")]
+[assembly: AssemblyVersion("2.4.39.0")]
+[assembly: AssemblyInformationalVersion("2.4.39-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
