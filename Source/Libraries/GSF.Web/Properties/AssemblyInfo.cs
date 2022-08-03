ac62bba7a07659909d98a4e7b43e98a17d46f9ba
gsfbuildbot <support@gridprotectionalliance.org>
2022-08-03 00:01:15 -0400

    GridSolutionsFramework: Version change for build v2.4.51.0-master.

--- Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.Web/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Web.UI;
 using GSF.Web.Hosting;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.4.50.0")]
-[assembly: AssemblyInformationalVersion("2.4.50-beta")]
+[assembly: AssemblyVersion("2.4.51.0")]
+[assembly: AssemblyInformationalVersion("2.4.51-beta")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.4.50.0")]
-[assembly: AssemblyInformationalVersion("2.4.50-beta")]
+[assembly: AssemblyVersion("2.4.51.0")]
+[assembly: AssemblyInformationalVersion("2.4.51-beta")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
