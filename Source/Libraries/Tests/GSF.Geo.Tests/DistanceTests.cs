6a4244479c8d8d8626731edc9012cc85ebe19cea
Christoph Lackner <clackner@gridprotectionalliance.org>
2022-08-04 13:09:08 -0400

    Added GSF.Geo Tests

---
+++ Source/Libraries/Tests/GSF.Geo.Tests/DistanceTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSF.Geo.Tests
+{
+    [TestClass]
+    public class DistanceTests
+    {
+        #region [ Constants ]
+        
+        private const double GPALongitude = 35.043695517962426;
+        private const double GPALattitude = -85.30907977980011;
+
+        private const double TVALongitude = 35.043413104632556;
+        private const double TVALattitude = -85.30980478148788;
+
+        
+        private const double SDGELongitude = 32.82440734588638;
+        private const double SDGELattitude = -117.14289125148301;
+
+        private const double SydneyLongitude = -33.856834016746;
+        private const double SydneyLattitude = 151.21531449799255;
+
+        private const double GPAtoTVA = 73;
+        private const double GPAtoSDGE = 5096750;
+        private const double GPAtoSydney = 2941070;
+        #endregion
+
+        #region [ Members ]
+
+        private GeoCoordinate GPAOffices = new GeoCoordinate(GPALattitude, GPALongitude);
+        private GeoCoordinate TVAOffices = new GeoCoordinate(TVALattitude, TVALongitude);
+        private GeoCoordinate SDGEOffices = new GeoCoordinate(SDGELattitude, SDGELongitude);
+        private GeoCoordinate Sydney = new GeoCoordinate(SydneyLattitude, SydneyLongitude);
+        #endregion
+
+        [TestMethod]
+        public void LattitudeTest()
+        {
+            Assert.IsTrue(GPAOffices.Latitude == GPALattitude);
+            Assert.IsTrue(Sydney.Latitude == SydneyLattitude);
+        }
+
+        [TestMethod]
+        public void LongitudeTest()
+        {
+            Assert.IsTrue(GPAOffices.Longitude == GPALongitude);
+            Assert.IsTrue(Sydney.Longitude == SydneyLongitude);
+        }
+
+        [TestMethod]
+        public void DistanceTest()
+        {
+            Assert.IsTrue(GPAOffices.Distance(TVAOffices) == GPAtoTVA);
+            Assert.IsTrue(GPAOffices.Distance(SDGEOffices) == GPAtoSDGE);
+            Assert.IsTrue(GPAOffices.Distance(Sydney) == GPAtoSydney);
+        }
+    }
+}
---
+++ Source/Libraries/Tests/GSF.Geo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,64 @@
+﻿//******************************************************************************************************
+//  AssemblyInfo.cs - Gbtc
+//
+//  Copyright © 2013, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/09/2013 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GSF.Geo.Tests")]
+[assembly: AssemblyDescription("GSF Geo Test Assembly")]
+[assembly: AssemblyCompany("Grid Protection Alliance")]
+[assembly: AssemblyProduct("Grid Solutions Framework")]
+[assembly: AssemblyCopyright("Copyright © GPA, 2013.  All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Assembly manifest attributes.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("EA111D31-5D1E-4FD7-8BDA-7567661832CB")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.4.52.0")]
+[assembly: AssemblyFileVersion("2.4.52.0")]
+[assembly: AssemblyInformationalVersion("2.4.52-beta")]
