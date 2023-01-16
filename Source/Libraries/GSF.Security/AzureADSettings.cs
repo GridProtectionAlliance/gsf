00f77c1a0d932d7695e2b2e7bee8e99c73a74733
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2023-01-16 15:40:55 -0500

    GSF.Security: Added AzureADSettings - can load from appsettings.json or categorized config file settings

---
+++ Source/Libraries/GSF.Security/AzureADSettings.cs
@@ -0,0 +1,180 @@
+﻿//******************************************************************************************************
+//  AzureADSettings.cs - Gbtc
+//
+//  Copyright © 2023, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/15/2023 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System.IO;
+using GSF.Configuration;
+using GSF.IO;
+using Newtonsoft.Json;
+
+namespace GSF.Security;
+
+/// <summary>
+/// Represents settings needed for Azure Active Directory (AD) authentication.
+/// </summary>
+public class AzureADSettings
+{
+    /// <summary>
+    /// Default category for Azure AD settings.
+    /// </summary>
+    public const string DefaultSettingsCategory = "AzureAD";
+
+    /// <summary>
+    /// Default value for <see cref="Instance"/> property.
+    /// </summary>
+    public const string DefaultInstance = "https://login.microsoftonline.com/";
+
+    /// <summary>
+    /// Default value for <see cref="TenantID"/> property.
+    /// </summary>
+    public const string DefaultTenantID = "";
+
+    /// <summary>
+    /// Default value for <see cref="ClientID"/> property.
+    /// </summary>
+    public const string DefaultClientID = "";
+
+    /// <summary>
+    /// Default value for <see cref="CallbackPath"/> property.
+    /// </summary>
+    public const string DefaultCallbackPath = "/signin-oidc";
+
+    /// <summary>
+    /// Default value for <see cref="SignedOutCallbackPath"/> property.
+    /// </summary>
+    public const string DefaultSignedOutCallbackPath = "/signout-oidc";
+
+    /// <summary>
+    /// Default value for <see cref="Enabled"/> property.
+    /// </summary>
+    public const bool DefaultEnabled = false;
+
+    private const string DefaultAzureADSettings = @$"{{
+  ""{DefaultSettingsCategory}"": {{
+    ""Instance"": ""{DefaultInstance}"",
+    ""TenantId"": ""[Enter the tenantId here]"",
+
+    // Client ID (application ID) obtained from the Azure portal
+    ""ClientId"": ""[Enter the Client Id here]"",
+    ""CallbackPath"": ""{DefaultCallbackPath}"",
+    ""SignedOutCallbackPath"": ""{DefaultSignedOutCallbackPath}"",
+    ""Enabled"":  false
+  }}
+}}";
+
+    /// <summary>
+    /// Gets or sets the Azure AD instance URL.
+    /// </summary>
+    public string Instance { get; set; } = DefaultInstance;
+
+    /// <summary>
+    /// Gets or sets the Azure AD tenant ID.
+    /// </summary>
+    public string TenantID { get; set; } = DefaultTenantID;
+
+    /// <summary>
+    /// Gets or sets the Azure AD client ID.
+    /// </summary>
+    public string ClientID { get; set; } = DefaultClientID;
+
+    /// <summary>
+    /// Gets or sets the Azure AD call-back path.
+    /// </summary>
+    public string CallbackPath { get; set; } = DefaultCallbackPath;
+
+    /// <summary>
+    /// Gets or sets the Azure AD signed out call-back path.
+    /// </summary>
+    public string SignedOutCallbackPath { get; set; } = DefaultSignedOutCallbackPath;
+
+    /// <summary>
+    /// Gets the flag that determines if Azure AD is enabled.
+    /// </summary>
+    public bool Enabled { get; set; } = DefaultEnabled;
+
+    /// <summary>
+    /// Gets the Azure AD authority (Instance + TenantID).
+    /// </summary>
+    public string Authority => $"{Instance}{TenantID}";
+
+    /// <summary>
+    /// Loads Azure AD settings from the specified JSON app settings file.
+    /// </summary>
+    /// <param name="filepath">JSON settings file to load. Defaults to local "appsettings.json".</param>
+    /// <returns>Loaded <see cref="AzureADSettings"/> settings instance.</returns>
+    public static AzureADSettings Load(string filepath = null)
+    {
+        if (string.IsNullOrEmpty(filepath))
+            filepath = "appsettings.json";
+
+        filepath = FilePath.GetAbsolutePath(filepath);
+
+        // Make sure default settings exist
+        if (!File.Exists(filepath))
+            File.WriteAllText(filepath, DefaultAzureADSettings);
+
+        dynamic settings = JsonConvert.DeserializeObject(File.ReadAllText(filepath));
+        settings = settings?[DefaultSettingsCategory];
+
+        return new AzureADSettings
+        {
+            Instance = settings?.Instance ?? DefaultInstance, 
+            TenantID = settings?.TenantId ?? DefaultTenantID, 
+            ClientID = settings?.ClientId ?? DefaultClientID, 
+            CallbackPath = settings?.CallbackPath ?? DefaultCallbackPath, 
+            SignedOutCallbackPath = settings?.SignedOutCallbackPath ?? DefaultSignedOutCallbackPath, 
+            Enabled = settings?.Enabled ?? DefaultEnabled
+        };
+    }
+
+    /// <summary>
+    /// Loads Azure AD settings from categorized settings.
+    /// </summary>
+    /// <param name="settingsCategory">Settings category to use for settings load.</param>
+    /// <returns>Loaded <see cref="AzureADSettings"/> settings instance.</returns>
+    public static AzureADSettings LoadFromConfig(string settingsCategory = null)
+    {
+        if (string.IsNullOrEmpty(settingsCategory))
+            settingsCategory = DefaultSettingsCategory;
+
+        // Make sure default settings exist
+        ConfigurationFile config = ConfigurationFile.Current;
+        CategorizedSettingsElementCollection settings = config.Settings[settingsCategory];
+
+        settings.Add(nameof(Instance), DefaultInstance, "Azure AD instance URL.");
+        settings.Add(nameof(TenantID), DefaultTenantID, "Azure AD tenant ID.");
+        settings.Add(nameof(ClientID), DefaultClientID, "Azure AD client ID.");
+        settings.Add(nameof(CallbackPath), DefaultCallbackPath, "Azure AD call-back path.");
+        settings.Add(nameof(SignedOutCallbackPath), DefaultSignedOutCallbackPath, "Azure AD signed out call-back path.");
+        settings.Add(nameof(Enabled), DefaultEnabled, "Flag that determines if Azure AD is enabled.");
+
+        return new AzureADSettings
+        {
+            Instance = settings[nameof(Instance)].ValueAs(DefaultInstance),
+            TenantID = settings[nameof(TenantID)].ValueAs(DefaultTenantID),
+            ClientID = settings[nameof(ClientID)].ValueAs(DefaultClientID),
+            CallbackPath = settings[nameof(CallbackPath)].ValueAs(DefaultCallbackPath),
+            SignedOutCallbackPath = settings[nameof(SignedOutCallbackPath)].ValueAs(DefaultSignedOutCallbackPath),
+            Enabled = settings[nameof(Enabled)].ValueAs(DefaultEnabled),
+        };
+    }
+}
