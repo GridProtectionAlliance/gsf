a1857678c00b248e2e1f03f6c7717fbf67a3d566
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-07-05 11:06:01 -0400

    Created SliceAdd function to combine two datasets

--- Source/Libraries/Adapters/GrafanaAdapters/GrafanaFunctions/GrafanaFunction.cs
+++ Source/Libraries/Adapters/GrafanaAdapters/GrafanaFunctions/GrafanaFunction.cs
@@ -76,6 +76,80 @@ namespace GrafanaAdapters.GrafanaFunctions
     }
 
     /// <summary>
+    /// Represents the "Add" function that adds a decimal number to a DataSourceValue.
+    /// </summary>
+    public class SliceAdd : IGrafanaFunction
+    {
+        /// <inheritdoc />
+        public string Name { get; } = "SliceAdd";
+
+        /// <inheritdoc />
+        public string Description { get; } = "Adds two DataSourceValue together to a certain degree of tolerance";
+
+        /// <inheritdoc />
+        public Type Type { get; } = typeof(SliceAdd);
+
+        /// <inheritdoc />
+        public Regex Regex { get; } = new Regex(string.Format(FunctionsModelHelper.ExpressionFormat, "SliceAdd"), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <inheritdoc />
+        public List<IParameter> Parameters { get; } =
+            new List<IParameter>
+            {
+                new Parameter<double>
+                {
+                    Default = 0,
+                    Description = "The level of tolerance.",
+                    Required = true,
+                    ParameterTypeName = "string"
+                },
+                new Parameter<IEnumerable<DataSourceValue>>
+                {
+                    Default = Enumerable.Empty<DataSourceValue>(),
+                    Description = "First datapoint",
+                    Required = true,
+                    ParameterTypeName = "data"
+                },
+                new Parameter<IEnumerable<DataSourceValue>>
+                {
+                    Default = Enumerable.Empty<DataSourceValue>(),
+                    Description = "Second datapoint",
+                    Required = true,
+                    ParameterTypeName = "data"
+                }
+            };
+
+        /// <inheritdoc />
+        public IEnumerable<DataSourceValue> Compute(List<IParameter> parameters)
+        {
+            // Get Values
+            double tolerance = (parameters[0] as IParameter<double>).Value;
+            IEnumerable<DataSourceValue> firstValues = (parameters[1] as IParameter<IEnumerable<DataSourceValue>>).Value;
+            IEnumerable<DataSourceValue> secondValues = (parameters[2] as IParameter<IEnumerable<DataSourceValue>>).Value;
+
+            //Compute
+            IEnumerable<DataSourceValue> combinedValues = firstValues.Zip(secondValues, (first, second) =>
+            {
+                double combinedValue = first.Value + second.Value;
+
+                return new DataSourceValue
+                {
+                    Value = combinedValue,
+                    Time = first.Time, 
+                    Target = first.Target 
+                };
+            });
+
+            return combinedValues;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Add"/> class.
+        /// </summary>
+        public SliceAdd() { }
+    }
+
+    /// <summary>
     /// Represents the "AbsoluteValue" function that takes the absolute value of a DataSourceValue.
     /// </summary>
     public class AbsoluteValue : IGrafanaFunction
