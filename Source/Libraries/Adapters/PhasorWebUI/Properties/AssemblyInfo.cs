e039f1a5ea3f903ae4286ea4f3b027383fa2dce6
gsfbuildbot <support@gridprotectionalliance.org>
2024-02-14 00:01:33 -0500

    GridSolutionsFramework: Version change for build v2.4.171.0-master.

--- Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.170.0")]
-[assembly: AssemblyFileVersion("2.4.170.0")]
-[assembly: AssemblyInformationalVersion("2.4.170-beta")]
+[assembly: AssemblyVersion("2.4.171.0")]
+[assembly: AssemblyFileVersion("2.4.171.0")]
+[assembly: AssemblyInformationalVersion("2.4.171-beta")]
--- Source/Tools/TCPRebroadcaster/TCPRebroadcasterConsole/Properties/AssemblyInfo.cs
+++ Source/Tools/TCPRebroadcaster/TCPRebroadcasterConsole/Properties/AssemblyInfo.cs
@@ -59,7 +59,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.4.170.0")]
-[assembly: AssemblyVersion("2.4.170.0")]
-[assembly: AssemblyFileVersion("2.4.170.0")]
-[assembly: AssemblyInformationalVersion("2.4.170-beta")]
+// [assembly: AssemblyVersion("2.4.171.0")]
+[assembly: AssemblyVersion("2.4.171.0")]
+[assembly: AssemblyFileVersion("2.4.171.0")]
+[assembly: AssemblyInformationalVersion("2.4.171-beta")]
