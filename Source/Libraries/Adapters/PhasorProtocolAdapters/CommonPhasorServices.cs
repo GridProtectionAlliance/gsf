52905d5b3bed380993257212e5b77c36acd4b06b
J. Ritchie Carroll <ritchiecarroll@users.noreply.github.com>
2024-07-05 11:00:10 -0500

    Update Source/Libraries/Adapters/PhasorProtocolAdapters/CommonPhasorServices.cs
    Co-authored-by: Stephen C. Wills <swills@gridprotectionalliance.org>

--- Source/Libraries/Adapters/PhasorProtocolAdapters/CommonPhasorServices.cs
+++ Source/Libraries/Adapters/PhasorProtocolAdapters/CommonPhasorServices.cs
@@ -915,7 +915,7 @@ public sealed class CommonPhasorServices : FacileActionAdapterBase
     private static TemplatedExpressionParser s_pointTagExpressionParser;
     private static Dictionary<string, DataRow> s_signalTypes;
 
-    private static readonly string[] s_commonVoltageLevels = ["44", "69", "115", "138", "161", "169", "230", "345", "500", "765", "1100"];
+    private static readonly string[] s_commonVoltageLevels = ["1100", "765", "500", "345", "230", "169", "161", "138", "115", "69", "44"];
 
     //Static Constructor
     static CommonPhasorServices()
