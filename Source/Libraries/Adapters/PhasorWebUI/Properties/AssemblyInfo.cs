08a88e95f3fa799074bfb19f2759c385cdd3edf2
gsfbuildbot <support@gridprotectionalliance.org>
2024-12-12 01:26:04 -0500

    GridSolutionsFramework: Version change for build v2.4.214.0-master.

--- Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/PhasorWebUI/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.213.0")]
-[assembly: AssemblyFileVersion("2.4.213.0")]
-[assembly: AssemblyInformationalVersion("2.4.213-beta")]
+[assembly: AssemblyVersion("2.4.214.0")]
+[assembly: AssemblyFileVersion("2.4.214.0")]
+[assembly: AssemblyInformationalVersion("2.4.214-beta")]
--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.4.213.0")]
-[assembly: AssemblyInformationalVersion("2.4.213-beta")]
+[assembly: AssemblyVersion("2.4.214.0")]
+[assembly: AssemblyInformationalVersion("2.4.214-beta")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
