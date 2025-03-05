e2226a09c9b02cd1d8bee788548d05fc6d776ef7
StephenCWills <swills@gridprotectionalliance.org>
2025-03-05 11:15:11 -0500

    Fixes for CsvFrameCollectorAdapter

--- Source/Libraries/Adapters/CsvAdapters/CsvFrameCollectorAdapter.cs
+++ Source/Libraries/Adapters/CsvAdapters/CsvFrameCollectorAdapter.cs
@@ -8,7 +8,7 @@
 //  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
 //  not use this file except in compliance with the License. You may obtain a copy of the License at:
 //
-//      http://www.opensource.org/licenses/MIT
+//      http://opensource.org/licenses/MIT
 //
 //  Unless agreed to in writing, the subject software distributed under the License is distributed on an
 //  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
@@ -22,7 +22,6 @@
 //******************************************************************************************************
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -31,7 +30,6 @@ using GSF;
 using GSF.Configuration;
 using GSF.Diagnostics;
 using GSF.IO;
-using GSF.Scheduling;
 using GSF.TimeSeries;
 using GSF.TimeSeries.Adapters;
 
@@ -51,7 +49,6 @@ namespace CsvAdapters
         /// Default value for the <see cref="TimestampFormat"/> property.
         /// </summary>
         public const string DefaultTimestampFormat = "yyyy-MM-dd HH:mm:ss.fffffff";
-
 
         /// <summary>
         /// Default value for the <see cref="FileNameTemplate"/> property.
@@ -73,34 +70,14 @@ namespace CsvAdapters
         /// </summary>
         public const double DefaultRolloverInterval = 60.0D;
 
-
-        /// <summary>
-        /// Default value for the <see cref="FramesPerSecond"/> property.
-        /// </summary>
-        public const int DefaultFramesPerSecond = 30;
-
-        /// <summary>
-        /// Default value for the <see cref="EnableTimeReasonabilityValidation"/> property.
-        /// </summary>
-        public const bool DefaultEnableTimeReasonabilityValidation = false;
-
-        /// <summary>
-        /// Default value for the <see cref="LagTime"/> property.
-        /// </summary>
-        public const double DefaultLagTime = 5.0D;
-
-        /// <summary>
-        /// Default value for the <see cref="LeadTime"/> property.
-        /// </summary>
-        public const double DefaultLeadTime = 5.0D;
-
         // Fields
-        private string m_activeFileName;
         private readonly object m_activeFileLock;
         private long m_lastTimestamp;
+        private long m_lastRolloverIndex;
         private long m_downsampleFrameCount;
         private long m_rolloverFrameCount;
         private long m_totalExports;
+        private bool m_needsOffload;
         private bool m_disposed;
 
         #endregion
@@ -127,9 +104,6 @@ namespace CsvAdapters
         [Description("Defines the path to the directory where CSV exports are written")]
         public string ExportPath { get; set; }
 
-        /// <inheritdoc/>
-        public override bool SupportsTemporalProcessing => false;
-
         /// <summary>
         /// Gets or sets the path to the directory where
         /// CSV exports are moved after a rollover.
@@ -140,7 +114,7 @@ namespace CsvAdapters
         public string OffloadPath { get; set; }
 
         /// <summary>
-        /// Gets or sets the inbtervall, in seconds
+        /// Gets or sets the interval, in seconds,
         /// to determine how often to roll over CSV files.
         /// </summary>
         [ConnectionStringParameter]
@@ -172,7 +146,6 @@ namespace CsvAdapters
         [Description("Defines the CSV export signalname template - accepts \"{MeasurementID}\" and \"{PointTag}\"")]
         public string SignalNameTemplate { get; set; }
 
-
         /// <summary>
         /// Gets or sets the downsampling interval, in seconds, set to zero for no downsampling.
         /// </summary>
@@ -182,37 +155,39 @@ namespace CsvAdapters
         public double DownsampleInterval { get; set; }
 
         /// <summary>
-        /// Gets or sets the number of frames per second for incoming data used to align timestamps when downsampling interval is greater than 0.0, set to zero to skip time alignment.
+        /// Gets the status of this <see cref="CsvFrameCollectorAdapter"/>.
         /// </summary>
-        [ConnectionStringParameter]
-        [DefaultValue(DefaultFramesPerSecond)]
-        [Description("Defines the number of frames per second for incoming data used to align timestamps when downsampling interval is greater than 0.0, set to zero to skip time alignment")]
-        public new int FramesPerSecond { get; set; }
+        public override string Status
+        {
+            get
+            {
+                string activeFileName;
+                lock (m_activeFileLock)
+                    activeFileName = GenerateActiveFileName(m_lastRolloverIndex * m_rolloverFrameCount);
 
-        /// <summary>
-        /// Gets or sets the flag that determines if timestamps should be validated against local clock for reasonability.
-        /// </summary>
-        [ConnectionStringParameter]
-        [DefaultValue(DefaultEnableTimeReasonabilityValidation)]
-        [Description("Defines the flag that determines if timestamps should be validated against local clock for reasonability")]
-        public bool EnableTimeReasonabilityValidation { get; set; }
+                StringBuilder status = new StringBuilder(base.Status);
 
-        /// <summary>
-        /// Gets or sets the allowed past time deviation tolerance against local clock for time reasonability validation, in seconds (can be sub-second).
-        /// </summary>
-        [ConnectionStringParameter]
-        [DefaultValue(DefaultLagTime)]
-        [Description("Defines the allowed past time deviation tolerance against local clock for time reasonability validation, in seconds (can be sub-second)")]
-        public new double LagTime { get; set; }
+                status.AppendLine($"               Export Path: {FilePath.TrimFileName(ExportPath, 51)}");
+                status.AppendLine($"              Offload Path: {FilePath.TrimFileName(OffloadPath, 51)}");
+                status.AppendLine($"         Rollover Interval: {RolloverInterval:N4} seconds");
+                status.AppendLine($"          Timestamp Format: {TimestampFormat}");
+                status.AppendLine($"    CSV File Name Template: {FileNameTemplate.TrimWithEllipsisEnd(51)}");
+                status.AppendLine($"     Downsampling Interval: {(DownsampleInterval > 0.0D ? $"{DownsampleInterval:N4} seconds)" : "Disabled - Full Resolution Export")}");
 
-        /// <summary>
-        /// Gets or sets the allowed future time deviation tolerance against local clock for time reasonability validation, in seconds (can be sub-second).
-        /// </summary>
-        [ConnectionStringParameter]
-        [DefaultValue(DefaultLeadTime)]
-        [Description("Defines the allowed future time deviation tolerance against local clock for time reasonability validation, in seconds (can be sub-second)")]
-        public new double LeadTime { get; set; }
+                if (DownsampleInterval > 0.0D)
+                    status.AppendLine($"         Frames Per Second: {FramesPerSecond:N0}");
 
+                status.AppendLine($"          Allowed Lag Time: {LagTime:N4} seconds");
+                status.AppendLine($"         Allowed Lead Time: {LeadTime:N4} seconds");
+                status.AppendLine($"    Active CSV Export File: {FilePath.TrimFileName(activeFileName, 51)}");
+                status.AppendLine($"         Total CSV Exports: {m_totalExports:N0}");
+
+                return status.ToString();
+            }
+        }
+
+        /// <inheritdoc/>
+        public override bool SupportsTemporalProcessing => false;
 
         #endregion
 
@@ -226,28 +201,23 @@ namespace CsvAdapters
             ConnectionStringParser<ConnectionStringParameterAttribute> parser = new ConnectionStringParser<ConnectionStringParameterAttribute>();
             parser.ParseConnectionString(ConnectionString, this);
 
-            long computedFrameCount;
-            // For framerate-aligned downsampling, compute the exact number of frames represented
-            // by the interval to accurately determine which frames need to be exported
+            Ticks rolloverIntervalTicks = Ticks.FromSeconds(RolloverInterval);
+            long computedFrameCount = ToFrameCount(rolloverIntervalTicks);
+            m_rolloverFrameCount = Math.Max(computedFrameCount, 1L);
+
             if (DownsampleInterval > 0.0D)
             {
+                // For framerate-aligned downsampling, compute the exact number of frames represented
+                // by the interval to accurately determine which frames need to be exported
                 Ticks downsampleIntervalTicks = Ticks.FromSeconds(DownsampleInterval);
                 computedFrameCount = ToFrameCount(downsampleIntervalTicks);
                 m_downsampleFrameCount = Math.Max(computedFrameCount, 1L);
             }
 
-                Ticks rolloverIntervalTicks = Ticks.FromSeconds(RolloverInterval);
-                computedFrameCount = ToFrameCount(rolloverIntervalTicks);
-                m_rolloverFrameCount = Math.Max(computedFrameCount, 1L);
-            
-
             base.Initialize();
 
             if (string.IsNullOrWhiteSpace(ExportPath))
                 ExportPath = Path.Combine("CSVExports", Name);
-
-            OffloadLingeringFiles();
-
         }
 
         /// <summary>
@@ -267,12 +237,14 @@ namespace CsvAdapters
 
             lock (m_activeFileLock)
             {
+                string activeFileName = GenerateActiveFileName(m_lastRolloverIndex * m_rolloverFrameCount);
+
                 foreach (string filePath in FilePath.EnumerateFiles(ExportPath, $"*{targetFileExtension}", SearchOption.TopDirectoryOnly, HandleException))
                 {
                     string fileName = Path.GetFileName(filePath);
 
                     // Ignore the active file, if it exists
-                    if (fileName == m_activeFileName)
+                    if (fileName == activeFileName)
                         continue;
 
                     string offloadFilePath = Path.Combine(OffloadPath, fileName);
@@ -283,52 +255,26 @@ namespace CsvAdapters
         }
 
         /// <summary>
-        /// Gets the status of this <see cref="CsvFrameCollectorAdapter"/>.
-        /// </summary>
-        public override string Status
-        {
-            get
-            {
-                StringBuilder status = new StringBuilder(base.Status);
-
-                status.AppendLine($"               Export Path: {FilePath.TrimFileName(ExportPath, 51)}");
-                status.AppendLine($"              Offload Path: {FilePath.TrimFileName(OffloadPath, 51)}");
-                status.AppendLine($"         Rollover Interval: {RolloverInterval:N4} seconds");
-                status.AppendLine($"          Timestamp Format: {TimestampFormat}");
-                status.AppendLine($"    CSV File Name Template: {FileNameTemplate.TrimWithEllipsisEnd(51)}");
-                status.AppendLine($"     Downsampling Interval: {(DownsampleInterval > 0.0D ? $"{DownsampleInterval:N4} seconds)" : "Disabled - Full Resolution Export")}");
-
-                if (DownsampleInterval > 0.0D)
-                    status.AppendLine($"         Frames Per Second: {FramesPerSecond:N0}");
-
-                status.AppendLine($" Time Reasonability Checks: {(EnableTimeReasonabilityValidation ? "Enabled" : "Disabled")}");
-                status.AppendLine($"          Allowed Lag Time: {LagTime:N4} seconds");
-                status.AppendLine($"         Allowed Lead Time: {LeadTime:N4} seconds");
-                status.AppendLine($"    Active CSV Export File: {FilePath.TrimFileName(m_activeFileName, 51)}");
-                status.AppendLine($"         Total CSV Exports: {m_totalExports:N0}");
-
-                return status.ToString();
-            }
-        }
-
-        /// <summary>
         /// Gets a short one-line status of this <see cref="CsvFrameCollectorAdapter"/>.
         /// </summary>
         public override string GetShortStatus(int maxLength) =>
             $"{ProcessedMeasurements:N0} measurements exported so far...".CenterText(maxLength);
 
+        /// <inheritdoc/>
+        public override void Start()
+        {
+            m_needsOffload = true;
+            base.Start();
+        }
 
         /// <summary>
         /// Serializes frame to data output stream.
         /// </summary>
-        private void ProcessFrame(IFrame frame)
+        private void ProcessFrame(string activeFileName, IFrame frame)
         {
             lock (m_activeFileLock)
             {
-                if (string.IsNullOrWhiteSpace(m_activeFileName))
-                    return;
-
-                string activeFilePath = Path.Combine(ExportPath, m_activeFileName);
+                string activeFilePath = Path.Combine(ExportPath, activeFileName);
                 bool writeHeader = !File.Exists(activeFilePath);
                 Directory.CreateDirectory(ExportPath);
 
@@ -347,9 +293,6 @@ namespace CsvAdapters
         /// <inheritdoc/>
         protected override void PublishFrame(IFrame frame, int index)
         {
-            if (EnableTimeReasonabilityValidation && !frame.Timestamp.UtcTimeIsValid(LagTime, LeadTime))
-                return;
-
             if (DownsampleInterval > 0.0D)
             {
                 // If last measurement timestamp is within the downsampling interval, then we have
@@ -358,24 +301,32 @@ namespace CsvAdapters
                     return;
 
                 // For framerate-aligned downsampling, only export measurements that fall on the downsampling interval
-                if ( ToFrameCount(frame.Timestamp) % m_downsampleFrameCount != 0)
+                if (ToFrameCount(frame.Timestamp) % m_downsampleFrameCount != 0)
                     return;
 
                 m_lastTimestamp = frame.Timestamp.Value;
             }
 
-            bool rollover = ToFrameCount(frame.Timestamp) % m_rolloverFrameCount == 0;
+            long rolloverIndex = ToFrameCount(frame.Timestamp) / m_rolloverFrameCount;
 
-            if (rollover)
-                RollOver();
+            if (rolloverIndex > m_lastRolloverIndex)
+            {
+                string previousFileName = GenerateActiveFileName(m_lastRolloverIndex * m_rolloverFrameCount);
+                RollOver(previousFileName);
+            }
 
             lock (m_activeFileLock)
             {
-                if (string.IsNullOrWhiteSpace(m_activeFileName) && (index == 0 || rollover))
-                    m_activeFileName = GenerateActiveFileName(frame.Timestamp);
+                m_lastRolloverIndex = rolloverIndex;
+                string activeFileName = GenerateActiveFileName(rolloverIndex * m_rolloverFrameCount);
+                ProcessFrame(activeFileName, frame);
             }
 
-            ProcessFrame(frame);
+            if (m_needsOffload)
+            {
+                OffloadLingeringFiles();
+                m_needsOffload = false;
+            }
         }
 
         // Computes the number of frames represented by the given time interval.
@@ -390,6 +341,16 @@ namespace CsvAdapters
             return baseFrameCount + subsecondFrameCount;
         }
 
+        // Computes the number of ticks since epoch represented by the given frame count.
+        private Ticks FromFrameCount(long frameCount)
+        {
+            // Prevent overflow using the same strategy as ToFrameCount()
+            long baseTicks = frameCount / FramesPerSecond * Ticks.PerSecond;
+            long subFramerateFrameCount = frameCount % FramesPerSecond;
+            long subFramerateTicks = (subFramerateFrameCount * Ticks.PerSecond + FramesPerSecond / 2L) / FramesPerSecond;
+            return baseTicks + subFramerateTicks;
+        }
+
         // Converts the measurement to an entry in CSV header format.
         private string ToHeader(MeasurementKey key)
         {
@@ -400,36 +361,28 @@ namespace CsvAdapters
         }
 
         // Generates the name of the next active file.
-        private string GenerateActiveFileName(Ticks timestamp) =>
+        private string GenerateActiveFileName(long frameCount) =>
+            GenerateActiveFileName((DateTime)FromFrameCount(frameCount));
+
+        // Generates the name of the next active file.
+        private string GenerateActiveFileName(DateTime timestamp) =>
             string.Format(FileNameTemplate, timestamp);
 
         // Rolls over the active file by moving it to the offload directory
         // and unsetting the active file name so that a new active file
         // can be generated the next time this adapter needs to update it.
-        private void RollOver()
+        private void RollOver(string activeFileName)
         {
-            string activeFileName;
-
-            lock (m_activeFileLock)
-            {
-                if (string.IsNullOrWhiteSpace(m_activeFileName))
-                    return;
-
-                activeFileName = m_activeFileName;
-                m_activeFileName = null;
-                m_totalExports++;
-            }
-
             string activeFilePath = Path.Combine(ExportPath, activeFileName);
 
             if (string.IsNullOrWhiteSpace(OffloadPath) || !File.Exists(activeFilePath))
                 return;
 
+            m_totalExports++;
             Directory.CreateDirectory(OffloadPath);
             string offloadFilePath = Path.Combine(OffloadPath, activeFileName);
             File.Move(activeFilePath, offloadFilePath);
         }
-
 
         // Handles the given exception.
         private void HandleException(Exception ex) =>
@@ -440,9 +393,9 @@ namespace CsvAdapters
             // The "Path traversal" security warning is not relevant
             // because the path comes from software configuration
             // performed by an administrator, not user input
-#pragma warning disable SG0018 // Path traversal
+            #pragma warning disable SG0018 // Path traversal
             return File.AppendText(filePath);
-#pragma warning restore SG0018
+            #pragma warning restore SG0018
         }
 
         #endregion
